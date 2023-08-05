b0cb979a8f58d490f1f874dcb93319cc209e54bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:18:21 +0000

    Add package statina/1.7.2/statina-1.7.2-py3-none-any.whl

---
+++ packages/statina/statina-1.7.2-py3-none-any.whl/statina/models/server/auth.py
@@ -0,0 +1,16 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    username: str
+    email: str
+    scopes: Optional[List]
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+    scopes: Optional[List[str]] = []
