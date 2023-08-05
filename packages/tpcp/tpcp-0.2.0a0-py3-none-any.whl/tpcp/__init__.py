53eadf84b5382774861671fb663c272a486ec977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:34 +0000

    Add package tpcp/0.2.0a0/tpcp-0.2.0a0-py3-none-any.whl

---
+++ packages/tpcp/tpcp-0.2.0a0-py3-none-any.whl/tpcp/__init__.py
@@ -0,0 +1,6 @@
+"""tpcp - Tiny Pipelines for Complicated Problems."""
+from tpcp._base import BaseAlgorithm, BaseOptimize
+
+__version__ = "0.2.0-alpha.0"
+
+__all__ = ["BaseAlgorithm", "BaseOptimize"]
