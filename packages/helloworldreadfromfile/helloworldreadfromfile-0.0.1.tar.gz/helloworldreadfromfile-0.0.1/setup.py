036882b8b13a0df79309d9df14eae45b17413bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:57 +0000

    Add package helloworldreadfromfile/0.0.1/helloworldreadfromfile-0.0.1.tar.gz

---
+++ packages/helloworldreadfromfile/helloworldreadfromfile-0.0.1.tar.gz/helloworldreadfromfile-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='helloworldreadfromfile',
+    version='0.0.1',
+    description='Simple library that knows how to read a txt given a file path',
+    py_modules=['helloworldreadfromfile'],
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Freeware"
+    ]
+)
