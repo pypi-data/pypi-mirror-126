05e89520ce17e6fd84e5567fbf0a430857398352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:42 +0000

    Add package dataclass-wizard/0.17.1/dataclass_wizard-0.17.1-py2.py3-none-any.whl

---
+++ packages/dataclass-wizard/dataclass_wizard-0.17.1-py2.py3-none-any.whl/dataclass_wizard/bases.py
@@ -0,0 +1,128 @@
+from typing import Callable, Type, Dict, Optional, ClassVar, Union, TypeVar
+
+from .enums import DateTimeTo, LetterCase
+
+
+# Create a generic variable that can be 'BaseMeta', or any subclass.
+M = TypeVar('M', bound='BaseMeta')
+
+
+class BaseMeta:
+    """
+    Base class definition for the `JSONWizard.Meta` inner class.
+    """
+    __slots__ = ()
+
+    # True to enable Debug mode for additional (more verbose) log output.
+    #
+    # For example, a message is logged whenever an unknown JSON key is
+    # encountered when `from_dict` or `from_json` is called.
+    #
+    # This also results in more helpful messages during error handling, which
+    # can be useful when debugging the cause when values are an invalid type
+    # (i.e. they don't match the annotation for the field) when unmarshalling
+    # a JSON object to a dataclass instance.
+    #
+    # Note there is a minor performance impact when DEBUG mode is enabled.
+    debug_enabled: ClassVar[bool] = False
+
+    # True to raise an class:`UnknownJSONKey` when an unmapped JSON key is
+    # encountered when `from_dict` or `from_json` is called; an unknown key is
+    # one that does not have a known mapping to a dataclass field.
+    #
+    # The default is to only log a "warning" for such cases, which is visible
+    # when `debug_enabled` is true and logging is properly configured.
+    raise_on_unknown_json_key: ClassVar[bool] = False
+
+    # A customized mapping of JSON keys to dataclass fields, that is used
+    # whenever `from_dict` or `from_json` is called.
+    #
+    # Note: this is in addition to the implicit field transformations, like
+    #   "myStr" -> "my_str"
+    #
+    # If the reverse mapping is also desired (i.e. dataclass field to JSON
+    # key), then specify the "__all__" key as a truthy value. If multiple JSON
+    # keys are specified for a dataclass field, only the first one provided is
+    # used in this case.
+    json_key_to_field: ClassVar[Dict[str, str]] = None
+
+    # How should :class:`time` and :class:`datetime` objects be serialized
+    # when converted to a Python dictionary object or a JSON string.
+    marshal_date_time_as: ClassVar[Union[DateTimeTo, str]] = None
+
+    # How JSON keys should be transformed to dataclass fields.
+    #
+    # Note that this only applies to keys which are to be set on dataclass
+    # fields; other fields such as the ones for `TypedDict` or `NamedTuple`
+    # sub-classes won't be similarly transformed.
+    key_transform_with_load: ClassVar[Union[LetterCase, str]] = None
+
+    # How dataclass fields should be transformed to JSON keys.
+    #
+    # Note that this only applies to dataclass fields; other fields such as
+    # the ones for `TypedDict` or `NamedTuple` sub-classes won't be similarly
+    # transformed.
+    key_transform_with_dump: ClassVar[Union[LetterCase, str]] = None
+
+    # The field name that identifies the tag for a class.
+    #
+    # When set to a value, a '__tag__' field will be populated in the
+    # dictionary object in the dump (serialization) process. When loading
+    # (or de-serializing) a dictionary object, the '__tag__' field will be
+    # used to load the corresponding dataclass, assuming the dataclass field
+    # is properly annotated as a Union type, ex.:
+    #   my_data: Union[Data1, Data2, Data3]
+    tag: ClassVar[str] = None
+
+    # Determines whether we should we skip / omit fields with default values
+    # (based on the `default` or `default_factory` argument specified for
+    # the :func:`dataclasses.field`) in the serialization process.
+    skip_defaults: ClassVar[bool] = False
+
+
+class BaseLoadHook:
+    """
+    Container class for type hooks.
+    """
+    __slots__ = ()
+
+    __LOAD_HOOKS__: ClassVar[Dict[Type, Callable]] = None
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        # (Re)assign the dict object so we have a fresh copy per class
+        cls.__LOAD_HOOKS__ = {}
+
+    @classmethod
+    def register_load_hook(cls, typ: Type, func: Callable):
+        """Registers the hook for a type, on the default loader by default."""
+        cls.__LOAD_HOOKS__[typ] = func
+
+    @classmethod
+    def get_load_hook(cls, typ: Type) -> Optional[Callable]:
+        """Retrieves the hook for a type, if one exists."""
+        return cls.__LOAD_HOOKS__.get(typ)
+
+
+class BaseDumpHook:
+    """
+    Container class for type hooks.
+    """
+    __slots__ = ()
+
+    __DUMP_HOOKS__: ClassVar[Dict[Type, Callable]] = None
+
+    def __init_subclass__(cls):
+        super().__init_subclass__()
+        # (Re)assign the dict object so we have a fresh copy per class
+        cls.__DUMP_HOOKS__ = {}
+
+    @classmethod
+    def register_dump_hook(cls, typ: Type, func: Callable):
+        """Registers the hook for a type, on the default dumper by default."""
+        cls.__DUMP_HOOKS__[typ] = func
+
+    @classmethod
+    def get_dump_hook(cls, typ: Type) -> Optional[Callable]:
+        """Retrieves the hook for a type, if one exists."""
+        return cls.__DUMP_HOOKS__.get(typ)
---
+++ packages/dataclass-wizard/dataclass_wizard-0.17.1-py2.py3-none-any.whl/dataclass_wizard/decorators.py
@@ -0,0 +1,131 @@
+from functools import wraps
+from typing import Any, Type, Callable, Union, TypeVar, cast
+
+from .constants import SINGLE_ARG_ALIAS, IDENTITY
+from .errors import ParseError
+
+
+T = TypeVar('T')
+
+
+def try_with_load(f):
+
+    @wraps(f)
+    def new_func(o: Any, ann_type: Type, *args, **kwargs):
+        try:
+            return f(o, ann_type, *args, **kwargs)
+
+        except ParseError as e:
+            # This means that a nested load hook raised an exception.
+            # Therefore, to help with debugging we should print the name
+            # of the outer load hook and the original object.
+            e.kwargs['load_hook'] = f.__name__
+            e.obj = o
+            # Re-raise the original error
+            raise
+
+        except Exception as e:
+            raise ParseError(e, o, ann_type, load_hook=f.__name__)
+
+    return new_func
+
+
+def discard_kwargs(f):
+
+    @wraps(f)
+    def new_func(*args, **_kwargs):
+        return f(*args)
+
+    return new_func
+
+
+def _alias(default: Callable) -> Callable[[T], T]:
+    """
+    Decorator which re-assigns a function `_f` to point to `default` instead.
+    Since global function calls in Python are somewhat expensive, this is
+    mainly done to reduce a bit of overhead involved in the functions calls.
+
+    For example, consider the below example::
+
+        def f2(o):
+            return o
+
+        def f1(o):
+            return f2(o)
+
+    Calling function `f1` will incur some additional overhead, as opposed to
+    simply calling `f2`.
+
+    Now assume we wrap `f1` with the `_alias` decorator::
+
+        def f2(o):
+            return o
+
+        @_alias(f2)
+        def f1(o):
+            ...
+
+    This will essentially perform the assignment of `f1 = f2`, so calling
+    `f1()` in this case has no additional function overhead, as opposed to
+    just calling `f2()`.
+    """
+
+    def new_func(_f: T) -> T:
+        return cast(T, default)
+
+    return new_func
+
+
+def _single_arg_alias(alias_func: Union[Callable, str] = None):
+    """
+    Decorator which wraps a function to set the :attr:`SINGLE_ARG_ALIAS` on
+    a function `f`, which is an alias function that takes only one argument.
+    This is useful mainly so that other functions can access this attribute,
+    and can opt to call it instead of function `f`.
+    """
+
+    def new_func(f):
+        setattr(f, SINGLE_ARG_ALIAS, alias_func)
+        return f
+
+    return new_func
+
+
+def _identity(_f: Callable = None, id: Union[object, str] = None):
+    """
+    Decorator which wraps a function to set the :attr:`IDENTITY` on a function
+    `f`, indicating that this is an identity function that returns its first
+    argument. This is useful mainly so that other functions can access this
+    attribute, and can opt to call it instead of function `f`.
+    """
+
+    def new_func(f):
+        setattr(f, IDENTITY, id)
+        return f
+
+    return new_func(_f) if _f else new_func
+
+
+def resolve_alias_func(f: Callable, _locals=None, raise_=False) -> Callable:
+    """
+    Resolve the underlying single-arg alias function for `f`, using the
+    provided function locals (which will be a dict). If `f` does not have an
+    associated alias function, we return `f` itself.
+
+    :raises AttributeError: If `raise_` is true and `f` is not a single-arg
+      alias function.
+    """
+
+    try:
+        single_arg_alias_func = getattr(f, SINGLE_ARG_ALIAS)
+
+    except AttributeError:
+        if raise_:
+            raise
+        return f
+
+    else:
+        if isinstance(single_arg_alias_func, str) and _locals is not None:
+            return _locals[single_arg_alias_func]
+
+        return single_arg_alias_func
