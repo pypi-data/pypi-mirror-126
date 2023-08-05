dde046604093465e35a6b0bb09ae20916a12f7a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:52 +0000

    Add package netpredict/0.0.1/netpredict-0.0.1.tar.gz

---
+++ packages/netpredict/netpredict-0.0.1.tar.gz/netpredict-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='netpredict',
+    version='0.0.1',
+    description='Real-time Deep Learning Predictions for TrafficForecasting',
+    long_description='',
+    url='https://github.com/esnet/netpredict',
+    author='mariam kiran',
+    author_email='mkrian@es.net',
+    license='Lawrence Berkeley National Laboratory',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
