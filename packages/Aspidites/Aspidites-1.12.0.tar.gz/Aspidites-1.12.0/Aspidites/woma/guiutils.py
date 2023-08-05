c12e76433dd83b7bcf5f951fe557ace33d72f16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:19 +0000

    Add package Aspidites/1.12.0/Aspidites-1.12.0.tar.gz

---
+++ packages/Aspidites/Aspidites-1.12.0.tar.gz/Aspidites-1.12.0/Aspidites/woma/guiutils.py
@@ -0,0 +1,10 @@
+"""Disabled as of 2.1.6 but it does seem to work.Except with CD
+"""
+# TODO Fix dearpygui module not found
+# import dearpygui.dearpygui as dpg
+#
+# import sys
+# this = sys.modules[__name__]
+#
+# for i in dir(dpg):
+#     setattr(this, i, getattr(dpg, i))
