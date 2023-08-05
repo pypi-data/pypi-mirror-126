6456afc4c64be73739d84a2c4fe3a56da7a3501d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:17 +0000

    Add package thechronos/1.0.1/thechronos-1.0.1.tar.gz

---
+++ packages/thechronos/thechronos-1.0.1.tar.gz/thechronos-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+#setup(name='chronos',
+setup(name='thechronos',
+      version='1.0.1',
+      description='Automatic isochrone fitting to photometric data',
+      author='David Nidever',
+      author_email='dnidever@montana.edu',
+      url='https://github.com/dnidever/chronos',
+      packages=find_packages(exclude=["tests"]),
+#      scripts=['bin/dopfit','bin/dopjointfit','bin/doppler'],
+      requires=['numpy','astropy(>=4.0)','scipy','dlnpyutils','emcee','cornner'],
+      include_package_data=True,
+)
