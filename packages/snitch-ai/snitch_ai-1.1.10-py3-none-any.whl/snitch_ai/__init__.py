57288a646066d8a63f42898d7060986914a47811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:36 +0000

    Add package snitch-ai/1.1.10/snitch_ai-1.1.10-py3-none-any.whl

---
+++ packages/snitch-ai/snitch_ai-1.1.10-py3-none-any.whl/snitch_ai/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+__version__ = "1.1.10"
+
+cloud_endpoint = "https://api.snit.ch/"
+endpoint_address = os.getenv("SNITCH_ENDPOINT_ADDRESS", cloud_endpoint)
+access_token = os.getenv("SNITCH_ACCESS_TOKEN")
+
+from snitch_ai.internal.project import create_project, get_project, select_project
