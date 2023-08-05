3bf8d94ef45464da6d93f5fc8cd445758524ff21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:16:33 +0000

    Add package homage-to-the-square/1.1.0/homage-to-the-square-1.1.0.tar.gz

---
+++ packages/homage-to-the-square/homage-to-the-square-1.1.0.tar.gz/homage-to-the-square-1.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name = 'homage-to-the-square',
+    packages=['homage'],
+    description = 'runs an homage to the square a la josef albers',
+    version = '1.1.0',
+    author="Tal Noznisky",
+    keyword="josef albers, bauhaus, homage to the square",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/talnoznisky/homage-to-the-square',
+    python_requires='>=3.0', # any python greater than 2.7
+    entry_points='''
+            [console_scripts]
+            homage-to-the-square=homage.main:main
+        ''',
+)
