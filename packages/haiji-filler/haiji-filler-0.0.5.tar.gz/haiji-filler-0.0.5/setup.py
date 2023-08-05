f822862607043ae352829e10d9889b8661a46aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:16 +0000

    Add package haiji-filler/0.0.5/haiji-filler-0.0.5.tar.gz

---
+++ packages/haiji-filler/haiji-filler-0.0.5.tar.gz/haiji-filler-0.0.5/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+def main():
+    description = 'Just for test ?'
+    setup(
+        # package name(editable)
+        # Note : name might end such as 'filler'
+        name='haiji-filler',
+        #name='pokemon-filler',
+        # version(editable)
+        version='0.0.5',
+        # entry point(editable)
+        entry_points = {
+            'console_scripts':
+            ['haiji_filler=habe_filler:main'],
+                        },
+        #--- in this case, no need to edit below ---#
+        classifiers=['Programming Language :: Python :: 3.9', ],
+        author='example',
+        author_email='example@example.jp',
+        url='https://www.google.co.jp',
+        description=description,
+        long_description=description,
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=[],
+        tests_require=[],
+        setup_requires=[],
+        )
+
+if __name__ == '__main__':
+    main()
