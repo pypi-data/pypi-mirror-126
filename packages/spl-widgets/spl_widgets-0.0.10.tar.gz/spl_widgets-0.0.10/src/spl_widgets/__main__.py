e30e12d033358d2d3f6210c7be2f3809ec64d8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:37 +0000

    Add package spl-widgets/0.0.10/spl_widgets-0.0.10.tar.gz

---
+++ packages/spl-widgets/spl_widgets-0.0.10.tar.gz/spl_widgets-0.0.10/src/spl_widgets/__main__.py
@@ -0,0 +1,36 @@
+from sys import argv; import os
+from . import gorilla_clean, stk_swx, widget_help
+
+modules_to_alias=[
+    "update",
+    "gorilla_clean",
+    "stk-swx"
+]
+
+cmd = argv[1:]
+
+if cmd[0] == "gorilla_clean":
+    gorilla_clean.main()
+
+elif cmd[0] == "stk-swx":
+    stk_swx.main()
+
+elif cmd[0] == "help":
+    widget_help.main()
+
+elif cmd[0] == "update_widgets":
+    os.system("pip install spl_widgets --upgrade")
+    zprofile = f"{os.getenv('HOME')}/.zprofile"
+    with open(zprofile,"r") as reader:
+        lines = reader.readlines()
+
+        for module in modules_to_alias:
+            alias_str = f'alias {module}="python3 -m spl_widgets {module}"\n'
+            for line in lines:
+                if line == alias_str: break
+            else: lines.append(alias_str)
+
+    with open(zprofile, "w") as writer:
+        writer.writelines(lines)
+
+else: print("Bad command")
