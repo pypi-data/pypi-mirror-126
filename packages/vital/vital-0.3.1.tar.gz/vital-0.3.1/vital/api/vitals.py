1563ed482ddfffe37d0a6d8dbc39d523742b4475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:26 +0000

    Add package vital/0.3.1/vital-0.3.1.tar.gz

---
+++ packages/vital/vital-0.3.1.tar.gz/vital-0.3.1/vital/api/vitals.py
@@ -0,0 +1,54 @@
+from typing import List, Mapping, Optional
+
+from vital.api.api import API
+
+
+class Vitals(API):
+    """Endpoints for getting activity data."""
+
+    def glucose(
+        self,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        Get glucose value API
+        :param str user_key: users key
+        :param str start_date: date in ISO format
+        :param str end_date: date in ISO format
+        :param Optional[str] provider: Provider of data whoop, strava etc.
+        """
+        return self.client.get(
+            f"/vitals/glucose/{user_key}",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
+
+    def cholesterol(
+        self,
+        type: str,
+        user_key: str,
+        start_date: str,
+        end_date: str,
+        provider: Optional[str] = "",
+    ) -> Mapping[str, List[Mapping]]:
+        """
+        Get glucose value API
+        :param str user_key: users key
+        :param str start_date: date in ISO format
+        :param str end_date: date in ISO format
+        :param Optional[str] provider: Provider of data whoop, strava etc.
+        """
+        return self.client.get(
+            f"/vitals/cholesterol/{type}/{user_key}",
+            params={
+                "start_date": start_date,
+                "end_date": end_date,
+                "provider": provider,
+            },
+        )
