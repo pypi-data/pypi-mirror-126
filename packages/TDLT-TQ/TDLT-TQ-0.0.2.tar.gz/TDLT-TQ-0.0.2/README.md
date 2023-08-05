a80c53bcfca40b08659d773aba37ad7f39aa5618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:53 +0000

    Add package TDLT-TQ/0.0.2/TDLT-TQ-0.0.2.tar.gz

---
+++ packages/TDLT-TQ/TDLT-TQ-0.0.2.tar.gz/TDLT-TQ-0.0.2/README.md
@@ -0,0 +1,5 @@
+TianQi's Deep Learning Tools
+
+or Tactful Deep Learning Tools
+
+use : import TDLT as T
---
+++ packages/TDLT-TQ/TDLT-TQ-0.0.2.tar.gz/TDLT-TQ-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TDLT-TQ",
+    version="0.0.2",
+    author="TianQi",
+    author_email="tianqi_729@163.com",
+    description="Tactful Deep Learning Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
