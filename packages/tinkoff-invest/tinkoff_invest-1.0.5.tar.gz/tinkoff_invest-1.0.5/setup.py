cb359054029443915f62c27095958ecab3e0a795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:02 +0000

    Add package tinkoff-invest/1.0.5/tinkoff_invest-1.0.5.tar.gz

---
+++ packages/tinkoff-invest/tinkoff_invest-1.0.5.tar.gz/tinkoff_invest-1.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='tinkoff_invest',
+    version='1.0.5',
+    packages=['tinkoff_invest', 'tinkoff_invest.models'],
+    url='https://github.com/asakharov/tinkoff_invest',
+    python_requires='>=3.8.0',
+    install_requires=[
+        "prettytable", "iso8601", "requests", "ujson", "urllib3", "websocket-client"
+    ],
+    license='MIT',
+    author='Alexey Sakharov',
+    author_email='alexey.sakharov@gmail.com',
+    description='Client library for Tinkoff broker'
+)
