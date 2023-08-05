078afc5615dbb4a27f6da56bde89b78118689f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:23 +0000

    Add package fipiran/0.5.0/fipiran-0.5.0-py3-none-any.whl

---
+++ packages/fipiran/fipiran-0.5.0-py3-none-any.whl/fipiran/__init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.5.0'
+
+from typing import TypedDict as _TypedDict
+
+from requests import get as _get
+
+
+_FIPIRAN = 'https://www.fipiran.com/'
+_YK = ''.maketrans('يك', 'یک')
+_API = 'https://fund.fipiran.ir/api/v1/'
+
+
+def _api(path) -> dict | list:
+    return _get(_API + path).json()
+
+
+def _fipiran(path: str, data=None, json_resp=False) -> str | dict | list:
+    resp = _get(f'{_FIPIRAN}{path}', data)
+    if json_resp is True:
+        return resp.json()
+    return resp.content.decode().translate(_YK)
+
+
+def search(term) -> list[
+    _TypedDict('AutoComplete', {'LVal18AFC': str, 'LSoc30': str})
+]:
+    return _fipiran('Home/AutoComplete', (('term', term),), True)
