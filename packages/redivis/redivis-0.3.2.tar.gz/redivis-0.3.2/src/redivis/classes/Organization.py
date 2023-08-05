a73b4bcd4e8f67de585832941bdc08a11f044a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:56 +0000

    Add package redivis/0.3.2/redivis-0.3.2.tar.gz

---
+++ packages/redivis/redivis-0.3.2.tar.gz/redivis-0.3.2/src/redivis/classes/Organization.py
@@ -0,0 +1,10 @@
+import warnings
+from .Dataset import Dataset
+
+
+class Organization:
+    def __init__(self, name):
+        self.name = name
+
+    def dataset(self, name, *, version="current"):
+        return Dataset(name, organization=self, version=version)
