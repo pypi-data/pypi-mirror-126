cf26ac0c7808f659995e09bf1d00d7fbb2377153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package latticeflow/0.0.1/latticeflow-0.0.1-py3-none-any.whl

---
+++ packages/latticeflow/latticeflow-0.0.1-py3-none-any.whl/latticeflow/info.py
@@ -0,0 +1,13 @@
+def print_info():
+    print("This is the latticeflow package. Visit latticeflow.ai for more info.")
+    print(
+        """
+
+ _       _   _   _           __ _               
+| | __ _| |_| |_(_) ___ ___ / _| | _____      __
+| |/ _` | __| __| |/ __/ _ \ |_| |/ _ \ \ /\ / /
+| | (_| | |_| |_| | (_|  __/  _| | (_) \ V  V / 
+|_|\__,_|\__|\__|_|\___\___|_| |_|\___/ \_/\_/  
+                                                
+    """
+    )
