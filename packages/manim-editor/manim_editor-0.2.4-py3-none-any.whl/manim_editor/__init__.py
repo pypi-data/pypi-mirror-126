46aefae6c61558bb789f14f894cdbcd08785a1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package manim-editor/0.2.4/manim_editor-0.2.4-py3-none-any.whl

---
+++ packages/manim-editor/manim_editor-0.2.4-py3-none-any.whl/manim_editor/__init__.py
@@ -0,0 +1,9 @@
+from .editor import PresentationSectionType, get_config, set_config
+from .config import Config
+
+set_config(Config)
+
+# NORMAL = PresentationSectionType.NORMAL
+# SKIP = PresentationSectionType.SKIP
+# LOOP = PresentationSectionType.LOOP
+# COMPLETE_LOOP = PresentationSectionType.COMPLETE_LOOP
---
+++ packages/manim-editor/manim_editor-0.2.4-py3-none-any.whl/manim_editor/editor/__init__.py
@@ -0,0 +1,7 @@
+from .manim_loader import *
+from .scene import *
+from .create_project import *
+from .load_project import *
+from .commands import *
+from .config import *
+from .edit_project import *
