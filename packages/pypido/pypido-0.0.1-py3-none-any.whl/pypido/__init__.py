7d23e923484948c0b4e11e3fcbf730a1a204c314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:33 +0000

    Add package pypido/0.0.1/pypido-0.0.1-py3-none-any.whl

---
+++ packages/pypido/pypido-0.0.1-py3-none-any.whl/pypido/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+'''pypido - simple tool to create and publish Pypi packages (version 0.0.1 = not yet implemented!!!)'''
+
+__version__ = '0.0.1'
+
+__requires__ = ['flit >=3.4.0']
