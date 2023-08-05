14691dd1d442f391e673d14d9b9bf92a2e467699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:56 +0000

    Add package nofnof-filler/1.0.0/nofnof_filler-1.0.0.tar.gz

---
+++ packages/nofnof-filler/nofnof_filler-1.0.0.tar.gz/nofnof_filler-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nofnof_filler', 'nofnof_filler.src']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'nofnof-filler',
+    'version': '1.0.0',
+    'description': 'Assignment program using python in 42rush',
+    'long_description': None,
+    'author': 'kegoshi, rsato, kkamioka, omotoki',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
