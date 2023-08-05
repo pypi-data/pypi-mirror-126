449864efb7b5ccc755e369cc9a5d0277155a5f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:35 +0000

    Add package Campfire-API/0.0.5/Campfire%20API-0.0.5.tar.gz

---
+++ packages/Campfire-API/Campfire%20API-0.0.5.tar.gz/Campfire API-0.0.5/campfire_api/errors.py
@@ -0,0 +1,22 @@
+class UnauthorizedError(Exception):
+    pass
+
+
+class AccountIsBannedError(Exception):
+    pass
+
+
+class EAlreadyExist(Exception):
+    pass
+
+
+class GoneError(Exception):
+    pass
+
+
+class UnknownError(Exception):
+    pass
+
+
+class NotFoundError(Exception):
+    pass
