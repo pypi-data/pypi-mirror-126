066e0f1402a85a9dafa697ab15591d76ecb9ada9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:26 +0000

    Add package libbiomedit/0.4.4/libbiomedit-0.4.4-py3-none-any.whl

---
+++ packages/libbiomedit/libbiomedit-0.4.4-py3-none-any.whl/libbiomedit/lib/classify.py
@@ -0,0 +1,98 @@
+import sys
+from typing import Union, Tuple, Any, Dict, Optional, Sequence
+from enum import Enum
+import collections.abc
+from abc import ABC
+
+import dataclasses
+
+from .secret import Secret
+
+
+# Starting with python 3.7, the typing module has a new API
+__origin_attr__ = "__extra__" if sys.version_info < (3, 7) else "__origin__"
+
+_classifiers = []
+
+
+class IsDataclass(type):
+    __dataclass_fields__: dict
+
+
+class IsTypingType(ABC):
+    __args__: Tuple[type, ...]
+
+
+AbstractType = Union[IsDataclass, IsTypingType, type]
+
+
+def _classifier(T):
+    """Decorator populating :_classifiers:"""
+
+    def decorator(f):
+        _classifiers.append((T, f))
+        return f
+
+    return decorator
+
+
+def classify(T: AbstractType):
+    try:
+        return next(C for C, check in _classifiers if check(T))
+    except StopIteration:
+        return None
+
+
+@_classifier(Any)
+def is_any(T: AbstractType) -> bool:
+    return T is Any
+
+
+@_classifier(Secret)
+def is_secret(T: AbstractType) -> bool:
+    return _is_raw_secret(getattr(T, "__origin__", object)) or _is_raw_secret(T)
+
+
+def _is_raw_secret(T: AbstractType) -> bool:
+    return isinstance(T, type) and issubclass(T, Secret)
+
+
+@_classifier(Tuple)
+def is_tuple(T: AbstractType) -> bool:
+    return getattr(T, __origin_attr__, None) is tuple
+
+
+@_classifier(Sequence)
+def is_seq(T: AbstractType) -> bool:
+    seq_type = getattr(T, __origin_attr__, None)
+    return isinstance(seq_type, type) and issubclass(seq_type, collections.abc.Sequence)
+
+
+@_classifier(IsDataclass)
+def is_dataclass(T: type) -> bool:
+    return dataclasses.is_dataclass(T)
+
+
+@_classifier(Optional)
+def is_optional(T: AbstractType) -> bool:
+    return (
+        getattr(T, "__origin__", None) is Union
+        and type(None) in getattr(T, "__args__", ())
+        and len(getattr(T, "__args__", ())) == 2
+    )
+
+
+@_classifier(Enum)
+def is_enum(T: AbstractType) -> bool:
+    return isinstance(T, type) and issubclass(T, Enum)
+
+
+@_classifier(Union)
+def is_union(T: AbstractType) -> bool:
+    return getattr(T, "__origin__", None) is Union
+
+
+@_classifier(Dict)
+def is_dict(T: AbstractType) -> bool:
+    t_origin = getattr(T, __origin_attr__, None)
+    return t_origin is dict
