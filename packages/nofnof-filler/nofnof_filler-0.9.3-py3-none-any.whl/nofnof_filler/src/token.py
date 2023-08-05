3ac97d61bbbf99dd02511961d6bf6a54a1a226cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:42 +0000

    Add package nofnof-filler/0.9.3/nofnof_filler-0.9.3-py3-none-any.whl

---
+++ packages/nofnof-filler/nofnof_filler-0.9.3-py3-none-any.whl/nofnof_filler/src/token.py
@@ -0,0 +1,22 @@
+class Token:
+    def __init__(self):
+        self.y = 0
+        self.x = 0
+        self.board = []
+
+    def read(self):
+        _, y, x = input().split()
+        self.y = int(y)
+        self.x = int(x[:-1])
+        self.board = [[0] * self.x for _ in range(self.y)]
+        for cnt_y in range(self.y):
+            _, map_line = input().split()
+            for cnt_x in range(self.x):
+                if map_line[cnt_x] == '.':
+                    continue
+                elif map_line[cnt_x] == '*':
+                    self.board[cnt_y][cnt_x] = 1
+                else:
+                    raise ValueError(f'Value Error| map.py| y:{cnt_y} x:{cnt_x}')
+                    
+                    
