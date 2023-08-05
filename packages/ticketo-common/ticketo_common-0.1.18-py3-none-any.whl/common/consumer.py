07ffb6ffeebe59b60bac7a2f6731b89f388f0237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:31 +0000

    Add package ticketo-common/0.1.18/ticketo_common-0.1.18-py3-none-any.whl

---
+++ packages/ticketo-common/ticketo_common-0.1.18-py3-none-any.whl/common/consumer.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+from signal import signal, SIGINT, SIGTERM
+from django.conf import settings
+
+
+class SignalHandler:
+    def __init__(self):
+        self.received_signal = False
+        signal(SIGINT, self._signal_handler)
+        signal(SIGTERM, self._signal_handler)
+
+    def _signal_handler(self, signal, frame):
+        print(f"handling signal {signal}, exiting gracefully")
+        self.received_signal = True
+
+
+class Sqs:
+
+    def __init__(self, exchanges):
+        self.exchanges = exchanges
+
+    def consume(self):
+        sqs_client_options = {
+            'region_name': settings.AWS_REGION_NAME,
+            'aws_access_key_id': settings.AWS_ACCESS_KEY_ID,
+            'aws_secret_access_key': settings.AWS_SECRET_ACCESS_KEY,
+            'endpoint_url': settings.AWS_SQS_ENDPOINT_URL
+        }
+        sqs = boto3.resource('sqs', **sqs_client_options)
+        queue = sqs.get_queue_by_name(QueueName=settings.SERVICE_QUEUE)
+        signal_handler = SignalHandler()
+        wait_time_seconds = settings.AWS_SQS_CONSUMER_WAIT_TIME_SECONDS or 10
+        while not signal_handler.received_signal:
+            for message in queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=wait_time_seconds):
+                body = json.loads(message.body)
+                exchange_fn = self.exchanges[body.get('Subject')]
+                exchange_fn(body.get('Message'))
+                message.delete()
