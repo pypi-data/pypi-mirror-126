1aa2bb28a435496b3ad0c5f0df01af625e6f8b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:52 +0000

    Add package liaison/0.3/liaison-0.3.tar.gz

---
+++ packages/liaison/liaison-0.3.tar.gz/liaison-0.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="liaison",
+    packages=["liaison"],
+    author="Julian Nash",
+    version="0.3",
+    description="Liaison is a Python library for defining schemas, parsing and validating payloads",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="python schema parser",
+    url="https://github.com/Julian-Nash/liaison",
+    project_urls={
+        "Bug Tracker": "https://github.com/Julian-Nash/liaison",
+        "Documentation": "https://github.com/Julian-Nash/liaison",
+        "Source Code": "https://github.com/Julian-Nash/liaison",
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    extras_require={
+        "dev": ["pytest", "black", "coverage"],
+        "test": ["pytest", "coverage"],
+    }
+)
