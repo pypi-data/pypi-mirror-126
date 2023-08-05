72380e0d84876ff9eae465d74a55aecb76e7ab71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:40 +0000

    Add package gamla/111/gamla-111.tar.gz

---
+++ packages/gamla/gamla-111.tar.gz/gamla-111/gamla/excepts_decorator.py
@@ -0,0 +1,46 @@
+import asyncio
+import functools
+from typing import Any, Callable, Tuple, Union
+
+from gamla import currying
+
+
+@currying.curry
+def excepts(
+    exception: Union[Tuple[Exception, ...], Exception],
+    handler: Callable[[Exception], Any],
+    function: Callable,
+):
+    if asyncio.iscoroutinefunction(function):
+
+        async def excepts(*args, **kwargs):
+            try:
+                return await function(*args, **kwargs)
+            except exception as error:
+                return handler(error)
+
+    else:
+
+        def excepts(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except exception as error:
+                return handler(error)
+
+    return functools.wraps(function)(excepts)
+
+
+def try_and_excepts(
+    exception: Union[Tuple[Exception, ...], Exception],
+    handler: Callable,
+    function: Callable,
+):
+    """Same as sync excepts only that the handler gets the original function params after the exception param."""
+
+    def try_and_excepts(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except exception as error:
+            return handler(error, *args, **kwargs)
+
+    return try_and_excepts
