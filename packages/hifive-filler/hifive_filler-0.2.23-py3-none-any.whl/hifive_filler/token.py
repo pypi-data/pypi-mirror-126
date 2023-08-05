328742245162d9d27613b0f1b1872dcc8b6e5de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:36 +0000

    Add package hifive-filler/0.2.23/hifive_filler-0.2.23-py3-none-any.whl

---
+++ packages/hifive-filler/hifive_filler-0.2.23-py3-none-any.whl/hifive_filler/token.py
@@ -0,0 +1,16 @@
+import hifive_filler.shape as shape
+
+class Token:
+	def __init__(self):
+		self.row_count = 0
+		self.column_count = 0
+		self.shape = shape.Shape()
+	
+	def update_dimensions(self, row_count, column_count):
+		self.row_count = row_count
+		self.column_count = column_count
+		self.shape.update_dimensions(row_count, column_count)
+	
+	def trim(self):
+		(max_row, max_col) = self.shape.get_max_dims()
+		self.shape.update_dimensions(max_row, max_col)
