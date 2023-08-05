067d5b000075d19e3bf9de28c07263666a80f913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:00 +0000

    Add package ssl-metrics-json-converter/0.1.1/ssl-metrics-json-converter-0.1.1.tar.gz

---
+++ packages/ssl-metrics-json-converter/ssl-metrics-json-converter-0.1.1.tar.gz/ssl-metrics-json-converter-0.1.1/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup
+
+from ssl_metrics_json_converter import version
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ssl-metrics-json-converter",
+    packages=["ssl_metrics_json_converter"],
+    version=version.version(),
+    description="SSL Metrics - JSON Converter",
+    author="Software and Systems Laboratory - Loyola University Chicago",
+    author_email="ssl-metrics@ssl.luc.edu",
+    license="BSD",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://ssl.cs.luc.edu/projects/metricsDashboard",
+    project_urls={
+        "Bug Tracker": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-json-converter/issues",
+        "GitHub Repository": "https://github.com/SoftwareSystemsLaboratory/ssl-metrics-json-converter",
+    },
+    keywords=[
+        "git",
+        "github",
+        "software engineering",
+        "metrics",
+        "software systems laboratory",
+        "ssl",
+        "loyola",
+        "loyola university chicago",
+        "luc",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy>=1.21.3",
+        "pandas>=1.3.4",
+        "progress>=1.6",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ssl-metrics-json-converter = ssl_metrics_json_converter.main:main",
+        ]
+    },
+)
