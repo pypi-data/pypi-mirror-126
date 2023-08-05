238e3f0edf5d794eba45d75dfd09c48910862bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:38 +0000

    Add package parametric-si/1.0.2/parametric-si-1.0.2.tar.gz

---
+++ packages/parametric-si/parametric-si-1.0.2.tar.gz/parametric-si-1.0.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='parametric-si',
+    version='1.0.2',
+    description='tools for parametric selective inference',
+    long_description=readme,
+    author='Takeuchi Lab',
+    author_email='miwa.daiki.mllab.nit@gmail.com',
+    url='https://github.com/takeuchi-lab/parametric-si',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "portion",
+        "sklearn"
+        ]
+)
