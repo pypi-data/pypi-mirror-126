e9e68020ccbdddf05df77d20df0b2662f3c7b3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:08 +0000

    Add package Odte/0.3.0/Odte-0.3.0.tar.gz

---
+++ packages/Odte/Odte-0.3.0.tar.gz/Odte-0.3.0/odte/__init__.py
@@ -0,0 +1,10 @@
+from .Odte import Odte
+
+__version__ = "0.3.0"
+
+__author__ = "Ricardo Montañana Gómez"
+__copyright__ = "Copyright 2020-2021, Ricardo Montañana Gómez"
+__license__ = "MIT License"
+__author_email__ = "ricardo.montanana@alu.uclm.es"
+
+__all__ = ["Odte"]
