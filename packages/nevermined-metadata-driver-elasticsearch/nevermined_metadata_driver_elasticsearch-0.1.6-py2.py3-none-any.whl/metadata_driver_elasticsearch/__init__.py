1ad221435d6635ba08dad0d6995c52f1d82384b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:11 +0000

    Add package nevermined-metadata-driver-elasticsearch/0.1.6/nevermined_metadata_driver_elasticsearch-0.1.6-py2.py3-none-any.whl

---
+++ packages/nevermined-metadata-driver-elasticsearch/nevermined_metadata_driver_elasticsearch-0.1.6-py2.py3-none-any.whl/metadata_driver_elasticsearch/__init__.py
@@ -0,0 +1,4 @@
+__author__ = """Nevermined"""
+__version__ = '0.1.6'
+
+from metadata_driver_elasticsearch import plugin
---
+++ packages/nevermined-metadata-driver-elasticsearch/nevermined_metadata_driver_elasticsearch-0.1.6-py2.py3-none-any.whl/metadata_driver_elasticsearch/indexes.py
@@ -0,0 +1,13 @@
+price = "service.attributes.main.price"
+license = "service.attributes.main.license"
+sample = "service.attributes.additionalInformation.links.type"
+categories = "service.attributes.additionalInformation.categories"
+tags = "service.attributes.additionalInformation.tags"
+created = "created"
+datePublished = "service.attributes.main.datePublished"
+dateCreated = "service.attributes.main.dateCreated"
+updated_frequency = "service.attributes.additionalInformation.updateFrequency"
+service_type = "service.attributes.main.type"
+did = "_id"
+
+list_indexes = [price, license, sample, categories, created, updated_frequency, service_type, did]
