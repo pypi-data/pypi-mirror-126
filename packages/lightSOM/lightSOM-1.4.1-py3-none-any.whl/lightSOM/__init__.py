19a0b1d9daa9b58630fd05cdc4d950a08980c48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:40 +0000

    Add package lightSOM/1.4.1/lightSOM-1.4.1-py3-none-any.whl

---
+++ packages/lightSOM/lightSOM-1.4.1-py3-none-any.whl/lightSOM/__init__.py
@@ -0,0 +1,10 @@
+from lightSOM.som_net import KSOM
+
+class SOM():
+    def __init__(self, backend="classic"):
+        self.backend=backend
+
+
+    def create(self, width, height, data,normalizer="var", distance="euclidean", lattice="hexa", feature_names=[], target=None,loadFile=None, pci=0, pbc=0, random_seed=None):
+        if self.backend=='classic':
+            return KSOM(width=width, height=height, data=data,  normalizer=normalizer, distance=distance, lattice=lattice, features_names=feature_names, target=target, loadFile=loadFile, PCI=pci, PBC=pbc, random_seed=random_seed)
