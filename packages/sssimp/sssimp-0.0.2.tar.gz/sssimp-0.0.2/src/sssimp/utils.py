03b6003eeb43351f11727336023ea0d09da6d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:26 +0000

    Add package sssimp/0.0.2/sssimp-0.0.2.tar.gz

---
+++ packages/sssimp/sssimp-0.0.2.tar.gz/sssimp-0.0.2/src/sssimp/utils.py
@@ -0,0 +1,19 @@
+from sssimp import APP_DIR
+
+def mkdir(path):
+    """
+    Creates a directory or the parent directory if `path` is not a directory.
+    Safely ignores if already exists
+    """
+    if not path.is_dir():
+        path = path.parent
+    path.mkdir(exist_ok=True, parents=True)
+
+
+def path_strip(path, parent = APP_DIR):
+    """Strips `parent` from `path`"""
+    parent = parent or APP_DIR
+    path = str(path)
+    path = path.removeprefix(str(parent))
+    path = path.removeprefix('/')
+    return path
