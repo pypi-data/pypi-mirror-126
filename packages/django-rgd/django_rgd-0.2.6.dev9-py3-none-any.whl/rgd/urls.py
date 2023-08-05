106fab5139afbc1f8e0fdbf0e4d0632daaf5a825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:50 +0000

    Add package django-rgd/0.2.6.dev9/django_rgd-0.2.6.dev9-py3-none-any.whl

---
+++ packages/django-rgd/django_rgd-0.2.6.dev9-py3-none-any.whl/rgd/urls.py
@@ -0,0 +1,86 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+from . import rest, views
+
+urlpatterns = [
+    # API Key Authentication
+    path('api/api-token-auth', obtain_auth_token, name='api-token-auth'),
+    # Pages
+    path('rgd', views.SpatialEntriesListView.as_view(), name='rgd-index'),
+    path(
+        'rgd/statistics',
+        views.StatisticsView.as_view(),
+        name='statistics',
+    ),
+    path(
+        'rgd/spatial_entries/<int:pk>/',
+        views.spatial_entry_redirect_view,
+        name='spatial-entry-detail',
+    ),
+    #############
+    # Search
+    path('api/rgd/search', rest.search.SearchSpatialEntryView.as_view()),
+    #############
+    # Other
+    path(
+        'api/rgd/status/<model>/<int:pk>',
+        rest.download.get_status,
+        name='get-status',
+    ),
+    path(
+        'api/rgd/spatial_entry/<int:spatial_id>',
+        rest.get.GetSpatialEntry.as_view(),
+        name='spatial-entry',
+    ),
+    path(
+        'api/rgd/spatial_entry/<int:spatial_id>/footprint',
+        rest.get.GetSpatialEntryFootprint.as_view(),
+        name='spatial-entry-footprint',
+    ),
+    path(
+        'api/rgd/checksum_file/<int:pk>',
+        rest.get.GetChecksumFile.as_view(),
+        name='checksum-file',
+    ),
+    path(
+        'api/rgd/checksum_file/<int:pk>/data',
+        rest.download.download_checksum_file,
+        name='checksum-file-data',
+    ),
+    path(
+        'api/rgd/checksum_file',
+        rest.post.CreateChecksumFile.as_view(),
+        name='checksum-create',
+    ),
+    path(
+        'api/rgd/collection',
+        rest.post.CreateCollection.as_view(),
+        name='collection-create',
+    ),
+    path(
+        'api/rgd/collection/<int:pk>',
+        rest.get.GetCollection.as_view(),
+        name='collection',
+    ),
+    path(
+        'api/rgd/collection_permission',
+        rest.post.CreateCollectionPermission.as_view(),
+        name='collection-permission-create',
+    ),
+    path(
+        'api/rgd/collection_permission/<int:pk>',
+        rest.get.GetCollectionPermission.as_view(),
+        name='collection-permission',
+    ),
+    path(
+        'api/rgd/spatial_asset',
+        rest.post.CreateSpatialAsset.as_view(),
+        name='spatial-asset-create',
+    ),
+    path(
+        'api/rgd/spatial_asset/<int:pk>',
+        rest.get.GetSpatialAsset.as_view(),
+        name='spatial-asset',
+    ),
+]
