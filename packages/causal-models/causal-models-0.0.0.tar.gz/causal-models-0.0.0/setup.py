83495281d21f8431c6d0d30d8b700d73fd677a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:49 +0000

    Add package causal-models/0.0.0/causal-models-0.0.0.tar.gz

---
+++ packages/causal-models/causal-models-0.0.0.tar.gz/causal-models-0.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='causal-models',
+    version='0.0.0',
+    description='Causal Models placeholder.',
+    author='Matthew Wardrop',
+    author_email='mpwardrop@gmail.com',
+)
