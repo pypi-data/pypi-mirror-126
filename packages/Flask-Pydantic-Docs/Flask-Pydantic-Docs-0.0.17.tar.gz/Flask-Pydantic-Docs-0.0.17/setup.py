58b4b47983e6cb2f7728e6860ef1bcbbad8fe4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:31 +0000

    Add package Flask-Pydantic-Docs/0.0.17/Flask-Pydantic-Docs-0.0.17.tar.gz

---
+++ packages/Flask-Pydantic-Docs/Flask-Pydantic-Docs-0.0.17.tar.gz/Flask-Pydantic-Docs-0.0.17/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+cwd = Path(__file__).resolve().parent
+
+readme = (cwd / "README.md").read_text()
+
+setup(
+    name="Flask-Pydantic-Docs",
+    version="0.0.17",
+    author="Steve Guo",
+    author_email="buildpeak@gmail.com",
+    description="Add openapi docs to flask app using Flask-Pydantic",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/buildpeak/flask_pydantic_docs",
+    packages=find_packages(exclude=["examples*", "tests*"]),
+    package_data={"flask_pydantic_docs": ["templates/*.html"]},
+    classifiers=[],
+    install_requires=[
+        "Flask",
+        "pydantic",
+        "flask-pydantic",
+    ],
+    zip_safe=False,
+    extras_require={},
+)
