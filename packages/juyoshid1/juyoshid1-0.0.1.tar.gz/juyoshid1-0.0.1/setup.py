cd085a4be105490bc85df63584331337639b6622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:11 +0000

    Add package juyoshid1/0.0.1/juyoshid1-0.0.1.tar.gz

---
+++ packages/juyoshid1/juyoshid1-0.0.1.tar.gz/juyoshid1-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    #name="haiji-filler'
+    name="juyoshid1",
+    version="0.0.1",
+    author="Team haiji",
+    author_email="haiji@student.42tokyo.jp",
+    description="",
+    package_dir={'src/': 'src'},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+                 ],
+                 )
