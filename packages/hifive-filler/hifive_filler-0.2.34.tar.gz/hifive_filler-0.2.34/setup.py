abf46689c0e3e4d788e8d50caae08e3bf3adefee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:47 +0000

    Add package hifive-filler/0.2.34/hifive_filler-0.2.34.tar.gz

---
+++ packages/hifive-filler/hifive_filler-0.2.34.tar.gz/hifive_filler-0.2.34/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hifive_filler']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['hifive_filler = hifive_filler.main:main']}
+
+setup_kwargs = {
+    'name': 'hifive-filler',
+    'version': '0.2.34',
+    'description': '',
+    'long_description': None,
+    'author': 'yusata',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
