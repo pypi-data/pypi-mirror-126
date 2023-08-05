8c6eac20d5a145d03f4c6d956e4cb44e8021b8a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:00 +0000

    Add package unin/1.0.2/unin-1.0.2.tar.gz

---
+++ packages/unin/unin-1.0.2.tar.gz/unin-1.0.2/unin/__init__.py
@@ -0,0 +1,14 @@
+'''This is a main folder of the unin package.'''
+
+#imports essential functions from the duality package.
+from duality import eoq
+from duality import hub
+from duality.misc.global_functions import *
+from duality import montecarlo
+from unin.misc._meta import (
+	__author__,
+	__copyright__,
+	__credits__,
+	__license__,
+	__version__,
+)
