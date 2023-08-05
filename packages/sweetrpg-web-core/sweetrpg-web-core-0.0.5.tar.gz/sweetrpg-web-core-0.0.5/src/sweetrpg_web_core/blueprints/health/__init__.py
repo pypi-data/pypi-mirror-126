9a84b1d8edbe470d03de2bd5d3be2b8f7ffb057a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:39 +0000

    Add package sweetrpg-web-core/0.0.5/sweetrpg-web-core-0.0.5.tar.gz

---
+++ packages/sweetrpg-web-core/sweetrpg-web-core-0.0.5.tar.gz/sweetrpg-web-core-0.0.5/src/sweetrpg_web_core/blueprints/health/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""health.py
+Health endpoints.
+"""
+
+from flask import Blueprint, current_app, jsonify
+from werkzeug.exceptions import HTTPException
+import json
+import os
+from sweetrpg_web_core import constants
+
+
+blueprint = Blueprint("health", __name__, url_prefix="/health")
+
+
+@blueprint.route("/status")
+def health_check():
+    r = {'services':{}}
+    build_info_path = os.environ.get(constants.BUILD_INFO_PATH)
+    if build_info_path:
+        with open(build_info_path, "r") as bi:
+            build_info = json.load(bi)
+            r["build"] = build_info
+
+    return r
+
+
+@blueprint.route("/ping")
+def ping():
+    return "pong"
