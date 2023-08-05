6a13ee146382e4050152802f791f0a7cab98f37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:42 +0000

    Add package professorerdo-first/0.1/professorerdo_first-0.1.tar.gz

---
+++ packages/professorerdo-first/professorerdo_first-0.1.tar.gz/professorerdo_first-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='professorerdo_first',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['professorerdo_first'],
+      author = 'Fan Fan',
+      author_email = 'mike.fan@foxmail.com',
+    zip_safe=False)
