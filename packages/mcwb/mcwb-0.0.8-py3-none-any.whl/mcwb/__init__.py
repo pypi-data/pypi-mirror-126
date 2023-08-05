9e768e37e7135f22e45a22337fad843cd652002b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:18 +0000

    Add package mcwb/0.0.8/mcwb-0.0.8-py3-none-any.whl

---
+++ packages/mcwb/mcwb-0.0.8-py3-none-any.whl/mcwb/__init__.py
@@ -0,0 +1,13 @@
+"""Methods to build in the world."""
+
+from mcwb.api import mktunnel
+from mcwb.types import (
+    Anchor, Anchor3, Anchor3Face, Cuboid, Direction, Items, Profile, Row, Vec3
+)
+from mcwb.volume import Volume
+
+
+__all__ = ['Anchor', 'Anchor3', 'Anchor3Face', 'Cuboid', 'Direction', 'Items',
+           'Profile', 'Row', 'Vec3', 'Volume',
+           'mktunnel'
+           ]
