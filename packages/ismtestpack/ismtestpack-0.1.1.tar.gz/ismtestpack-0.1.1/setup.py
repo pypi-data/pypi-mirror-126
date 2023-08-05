802c8a5b8ac14b1c017af4a3baef835f11f38279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package ismtestpack/0.1.1/ismtestpack-0.1.1.tar.gz

---
+++ packages/ismtestpack/ismtestpack-0.1.1.tar.gz/ismtestpack-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ismtestpack',
+    version='0.1.1',    
+    description='A example Python package',
+    author='Ismail',
+    install_requires=['numpy<=1.10','opencv-python'],
+
+)
