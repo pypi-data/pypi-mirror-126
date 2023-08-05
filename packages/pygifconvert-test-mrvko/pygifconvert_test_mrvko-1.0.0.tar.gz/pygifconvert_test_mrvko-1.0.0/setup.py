4da5ccf0dd89716befa620fb85f949dd3028579f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:06 +0000

    Add package pygifconvert-test-mrvko/1.0.0/pygifconvert_test_mrvko-1.0.0.tar.gz

---
+++ packages/pygifconvert-test-mrvko/pygifconvert_test_mrvko-1.0.0.tar.gz/pygifconvert_test_mrvko-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'pygifconvert_test_mrvko',
+    version          = '1.0.0',
+    description      = 'Test package for distribution',
+    author           = 'Marvinko',
+    author_email     = 'dong7863@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['pillow'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['GIFCONVERTER', 'gifconverter'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
