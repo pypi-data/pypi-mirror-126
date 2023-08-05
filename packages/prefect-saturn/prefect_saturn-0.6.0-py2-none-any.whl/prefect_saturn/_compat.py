b337a6c9651be09f06075ac4509a69ebcfc0d998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:14 +0000

    Add package prefect-saturn/0.6.0/prefect_saturn-0.6.0-py2-none-any.whl

---
+++ packages/prefect-saturn/prefect_saturn-0.6.0-py2-none-any.whl/prefect_saturn/_compat.py
@@ -0,0 +1,36 @@
+# pylint: disable=unused-import
+
+"""
+This module is used to handle compatibility with multiple
+versions of dependencies. For example, some import paths
+were deprecated in ``prefect`` 0.14.x and will be removed in the
+next major release of ``prefect``.
+"""
+
+# prefect.environments.storage was deprecated in prefect 0.14.x
+try:
+    from prefect.storage import Webhook  # noqa: F401
+except (ImportError, ModuleNotFoundError):
+    from prefect.environments.storage import Webhook  # type: ignore # noqa: F401
+
+# prefect.engine.executors was deprecated in prefect 0.14.x
+try:
+    from prefect.executors import DaskExecutor, LocalExecutor  # noqa: F401
+except (ImportError, ModuleNotFoundError):
+    from prefect.engine.executors import DaskExecutor, LocalExecutor  # noqa: F401
+
+# prefect.run_configs was introduced in prefect 0.13.10
+try:
+    from prefect.run_configs import KubernetesRun  # noqa: F401
+
+    RUN_CONFIG_AVAILABLE = True
+except (ImportError, ModuleNotFoundError):
+    RUN_CONFIG_AVAILABLE = False
+
+# prefect.environments.KubernetesJobEnvironment was marked "deprecated" in prefect 0.14.x
+try:
+    from prefect.environments import KubernetesJobEnvironment  # noqa: F401
+
+    KUBE_JOB_ENV_AVAILABLE = True
+except (ImportError, ModuleNotFoundError):
+    KUBE_JOB_ENV_AVAILABLE = False
