635fbfafae69192cc13814e7523341fa490bd7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:18 +0000

    Add package nofnof-filler/0.9.0/nofnof_filler-0.9.0.tar.gz

---
+++ packages/nofnof-filler/nofnof_filler-0.9.0.tar.gz/nofnof_filler-0.9.0/nofnof_filler/main.py
@@ -0,0 +1,18 @@
+from noFillNoFuture import NoFillNoFuture
+
+""" THIS CODE IS OUR ORIGINAL!!!!!!!!!! """
+""" DO YOU UNDERSTAND THIS MEAN?????"""
+""" YOU ARE REALLY LEARN PYTHON????????? """
+""" WRITE CODE YOURSELF!!!!!!!!!!!!!!!!!!!!!!!"""
+"""DO NOT COPYYYYYYYYY"""
+
+def main():
+    nfnf = NoFillNoFuture()
+    while True:
+        nfnf.search()
+        print(f"{nfnf.coordinate_mapping_y} {nfnf.coordinate_mapping_x}")
+
+
+if __name__ == '__main__':
+    main()
+
