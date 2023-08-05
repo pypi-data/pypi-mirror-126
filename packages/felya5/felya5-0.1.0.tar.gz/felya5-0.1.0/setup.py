0670b811377a94d56ef190b6692c543f07c51015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package felya5/0.1.0/felya5-0.1.0.tar.gz

---
+++ packages/felya5/felya5-0.1.0.tar.gz/felya5-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='felya5',
+    version='0.1.0',
+    description='A example Python package',    
+    author='Ofelia Epremyan',
+    install_requires=['numpy<1.16', 'matplotlib'],
+
+)
