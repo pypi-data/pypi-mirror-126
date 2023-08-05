678a60764a204ee5c8cd2c6f08c258e5477a9beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package django-wildewidgets/0.8.0/django_wildewidgets-0.8.0-py2.py3-none-any.whl

---
+++ packages/django-wildewidgets/django_wildewidgets-0.8.0-py2.py3-none-any.whl/wildewidgets/__init__.py
@@ -0,0 +1,31 @@
+__version__ = "0.8.0"
+
+from .wildewidgets import (
+    AltairChart,
+    BarChart, 
+    BasicMenu,
+    DoughnutChart,
+    Histogram,
+    HorizontalBarChart, 
+    HorizontalHistogram,
+    HorizontalStackedBarChart,
+    LightMenu,
+    MenuMixin,
+    PieChart,
+    StackedBarChart, 
+    TabbedWidget,
+    TemplateWidget,
+    WildewidgetDispatch,
+)
+
+from .tables import (
+    BasicModelTable,
+    DataTable,
+    DataTableFilter,
+    DataTableStyler,
+    TableView,
+)
+
+from .views import (
+    TableActionFormView,
+)
