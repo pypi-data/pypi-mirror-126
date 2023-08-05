12e2a2ec05c13865f5d92a66bd80cdd0e71f7f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:35 +0000

    Add package omero-cli-zarr/0.2.1/omero_cli_zarr-0.2.1-py3-none-any.whl

---
+++ packages/omero-cli-zarr/omero_cli_zarr-0.2.1-py3-none-any.whl/omero_zarr/util.py
@@ -0,0 +1,35 @@
+import time
+
+from zarr.storage import FSStore
+
+
+def print_status(t0: int, t: int, count: int, total: int) -> None:
+    """Prints percent done and ETA.
+    t0: start timestamp in seconds
+    t: current timestamp in seconds
+    count: number of tasks done
+    total: total number of tasks
+    """
+    percent_done = float(count) * 100 / total
+    dt = t - t0
+    if dt > 0:
+        rate = float(count) / (t - t0)
+        eta_f = float(total - count) / rate
+        eta = time.strftime("%H:%M:%S", time.gmtime(eta_f))
+    else:
+        eta = "NA"
+    status = f"{percent_done:.2f}% done, ETA: {eta}"
+    print(status, end="\r", flush=True)
+
+
+def open_store(name: str) -> FSStore:
+    """
+    Create an FSStore instance that supports nested storage of chunks.
+    """
+    return FSStore(
+        name,
+        auto_mkdir=True,
+        key_separator="/",
+        normalize_keys=False,
+        mode="w",
+    )
