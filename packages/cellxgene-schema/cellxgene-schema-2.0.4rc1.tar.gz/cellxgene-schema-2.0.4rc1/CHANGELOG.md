8306513da7ee69b3fb257fc7a0c49b90e5e66efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:39 +0000

    Add package cellxgene-schema/2.0.4rc1/cellxgene-schema-2.0.4rc1.tar.gz

---
+++ packages/cellxgene-schema/cellxgene-schema-2.0.4rc1.tar.gz/cellxgene-schema-2.0.4rc1/CHANGELOG.md
@@ -0,0 +1,51 @@
+# Changelog
+All notable changes to the python package `cellxgene-schema` are documented in this file.
+
+The format of this changelog is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [2.0.4] - 2021-11-01    
+    
+### Added    
+    
+- A check during raw data validation in `validate.Validator`. When both `adata.X` and `adata.raw.X` are present, but `adata.raw.X` contains non-integer values a warning is added.         
+
+## [2.0.3] - 2021-10-08
+### Changed
+- Replace print with logging in `Validator` and `AnnDataLabelAppender`.
+- Return the list of errors from `validate` so they can be used by callers.
+- Add verbose option to CLI, it prints a progress log.
+
+## [2.0.2] - 2021-10-07
+
+### Changed
+
+- Adds the assay ontology term EFO:0008939 (snmC-seq) to the list of assays for which validation of raw data is bypassed.
+
+## [2.0.1] - 2021-10-06
+
+### Changed
+
+- Improves the performance of `Validator._is_raw()` in `validate.py`, and addresses timeouts when validating datasets with high number of non-zero values in the expression matrix.
+
+## [2.0.0] - 2021-09-15
+
+### Added
+
+- All **MUST** requirements in [schema version 2.0.0](https://github.com/chanzuckerberg/single-cell-curation/blob/main/schema/2.0.0/corpora_schema.md) are strictly enforced. For failures, an error message is displayed and validation fails. 
+- Some **STRONGLY RECOMMENDED** requirements in the schema version 2.0.0 are checked. Warnings are displayed when recommended best practices are not observed.
+- Pinned versions of ontology and feature references (see `./cellxgene_schema/ontology_files/`).
+- Downloader and parser for ontology and feature references (see `./scripts/`).
+- Option to apply human-readable labels for ontology and feature references: `cellxgene-schema validate --add-labels`.
+- Tests that mirror the requirements in schema version 2.0.0.
+
+### Changed
+
+- `cellxgene-schema validate` validates schema version 2.0.0. The implementation is *from scratch*. 
+- Ontology validation and label retrieval depend on downloaded references instead of the EBI Ontology Service (see `./cellxgene_schema/ontology.py`).
+- Gene/feature validation and label retrieval depend on downloaded references (see `./cellxgene_schema/ontology.py`).
+
+### Removed
+
+- Subcommand `cellxgene-schema apply`.
+- Support for schema versions 1.x.x.
---
+++ packages/cellxgene-schema/cellxgene-schema-2.0.4rc1.tar.gz/cellxgene-schema-2.0.4rc1/tests/test_validate.py
@@ -0,0 +1,200 @@
+import unittest
+
+from cellxgene_schema.write_labels import AnnDataLabelAppender
+from cellxgene_schema.ontology import OntologyChecker
+from cellxgene_schema.schema import get_schema_definition
+from cellxgene_schema.validate import Validator
+from fixtures.examples_validate import adata_minimal, SCHEMA_VERSION, adata_with_labels, adata
+
+
+# Tests for internal functions of the Validator and LabelWriter classes.
+
+
+class TestFieldValidation(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.schema_def = get_schema_definition(SCHEMA_VERSION)
+        cls.OntologyChecker = OntologyChecker()
+
+    def setUp(self):
+        self.validator = Validator()
+        self.validator.adata = adata_minimal
+        self.column_name = "cell_type_ontology_term_id"
+        self.column_schema = self.schema_def["components"]["obs"]["columns"][
+            self.column_name
+        ]
+        self.curie_constraints = self.schema_def["components"]["obs"]["columns"][
+            self.column_name
+        ]["curie_constraints"]
+
+    def test_schema_definition(self):
+
+        """
+        Tests that the definition of schema is well-defined
+        """
+
+        self.assertIsInstance(self.schema_def["components"], dict)
+        self.assertIsInstance(self.schema_def["components"]["obs"], dict)
+        self.assertIsInstance(self.schema_def["components"]["obs"]["columns"], dict)
+
+        # Check that any columns in obs that are "curie" have "curie_constraints" and "ontologies" under the constraints
+        for i in self.schema_def["components"]["obs"]["columns"]:
+            self.assertTrue(
+                "type" in self.schema_def["components"]["obs"]["columns"][i]
+            )
+            if i == "curie":
+                self.assertIsInstance(
+                    self.schema_def["components"]["obs"]["columns"][i][
+                        "curie_constrains"
+                    ],
+                    dict,
+                )
+                self.assertIsInstance(
+                    self.schema_def["components"]["obs"]["columns"][i][
+                        "curie_constrains"
+                    ]["ontolgies"],
+                    list,
+                )
+
+                # Check that the allowed ontologies are in the ontology checker
+                for ontology_name in self.schema_def["components"]["obs"]["columns"][i][
+                    "curie_constrains"
+                ]["ontolgies"]:
+                    self.assertTrue(
+                        self.OntologyChecker.is_valid_ontology(ontology_name)
+                    )
+
+    def test_validate_ontology_good(self):
+        self.validator._validate_curie(
+            "CL:0000066", self.column_name, self.curie_constraints
+        )
+        self.validator._validate_curie(
+            "CL:0000192", self.column_name, self.curie_constraints
+        )
+        self.assertFalse(self.validator.errors)
+
+    def test_validate_ontology_wrong_ontology(self):
+        self.validator._validate_curie(
+            "EFO:0009899", self.column_name, self.curie_constraints
+        )
+        self.assertTrue(self.validator.errors)
+
+    def test_validate_ontology_wrong_term(self):
+        self.validator._validate_curie(
+            "NO_TERM2", self.column_name, self.curie_constraints
+        )
+        self.assertTrue(self.validator.errors)
+
+
+class TestAddLabelFunctions(unittest.TestCase):
+    def setUp(self):
+
+        # Set up test data
+        self.test_adata = adata
+        self.test_adata_with_labels = adata_with_labels
+        self.schema_def = get_schema_definition(SCHEMA_VERSION)
+
+        validator = Validator()
+        validator.adata = self.test_adata
+        validator.validate_adata()
+        self.writer = AnnDataLabelAppender(validator)
+
+    def test_get_dictionary_mapping_feature_id(self):
+
+        # Good
+        ids = [
+            "ERCC-00002",
+            "ENSG00000127603",
+            "ENSMUSG00000059552",
+            "ENSSASG00005000004",
+        ]
+        labels = [
+            "ERCC-00002 (spike-in control)",
+            "MACF1",
+            "Trp53",
+            "S_ENSSASG00005000004",
+        ]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        self.assertEqual(self.writer._get_mapping_dict_feature_id(ids), expected_dict)
+
+        # Bad
+        ids = ["NO_GENE"]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        with self.assertRaises(KeyError):
+            self.writer._get_mapping_dict_feature_id(ids)
+
+    def test_get_dictionary_mapping_feature_reference(self):
+
+        # Good
+        ids = [
+            "ERCC-00002",
+            "ENSG00000127603",
+            "ENSMUSG00000059552",
+            "ENSSASG00005000004",
+        ]
+        labels = [
+            "NCBITaxon:32630",
+            "NCBITaxon:9606",
+            "NCBITaxon:10090",
+            "NCBITaxon:2697049",
+        ]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        self.assertEqual(
+            self.writer._get_mapping_dict_feature_reference(ids), expected_dict
+        )
+
+        # Bad
+        ids = ["NO_GENE"]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        with self.assertRaises(KeyError):
+            self.writer._get_mapping_dict_feature_id(ids)
+
+    def test_get_dictionary_mapping_curie(self):
+
+        # Good
+        ids = ["CL:0000066", "CL:0000192"]
+        labels = ["epithelial cell", "smooth muscle cell"]
+        curie_constraints = self.schema_def["components"]["obs"]["columns"][
+            "cell_type_ontology_term_id"
+        ]["curie_constraints"]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        self.assertEqual(
+            self.writer._get_mapping_dict_curie(ids, curie_constraints), expected_dict
+        )
+
+        ids = ["EFO:0009899", "EFO:0009922"]
+        labels = ["10x 3' v2", "10x 3' v3"]
+        curie_constraints = self.schema_def["components"]["obs"]["columns"][
+            "assay_ontology_term_id"
+        ]["curie_constraints"]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        self.assertEqual(
+            self.writer._get_mapping_dict_curie(ids, curie_constraints), expected_dict
+        )
+
+        ids = ["MONDO:0100096"]
+        labels = ["COVID-19"]
+        curie_constraints = self.schema_def["components"]["obs"]["columns"][
+            "disease_ontology_term_id"
+        ]["curie_constraints"]
+        expected_dict = {i: j for i, j in zip(ids, labels)}
+        self.assertEqual(
+            self.writer._get_mapping_dict_curie(ids, curie_constraints), expected_dict
+        )
+
+        # Bad
+        curie_constraints = self.schema_def["components"]["obs"]["columns"][
+            "cell_type_ontology_term_id"
+        ]["curie_constraints"]
+
+        ids = ["CL:0000066", "CL:0000192_FOO"]
+        with self.assertRaises(ValueError):
+            self.writer._get_mapping_dict_curie(ids, curie_constraints)
+
+        ids = ["CL:0000066", "CL:0000192", "UBERON:0002048"]
+        with self.assertRaises(ValueError):
+            self.writer._get_mapping_dict_curie(ids, curie_constraints)
+
+        ids = ["CL:NO_TERM"]
+        with self.assertRaises(ValueError):
+            self.writer._get_mapping_dict_curie(ids, curie_constraints)
