fdadcb366a5cb88ff1764819645c24392f9fcb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:47 +0000

    Add package setux-logger/0.21.44.0/setux_logger-0.21.44.0-py3-none-any.whl

---
+++ packages/setux-logger/setux_logger-0.21.44.0-py3-none-any.whl/setux/logger/logger.py
@@ -0,0 +1,23 @@
+from logging import getLogger
+from logging.config import dictConfig
+
+from setux.core.logger import Logger, Deploy
+from .config import config
+
+
+dictConfig(config)
+
+
+logger = Logger(getLogger('Setux'))
+
+debug = logger.debug
+info = logger.info
+error = logger.error
+exception = logger.exception
+
+
+deploy = Deploy(getLogger('Deploy'), logger)
+
+green = deploy.green
+yellow = deploy.yellow
+red = deploy.red
