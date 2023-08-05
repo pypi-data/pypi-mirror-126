7b38152c90b9ca3d13d6507bc304f88385513907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package aamarpay/0.0.4/aamarpay-0.0.4.tar.gz

---
+++ packages/aamarpay/aamarpay-0.0.4.tar.gz/aamarpay-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+requires = [
+
+    'requests==2.26.0',
+]
+setuptools.setup(
+    name='aamarpay',
+    version='0.0.4',
+    description='Payment gateway libarary',
+    long_description='long_description',
+    url='https://github.com/sanjidbillah/aamarPay-python',
+    author='Masum billah sanjid',
+    author_email='masumbillahsanjid@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Payment',
+    packages=setuptools.find_packages(),
+    install_requires=requires
+)
