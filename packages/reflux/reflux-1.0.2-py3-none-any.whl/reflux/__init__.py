3bc81797599dc7557ba9d52cfb4913bd258481fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:19 +0000

    Add package reflux/1.0.2/reflux-1.0.2-py3-none-any.whl

---
+++ packages/reflux/reflux-1.0.2-py3-none-any.whl/reflux/__init__.py
@@ -0,0 +1,8 @@
+from reflux.theme import Theme
+
+from reflux.resources import shelf as Resources
+
+__all__ = ["Theme", "Resources"]
+__author__ = "frissyn"
+__version__ = "1.0.2"
+__doc__ = "Extensive styling and theming functionality for Replit."
