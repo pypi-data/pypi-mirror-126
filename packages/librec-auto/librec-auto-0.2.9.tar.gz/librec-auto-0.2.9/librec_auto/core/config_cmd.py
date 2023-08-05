4cc84f42f258ca856ced63a4e5c2a7819408a0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:28 +0000

    Add package librec-auto/0.2.9/librec-auto-0.2.9.tar.gz

---
+++ packages/librec-auto/librec-auto-0.2.9.tar.gz/librec-auto-0.2.9/librec_auto/core/config_cmd.py
@@ -0,0 +1,372 @@
+from collections import OrderedDict, defaultdict
+from librec_auto.core.util.errors import InvalidConfiguration
+from librec_auto.core.util import Files, utils, build_parent_path, LibrecProperties, \
+    xml_load_from_path, Library, LibraryColl, merge_elements, VarColl, JavaVersionException
+from librec_auto.core.config_lib import ConfigLibCollection, ConfigLib
+from librec_auto.core.util.xml_utils import single_xpath
+from librec_auto.core.util.protected_feature import ProtectedFeature
+
+from lxml import etree
+from typing import List
+import copy
+import logging
+from pathlib import Path
+import re
+import os
+
+
+class ConfigCmd:
+    """
+    Loads the configuration file, inserts appropriate library contents,
+    identifies the parameter variations and creates separate configurations
+    for all combinations.
+    """
+
+    _PARAM_NAME_PATH_RE = ".+\[@name='(.+)'\]"
+    _CV_DIR_RE = "cv_\d+"
+
+    def __init__(self, config_file, target, log_filename=None):
+
+        self._files = Files()
+        self._target = target
+        self._log_filename = log_filename
+        self._count = None
+
+        self._files.set_study_path(target)
+        self._files.set_config_file(config_file)
+        self._files.create_temp_dir()
+
+        self._xml_input = self.read_xml(self._files.get_config_file_path())
+        self.protected_features = ProtectedFeature(ProtectedFeature.parse_protected(self), 
+                                                   self._files.get_temp_dir_path())
+
+        if self.is_valid():
+            self._files.set_data_path(self._xml_input)
+            self._var_coll = VarColl()
+            self._libraries = LibraryColl()
+            self._bbo_steps = 0
+
+            self._key_password = None
+
+    def get_target(self):
+        return self._target
+
+    def set_target(self, target):
+        self._target = target
+
+    def get_xml(self):
+        return self._xml_input
+
+    def get_librec_auto_log_name(self):
+        return self._log_filename
+
+    # def get_var_data(self):
+    #     return self._var_data
+
+    def get_key_password(self):
+        return self._key_password
+
+    def set_key_password(self, pw):
+        self._key_password = pw
+
+    def get_value_conf(self, subexp_no):
+        return self._var_coll.var_confs[subexp_no]
+
+    def has_alg_script(self):
+        alg_script_elem = single_xpath(self._xml_input, '/librec-auto/alg/script')
+        return (alg_script_elem is not None)
+
+    def get_bbo_steps(self):
+        if self._bbo_steps > 0:
+            return self._bbo_steps
+        else:
+            return None
+
+    def get_sub_exp_count(self):
+        exp_count = len(self._var_coll.var_confs)
+
+        if self.get_bbo_steps() is not None:
+            self._count = self.get_bbo_steps()
+
+        if self._count is not None:
+            return self._count
+        elif exp_count == 0:
+            return 1
+        else:
+            return exp_count
+
+    def get_files(self):
+        return self._files
+
+    def read_xml(self, path_str):
+        path = self._files.get_config_file_path()
+        if (path.exists()):
+            xml_input = xml_load_from_path(path)
+            return xml_input
+        else:
+            return None
+
+    def ensure_experiments(self, exp_no = None):
+        if self.get_bbo_steps() is None:
+            exp_count = len(self._var_coll.var_confs)
+            if exp_no is not None:
+                exp_count = exp_no
+            if exp_count == 0:
+                exp_count = 1
+        else:
+            exp_count = self.get_bbo_steps()
+        self.get_files().ensure_exp_paths(exp_count)
+
+    def load_libraries(self):
+        lib_paths = []
+        lib_elems = self._xml_input.xpath('/librec-auto/library')
+        for elem in lib_elems:
+            self._libraries.add_lib(
+                Library(elem.text, elem.get('src'), self._files))
+
+    # Process config takes the config file and produces a dictionary of the following form:
+    # xpath-string => list of values
+    # or xpath-string => (range-to, range-from) pair
+    # Right now, we will assume the first
+    def process_config(self):
+        self.setup_bbo()
+        self._var_data = defaultdict(list)
+        self.substitute_library()
+        self.collect_vars()
+        self.ensure_experiments()
+        # self.label_repeats()
+
+    def setup_bbo(self):
+        opt_elem = single_xpath(self._xml_input, '/librec-auto/optimize')
+        if opt_elem is None:
+            self._bbo_steps = 0
+        else:
+            self._bbo_steps = int(single_xpath(opt_elem,'iterations').text)
+
+
+    # Have to wait to writes experiment-specific XML configurations to each exp directory
+    # in case a purge is happening.
+    def setup_exp_configs(self, startflag = None):
+        self.write_exp_configs(startflag)
+
+    def substitute_library(self):
+        ref_elems = self._xml_input.xpath('//*[@ref]')
+        pro_feat_names = self.protected_features.get_protected_feature_names()
+        for ref_elem in ref_elems:
+            ref_name = ref_elem.get('ref')
+            named_elem = self._libraries.get_elem(ref_name)
+            if named_elem is not None:
+                merged_elem = merge_elements(named_elem, ref_elem)
+                ref_elem.getparent().replace(ref_elem, merged_elem)
+            else:
+                if ref_name in pro_feat_names:
+                    continue
+                else:    
+                    logging.warning(f"No such element in library {ref_name}")
+
+    def collect_vars(self):
+        self.collect_librec_vars()
+        self.collect_rerank_vars()
+        self._var_coll.compute_var_configurations()
+
+    def collect_librec_vars(self):
+        Tag = 'value'
+
+        value_elems = self._xml_input.xpath(
+            '/librec-auto/*[not(self::rerank)]/*/value')
+        
+        value_optimize_elems = self._xml_input.xpath(
+            '/librec-auto/alg/*//lower')
+
+        check_multiple_values = [elem.getparent() for elem in value_elems]
+        check_multiple_values = list(set(check_multiple_values))
+
+        has_optimize = len(self._xml_input.xpath('/librec-auto/optimize')) > 0
+
+        if len(check_multiple_values) != len(value_elems) and len(value_optimize_elems) > 0:
+            raise InvalidConfiguration("optimization", "You may only use upper/lower for optimizing ranges")
+        elif len(value_optimize_elems) > 0 and not has_optimize:
+            raise InvalidConfiguration("optimization", "You may only use upper/lower for optimizing ranges")
+        elif len(value_elems) > 0 and has_optimize:
+            raise InvalidConfiguration("optimization", "Use of upper/lower value bounds requires optimize tag")
+        elif len(value_optimize_elems) > 0:
+            value_elems = value_optimize_elems + self._xml_input.xpath(
+            '/librec-auto/alg/*//upper')
+            parents = [elem.getparent() for elem in value_elems]
+
+            Tag = 'lower'
+
+        parents = [elem.getparent() for elem in value_elems]
+        parents = list(set(parents))
+        if Tag == 'value':
+            for parent in parents:
+                vals = [elem.text for elem in parent.iterchildren(tag=Tag)]
+                parent_path = build_parent_path(parent)
+                self._var_coll.add_var('librec', parent_path, vals)
+
+        else:
+            for parent in parents:
+                val_lower = [elem.text for elem in parent.iterchildren(tag='lower')]
+                val_upper = [elem.text for elem in parent.iterchildren(tag='upper')]
+                vals = []
+                # print(val_lower,val_upper)
+                for i in range(len(val_lower)):
+                    vals.append(val_lower[i])
+                    vals.append(val_upper[i])
+
+                parent_path = build_parent_path(parent)
+                self._var_coll.add_var('librec', parent_path, vals)
+
+    def collect_rerank_vars(self):
+        value_elems = self._xml_input.xpath('/librec-auto/rerank/*//value')
+        parents = [elem.getparent() for elem in value_elems]
+        parents = list(set(parents))
+        for parent in parents:
+            vals = [elem.text for elem in parent.iterchildren(tag='value')]
+            parent_path = build_parent_path(parent)
+            self._var_coll.add_var('rerank', parent_path, vals)
+
+    # Write versions of the config file in which the parameters with multiple values are replaced with
+    # a single value
+    def write_exp_configs(self, startflag = None, val = None, iteration = None):
+        
+        configs = list(
+                zip(self.get_files().get_exp_paths_iterator(),
+                    iter(self._var_coll.var_confs)))
+        if self.get_bbo_steps() is not None and startflag is None:
+            exp, vconf = configs[0]
+
+            for x in range(len(val)):
+                vconf.vars[x].val = val[x]
+            vconf.exp_no = None
+            vconf.exp_dir = exp.exp_name
+
+            current_exp_path = self._files.get_study_path() / self._files.get_exp_name(iteration)
+
+            exp.set_path('conf', current_exp_path / 'conf')
+            exp.exp_name = self._files.get_exp_name(iteration)
+            self.write_exp_config(exp, vconf, current_exp_path)
+
+
+        elif startflag is not None:
+            i = 0
+            for exp, vconf in configs[:1]:
+                vconf.exp_no = i
+                vconf.exp_dir = exp.exp_name
+                self.write_exp_config(exp, vconf)
+        else:
+            i = 0
+            for exp, vconf in configs:
+                vconf.exp_no = i
+                vconf.exp_dir = exp.exp_name
+                self.write_exp_config(exp, vconf)
+
+    def write_exp_config(self, exp, vconf, iteration = None):
+        new_xml = copy.deepcopy(self._xml_input)
+        # Remove libraries. All substitutions have already happened.
+        for lib in new_xml.xpath('/librec-auto/library'):
+            lib.getparent().remove(lib)
+
+        pat = re.compile(ConfigCmd._PARAM_NAME_PATH_RE)
+        for vinfo in vconf.vars:
+            var_elem = new_xml.xpath(vinfo.path)[0]
+            attr_save = copy.copy(var_elem.attrib)
+            var_elem.clear()
+            var_elem.text = str(vinfo.val)
+            var_elem.set("var", "true")
+            if var_elem.tag == 'param':  # params are distinguished by name attributes
+                mat = pat.match(vinfo.path)
+                var_elem.attrib['name'] = mat.group(1)
+            for key in attr_save.keys():
+                var_elem.attrib[key] = attr_save[key]
+        new_xml = self.protected_features.replace_referenced_protected_features(new_xml)
+        new_xml.append(
+            etree.Comment(
+                'This configuration file was automatically generated by librec-auto. '
+                +
+                'Editing may produce unpredictable results and is not recommended.'
+            ))
+        outpath = exp.get_path('conf') / Files.DEFAULT_CONFIG_FILENAME
+        logging.info('Writing config file ' + str(outpath))
+        new_xml.getroottree().write(outpath.absolute().as_posix(),
+                                    pretty_print=True)
+
+        props = LibrecProperties(new_xml, self._files)
+        exp.add_to_config(props.properties, 'librec_result')
+
+        if iteration is not None:
+            props.properties['dfs.result.dir'] = exp.exp_name + '/result'
+
+        props.save(exp)
+
+        if vconf.ref_config:
+            path = exp.get_ref_exp_flag_path()
+            with path.open(mode='w') as fh:
+                fh.write(vconf.ref_config.exp_dir)
+                fh.write('\n')
+
+    def has_rerank(self):
+        rerank_elems = self._xml_input.xpath('/librec-auto/rerank')
+        return len(rerank_elems) > 0
+
+    def has_post(self):
+        post_elems = self._xml_input.xpath('/librec-auto/post')
+        return len(post_elems) > 0
+
+    def cross_validation(self):
+        model_elem = single_xpath(self._xml_input,
+                                  '/librec-auto/splitter/model')
+        if model_elem.text == 'kcv':
+            return int(model_elem.get('count'))
+        else:
+            return 1
+
+    def is_valid(self):
+        return self._xml_input is not None
+
+    def thread_count(self):
+        thread_elems = self._xml_input.xpath('/librec-auto/thread-count')
+        if len(thread_elems) == 0:
+            return 1
+        else:
+            return int(thread_elems[0].text)
+
+    def get_python_metrics(self):
+        """
+        Gets the XML elements for python-side metrics
+        """
+        return self._xml_input.findall('metric/script[@lang="python3"]')
+
+    def get_cv_directories(self, absolute=False) -> List[Path]:
+        """
+        Gets the list of cv directories as Path objects.
+        """
+        data_path = self._files.get_data_path()
+
+        # Should really dispatch through the Files object
+        split_dir = data_path / Path('split')  # cv splits live here
+
+        dir_strings = os.listdir(split_dir)  # ['cv_1', 'cv_2', ...]
+
+        dir_pat = re.compile(self._CV_DIR_RE)
+
+        cv_dir_strings = list(filter(dir_pat.match, dir_strings))
+
+        dirs = [split_dir / cv_dir_string for cv_dir_string in cv_dir_strings]
+
+        if absolute:
+            return [dir.absolute() for dir in dirs]
+        else:
+            return dirs
+
+def read_config_file(config_file, target, log_filename=None):
+    config = ConfigCmd(config_file, target, log_filename)
+    print(target)
+    if config.is_valid():
+        config.load_libraries()
+        config.process_config()
+    else:
+        raise InvalidConfiguration("Configuration file load error", "There was an error loading the configuration file.")
+    return config
+
+    
---
+++ packages/librec-auto/librec-auto-0.2.9.tar.gz/librec-auto-0.2.9/librec_auto/core/util/study_status.py
@@ -0,0 +1,247 @@
+import os
+from datetime import datetime
+from subprocess import check_output
+from librec_auto.core.util.xml_utils import xml_load_from_path, single_xpath
+from librec_auto.core.util import Files
+from pathlib import Path
+from collections import defaultdict
+from lxml import etree
+from .status import move_field_from_element
+
+class ExperimentData:
+    '''
+    The Python metric data from an experiment.
+    '''
+
+    def __init__(self, experiment):
+        self.exp_no = int(experiment.attrib['count'])
+
+        # Used for storing parameters changed
+        self._param = []
+        # Dictionary mapping paramters to their values
+        self._param_vals = {}
+        # Dicitonary of lists to keep track of metric values
+        self._metric_info = defaultdict(list)
+        # Dictionary of average metric values
+        self._metric_avg = {}
+
+        # Parameters
+        params = experiment.xpath('meta/param')
+        for i, param in enumerate(params):
+            # getting names of adjusted parameters
+            self._param.append(param.find('name').text)
+
+            # getting values each paramter was set to
+            self._param_vals[self._param[i]] = float(param.find('value').text)
+
+        # Folds
+        folds = experiment.xpath('results/folds')
+        for fold in folds:
+            for cv in fold:
+                for met in cv.getchildren():
+                    # Add each 
+                    self._metric_info[met.attrib['name']].append(float(met.text))
+        
+        # Averages
+        averages = experiment.xpath('results/averages')
+        for ave in averages:
+            for met in ave.getchildren():
+                self._metric_avg[met.attrib['name']] = float(met.text)
+
+
+
+class StudyStatus:
+    '''
+    The output (metrics) from a study.
+    '''
+
+    def __init__(self, config):
+        self._config = config
+        self._experiments = {}
+
+        status_path = config.get_files().get_status_path()
+        study_xml = None
+        if status_path.exists():
+            study_xml = xml_load_from_path(status_path)
+
+        if study_xml is None:
+            create_study_output(config)
+            study_xml = xml_load_from_path(status_path)
+        
+        # if the output xml has one element it's check, 0 nothing has ran
+        # if there's more than that then the experiment ran and user is evaluating
+        if study_xml is None or len(study_xml.getchildren()) <= 1:
+            create_study_output(config)
+            study_xml = xml_load_from_path(status_path)
+            
+        for exp in study_xml.xpath('//experiment'):
+            # keep track of experiment's name
+            exp_name = Files.get_exp_name(exp.attrib['count'])
+            
+            self._experiments[exp_name] = ExperimentData(exp)
+
+        model = single_xpath(study_xml, '/study/config/splitter/model')
+        if model.text == 'kcv':
+            self._kcv_count = int(model.attrib['count'])
+        else:
+            self._kcv_count = 0
+        time = single_xpath(study_xml, 'completed_at').text
+        time_obj = datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
+        self._timestamp = time_obj.strftime("%Y%m%d_%H%M%S")
+
+    def get_metric_names(self):
+        curr = self._experiments[Files.get_exp_name(0)]
+        return list(curr._metric_info.keys())
+
+    def get_exp_params(self):
+        curr = self._experiments[Files.get_exp_name(0)]
+        return curr._param
+
+    def get_metric_averages(self, metric):
+        avgs = []
+        for exp in self._experiments.values():
+            avgs.append(exp._metric_avg[metric])
+        return avgs
+
+    def get_exp_param_values(self, experiment):
+        if not experiment in self._experiments.keys():
+            print(f'** Error: ** Invalid experiment name: {experiment}')
+            return []
+
+        param_value_list = []
+        exp_params = self._experiments[experiment]._param_vals
+        for param in exp_params:
+            param_value_list.append((param, exp_params[param]))
+        return param_value_list
+
+    def get_metric_folds(self, experiment, metric):
+        return self._experiments[experiment]._metric_info[metric]
+
+
+def check_output_xml(filepath):
+    if os.path.exists(filepath):
+        doc = xml_load_from_path(Path(filepath))
+        num_elements = len(doc.getchildren())
+        return num_elements
+    return 0
+    
+
+def create_study_output(config) -> None:
+    """Creates a study-wide output.xml file
+
+    Args:
+        config (ConfigCmd): The config file for this study
+    """
+    
+    # 2021-7-19 - WK: rewriting so that if an output xml already exists
+    # (which it should if check was ran) these get added to file rather
+    # than overwriting
+    study_path = config.get_files().get_study_path()
+    output_file_path = str(study_path / "output.xml")
+    file_found = False
+
+    output_xml_curr_elems = check_output_xml(output_file_path)
+
+    # Create the root level tree. 
+    # if check is there (the only way for one element to be in output.xml)
+    # preserve it so it's not overwritten. 
+    if output_xml_curr_elems == 1:
+        # only check is present in the output file
+        output_tree = etree.parse(output_file_path).getroot()
+        check_tree = output_tree.find('check')
+        file_found = True
+    else:
+        output_tree = etree.Element("study")
+        check_tree = None
+    
+    # Add an experiment count element at the root level.
+    if file_found:
+        experiment_count_element = etree.Element("experiment_count")
+        experiment_count_element.text = str(config.get_sub_exp_count())
+        output_tree.insert(0, experiment_count_element)
+    else:
+        experiment_count_element = etree.SubElement(output_tree,
+                                                    "experiment_count")
+        experiment_count_element.text = str(config.get_sub_exp_count())
+
+    # Add a completion timestamp.
+    if file_found:
+        completion_timestamp_element = etree.Element("completed_at")
+        completion_timestamp_element.text = str(datetime.now())
+        output_tree.insert(0, completion_timestamp_element)
+    else:
+        completion_timestamp_element = etree.SubElement(output_tree,
+                                                    "completed_at")
+        completion_timestamp_element.text = str(datetime.now())
+    
+
+    # Add an element to contain the experiments results.
+    experiments_element = etree.SubElement(output_tree, "experiments")
+
+    # Adds the config.xml content to the output.xml file.
+    study_config_path = config.get_files().get_config_file_path()
+
+    study_config_element = etree.parse(str(study_config_path)).getroot()
+    study_config_element.tag = 'config'  # Rename the tag to 'config'
+
+    # Add a comment to the config element.
+    comment = etree.Comment(
+        ' This is the configuration used to run the study. ')
+    study_config_element.insert(1, comment)
+
+    # Add config to output_tree
+    move_field_from_element(study_config_element, None, output_tree)
+
+    # For each experiment number...
+    for i in range(0, config.get_sub_exp_count()):
+
+        # Get the experiment's output path.
+        output_path = str(
+            config.get_files().get_exp_paths(i).get_path('output'))
+        
+        # created if statement to prevent OSError: File Not Found when making
+        # output.xml from a thrown error. (output won't exist yet)
+        if config.get_files().get_exp_paths(i).get_path('output').exists():
+            experiment_xml_root = etree.parse(output_path).getroot()
+
+            # Remove statuses from the experiment tree.
+            move_field_from_element(experiment_xml_root, "statuses")
+
+            move_field_from_element(experiment_xml_root, None, experiments_element)
+
+    librec_auto_log = str(study_path / config.get_librec_auto_log_name())
+    with open(librec_auto_log, 'r') as log_file:
+        if check_tree is not None:
+            if not os.stat(librec_auto_log).st_size == 0:
+                for line in log_file:
+                    new_line = line.strip('\n')
+                    message_element = etree.Element("message", 
+                                            {'src': librec_auto_log})
+                    message_element.text = new_line
+                    check_tree.append(message_element)
+            else:
+                message_element = etree.Element("message")
+                message_element.text = "No warnings found in log"
+                check_tree.append(message_element)
+        else: 
+            check_element = etree.Element("check")
+            if not os.stat(librec_auto_log).st_size == 0:
+                for line in log_file:
+                    new_line = line.strip('\n')
+                    message_element = etree.SubElement(check_element, 
+                                            "message", 
+                                            {'src': librec_auto_log})
+                    message_element.text = new_line
+            else:
+                message_element = etree.SubElement(check_element, "message")
+                message_element.text = "No warnings found in log"
+            output_tree.insert(2, check_element)
+        log_file.close()
+
+    # Save/write the output tree.
+    output_tree.getroottree().write(output_file_path, pretty_print=True)
+
+    # These three lines reload the XML file to properly format it.
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.parse(output_file_path, parser)
+    tree.write(output_file_path, encoding='utf-8', pretty_print=True)
