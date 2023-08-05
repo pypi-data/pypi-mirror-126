60933728dc8690e177a0561b077966603a81a636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:34 +0000

    Add package test-cpj-filler/0.1.0/test_cpj_filler-0.1.0.tar.gz

---
+++ packages/test-cpj-filler/test_cpj_filler-0.1.0.tar.gz/test_cpj_filler-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+entry_points = \
+        {'console_scripts': ['test-cpj-filler = filler:main']}
+
+setuptools.setup(
+    name="test_cpj_filler",
+    version="0.1.0",
+    author="rishikaw",
+    author_email="rishikaw@student.42tokyo.jp",
+    description="",
+    package_dir={'src/': 'src'},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points=entry_points,
+)
