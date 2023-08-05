b444246ec918a7653981cf899c1f574bf3ddd04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:18 +0000

    Add package fresh-canvas/0.1.2/fresh-canvas-0.1.2.tar.gz

---
+++ packages/fresh-canvas/fresh-canvas-0.1.2.tar.gz/fresh-canvas-0.1.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fresh-canvas',
+      version='0.1.2',
+      py_modules=['fresh-canvas'],
+      description='Fresh Canvas | The Project Base',
+      author='Tushar Iyer',
+      author_email='redacted@email.site',
+      url='https://github.com/tushariyer/canvas',
+      )
