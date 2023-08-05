ab75dc1fa0c51330fd5cfaccab1ca6b81f44116d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:46 +0000

    Add package libumccr/0.1.0rc1/libumccr-0.1.0rc1-py3-none-any.whl

---
+++ packages/libumccr/libumccr-0.1.0rc1-py3-none-any.whl/libumccr/ica/wes.py
@@ -0,0 +1,35 @@
+import logging
+
+from libica.openapi import libwes
+
+from libumccr import libjson
+from libumccr.ica import configuration
+
+logger = logging.getLogger(__name__)
+
+
+def get_run(wfr_id, to_dict=False, to_json=False):
+    """
+    Get workflow run from WES endpoint ala `ica workflows runs get wfr.81cf25dxxx`
+
+    :param wfr_id:
+    :param to_json: False by default
+    :param to_dict: False by default
+    :return: instance of libwes.WorkflowRun unless to_dict or to_json
+    """
+
+    with libwes.ApiClient(configuration(libwes)) as api_client:
+        run_api = libwes.WorkflowRunsApi(api_client)
+        try:
+            logger.info(f"Getting '{wfr_id}' from WES RUN API endpoint")
+            wfl_run: libwes.WorkflowRun = run_api.get_workflow_run(run_id=wfr_id)
+
+            if to_dict:
+                return wfl_run.to_dict()
+
+            elif to_json:
+                return libjson.dumps(wfl_run.to_dict())
+
+            return wfl_run
+        except libwes.ApiException as e:
+            logger.error(f"Exception when calling get_workflow_run: \n{e}")
