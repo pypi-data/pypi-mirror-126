d75756be71edf66f6b995a712e0da0aaa3b5b48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:42 +0000

    Add package manim-editor/0.2.3/manim_editor-0.2.3.tar.gz

---
+++ packages/manim-editor/manim_editor-0.2.3.tar.gz/manim_editor-0.2.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['manim_editor',
+ 'manim_editor.app',
+ 'manim_editor.app.error',
+ 'manim_editor.app.main',
+ 'manim_editor.editor']
+
+package_data = \
+{'': ['*'],
+ 'manim_editor.app': ['static/img/*', 'static/webpack/*', 'templates/*'],
+ 'manim_editor.app.error': ['templates/*'],
+ 'manim_editor.app.main': ['templates/*']}
+
+install_requires = \
+['Flask>=2.0.2,<3.0.0',
+ 'click>=8.0.3,<9.0.0',
+ 'jsonschema>=4.1.2,<5.0.0',
+ 'waitress>=2.0.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['manedit = manim_editor.__main__:main',
+                     'manim_editor = manim_editor.__main__:main']}
+
+setup_kwargs = {
+    'name': 'manim-editor',
+    'version': '0.2.3',
+    'description': 'Editor and Presenter for Manim Generated Content',
+    'long_description': '<p align="center">\n    <a href="#"><img src="banner.png"></a>\n    <br />\n    <br />\n    <a href="https://pypi.org/project/manim-editor/"><img src="https://img.shields.io/pypi/v/manim-editor.svg?style=flat&logo=pypi" alt="PyPI Latest Release"></a>\n    <a href="https://manim-editor.readthedocs.io/en/stable/"><img src=\'https://readthedocs.org/projects/manim-editor/badge/?version=stable\' alt=\'Documentation Status\' /></a>\n    <a href="http://choosealicense.com/licenses/mit/"><img src="https://img.shields.io/badge/license-MIT-red.svg?style=flat" alt="MIT License"></a>\n    <a href="https://github.com/ManimEditorProject/manim_editor/actions/workflows/build_pages.yml"><img src="https://github.com/ManimEditorProject/manim_editor/actions/workflows/build_pages.yml/badge.svg" alt="MIT License"></a>\n    <br />\n    <br />\n    <i>Editor and Presenter for Manim Generated Content.</i>\n</p>\n<hr />\n\nTake a look at the [Working Example](https://manimeditorproject.github.io/manim_editor/).\nMore information can be found on the [documentation](https://manim-editor.readthedocs.io/en/stable/).\n',
+    'author': 'christopher-besch',
+    'author_email': 'christopher.besch@gmx.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ManimEditorProject/manim_editor',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
