b1424a71cb923a64b4e0a91c23f16225361e53da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:32 +0000

    Add package dsnd-probabilityAC/1.0/dsnd_probabilityAC-1.0.tar.gz

---
+++ packages/dsnd-probabilityAC/dsnd_probabilityAC-1.0.tar.gz/dsnd_probabilityAC-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probabilityAC',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probabilityAC'],
+      author='Adriana Chavez',
+      author_mail = 'adri92xd@gmail.com',
+      zip_safe=False)
