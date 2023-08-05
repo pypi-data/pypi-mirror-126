1be976386e3c583a244c3ce20db635f99b25d238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:41 +0000

    Add package gardenlinux/1.1518.0/gardenlinux-1.1518.0-py3-none-any.whl

---
+++ packages/gardenlinux/gardenlinux-1.1518.0-py3-none-any.whl/dso/deliverydb/db.py
@@ -0,0 +1,44 @@
+import logging
+
+import sqlalchemy
+
+from dso.deliverydb.model import Base
+import dso.model
+import dso.util
+
+
+class DeliveryDB:
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        hostname: str,
+        port: int,
+        dialect: str = 'postgresql',
+    ):
+        self._engine = sqlalchemy.create_engine(
+            f'{dialect}://{username}:{password}@{hostname}:{port}',
+            echo=True,
+            future=True,
+        )
+
+        self.Base = Base
+        # we configured our own root logger and use log propagation
+        # therefore pop streamhandler to not have duplicate output
+        logging.getLogger('sqlalchemy.engine.Engine').handlers.pop()
+        self.Base.metadata.create_all(self._engine)
+        self.Session = sqlalchemy.orm.Session(self._engine)
+
+    def insert_compliance_issue(
+        self,
+        artifact: dict,
+        meta: dict,
+        data: dict,
+    ):
+        scan = dso.deliverydb.model.Scan(
+            artifact=artifact,
+            meta=meta,
+            data=data,
+        )
+
+        self.Session.add(scan)
