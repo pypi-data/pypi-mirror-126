7fbac54e0d09a5069995fd38d9ecd120f186493c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:44 +0000

    Add package helloworld-for-pip/0.0.6/helloworld-for-pip-0.0.6.tar.gz

---
+++ packages/helloworld-for-pip/helloworld-for-pip-0.0.6.tar.gz/helloworld-for-pip-0.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+NAME = 'helloworld-for-pip'
+VERSION = '0.0.6'
+AUTHOR = 'SimoneGasperini'
+AUTHOR_EMAIL = 'simone.gasperini2@studio.unibo.it'
+PYTHON_VERSION = '>=3.8'
+
+
+def get_requirements():
+    with open('./requirements.txt', 'r') as f:
+        reqs = f.read().splitlines()
+    return reqs
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    python_requires=PYTHON_VERSION,
+)
