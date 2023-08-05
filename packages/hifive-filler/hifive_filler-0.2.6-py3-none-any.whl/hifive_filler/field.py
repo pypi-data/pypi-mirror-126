d3915f85a25cb6c9d0f7692b20e72c34c88c5b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:15 +0000

    Add package hifive-filler/0.2.6/hifive_filler-0.2.6-py3-none-any.whl

---
+++ packages/hifive-filler/hifive_filler-0.2.6-py3-none-any.whl/hifive_filler/field.py
@@ -0,0 +1,15 @@
+import hifive_filler.shape
+
+class Field:
+	def __init__(self):
+		self.row_count = 0
+		self.column_count = 0
+		self.player = shape.Shape()
+		self.opponent = shape.Shape()
+	
+	def update_dimensions(self, row_count, column_count):
+		self.row_count = row_count
+		self.column_count = column_count
+		self.player.update_dimensions(row_count, column_count)
+		self.opponent.update_dimensions(row_count, column_count)
+	
