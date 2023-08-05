91ff4785208d36593b86ad0d56069ec775887f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:29 +0000

    Add package nofnof-filler/0.9.1/nofnof_filler-0.9.1-py3-none-any.whl

---
+++ packages/nofnof-filler/nofnof_filler-0.9.1-py3-none-any.whl/nofnof_filler/src/mapping.py
@@ -0,0 +1,35 @@
+class Mapping():
+    """
+    ex)
+    -> when we are player2
+    ...O..
+    ...o..
+    ..XXx.
+    -> self.board
+    [[0, 0, 0, 1, 0, 0],
+     [0, 0, 0, 1, 0, 0],
+     [0, 0, 0, 0, 0, 0],
+     [0, 0, 2, 2, 2, 0]]
+    """
+    def __init__(self):
+        self.y = 0
+        self.x = 0
+        self.board = []
+
+    def read(self):
+        _, y, x = input().split()
+        self.y = int(y)
+        self.x = int(x[:-1])  # To remove ':'
+        _ = input()  # To remove "0123456789012345..."
+        self.board = [[0] * self.x for _ in range(self.y)]
+        for cnt_y in range(self.y):
+            _, map_line = input().split()
+            for cnt_x in range(self.x):
+                if map_line[cnt_x] == '.':
+                    continue
+                elif map_line[cnt_x] == 'O' or map_line[cnt_x] == 'o':
+                    self.board[cnt_y][cnt_x] = 1
+                elif map_line[cnt_x] == 'X' or map_line[cnt_x] == 'x':
+                    self.board[cnt_y][cnt_x] = 2
+                else:
+                    raise ValueError(f"Value Error| map.py| y:{cnt_y} x:{cnt_x}")
