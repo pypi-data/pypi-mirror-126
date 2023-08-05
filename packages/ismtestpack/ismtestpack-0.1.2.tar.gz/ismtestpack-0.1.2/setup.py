a24687822ee1b8aaee79c14201b041eb3830c41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:56 +0000

    Add package ismtestpack/0.1.2/ismtestpack-0.1.2.tar.gz

---
+++ packages/ismtestpack/ismtestpack-0.1.2.tar.gz/ismtestpack-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ismtestpack',
+    version='0.1.2',    
+    description='A example Python package',
+    author='Ismail',
+    install_requires=['numpy<=1.15','matplotlib', 'typing' ],
+
+)
