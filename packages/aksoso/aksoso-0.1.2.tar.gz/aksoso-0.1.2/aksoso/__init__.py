674b6b93334f38969e22d106c082879b694d2eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:02 +0000

    Add package aksoso/0.1.2/aksoso-0.1.2.tar.gz

---
+++ packages/aksoso/aksoso-0.1.2.tar.gz/aksoso-0.1.2/aksoso/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+"""
+    @header __init__.py.py
+    @abstract   
+    
+    @MyBlog: http://www.kuture.com.cn
+    @author  Created by Kuture on 2021/11/4
+    @version 1.0.0 2021/11/4 Creation()
+    
+    @Copyright © 2021年 Mr.Li All rights reserved
+"""
