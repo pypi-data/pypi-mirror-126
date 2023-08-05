0386bc77ab4e85b222743be52be5ae39d4f68ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:17:29 +0000

    Add package twrpdtgen/2.1.0/twrpdtgen-2.1.0.tar.gz

---
+++ packages/twrpdtgen/twrpdtgen-2.1.0.tar.gz/twrpdtgen-2.1.0/twrpdtgen/__init__.py
@@ -0,0 +1,11 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+from pathlib import Path
+
+__version__ = "2.1.0"
+
+module_path = Path(__file__).parent
+current_path = Path(os.getcwd())
+
+jinja_env = Environment(loader=FileSystemLoader(module_path / 'templates'),
+						autoescape=True, trim_blocks=True, lstrip_blocks=True)
