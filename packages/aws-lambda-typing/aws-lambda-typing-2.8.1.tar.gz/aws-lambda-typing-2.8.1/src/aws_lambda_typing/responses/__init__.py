a04d07e9bc0c973eb19876c5fe459e9061c47987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:01 +0000

    Add package aws-lambda-typing/2.8.1/aws-lambda-typing-2.8.1.tar.gz

---
+++ packages/aws-lambda-typing/aws-lambda-typing-2.8.1.tar.gz/aws-lambda-typing-2.8.1/src/aws_lambda_typing/responses/__init__.py
@@ -0,0 +1,11 @@
+from .api_gateway_authorizer import APIGatewayAuthorizerResponse
+from .api_gateway_proxy import (
+    APIGatewayProxyResponseV1 as APIGatewayProxyResponseV1,
+)
+from .api_gateway_proxy import (
+    APIGatewayProxyResponseV2 as APIGatewayProxyResponseV2,
+)
+from .iot import (
+    IoTPreProvisioningHookResponse as IoTPreProvisioningHookResponse,
+)
+from .s3_batch import S3BatchResponse as S3BatchResponse
