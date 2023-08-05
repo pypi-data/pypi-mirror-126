0d8206f660a64871b12136055d55b2eeea6aff78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:17:44 +0000

    Add package nowcasting-dataset/1.0.31/nowcasting_dataset-1.0.31-py3-none-any.whl

---
+++ packages/nowcasting-dataset/nowcasting_dataset-1.0.31-py3-none-any.whl/nowcasting_dataset/config/save.py
@@ -0,0 +1,32 @@
+""" Save functions for the configuration model"""
+import logging
+from typing import Optional, Union
+
+import fsspec
+import yaml
+from pathy import Pathy
+
+from nowcasting_dataset.config.model import Configuration
+
+logger = logging.getLogger(__name__)
+
+
+def save_yaml_configuration(
+    configuration: Configuration, filename: Optional[Union[str, Pathy]] = None
+):
+    """
+    Save a local yaml file which has the a configuration in it.
+
+    If `filename` is None then saves to configuration.output_data.filepath / configuration.yaml.
+
+    Will save to GCP, AWS, or local, depending on the protocol suffix of filepath.
+    """
+    # make a dictionary from the configuration
+    d = configuration.dict()
+    if filename is None:
+        filename = Pathy(configuration.output_data.filepath) / "configuration.yaml"
+    logger.info(f"Saving configuration to {filename}")
+
+    # save to a yaml file
+    with fsspec.open(filename, "w") as yaml_file:
+        yaml.safe_dump(d, yaml_file, default_flow_style=False)
