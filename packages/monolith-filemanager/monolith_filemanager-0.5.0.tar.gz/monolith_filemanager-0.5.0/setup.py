95afaf5deae669c7fa0e945f613757140d3e3fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:36 +0000

    Add package monolith-filemanager/0.5.0/monolith_filemanager-0.5.0.tar.gz

---
+++ packages/monolith-filemanager/monolith_filemanager-0.5.0.tar.gz/monolith_filemanager-0.5.0/setup.py
@@ -0,0 +1,79 @@
+import pathlib
+
+import setuptools
+from setuptools import find_packages
+from setuptools.command.build_py import build_py as build_py_orig
+
+
+# from setuptools import dist
+# dist.Distribution().fetch_build_eggs(['Cython==0.29'])
+# from Cython.Build import cythonize
+
+
+class CustomBuildPy(build_py_orig):
+    """
+    subclass build_py so that we collect no .py files inside the built pip package
+    this is done by overriding build_packages method with a noop
+    """
+    def build_packages(self):
+        pass
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(str(pathlib.Path(__file__).parent.absolute()) + "/monolith_filemanager/version.py", "r") as fh:
+    version = fh.read().split("=")[1].replace("'", "")
+
+directives = {
+    'language_level': 3,
+    'always_allow_keywords': True
+}
+
+setuptools.setup(
+    name="monolith_filemanager",
+    version=version,
+    author="Maxwell Flitton",
+    author_email="maxwell@gmail.com",
+    description="Python package for reading and writing files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MonolithAILtd/monolith-filemanager",
+    install_requires=[
+        "gmsh>=4.5.6",
+        "h5py>=2.10.0",
+        "joblib>=0.15.0",
+        "scipy>=1.4.1",
+        "numpy>=1.16.4",
+        "pandas>=0.25.1",
+        "pyvista>=0.24.2",
+        "PyYAML>=5.1.2",
+        "globre>=0.1.5",
+        "dill>=0.2.8"
+    ],
+    extras_require={
+        'flask': ["Flask>=1.0.0", "tensorflow>=2.2.0", "boto3>=1.16.43"],
+        '3d': ["pyvista>=0.24.2", "gmsh>=4.5.6"],
+        'matlab': ["scipy>=1.4.1"]
+    },
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'file-hello = monolith_filemanager.console_commands.hello:print_logo',
+            'file-install-flask = monolith_filemanager.console_commands.install_flask:install_flask',
+            'file-install-tensorflow = monolith_filemanager.console_commands.install_tensorflow:install_tensorflow',
+            'file-install-aws = monolith_filemanager.console_commands.install_boto:install_boto'
+        ],
+    }
+    # ext_modules=cythonize("caching/**/*.py", exclude="tests/**/*.py", compiler_directives=directives, nthreads=4),
+    # cmdclass={'build_py': CustomBuildPy},
+    # include_package_data=False,
+    # options={"bdist_wheel": {"universal": "1"}}
+)
---
+++ packages/monolith-filemanager/monolith_filemanager-0.5.0.tar.gz/monolith_filemanager-0.5.0/tests/file/test_pandas.py
@@ -0,0 +1,120 @@
+import tempfile
+from unittest import TestCase, main
+from unittest.mock import patch, MagicMock
+
+import dask.dataframe as dd
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from parameterized import parameterized
+
+from monolith_filemanager.file.errors import PandasFileError
+from monolith_filemanager.file.pandas_file import PandasFile
+
+file_types = PandasFile.SUPPORTED_FORMATS
+CHUNK_SIZE = '256MB'
+STORAGE_OPTIONS = {'config_kwargs': {'max_pool_connections': 32}, 'skip_instance_cache': True}
+
+
+class TestPandasFile(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.test_dir = tempfile.TemporaryDirectory()
+        cls.example_df = pd.DataFrame({'a': [1, 2], 'b': [3, 4], 'c': [5, 6]})
+        cls.example_df.to_parquet(f'{cls.test_dir.name}/data.parquet')
+        cls.example_df.to_csv(f'{cls.test_dir.name}/data.csv', index=False)
+        cls.example_df.to_excel(f'{cls.test_dir.name}/data.xls', index=False)
+        cls.example_df.to_excel(f'{cls.test_dir.name}/data.xlsx', index=False)
+        cls.example_df.to_csv(f'{cls.test_dir.name}/data.dat', index=False, sep=',')
+        cls.example_df.to_csv(f'{cls.test_dir.name}/data.data', index=False, sep=',')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.test_dir.cleanup()
+
+    @patch("monolith_filemanager.file.pandas_file.File.__init__")
+    def test___init__(self, mock_file_init):
+        PandasFile(path="test")
+        mock_file_init.return_value = None
+        mock_file_init.assert_called_once_with(path="test")
+
+    @patch("monolith_filemanager.file.pandas_file.PandasFile.__init__")
+    def test_write(self, mock_init):
+        mock_init.return_value = None
+        test = PandasFile(path="test")
+
+        test.path = MagicMock()
+        test_data = pd.DataFrame([{"one": 1, "two": 2}, {"one": 1, "two": 2}])
+        test_data_dask = dd.from_pandas(test_data, npartitions=1)
+
+        with self.assertRaisesRegex(ValueError, 'Failed writing dataframe to file\. (.*)'):
+            test.write(data=test_data, repartition=False, scheduler='test_scheduler')
+
+        with self.assertRaises(Exception):
+            test.write(data="test")
+        with self.assertRaises(Exception):
+            test.write(data=1)
+        with self.assertRaises(Exception):
+            test.write(data=[])
+
+    @patch("monolith_filemanager.file.pandas_file.PandasFile.__init__", return_value=None)
+    def test_write_with_callback(self, _):
+        test = PandasFile(path="test")
+        cb = MagicMock()
+
+        test.path = MagicMock()
+        test_data = pd.DataFrame([{"one": 1, "two": 2}, {"one": 1, "two": 2}])
+        test_data_dask = dd.from_pandas(test_data, npartitions=1)
+
+        with self.assertRaisesRegex(ValueError, 'Failed writing dataframe to file\. (.*)'):
+            test.write(data=test_data, cb=cb)
+
+        with self.assertRaises(Exception):
+            test.write(data="test")
+        with self.assertRaises(Exception):
+            test.write(data=1)
+        with self.assertRaises(Exception):
+            test.write(data=[])
+
+        cb.__enter__.assert_called_once()
+        cb.__exit__.assert_called_once()
+
+    @patch("monolith_filemanager.file.pandas_file.PandasFile._read_dask", return_value=None)
+    @patch("monolith_filemanager.file.pandas_file.PandasFile.__init__", return_value=None)
+    def test_read_lazy(self, _, mock__read_dask):
+        PandasFile(path='test').read(lazy=True)
+        mock__read_dask.assert_called_once_with(CHUNK_SIZE, storage_options=STORAGE_OPTIONS)
+
+    @patch("monolith_filemanager.file.pandas_file.PandasFile._read_dask", return_value=MagicMock())
+    @patch("monolith_filemanager.file.pandas_file.PandasFile.__init__", return_value=None)
+    def test_read_eager(self, _, mock__read_dask):
+        PandasFile(path='test').read(lazy=False)
+        mock__read_dask.assert_called_once_with(CHUNK_SIZE, storage_options=STORAGE_OPTIONS)
+
+    @patch("monolith_filemanager.file.pandas_file.PandasFile.__init__", return_value=None)
+    def test__read_dask_invalid_filetype(self, _):
+        test = PandasFile(path='test')
+        test.path = MagicMock()
+        test.path.file_type = 'INVALID FILETYPE'
+
+        with self.assertRaises(PandasFileError):
+            _ = test._read_dask(chunk_size=1)
+
+    @parameterized.expand([(f, lazy) for f in file_types for lazy in (True, False)])
+    def test_read_functional(self, file_type, lazy):
+        file = PandasFile(path=f'{self.test_dir.name}/data.{file_type}')
+
+        if file_type in ('dat', 'data'):
+            df = file.read(lazy=lazy, sep=',')
+        else:
+            df = file.read(lazy=lazy)
+
+        if lazy:
+            self.assertIsInstance(df, dd.DataFrame)
+            df = df.compute()
+
+        self.assertIsInstance(df, pd.DataFrame)
+        assert_frame_equal(df, self.example_df)
+
+
+if __name__ == "__main__":
+    main()
