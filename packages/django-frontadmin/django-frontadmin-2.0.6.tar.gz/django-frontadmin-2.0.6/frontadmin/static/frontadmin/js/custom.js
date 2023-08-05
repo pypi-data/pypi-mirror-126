bf30a501383679db137009fba18021b1cc626c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:05 +0000

    Add package django-frontadmin/2.0.6/django-frontadmin-2.0.6.tar.gz

---
+++ packages/django-frontadmin/django-frontadmin-2.0.6.tar.gz/django-frontadmin-2.0.6/frontadmin/static/frontadmin/js/custom.js
@@ -0,0 +1,4 @@
+$(function() {
+    $(".sidebar-mini :input:not([type=button]):not([type=submit]):not([type=reset])").addClass("form-control");
+    $(".has-error :input:not([type=button]):not([type=submit]):not([type=reset])").addClass("is-invalid");
+});
