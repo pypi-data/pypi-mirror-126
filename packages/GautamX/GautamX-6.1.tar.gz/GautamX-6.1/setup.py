4045ed65616bd535d7ba3a8c6e9c3617bb699c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:19 +0000

    Add package GautamX/6.1/GautamX-6.1.tar.gz

---
+++ packages/GautamX/GautamX-6.1.tar.gz/GautamX-6.1/setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup, find_packages
+import pathlib
+
+CWD = pathlib.Path(__file__).parent
+
+README = (CWD / "README.md").read_text()
+
+setup(
+    name='GautamX',
+    version='6.1',
+    packages=find_packages(),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/gautam1834/mirrorx',
+    license='GPL3.0',
+    author='',
+    author_email='',
+    include_package_data=True,
+    description='Telegram Mirror Bot',
+    platforms="any",
+    install_requires=[
+        "requests",
+        "appdirs",
+        "aria2p",
+        "progress",
+        "psutil",
+        "python-telegram-bot",
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "js2py",
+        "python-dotenv",
+        "tenacity",
+        "python-magic",
+        "beautifulsoup4",
+        "Pyrogram",
+        "TgCrypto",
+        "yt-dlp",
+        "lxml",
+        "telegraph",
+        "speedtest-cli",
+        "messages",
+        "pybase64",
+        "lk21",
+        "cloudscrape",
+        "pyshorteners",
+        "psycopg2-binary",
+        "megasdkrestclient",
+        "heroku3"
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts":[
+            "MirrorXBot = bot.__main__:main"
+        ]
+
+    },
+    package_data={
+        "": ["data/*.dat", "data/aria.conf"],
+    },
+    scripts=['bin/extract', 'bin/pextract'],
+)
