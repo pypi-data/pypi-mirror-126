b003db27a422f07a17e527f84e8034c2d05bc0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package llcsciencesdk/0.1.7/llcsciencesdk-0.1.7.tar.gz

---
+++ packages/llcsciencesdk/llcsciencesdk-0.1.7.tar.gz/llcsciencesdk-0.1.7/llcsciencesdk/urls.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+
+ApiUrls = namedtuple(
+    "ApiUrls",
+    ["AUTH_URL",
+     "GET_MODEL_INPUT_URL",
+     "GET_OLD_MODEL_INPUT_URL"
+     ]
+)
+
+
+def make_urls(environment):
+    BASE_API_URL = (
+        "https://internal-landlifecompany.appspot.com"
+    )
+
+    if environment == "staging":
+        BASE_API_URL = (
+            "https://staging-science-admin-dot-internal-landlifecompany.ue.r.appspot.com"
+        )
+
+    if environment == "local":
+        BASE_API_URL = (
+            "http://127.0.0.1:8000"
+        )
+
+    return ApiUrls(
+        AUTH_URL=f'{BASE_API_URL}/api/v1/token/',
+        GET_MODEL_INPUT_URL=f"{BASE_API_URL}/sciencemodel/fasttrackinput/planting_design_config/",
+        GET_OLD_MODEL_INPUT_URL=f"{BASE_API_URL}/api/v1/llcmodel/model_input?model_run_ids"
+    )
