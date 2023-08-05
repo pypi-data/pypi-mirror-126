ad063c191fa46efba2aa883451fe6476cd8d541b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:36 +0000

    Add package cloudplatform-sdks/0.0.37/cloudplatform_sdks-0.0.37-py2.py3-none-any.whl

---
+++ packages/cloudplatform-sdks/cloudplatform_sdks-0.0.37-py2.py3-none-any.whl/cloudplatform_sdks/alicloud_models/__init__.py
@@ -0,0 +1,9 @@
+from .cdn import AliCdnDomain
+from .nas import AliNas
+from .dds import AliMongoDB
+from .kafka import AliKafka
+from .vpn import AliVpn, AliCustomer, AliIPsec, AliRouteEntry
+from .ess import AliEss
+from .vpc import AliVpc
+from .cs import AliCs
+
