170ad63d02c92e13a0ab334222767cfaeb2d8a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:31 +0000

    Add package ble2mqtt/0.1.0a39/ble2mqtt-0.1.0a39.tar.gz

---
+++ packages/ble2mqtt/ble2mqtt-0.1.0a39.tar.gz/ble2mqtt-0.1.0a39/ble2mqtt/utils.py
@@ -0,0 +1,15 @@
+MAX_RSSI = 0
+MIN_RSSI = -100
+
+
+def format_binary(data: bytes, delimiter=' '):
+    return delimiter.join(format(x, '02x') for x in data)
+
+
+def cr2032_voltage_to_percent(mvolts: int):
+    coeff = 0.8  # >2.9V counts as 100% = (2900 - 2100)/100
+    return max(min(int(round((mvolts/1000 - 2.1)/coeff, 2) * 100), 100), 0)
+
+
+def rssi_to_linkquality(rssi):
+    return max(int(round(255 * (rssi - MIN_RSSI) / (MAX_RSSI - MIN_RSSI))), 0)
