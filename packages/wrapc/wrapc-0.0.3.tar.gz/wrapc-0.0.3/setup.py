0eb32c2c5e4390045930b92b13d21e8c66f03c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:52 +0000

    Add package wrapc/0.0.3/wrapc-0.0.3.tar.gz

---
+++ packages/wrapc/wrapc-0.0.3.tar.gz/wrapc-0.0.3/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['wrapc']
+install_requires = \
+['argcomplete>=1.12.3,<2.0.0', 'argparse>=1.4.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['wrapc = wrapc:main']}
+
+setup_kwargs = {
+    'name': 'wrapc',
+    'version': '0.0.3',
+    'description': 'Wrapper script for starting a command line tool with bash completion',
+    'long_description': None,
+    'author': 'Dick Marinus',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)
