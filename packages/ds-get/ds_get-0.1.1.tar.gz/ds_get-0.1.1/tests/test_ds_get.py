e10dfa72f9eea015ab9d3ef85ce4864e6ff641c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:51 +0000

    Add package ds-get/0.1.1/ds_get-0.1.1.tar.gz

---
+++ packages/ds-get/ds_get-0.1.1.tar.gz/ds_get-0.1.1/tests/test_ds_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""Tests for `ds_get` package."""
+
+
+import unittest
+
+
+class TestDs_get(unittest.TestCase):
+    """Tests for `ds_get` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
