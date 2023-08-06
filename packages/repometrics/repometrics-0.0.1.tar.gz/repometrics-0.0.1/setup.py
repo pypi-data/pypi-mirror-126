395c9d683b9be8b7a342fb17c72236d029971118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:01 +0000

    Add package repometrics/0.0.1/repometrics-0.0.1.tar.gz

---
+++ packages/repometrics/repometrics-0.0.1.tar.gz/repometrics-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='repometrics',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/alextxj/RepoMetrics',
+    license='MIT',
+    author='Alex Tian Xing Jian',
+    author_email='tian.xingjian@hotmail.com',
+    description='CLI to display current repository files path and languages.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['pygments'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'repometrics = RepoMetrics.RepoMetrics:main'
+        ]
+    }
+)
