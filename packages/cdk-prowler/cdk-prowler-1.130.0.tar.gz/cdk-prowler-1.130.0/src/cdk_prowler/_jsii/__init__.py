0b5f0d6255b72340395897a42ec5113a009773af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:00 +0000

    Add package cdk-prowler/1.130.0/cdk-prowler-1.130.0.tar.gz

---
+++ packages/cdk-prowler/cdk-prowler-1.130.0.tar.gz/cdk-prowler-1.130.0/src/cdk_prowler/_jsii/__init__.py
@@ -0,0 +1,33 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.assertions._jsii
+import aws_cdk.aws_codebuild._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import cdk_iam_floyd._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-prowler", "1.130.0", __name__[0:-6], "cdk-prowler@1.130.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
