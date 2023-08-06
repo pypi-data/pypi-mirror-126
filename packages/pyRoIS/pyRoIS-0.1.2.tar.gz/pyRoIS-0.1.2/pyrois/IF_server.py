bcdf7bfcd44bf8507bfe5530fc6798c0988fc71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:43 +0000

    Add package pyRoIS/0.1.2/pyRoIS-0.1.2.tar.gz

---
+++ packages/pyRoIS/pyRoIS-0.1.2.tar.gz/pyRoIS-0.1.2/pyrois/IF_server.py
@@ -0,0 +1,39 @@
+# IF_server.py
+#
+# Copyright 2020 Eiichi Inohira
+# This software may be modified and distributed under the terms
+# of the MIT license
+#
+# For python 3
+
+import sys
+
+if sys.version_info.major == 2:
+    import SocketServer
+    import SimpleXMLRPCServer
+
+    class ThreadingXMLRPCServer(SocketServer.ThreadingMixIn, SimpleXMLRPCServer.SimpleXMLRPCServer):
+        pass
+else:
+    import socketserver
+    import xmlrpc.server
+
+    class ThreadingXMLRPCServer(socketserver.ThreadingMixIn, xmlrpc.server.SimpleXMLRPCServer):
+        """ThreadingXMLRPCServer
+        """
+        pass
+
+class IF_server:
+    """IF_Server
+    """
+    def __init__(self, port):
+        self._server = ThreadingXMLRPCServer(
+            ("0.0.0.0", port), logRequests=False)
+
+    def run(self, _IF):
+        """IF_Server
+        """
+        self._server.register_instance(_IF)
+        self._server.register_introspection_functions()
+        # print("server running")
+        self._server.serve_forever()
