3823e7e3fd963674da801283ff223e9515252c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:34 +0000

    Add package fc-bigdate9/1.0/fc_bigdate9-1.0.tar.gz

---
+++ packages/fc-bigdate9/fc_bigdate9-1.0.tar.gz/fc_bigdate9-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "fc_bigdate9",
+    version= "1.0",
+    py_modules= ["fc_xd_bigdate"],
+    author= "大数据3",
+    author_email= "3145057077@qq.com",
+    description= "大数据3"
+)
