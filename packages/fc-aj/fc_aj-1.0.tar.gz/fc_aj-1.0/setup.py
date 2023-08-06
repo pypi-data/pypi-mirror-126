2a9dcf014bec36cc501bab2c8b405c05e82139c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:38 +0000

    Add package fc-aj/1.0/fc_aj-1.0.tar.gz

---
+++ packages/fc-aj/fc_aj-1.0.tar.gz/fc_aj-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fc_aj",
+    version = "1.0",
+    py_modules =["fc_xd_bigdata"],
+    author = "啊佳",
+    author_email = "fc@aj.com",
+    description ="啊佳"
+)
