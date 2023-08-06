44f72985fa1c79d11144eb74db01de6eb2400565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:48 +0000

    Add package fc-digdata999/1.0/fc_digdata999-1.0.tar.gz

---
+++ packages/fc-digdata999/fc_digdata999-1.0.tar.gz/fc_digdata999-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name="fc_digdata999",
+        version="1.0",
+        py_modules="[fc_mkd_bigdata6]",
+        author= "大数据3",
+        author_email="2294090178@digadta.com",
+        aescription="大数据3"
+)
+
+
