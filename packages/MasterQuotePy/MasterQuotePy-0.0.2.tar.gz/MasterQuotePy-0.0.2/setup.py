237fc039b187c5cb5216c7c81db223779417bf2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:23 +0000

    Add package MasterQuotePy/0.0.2/MasterQuotePy-0.0.2.tar.gz

---
+++ packages/MasterQuotePy/MasterQuotePy-0.0.2.tar.gz/MasterQuotePy-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+# readme.md = github readme.md, 這裡可接受markdown寫法
+# 如果沒有的話，需要自己打出介紹此專案的檔案，再讓程式知道
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MasterQuotePy",
+    version="0.0.2",
+    author="SYLIN",
+    author_email="sylin@masterlink.com.tw",
+    description="MasterLink TWSE/TAIFEX API package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
