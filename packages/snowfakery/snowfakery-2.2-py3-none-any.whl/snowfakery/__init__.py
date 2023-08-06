87b915d8830a859d3f764728d99f05d1f06185aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:49 +0000

    Add package snowfakery/2.2/snowfakery-2.2-py3-none-any.whl

---
+++ packages/snowfakery/snowfakery-2.2-py3-none-any.whl/snowfakery/__init__.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from .plugins import (
+    SnowfakeryPlugin,
+    lazy,
+    memorable,
+    PluginResult,
+    PluginResultIterator,
+)
+from .api import generate_data, SnowfakeryApplication
+
+from snowfakery.utils.backports import py36  # noQA
+
+# TODO: when Python 3.6 is irrelevant, make this lazy:
+
+# https://www.python.org/dev/peps/pep-0562/
+
+version_file = Path(__file__).parent / "version.txt"
+with version_file.open() as f:
+    version = f.read().strip()
+
+__all__ = (
+    "generate_data",
+    "SnowfakeryApplication",
+    "SnowfakeryPlugin",
+    "lazy",
+    "memorable",
+    "PluginResult",
+    "PluginResultIterator",
+)
