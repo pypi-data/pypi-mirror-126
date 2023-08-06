c5df6b5f5591cf1f0eee55920eca41c940231957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:22 +0000

    Add package scikit-weak/0.1.5/scikit-weak-0.1.5.tar.gz

---
+++ packages/scikit-weak/scikit-weak-0.1.5.tar.gz/scikit-weak-0.1.5/scikit_weak/classification/__init__.py
@@ -0,0 +1,8 @@
+from ._ensemble import RRLClassifier
+from ._neighbors import WeaklySupervisedKNeighborsClassifier, WeaklySupervisedRadiusClassifier
+
+__all__ = [
+    "RRLClassifier",
+    "WeaklySupervisedKNeighborsClassifier",
+    "WeaklySupervisedRadiusClassifier"
+]
