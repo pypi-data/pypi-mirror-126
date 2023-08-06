0ea0748f7496921b97b7cc58ce90d7068330cd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package carto2gpd/1.0.9/carto2gpd-1.0.9.tar.gz

---
+++ packages/carto2gpd/carto2gpd-1.0.9.tar.gz/carto2gpd-1.0.9/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['carto2gpd', 'carto2gpd.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['geopandas>=0.10.1,<0.11.0',
+ 'importlib-metadata>=4.8.1,<5.0.0',
+ 'requests>=2.26.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'carto2gpd',
+    'version': '1.0.9',
+    'description': 'A Python utility to query a CARTO database and return a geopandas GeoDataFrame',
+    'long_description': None,
+    'author': 'Nick Hand',
+    'author_email': 'nick.hand@phila.gov',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
