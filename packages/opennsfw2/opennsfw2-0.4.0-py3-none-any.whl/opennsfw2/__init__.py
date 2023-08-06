096611302bd199c32899d34ce7a08bf9d2fdbe50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:38 +0000

    Add package opennsfw2/0.4.0/opennsfw2-0.4.0-py3-none-any.whl

---
+++ packages/opennsfw2/opennsfw2-0.4.0-py3-none-any.whl/opennsfw2/__init__.py
@@ -0,0 +1,8 @@
+from ._image import Preprocessing
+from ._image import preprocess_image
+from ._inference import predict
+from ._model import make_open_nsfw_model
+
+__version__ = "0.4.0"
+__author__ = "Bosco Yung"
+__license__ = "MIT"
