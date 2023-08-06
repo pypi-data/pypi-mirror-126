dc3e56ba29ebb82dc89291d7f9d8f88d988bace8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:03 +0000

    Add package netfoundry/5.1.0/netfoundry-5.1.0.tar.gz

---
+++ packages/netfoundry/netfoundry-5.1.0.tar.gz/netfoundry-5.1.0/netfoundry/__init__.py
@@ -0,0 +1,6 @@
+""" Interface to NetFoundry API
+"""
+
+from .network import Network
+from .network_group import NetworkGroup
+from .organization import Organization
---
+++ packages/netfoundry/netfoundry-5.1.0.tar.gz/netfoundry-5.1.0/setup.py
@@ -0,0 +1,31 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='netfoundry',
+    version='5.1.0',
+    py_modules=['netfoundry'],
+    url='https://developer.netfoundry.io/guides/python/',
+    description='Interface to the NetFoundry network-as-code orchestration Platform',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author='Kenneth Bingham',
+    author_email='support@netfoundry.io',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests >= 2.25.1',
+        'pysocks >= 1.7.1',
+        'pyjwt >= 2.0.0',
+        'inflect >= 5.3'
+    ]
+)
+
