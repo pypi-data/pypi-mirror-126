c4d36306afe0c5d6b9e814dfaab6db6efada007c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package flask-scaffolding/0.0.17/flask_scaffolding-0.0.17.tar.gz

---
+++ packages/flask-scaffolding/flask_scaffolding-0.0.17.tar.gz/flask_scaffolding-0.0.17/setup.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import setuptools
+from distutils import sysconfig
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_scaffolding",
+    version="0.0.17",
+    author="Muhammad Yunus",
+    author_email="yunusmuhammad007@gmail.com",
+    description="Flask Scaffolding with buiiltin Authentication & Authorization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Muhammad-Yunus/Flask-Scaffolding-Base",
+    project_urls={
+        "Bug Tracker": "https://github.com/Muhammad-Yunus/Flask-Scaffolding-Base/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    package_data={
+        'scaffold': ['base/instance/*.py', 
+                     'base/app/**/*.html', 
+                     'base/app/**/**/*.html', 
+                     'base/app/**/**/*.css']
+        },
+    python_requires=">=3.6"
+)
