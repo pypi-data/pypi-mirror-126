52f7f44a65aab13ab5ee23d7ba78ca6e61b44cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:30 +0000

    Add package foo-pkg-Liudongbo/0.3/foo-pkg-Liudongbo-0.3.tar.gz

---
+++ packages/foo-pkg-Liudongbo/foo-pkg-Liudongbo-0.3.tar.gz/foo-pkg-Liudongbo-0.3/foo/bar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import test
+
+def add(a,b):#加法
+    return a+b
+def sub(a,b):#减法
+    return test.sub(a, b)
+def mul(a,b):#乘法
+    return a*b
---
+++ packages/foo-pkg-Liudongbo/foo-pkg-Liudongbo-0.3.tar.gz/foo-pkg-Liudongbo-0.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup,find_packages
+setup(name='foo-pkg-Liudongbo',
+      version='0.3',
+      description='a small example',
+      url='https://github.com/lwxGitHub123/foo/',
+      author='liudongbo',
+      author_email='350272438@qq.com',
+      license='MIT Licence',
+      packages=find_packages(),
+      zip_safe=True,
+      include_package_data=True,
+      platforms="any",
+      install_requires=[]
+     )
