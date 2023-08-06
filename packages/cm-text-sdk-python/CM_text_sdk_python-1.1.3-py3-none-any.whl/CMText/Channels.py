217106dc0f86654b23c5d0dda9a3c4215306766f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:23 +0000

    Add package cm-text-sdk-python/1.1.3/CM_text_sdk_python-1.1.3-py3-none-any.whl

---
+++ packages/cm-text-sdk-python/CM_text_sdk_python-1.1.3-py3-none-any.whl/CMText/Channels.py
@@ -0,0 +1,25 @@
+#Class chanels
+#Contains all known channels
+class Channels:
+
+    channels = {'SMS',
+                'Whatsapp',
+                'Push',
+                'RCS',
+                'Viber',
+                'Voice',
+                'iMessage',
+                'Line',
+                'Twitter',
+                'MobilePush',
+                'Facebook Messenger',
+                'Google Business Messages',
+                'Instagram'
+                }
+
+    def Check_Channels(self, allowedChannels):
+        for allowedChannel in allowedChannels:
+            if(not allowedChannel in self.channels):
+                print(allowedChannel + ' is not a valid Channel name. Check Spelling?')
+                return False
+        return True
