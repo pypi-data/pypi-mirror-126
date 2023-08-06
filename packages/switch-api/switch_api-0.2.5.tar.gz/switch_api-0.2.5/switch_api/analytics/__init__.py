af1a2171b3cf0e315e80bb645c9b2333d824f5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:58 +0000

    Add package switch-api/0.2.5/switch_api-0.2.5.tar.gz

---
+++ packages/switch-api/switch_api-0.2.5.tar.gz/switch_api-0.2.5/switch_api/analytics/__init__.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Switch Automation Pty Ltd. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""
+A module for .....
+"""
+
+from .analytics import (get_parent_modules_list, get_clone_modules_list, run_clone_modules, sensor_has_not_changed)
+
+__all__ = ['get_parent_modules_list', 'get_clone_modules_list', 'run_clone_modules', 'sensor_has_not_changed']
