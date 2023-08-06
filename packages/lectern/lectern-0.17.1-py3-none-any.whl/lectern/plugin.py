8455a66f05c669f038cb4b85e01f04048bffec57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package lectern/0.17.1/lectern-0.17.1-py3-none-any.whl

---
+++ packages/lectern/lectern-0.17.1-py3-none-any.whl/lectern/plugin.py
@@ -0,0 +1,51 @@
+__all__ = [
+    "beet_default",
+    "lectern",
+]
+
+
+import subprocess
+from typing import List, Optional
+
+from beet import Context, configurable
+from pydantic import BaseModel
+
+from .document import Document
+
+
+class LecternOptions(BaseModel):
+    load: List[str] = []
+    snapshot: Optional[str] = None
+    external_files: Optional[str] = None
+    scripts: List[List[str]] = []
+
+
+def beet_default(ctx: Context):
+    ctx.require(lectern)
+
+
+@configurable(validator=LecternOptions)
+def lectern(ctx: Context, opts: LecternOptions):
+    """Plugin that handles markdown files with lectern."""
+    document = ctx.inject(Document)
+
+    for pattern in opts.load:
+        for path in ctx.directory.glob(pattern):
+            document.load(path)
+
+    for arguments in opts.scripts:
+        result = subprocess.run(
+            arguments,
+            cwd=ctx.directory,
+            check=True,
+            stdout=subprocess.PIPE,
+        )
+        document.add_text(result.stdout.decode())
+
+    yield
+
+    if opts.snapshot:
+        document.save(
+            ctx.directory / opts.snapshot,
+            ctx.directory / opts.external_files if opts.external_files else None,
+        )
