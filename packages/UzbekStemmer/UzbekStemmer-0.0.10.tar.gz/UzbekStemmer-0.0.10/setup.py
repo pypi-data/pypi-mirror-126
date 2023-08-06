2df637391ef7c4049b2da6e073d7b381e7335373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:30 +0000

    Add package UzbekStemmer/0.0.10/UzbekStemmer-0.0.10.tar.gz

---
+++ packages/UzbekStemmer/UzbekStemmer-0.0.10.tar.gz/UzbekStemmer-0.0.10/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="UzbekStemmer",
+    version="0.0.10",
+    author="Maksud Sharipov, Ulugbek Salaev, Allabergan Yuldashev, Jasur Sobirov",
+    author_email="ulugbek0302@gmail.com",
+    description="Uzbek Stemmer for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UlugbekSalaev/UzbekStemmer",
+    project_urls={
+        "Bug Tracker": "https://github.com/UlugbekSalaev/UzbekStemmer/issues",
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    include_package_data=True,
+)
