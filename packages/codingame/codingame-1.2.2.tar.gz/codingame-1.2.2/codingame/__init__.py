e9c9ef07d36585c769dc48957cfc6a89ef96f25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:06 +0000

    Add package codingame/1.2.2/codingame-1.2.2.tar.gz

---
+++ packages/codingame/codingame-1.2.2.tar.gz/codingame-1.2.2/codingame/__init__.py
@@ -0,0 +1,84 @@
+"""
+CodinGame API Wrapper
+=====================
+
+Wrapper for the undocumented CodinGame API.
+"""
+
+from typing import NamedTuple
+
+VersionInfo = NamedTuple(
+    "VersionInfo", major=int, minor=int, micro=int, releaselevel=str, serial=int
+)
+
+version_info = VersionInfo(major=1, minor=2, micro=2, releaselevel="", serial=0)
+
+__title__ = "codingame"
+__author__ = "takos22"
+__version__ = "1.2.2"
+
+from . import types  # needed for including codingame.types submodule in release
+from .clash_of_code import ClashOfCode, Player
+from .client import Client
+from .codingamer import CodinGamer
+from .exceptions import (
+    ChallengeNotFound,
+    ClashOfCodeNotFound,
+    CodinGameAPIError,
+    CodinGamerNotFound,
+    EmailNotLinked,
+    EmailRequired,
+    IncorrectPassword,
+    LoginError,
+    LoginRequired,
+    MalformedEmail,
+    NotFound,
+    PasswordRequired,
+    PuzzleNotFound,
+    WrongCaptchaAnswer,
+)
+from .leaderboard import (
+    ChallengeLeaderboard,
+    ChallengeRankedCodinGamer,
+    GlobalLeaderboard,
+    GlobalRankedCodinGamer,
+    League,
+    PuzzleLeaderboard,
+    PuzzleRankedCodinGamer,
+)
+from .notification import Notification
+
+__all__ = (
+    # Client
+    Client,
+    # CodinGamer
+    CodinGamer,
+    # Clash of Code
+    ClashOfCode,
+    Player,
+    # Notification
+    Notification,
+    # Leaderboard
+    GlobalLeaderboard,
+    GlobalRankedCodinGamer,
+    League,
+    ChallengeLeaderboard,
+    ChallengeRankedCodinGamer,
+    PuzzleLeaderboard,
+    PuzzleRankedCodinGamer,
+    # Exceptions
+    CodinGameAPIError,
+    LoginError,
+    EmailRequired,
+    MalformedEmail,
+    PasswordRequired,
+    EmailNotLinked,
+    IncorrectPassword,
+    WrongCaptchaAnswer,
+    LoginRequired,
+    NotFound,
+    CodinGamerNotFound,
+    ClashOfCodeNotFound,
+    ChallengeNotFound,
+    PuzzleNotFound,
+)
