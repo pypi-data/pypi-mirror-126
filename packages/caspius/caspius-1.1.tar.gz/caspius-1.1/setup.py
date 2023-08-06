c277948be56716ff573582add95fdbd4dfb9c838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:07 +0000

    Add package caspius/1.1/caspius-1.1.tar.gz

---
+++ packages/caspius/caspius-1.1.tar.gz/caspius-1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['caspius']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'caspius',
+    'version': '1.1',
+    'description': 'Simple log system for python',
+    'long_description': None,
+    'author': 'Dan Gost',
+    'author_email': 'dangost16@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
