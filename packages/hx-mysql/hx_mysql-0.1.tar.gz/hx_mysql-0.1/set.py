b526ebd3d2725cfdee1c4fcde63b2d561f2d0a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:50 +0000

    Add package hx-mysql/0.1/hx_mysql-0.1.tar.gz

---
+++ packages/hx-mysql/hx_mysql-0.1.tar.gz/hx_mysql-0.1/set.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='hx_mysql',
+    version="0.1",
+    py_modules=["shisql"],
+    author="小石",
+    author_email="480523858@qq.com",
+    description="大数据1的小石"
+)
