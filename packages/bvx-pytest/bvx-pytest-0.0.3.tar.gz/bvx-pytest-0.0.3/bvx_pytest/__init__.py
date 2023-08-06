985a15e66e763d74955c6b4f537a062580d66cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:53 +0000

    Add package bvx-pytest/0.0.3/bvx-pytest-0.0.3.tar.gz

---
+++ packages/bvx-pytest/bvx-pytest-0.0.3.tar.gz/bvx-pytest-0.0.3/bvx_pytest/__init__.py
@@ -0,0 +1,4 @@
+from .defines import BVX_PYTEST_DOCKER_NETWORK_NAME
+# from .es import setup_function as setup_function
+
+# EOF
---
+++ packages/bvx-pytest/bvx-pytest-0.0.3.tar.gz/bvx-pytest-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# python setup.py sdist register upload
+from setuptools import setup
+
+setup(
+    name='bvx-pytest',
+    version='0.0.3',
+    description='pytest helpers.',
+    author='Kazuhiro Kotsutsumi',
+    url='https://github.com/kotsutsumi/bvx-pytest',
+    packages=[
+        'bvx_pytest',
+    ],
+    include_package_data=True,
+    license='The MIT License',
+    install_requires=[
+        'bvx-docker',
+        'bvx-network',
+        'docker',
+        'elasticsearch',
+    ],
+)
+
+# EOF
