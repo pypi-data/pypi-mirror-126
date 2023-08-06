b5a99e0e3fea07f9c82bd2328269f0dd6cd4fb80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:08 +0000

    Add package fc-bigdata3/1.0/fc_bigdata3-1.0.tar.gz

---
+++ packages/fc-bigdata3/fc_bigdata3-1.0.tar.gz/fc_bigdata3-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+			name = "fc_bigdata3",
+			version = "1.0",
+			py_modules = ["fc_xd_bigdata3"],
+			author = "大数据3",
+			author_email = "fc@bigdata.com",
+			description = "大数据3"
+	  )
