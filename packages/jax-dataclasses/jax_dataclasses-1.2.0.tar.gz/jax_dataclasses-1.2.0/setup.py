caada752eb646045fb0f44742726aa9cb418db68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package jax-dataclasses/1.2.0/jax_dataclasses-1.2.0.tar.gz

---
+++ packages/jax-dataclasses/jax_dataclasses-1.2.0.tar.gz/jax_dataclasses-1.2.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="jax_dataclasses",
+    version="1.2.0",
+    description="Dataclasses + JAX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/brentyi/jax_dataclasses",
+    author="brentyi",
+    author_email="brentyi@berkeley.edu",
+    license="MIT",
+    packages=find_packages(),
+    package_data={"jax_dataclasses": ["py.typed"]},
+    python_requires=">=3.7",
+    install_requires=[
+        "flax",
+        "jax",
+        "jaxlib",
+    ],
+    extras_require={
+        "testing": [
+            "pytest",
+            "pytest-cov",
+            # "hypothesis",
+            # "hypothesis[numpy]",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
