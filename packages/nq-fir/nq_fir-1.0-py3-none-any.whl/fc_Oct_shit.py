bc281d165be492f2de6966eb1bba3ace05615c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:47 +0000

    Add package nq-fir/1.0/nq_fir-1.0-py3-none-any.whl

---
+++ packages/nq-fir/nq_fir-1.0-py3-none-any.whl/fc_Oct_shit.py
@@ -0,0 +1,11 @@
+# import time 
+
+# def dao(time_dao):
+#     while time_dao > 0:
+#         print("倒计时：", time_dao)
+#         time.sleep(1)
+#         time_dao -= 1
+
+# dao(6)
+
+name = "Hello World"
