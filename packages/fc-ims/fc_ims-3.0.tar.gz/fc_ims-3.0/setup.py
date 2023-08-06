a512c966649ecea58fe5b8309fa3005ff7d924af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package fc-ims/3.0/fc_ims-3.0.tar.gz

---
+++ packages/fc-ims/fc_ims-3.0.tar.gz/fc_ims-3.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fc_ims",
+    version = "3.0",
+    py_modules = ["fc_ims"],
+    author = "fc",
+    author_email = "fc@bigdata.com",
+    description= "信息管理系统"
+)
