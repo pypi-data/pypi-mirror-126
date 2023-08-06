9bf8f394d9bda91198a9a3a996c42fdca6ac70d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:28 +0000

    Add package BlueWhale3/3.31.1/BlueWhale3-3.31.1.tar.gz

---
+++ packages/BlueWhale3/BlueWhale3-3.31.1.tar.gz/BlueWhale3-3.31.1/Orange/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.31.1'
+version = '3.31.1'
+full_version = '3.31.1.dev0+7966000'
+git_revision = '7966000e4d1c2348741394cea27fa6f74424d166'
+release = False
+
+if not release:
+    version = full_version
