214f704684d8dc3e4984bc65f939ca06c9bf6dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:05 +0000

    Add package fc-skx1.0/1.0/fc_skx1.0-1.0.tar.gz

---
+++ packages/fc-skx1.0/fc_skx1.0-1.0.tar.gz/fc_skx1.0-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "fc_skx1.0",
+    #名字
+    version = "1.0",
+    #版本号
+    py_modules = ["fc_skx_bigdate"],
+    #包名
+    author = "skx",
+    
+    author_email = "1802628079@qq.com",
+    description = "skx"
+
+)
