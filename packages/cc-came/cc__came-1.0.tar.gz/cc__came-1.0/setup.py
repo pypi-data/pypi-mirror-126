12feaa8604e56d7d511bbe0879ef852ce8274a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:31 +0000

    Add package cc-came/1.0/cc__came-1.0.tar.gz

---
+++ packages/cc-came/cc__came-1.0.tar.gz/cc__came-1.0/setup.py
@@ -0,0 +1,10 @@
+from  setuptools import setup
+
+setup(
+    name="cc__came",
+    version="1.0",
+    py_modules=["cc__came"],
+    author="Mis__qiaoyun",
+    author_email="3233175474@qq.com",
+    description="zuozhe"
+)
