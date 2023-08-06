ffb8cf4ff36fbf75a5700b153472c6f86990de50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:32 +0000

    Add package TSmodel/0.2/TSmodel-0.2.tar.gz

---
+++ packages/TSmodel/TSmodel-0.2.tar.gz/TSmodel-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='TSmodel',
+  version='0.2',
+  description='AUTOCALCULATE ARIMA MODEL',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='MAINAK RAY',
+  author_email='mainakr748@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='ARIMA', 
+  packages=['TSmodel'],
+  install_requires=['sklearn','statsmodels','numpy'] 
+  )
