c54943ad9f8af1730aacef3d3e4be9ece06baaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:54 +0000

    Add package chemml/1.0/chemml-1.0.tar.gz

---
+++ packages/chemml/chemml-1.0.tar.gz/chemml-1.0/chemml/__init__.py
@@ -0,0 +1,7 @@
+# __name__ = "chemml"
+__version__ = "1.0"
+__author__ = ["Aditya Sonpal (adityaso@buffalo.edu)", "Garuav Vishwakarma (gvishwak@buffalo.edu) ", "Aatish Pradhan (aatishpr@buffalo.edu)","Mojtaba Haghighatlari (mojtabah@buffalo.edu)", "Johannes Hachmann (hachmann@buffalo.edu)"]
+
+
+# import sys
+# sys.dont_write_bytecode = True
