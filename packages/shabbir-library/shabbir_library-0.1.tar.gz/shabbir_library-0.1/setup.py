400fb7f0b45c5c93d425c7e3acaf76bcbdc9c128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:20 +0000

    Add package shabbir-library/0.1/shabbir_library-0.1.tar.gz

---
+++ packages/shabbir-library/shabbir_library-0.1.tar.gz/shabbir_library-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shabbir_library',
+      version='0.1',
+      description='operation_library',
+      packages=['shabbir_library'],
+      author_email='shabbirg89@gmail.com',
+      zip_safe=False)
---
+++ packages/shabbir-library/shabbir_library-0.1.tar.gz/shabbir_library-0.1/shabbir_library/__init__.py
@@ -0,0 +1,4 @@
+from .operation import add
+from .operation import subtract
+from .operation import multiply
+from .operation import divide
