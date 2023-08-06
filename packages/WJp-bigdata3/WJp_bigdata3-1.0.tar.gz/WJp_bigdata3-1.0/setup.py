368a0d242bdee2ebe3075dc5e9474ee1d0a3c323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:11 +0000

    Add package WJp-bigdata3/1.0/WJp_bigdata3-1.0.tar.gz

---
+++ packages/WJp-bigdata3/WJp_bigdata3-1.0.tar.gz/WJp_bigdata3-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="WJp_bigdata3",
+    version="1.0",
+    py_modules=["WJp_bigdata3"],
+    author="Wjp",
+    author_email="2878508824@qq.com",
+    description="大数据3Wjp"
+    )
