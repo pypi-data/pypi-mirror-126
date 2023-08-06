6fe441079388fbd263a5048a5fb0790c8cd26417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:36 +0000

    Add package ran-zk/1.0/ran_zk-1.0.tar.gz

---
+++ packages/ran-zk/ran_zk-1.0.tar.gz/ran_zk-1.0/zzz_k.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "ran_zk",
+    version= "1.0",
+    py_modules= ["fc_ims_zk"],
+    author= "大数据一班人",
+    author_email= "fc@bigdata.com",
+    description= "大数据一班人"
+)
