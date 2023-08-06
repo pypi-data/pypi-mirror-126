a8cb97991cbba0cccd565a1e80df6f234d63813f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:56 +0000

    Add package py-tgcalls/0.8.1/py_tgcalls-0.8.1-cp310-cp310-macosx_10_14_x86_64.whl

---
+++ packages/py-tgcalls/py_tgcalls-0.8.1-cp310-cp310-macosx_10_14_x86_64.whl/pytgcalls/types/input_stream/video_parameters.py
@@ -0,0 +1,13 @@
+from ..py_object import PyObject
+
+
+class VideoParameters(PyObject):
+    def __init__(
+        self,
+        width: int = 640,
+        height: int = 360,
+        frame_rate: int = 20,
+    ):
+        self.width: int = width
+        self.height: int = height
+        self.frame_rate: int = frame_rate
