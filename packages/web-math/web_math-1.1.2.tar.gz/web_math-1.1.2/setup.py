e5acff2582b0bc185d0f88d5305dcb2da0d773c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:54 +0000

    Add package web-math/1.1.2/web_math-1.1.2.tar.gz

---
+++ packages/web-math/web_math-1.1.2.tar.gz/web_math-1.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+requirements = []
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+# See note below for more information about classifiers
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+setup(
+    name="web_math",
+    version="1.1.2",
+    description="For online math api",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/mcbabo/webmath",
+    author="Moritz Joksch",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="discord discord-calc calculator easy-calc simple-calculator discord.py pycord python math",
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True
+)
