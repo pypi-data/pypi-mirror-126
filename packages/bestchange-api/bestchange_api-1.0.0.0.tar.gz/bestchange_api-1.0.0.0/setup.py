2f24a4d16e6701bd733e209380a48245f9e2b44d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:19 +0000

    Add package bestchange-api/1.0.0.0/bestchange_api-1.0.0.0.tar.gz

---
+++ packages/bestchange-api/bestchange_api-1.0.0.0.tar.gz/bestchange_api-1.0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bestchange_api',
+      version='1.0.0.0',
+      description='',
+      packages=['bestchange_api'],
+      author_email='maksam07@gmail.com',
+      zip_safe=False)
