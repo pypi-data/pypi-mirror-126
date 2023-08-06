f4464439297927eab0857e063ddb1979b81910db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:19 +0000

    Add package fp-functions/0.1/fp_functions-0.1.tar.gz

---
+++ packages/fp-functions/fp_functions-0.1.tar.gz/fp_functions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup;
+
+setup(
+    name="fp_functions",
+    version="0.1",
+    description="A small Python library that adds some functional programming features",
+    author="Thomas Gregory",
+    license="Apache License, Version 2.0",
+    py_modules=["fp_functions"]
+)
