545098686b6ed557df2e572fd11e851b83dfcd82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:04 +0000

    Add package antidote/1.0.1/antidote-1.0.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/antidote/antidote-1.0.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/antidote/_internal/utils/__init__.py
@@ -0,0 +1,31 @@
+import enum
+from typing import Set, TypeVar
+
+from .debug import debug_repr, short_id
+from .immutable import FinalImmutable, Immutable
+from .meta import AbstractMeta, FinalMeta
+from .slots import SlotRecord
+from .. import API
+
+Im = TypeVar('Im', bound=Immutable)
+
+
+@API.private
+class Default(enum.Enum):
+    sentinel = enum.auto()
+
+
+@API.private
+class Copy(enum.Enum):
+    IDENTICAL = enum.auto()
+
+    @staticmethod
+    def immutable(current: Im, **kwargs: object) -> Im:
+        return type(current)(**{
+            attr: getattr(current, attr) if value is Copy.IDENTICAL else value
+            for attr, value in kwargs.items()
+        })
+
+
+__all__ = ['debug_repr', 'short_id', 'FinalImmutable', 'Immutable', 'AbstractMeta',
+           'FinalMeta', 'SlotRecord', 'API', 'Default', 'Copy']
