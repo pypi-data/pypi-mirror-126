930b2094e2db3cbe4e95e77f60e23f0b4f852b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:44 +0000

    Add package mlflow-iap-token/0.3.0/mlflow_iap_token-0.3.0-py3-none-any.whl

---
+++ packages/mlflow-iap-token/mlflow_iap_token-0.3.0-py3-none-any.whl/mlflow_iap_token/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.0'
+
+from .iap_token import get_token
+from .iap_token import fetch_mlflow_token
+from .iap_token import IdentityAwareProxyPluginRequestHeaderProvider
+
+__all__ = ['get_token', 'fetch_mlflow_token', 'IdentityAwareProxyPluginRequestHeaderProvider']
