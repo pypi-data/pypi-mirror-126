227fc47669634d3723dd8671c38cb141102e74ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:21 +0000

    Add package corai/1.301/CorAI-1.301.tar.gz

---
+++ packages/corai/CorAI-1.301.tar.gz/CorAI-1.301/corai/src/train/__init__.py
@@ -0,0 +1,4 @@
+from .fit import nn_fit
+from .kfold_training import nn_kfold_train, initialise_estimator, train_kfold_a_fold_after_split
+from .nntrainparameters import NNTrainParameters
+from .train import nn_train
