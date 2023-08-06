0ebdf54e7365c453749cd2ddc8e5e1a659d33baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:47 +0000

    Add package python-framework/0.2.128/python_framework-0.2.128.tar.gz

---
+++ packages/python-framework/python_framework-0.2.128.tar.gz/python_framework-0.2.128/python_framework/api/src/constant/ConfigurationKeyConstant.py
@@ -0,0 +1,17 @@
+SCHEDULER_ENABLE_KEY = 'api.scheduler.enable'
+SCHEDULER_MUTE_LOGS_KEY = 'api.scheduler.mute-logs'
+
+API_SERVER_SCHEME = 'api.server.scheme'
+API_SERVER_HOST = 'api.server.host'
+API_SERVER_PORT = 'api.server.port'
+API_SERVER_BASE_URL = 'api.server.base-url'
+
+API_SESSION_SECRET = 'api.session.secret'
+API_SESSION_ALGORITHM = 'api.session.algorithm'
+API_SESSION_HEADER = 'api.session.header'
+API_SESSION_TYPE = 'api.session.type'
+
+API_SECURITY_SECRET = 'api.security.secret'
+API_SECURITY_ALGORITHM = 'api.security.algorithm'
+API_SECURITY_HEADER = 'api.security.header'
+API_SECURITY_TYPE = 'api.security.type'
