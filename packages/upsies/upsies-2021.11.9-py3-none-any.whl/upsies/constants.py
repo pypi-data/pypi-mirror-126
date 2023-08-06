2cc9ccda6d3f0713aa5585462990db80b63f1ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:45 +0000

    Add package upsies/2021.11.9/upsies-2021.11.9-py3-none-any.whl

---
+++ packages/upsies/upsies-2021.11.9-py3-none-any.whl/upsies/constants.py
@@ -0,0 +1,38 @@
+"""
+Fixed values that do not change during runtime
+"""
+
+import os
+
+from xdg.BaseDirectory import xdg_cache_home as XDG_CACHE_HOME
+from xdg.BaseDirectory import xdg_config_home as XDG_CONFIG_HOME
+
+from . import __project_name__
+
+CACHE_DIRPATH = os.path.join(XDG_CACHE_HOME, __project_name__)
+"""Path cache directory"""
+
+CONFIG_FILEPATH = os.path.join(XDG_CONFIG_HOME, __project_name__, 'config.ini')
+"""Path to general configuration file"""
+
+TRACKERS_FILEPATH = os.path.join(XDG_CONFIG_HOME, __project_name__, 'trackers.ini')
+"""Path to trackers configuration file"""
+
+IMGHOSTS_FILEPATH = os.path.join(XDG_CONFIG_HOME, __project_name__, 'imghosts.ini')
+"""Path to image hosting services configuration file"""
+
+CLIENTS_FILEPATH = os.path.join(XDG_CONFIG_HOME, __project_name__, 'clients.ini')
+"""Path to BitTorrent clients configuration file"""
+
+VIDEO_FILE_EXTENSIONS = ('mkv', 'mp4', 'avi', 'wmv', 'vob', 'm2ts', 'mts', 'ts')
+"""Sequence of file extensions to recognize video files"""
+
+GUESSIT_OPTIONS = {
+    'expected_title': [
+        'The Collector',
+        'xXx',
+    ],
+    'advanced_config': {
+    },
+}
+"""`options` argument for :meth:`.GuessItApi.guessit`"""
