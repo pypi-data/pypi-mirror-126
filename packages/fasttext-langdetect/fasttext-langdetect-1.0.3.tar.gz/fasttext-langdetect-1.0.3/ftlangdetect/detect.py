c2b9d597a7b2439bf0ce47e33091304c622f48f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:12 +0000

    Add package fasttext-langdetect/1.0.3/fasttext-langdetect-1.0.3.tar.gz

---
+++ packages/fasttext-langdetect/fasttext-langdetect-1.0.3.tar.gz/fasttext-langdetect-1.0.3/ftlangdetect/detect.py
@@ -0,0 +1,45 @@
+import os
+from typing import Dict, Union
+
+import fasttext
+import wget
+
+models = {"low_mem": None, "high_mem": None}
+FTLANG_CACHE = os.getenv("FTLANG_CACHE", "/tmp/fasttext-langdetect")
+
+
+def download_model(name):
+    url = f"https://dl.fbaipublicfiles.com/fasttext/supervised-models/{name}"
+    target_path = os.path.join(FTLANG_CACHE, name)
+    if not os.path.exists(target_path):
+        os.makedirs(FTLANG_CACHE, exist_ok=True)
+        wget.download(url=url, out=target_path)
+    return target_path
+
+
+def get_or_load_model(low_memory=False):
+    if low_memory:
+        model = models.get("low_mem", None)
+        if not model:
+            model_path = download_model("lid.176.ftz")
+            model = fasttext.load_model(model_path)
+            models["low_mem"] = model
+        return model
+    else:
+        model = models.get("high_mem", None)
+        if not model:
+            model_path = download_model("lid.176.bin")
+            model = fasttext.load_model(model_path)
+            models["high_mem"] = model
+        return model
+
+
+def detect(text: str, low_memory=False) -> Dict[str, Union[str, float]]:
+    model = get_or_load_model(low_memory)
+    labels, scores = model.predict(text)
+    label = labels[0].replace("__label__", '')
+    score = min(float(scores[0]), 1.0)
+    return {
+        "lang": label,
+        "score": score,
+    }
