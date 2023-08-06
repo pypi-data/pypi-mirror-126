761f54e4ce373be73d829ffaf0f852ffaf448bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:25 +0000

    Add package nnMorpho/2.0.3/nnMorpho-2.0.3.tar.gz

---
+++ packages/nnMorpho/nnMorpho-2.0.3.tar.gz/nnMorpho-2.0.3/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name='nnMorpho',
+  packages=['nnMorpho'],
+  version='2.0.3',
+  license='MIT',
+  description='A library for GPU-accelerated and Machine-Learning adapted'
+              ' Mathematical Morphology',
+  author='Gonzalo Romero-García',
+  author_email='tritery@hotmail.com',
+  url='https://github.com/Manza12/nnMorpho',
+  download_url='https://github.com/Manza12/nnMorpho/archive/v_0.1.tar.gz',
+  keywords=['Mathematical Morphology', 'PyTorch', 'GPU', 'CUDA'],
+  install_requires=[
+          'numpy',
+          'torch'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
