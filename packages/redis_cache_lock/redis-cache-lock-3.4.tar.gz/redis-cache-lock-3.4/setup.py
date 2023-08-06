27678aa20fe8ce1629f47648e7c82f945434b8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:03 +0000

    Add package redis_cache_lock/3.4/redis-cache-lock-3.4.tar.gz

---
+++ packages/redis_cache_lock/redis-cache-lock-3.4.tar.gz/redis-cache-lock-3.4/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+import pkg_resources
+import setuptools
+
+pkg_resources.require("setuptools>=48")
+
+if __name__ == "__main__":
+    setuptools.setup()
