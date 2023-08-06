ae416a7b82e1edb648fdd091bce37b9dc8e6f96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:35 +0000

    Add package shakenfist/0.4.20.0rc3/shakenfist-0.4.20.0rc3.tar.gz

---
+++ packages/shakenfist/shakenfist-0.4.20.0rc3.tar.gz/shakenfist-0.4.20.0rc3/shakenfist/daemons/main.py
@@ -0,0 +1,228 @@
+# Copyright 2019 Michael Still
+
+import setproctitle
+import time
+import os
+import psutil
+
+from shakenfist.config import config
+from shakenfist.daemons import daemon
+from shakenfist.daemons import external_api as external_api_daemon
+from shakenfist.daemons import cleaner as cleaner_daemon
+from shakenfist.daemons import queues as queues_daemon
+from shakenfist.daemons import net as net_daemon
+from shakenfist.daemons import resources as resource_daemon
+from shakenfist.daemons import triggers as trigger_daemon
+from shakenfist import db
+from shakenfist import etcd
+from shakenfist import instance
+from shakenfist.ipmanager import IPManager
+from shakenfist import logutil
+from shakenfist import net
+from shakenfist.networkinterface import interfaces_for_instance
+from shakenfist.node import Node
+from shakenfist.util import general as util_general
+from shakenfist.util import process as util_process
+from shakenfist.util import network as util_network
+
+
+LOG, HANDLER = logutil.setup('main')
+
+
+def restore_instances():
+    # Ensure all instances for this node are defined and have up to date data.
+    networks = []
+    instances = []
+    for inst in instance.Instances([instance.this_node_filter,
+                                    instance.healthy_states_filter]):
+        instance_problems = []
+        inst_interfaces = inst.interfaces
+        if not inst_interfaces:
+            inst_interfaces = []
+        updated_interfaces = False
+
+        for ni in interfaces_for_instance(inst):
+            if ni.network_uuid not in networks:
+                networks.append(ni.network_uuid)
+            if ni.uuid not in inst_interfaces:
+                inst_interfaces.append(ni.uuid)
+                updated_interfaces = True
+
+        # We do not need a lock here because this loop only runs on the node
+        # with the instance, and interfaces don't change post instance
+        # creation.
+        if updated_interfaces:
+            inst.interfaces = inst_interfaces
+
+        # TODO(mikal): do better here.
+        # for disk in inst.disk_spec:
+        #     if disk.get('base'):
+        #         img = images.Image.new(disk['base'])
+        #         # NOTE(mikal): this check isn't great -- it checks for the original
+        #         # downloaded image, not the post transcode version
+        #         if (img.state in [dbo.STATE_DELETED, dbo.STATE_ERROR] or
+        #                 not os.path.exists(img.version_image_path())):
+        #             instance_problems.append(
+        #                 '%s missing from image cache' % disk['base'])
+        #             img.delete()
+
+        if instance_problems:
+            inst.enqueue_delete_due_error(
+                'instance bad on startup: %s' % '; '.join(instance_problems))
+        else:
+            instances.append(inst)
+
+    with util_general.RecordedOperation('restore networks', None):
+        for network in networks:
+            try:
+                n = net.Network.from_db(network)
+                if not n.is_dead():
+                    LOG.with_object(n).info('Restoring network')
+                    n.create_on_hypervisor()
+                    n.ensure_mesh()
+            except Exception as e:
+                util_general.ignore_exception(
+                    'restore network %s' % network, e)
+
+    with util_general.RecordedOperation('restore instances', None):
+        for inst in instances:
+            try:
+                with db.get_lock(
+                        'instance', None, inst.uuid, ttl=120, timeout=120,
+                        op='Instance restore'):
+                    started = ['on', 'transition-to-on',
+                               instance.Instance.STATE_INITIAL, 'unknown']
+                    if inst.power_state not in started:
+                        continue
+
+                    LOG.with_object(inst).info('Restoring instance')
+                    inst.create_on_hypervisor()
+            except Exception as e:
+                util_general.ignore_exception(
+                    'restore instance %s' % inst.uuid, e)
+                inst.etcd.enqueue_delete_due_error(
+                    'exception while restoring instance on daemon restart')
+
+
+DAEMON_IMPLEMENTATIONS = {
+    'api': external_api_daemon,
+    'cleaner': cleaner_daemon,
+    'net': net_daemon,
+    'queues': queues_daemon,
+    'resources': resource_daemon,
+    'triggers': trigger_daemon
+}
+
+
+DAEMON_PIDS = {}
+
+
+def main():
+    global DAEMON_IMPLEMENTATIONS
+    global DAEMON_PIDS
+
+    LOG.info('Starting...')
+    setproctitle.setproctitle(
+        daemon.process_name('main') + '-v%s' % util_general.get_version())
+
+    # Log configuration on startup
+    for key, value in config.dict().items():
+        LOG.info('Configuration item %s = %s' % (key, value))
+
+    daemon.set_log_level(LOG, 'main')
+
+    # Check in early and often, also reset processing queue items.
+    etcd.clear_stale_locks()
+    Node.observe_this_node()
+    etcd.restart_queues()
+
+    def _start_daemon(d):
+        pid = os.fork()
+        if pid == 0:
+            DAEMON_IMPLEMENTATIONS[d].Monitor(d).run()
+        DAEMON_PIDS[pid] = d
+        LOG.with_field('pid', pid).info('Started %s' % d)
+
+    # Resource usage publisher, we need this early because scheduling decisions
+    # might happen quite early on.
+    _start_daemon('resources')
+
+    # If I am the network node, I need some setup
+    if config.NODE_IS_NETWORK_NODE:
+        # Bootstrap the floating network in the Networks table
+        floating_network = net.Network.from_db('floating')
+        if not floating_network:
+            floating_network = net.Network.create_floating_network(
+                config.FLOATING_NETWORK)
+
+        subst = {
+            'egress_bridge': util_network.get_safe_interface_name(
+                'egr-br-%s' % config.NODE_EGRESS_NIC),
+            'egress_nic': config.NODE_EGRESS_NIC
+        }
+
+        if not util_network.check_for_interface(subst['egress_bridge']):
+            # NOTE(mikal): Adding the physical interface to the physical bridge
+            # is considered outside the scope of the orchestration software as
+            # it will cause the node to lose network connectivity. So instead
+            # all we do is create a bridge if it doesn't exist and the wire
+            # everything up to it. We can do egress NAT in that state, even if
+            # floating IPs don't work.
+            with util_general.RecordedOperation('create physical bridge', None):
+                # No locking as read only
+                ipm = IPManager.from_db('floating')
+                subst['master_float'] = ipm.get_address_at_index(1)
+                subst['netmask'] = ipm.netmask
+
+                # We need to copy the MTU of the interface we are bridging to
+                # or weird networking things happen.
+                mtu = util_network.get_interface_mtu(config.NODE_EGRESS_NIC)
+
+                util_network.create_interface(
+                    subst['egress_bridge'], 'bridge', '', mtu=mtu)
+
+                util_process.execute(None,
+                                     'ip link set %(egress_bridge)s up' % subst)
+                util_process.execute(None,
+                                     'ip addr add %(master_float)s/%(netmask)s '
+                                     'dev %(egress_bridge)s' % subst)
+
+                util_process.execute(None,
+                                     'iptables -A FORWARD -o %(egress_nic)s '
+                                     '-i %(egress_bridge)s -j ACCEPT' % subst)
+                util_process.execute(None,
+                                     'iptables -A FORWARD -i %(egress_nic)s '
+                                     '-o %(egress_bridge)s -j ACCEPT' % subst)
+                util_process.execute(None,
+                                     'iptables -t nat -A POSTROUTING '
+                                     '-o %(egress_nic)s -j MASQUERADE' % subst)
+
+    def _audit_daemons():
+        running_daemons = []
+        for pid in DAEMON_PIDS:
+            running_daemons.append(DAEMON_PIDS[pid])
+
+        for d in DAEMON_IMPLEMENTATIONS:
+            if d not in running_daemons:
+                _start_daemon(d)
+
+        for d in DAEMON_PIDS:
+            if not psutil.pid_exists(d):
+                LOG.warning('%s pid is missing, restarting' % DAEMON_PIDS[d])
+                _start_daemon(DAEMON_PIDS[d])
+
+    _audit_daemons()
+    restore_instances()
+
+    while True:
+        time.sleep(10)
+
+        wpid, _ = os.waitpid(-1, os.WNOHANG)
+        while wpid != 0:
+            LOG.warning('%s died (pid %d)'
+                        % (DAEMON_PIDS.get(wpid, 'unknown'), wpid))
+            del DAEMON_PIDS[wpid]
+            wpid, _ = os.waitpid(-1, os.WNOHANG)
+
+        _audit_daemons()
+        Node.observe_this_node()
---
+++ packages/shakenfist/shakenfist-0.4.20.0rc3.tar.gz/shakenfist-0.4.20.0rc3/shakenfist/tests/test_scheduler.py
@@ -0,0 +1,507 @@
+import copy
+import mock
+import time
+
+from shakenfist.baseobject import DatabaseBackedObject as dbo
+from shakenfist import exceptions
+from shakenfist.instance import Instance
+from shakenfist import scheduler
+from shakenfist.tests import base
+from shakenfist.config import SFConfig
+
+
+class FakeInstance(Instance):
+    def add_event(self, operation, phase, duration=None, message=None):
+        print('op = %s, phase = %s, message = %s'
+              % (operation, phase, message))
+
+
+class FakeNode(object):
+    def __init__(self, fqdn, ip):
+        self.uuid = fqdn
+        self.ip = ip
+        self.state = dbo.STATE_CREATED
+
+    def unique_label(self):
+        return ('fake_node', self.uuid)
+
+
+class FakeDB(object):
+    def __init__(self, nodes):
+        self.metrics = {}
+        self.nodes = nodes
+
+    def set_node_metrics_same(self, metrics):
+        for n in self.nodes:
+            self.metrics[n] = copy.copy(metrics)
+            self.metrics[n]['is_hypervisor'] = not n.endswith('_net')
+
+    def get_metrics(self, node_name):
+        if node_name not in self.metrics:
+            raise exceptions.ReadException
+        return self.metrics[node_name]
+
+
+class FakeInterface(object):
+    def __init__(self, network_uuid):
+        self.network_uuid = network_uuid
+
+
+fake_config = SFConfig(
+    NODE_NAME='node01',
+    SCHEDULER_CACHE_TIMEOUT=30,
+    CPU_OVERCOMMIT_RATIO=16.0,
+    RAM_OVERCOMMIT_RATIO=1.5,
+    RAM_SYSTEM_RESERVATION=5.0,
+    NETWORK_NODE_IP='10.0.0.1',
+    LOG_METHOD_TRACE=1,
+)
+
+
+class SchedulerTestCase(base.ShakenFistTestCase):
+    def setUp(self):
+        super(SchedulerTestCase, self).setUp()
+
+        self.recorded_op = mock.patch(
+            'shakenfist.util.general.RecordedOperation')
+        self.recorded_op.start()
+        self.addCleanup(self.recorded_op.stop)
+
+        self.mock_config = mock.patch(
+            'shakenfist.scheduler.config', fake_config)
+        self.mock_config.start()
+        self.addCleanup(self.mock_config.stop)
+
+        self.mock_see_this_node = mock.patch(
+            'shakenfist.node.Node.observe_this_node')
+        self.mock_see_this_node.start()
+        self.addCleanup(self.mock_see_this_node.stop)
+
+        self.mock_add_event = mock.patch('shakenfist.db.add_event')
+        self.mock_add_event.start()
+        self.addCleanup(self.mock_add_event.stop)
+
+
+class LowResourceTestCase(SchedulerTestCase):
+    """Test low resource exceptions."""
+
+    def setUp(self):
+        super(LowResourceTestCase, self).setUp()
+
+        self.fake_db = FakeDB(['node1_net', 'node2', 'node3', 'node4'])
+
+        mock_db_get_metrics = mock.patch('shakenfist.db.get_metrics',
+                                         side_effect=self.fake_db.get_metrics)
+        mock_db_get_metrics.start()
+        self.addCleanup(mock_db_get_metrics.stop)
+
+        self.mock_config = mock.patch(
+            'shakenfist.scheduler.config', fake_config)
+        self.mock_config.start()
+        self.addCleanup(self.mock_config.stop)
+
+        mock_get_nodes = mock.patch(
+            'shakenfist.scheduler.Nodes',
+            return_value=[FakeNode('node1_net', '10.0.0.1'),
+                          FakeNode('node2', '10.0.0.2'),
+                          FakeNode('node3', '10.0.0.3'),
+                          FakeNode('node4', '10.0.0.4')])
+        mock_get_nodes.start()
+        self.addCleanup(mock_get_nodes.stop)
+
+        self.mock_get_instances = mock.patch('shakenfist.instance.Instances')
+        self.mock_get_instances.start()
+        self.addCleanup(self.mock_get_instances.stop)
+
+    def test_no_metrics(self):
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        exc = self.assertRaises(exceptions.LowResourceException,
+                                scheduler.Scheduler().place_instance,
+                                fake_inst,
+                                [])
+        self.assertEqual('No nodes with metrics', str(exc))
+
+    def test_requested_too_many_cpu(self):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 5,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 6,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        exc = self.assertRaises(exceptions.LowResourceException,
+                                scheduler.Scheduler().place_instance,
+                                fake_inst,
+                                [])
+        self.assertEqual('Requested vCPUs exceeds vCPU limit', str(exc))
+
+    def test_not_enough_cpu(self):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'cpu_total_instance_vcpus': 4*16,
+            'memory_available': 5*1024+1024-1,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_available': 4
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        exc = self.assertRaises(exceptions.LowResourceException,
+                                scheduler.Scheduler().place_instance,
+                                fake_inst,
+                                [])
+        self.assertEqual('No nodes with enough idle CPU', str(exc))
+
+    def test_not_enough_ram_for_system(self):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 5*1024+1024-1,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        exc = self.assertRaises(exceptions.LowResourceException,
+                                scheduler.Scheduler().place_instance,
+                                fake_inst,
+                                [])
+        self.assertEqual('No nodes with enough idle RAM', str(exc))
+
+    def test_not_enough_ram_on_node(self):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 10000,
+            'memory_max': 10000,
+            'memory_total_instance_actual': 15001,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        exc = self.assertRaises(exceptions.LowResourceException,
+                                scheduler.Scheduler().place_instance,
+                                fake_inst,
+                                [])
+        self.assertEqual('No nodes with enough idle RAM', str(exc))
+
+    @mock.patch('shakenfist.artifact.Artifacts', return_value=[])
+    def test_not_enough_disk(self, mock_get_artifacts):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 22000,
+            'memory_max': 24000,
+            'disk_free': 20*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 21
+            }]})
+
+        exc = self.assertRaises(exceptions.LowResourceException,
+                                scheduler.Scheduler().place_instance,
+                                fake_inst,
+                                [])
+        self.assertEqual('No nodes with enough disk space', str(exc))
+
+    @mock.patch('shakenfist.artifact.Artifacts', return_value=[])
+    def test_ok(self, mock_get_artifacts):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 22000,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        nodes = scheduler.Scheduler().place_instance(fake_inst, [])
+        self.assertSetEqual(set(self.fake_db.nodes)-{'node1_net', },
+                            set(nodes))
+
+
+class CorrectAllocationTestCase(SchedulerTestCase):
+    """Test correct node allocation."""
+
+    def setUp(self):
+        super(CorrectAllocationTestCase, self).setUp()
+
+        self.fake_db = FakeDB(['node1_net', 'node2', 'node3', 'node4'])
+
+        mock_db_get_metrics = mock.patch('shakenfist.db.get_metrics',
+                                         side_effect=self.fake_db.get_metrics)
+        mock_db_get_metrics.start()
+        self.addCleanup(mock_db_get_metrics.stop)
+
+        self.mock_config = mock.patch(
+            'shakenfist.scheduler.config', fake_config)
+        self.mock_config.start()
+        self.addCleanup(self.mock_config.stop)
+
+        mock_get_nodes = mock.patch(
+            'shakenfist.scheduler.Nodes',
+            return_value=[FakeNode('node1_net', '10.0.0.1'),
+                          FakeNode('node2', '10.0.0.2'),
+                          FakeNode('node3', '10.0.0.3'),
+                          FakeNode('node4', '10.0.0.4')])
+        mock_get_nodes.start()
+        self.addCleanup(mock_get_nodes.stop)
+
+    @mock.patch('shakenfist.instance.Instance._db_get_attribute')
+    @mock.patch('shakenfist.instance.Instance._db_get',
+                return_value={
+                    'uuid': 'inst-1',
+                    'cpus': 1,
+                    'memory': 1024,
+                    'node': 'node3',
+                    'disk_spec': [{'base': 'cirros', 'size': 21}]
+                })
+    @mock.patch('shakenfist.etcd.get_all',
+                return_value=[(None, {
+                    'uuid': 'inst-1',
+                    'cpus': 1,
+                    'memory': 1024,
+                    'node': 'node3',
+                    'disk_spec': [{'base': 'cirros', 'size': 21}]
+                })])
+    @mock.patch('shakenfist.artifact.Artifacts', return_value=[])
+    def test_any_node_but_not_network_node(
+            self, mock_get_artifacts, mock_get_instances,
+            mock_get_instance, mock_instance_attribute):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 22000,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+        nets = [{'network_uuid': 'uuid-net2'}]
+
+        nodes = scheduler.Scheduler().place_instance(fake_inst, nets)
+        self.assertSetEqual(set(self.fake_db.nodes)-{'node1_net', },
+                            set(nodes))
+
+
+class ForcedCandidatesTestCase(SchedulerTestCase):
+    """Test when we force candidates."""
+
+    def setUp(self):
+        super(ForcedCandidatesTestCase, self).setUp()
+
+        self.fake_db = FakeDB(['node1_net', 'node2', 'node3', 'node4'])
+
+        mock_db_get_metrics = mock.patch('shakenfist.db.get_metrics',
+                                         side_effect=self.fake_db.get_metrics)
+        mock_db_get_metrics.start()
+        self.addCleanup(mock_db_get_metrics.stop)
+
+        self.mock_config = mock.patch(
+            'shakenfist.scheduler.config', fake_config)
+        self.mock_config.start()
+        self.addCleanup(self.mock_config.stop)
+
+        mock_get_nodes = mock.patch(
+            'shakenfist.scheduler.Nodes',
+            return_value=[FakeNode('node1_net', '10.0.0.1'),
+                          FakeNode('node2', '10.0.0.2'),
+                          FakeNode('node3', '10.0.0.3'),
+                          FakeNode('node4', '10.0.0.4')])
+        mock_get_nodes.start()
+        self.addCleanup(mock_get_nodes.stop)
+
+        self.mock_get_instances = mock.patch('shakenfist.instance.Instances')
+        self.mock_get_instances.start()
+        self.addCleanup(self.mock_get_instances.stop)
+
+    @mock.patch('shakenfist.artifact.Artifacts', return_value=[])
+    def test_only_two(self, mock_get_artifacts):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 22000,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        nodes = scheduler.Scheduler().place_instance(
+            fake_inst, [], candidates=['node1_net', 'node2'])
+        self.assertSetEqual({'node2', }, set(nodes))
+
+    @mock.patch('shakenfist.artifact.Artifacts', return_value=[])
+    def test_no_such_node(self, mock_get_artifacts):
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 22000,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024
+        })
+
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        self.assertRaises(
+            exceptions.CandidateNodeNotFoundException,
+            scheduler.Scheduler().place_instance,
+            fake_inst, [], candidates=['barry'])
+
+
+class MetricsRefreshTestCase(SchedulerTestCase):
+    """Test that we refresh metrics."""
+
+    def setUp(self):
+        super(MetricsRefreshTestCase, self).setUp()
+
+        self.fake_db = FakeDB(['node1_net', 'node2', 'node3', 'node4'])
+
+        mock_db_get_metrics = mock.patch('shakenfist.db.get_metrics',
+                                         side_effect=self.fake_db.get_metrics)
+        mock_db_get_metrics.start()
+        self.addCleanup(mock_db_get_metrics.stop)
+
+        self.mock_config = mock.patch(
+            'shakenfist.scheduler.config', fake_config)
+        self.mock_config.start()
+        self.addCleanup(self.mock_config.stop)
+
+        mock_get_nodes = mock.patch(
+            'shakenfist.scheduler.Nodes',
+            return_value=[FakeNode('node1_net', '10.0.0.1'),
+                          FakeNode('node2', '10.0.0.2'),
+                          FakeNode('node3', '10.0.0.3'),
+                          FakeNode('node4', '10.0.0.4')])
+        mock_get_nodes.start()
+        self.addCleanup(mock_get_nodes.stop)
+
+        self.mock_get_instances = mock.patch('shakenfist.instance.Instances')
+        self.mock_get_instances.start()
+        self.addCleanup(self.mock_get_instances.stop)
+
+    @mock.patch('shakenfist.artifact.Artifacts', return_value=[])
+    def test_refresh(self, mock_get_artifacts):
+        fake_inst = FakeInstance({
+            'uuid': 'fakeuuid',
+            'cpus': 1,
+            'memory': 1024,
+            'disk_spec': [{
+                'base': 'cirros',
+                        'size': 8
+            }]})
+
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 22000,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+
+        s = scheduler.Scheduler()
+        s.place_instance(fake_inst, [])
+        self.assertEqual(22000, s.metrics['node1_net']['memory_available'])
+
+        self.fake_db.set_node_metrics_same({
+            'cpu_max_per_instance': 16,
+            'cpu_max': 4,
+            'memory_available': 11000,
+            'memory_max': 24000,
+            'disk_free': 2000*1024*1024*1024,
+            'cpu_total_instance_vcpus': 4,
+            'cpu_available': 12
+        })
+        s.metrics_updated = time.time() - 400
+        s.place_instance(fake_inst, [])
+        self.assertEqual(11000, s.metrics['node1_net']['memory_available'])
