a0f7808ae4ea612d7b47c95ccb059b99772f6d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:25 +0000

    Add package cvhub/0.1.6/cvhub-0.1.6.tar.gz

---
+++ packages/cvhub/cvhub-0.1.6.tar.gz/cvhub-0.1.6/src/mgmatting/__init__.py
@@ -0,0 +1,12 @@
+import toml
+
+from .utils import *
+
+
+def load_config(config_path):
+    with open(config_path) as f:
+        utils.load_config(toml.load(f))
+
+    # Check if toml config file is loaded
+    if CONFIG.is_default:
+        raise ValueError("No .toml config loaded.")
