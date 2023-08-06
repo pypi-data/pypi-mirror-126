b9028df474625c3849b351661742f37ea40cb724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:29 +0000

    Add package WJK/1.0/WJK-1.0.tar.gz

---
+++ packages/WJK/WJK-1.0.tar.gz/WJK-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+           name = "WJK",
+           version = "1.0",
+           py_modules = ["qingqing"],
+           author = "庆庆",
+           author_email = "1607210015@qq.com",
+           description = "庆庆"
+    )
