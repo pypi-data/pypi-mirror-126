5d798f8bcf63563b18a4ee6091097db0e65ad826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:37 +0000

    Add package duality/3.1.1.2/duality-3.1.1.2.tar.gz

---
+++ packages/duality/duality-3.1.1.2.tar.gz/duality-3.1.1.2/duality/__init__.py
@@ -0,0 +1,20 @@
+'''This is a main folder of the duality package.'''
+
+#imports essential functions from the duality package.
+from duality.eoq.eoq import EOQ
+from duality.hub import hub
+from duality.montecarlo.montecarlo import MonteCarlo
+from duality.misc._meta import (
+	__author__,
+	__copyright__,
+	__credits__,
+	__license__,
+	__version__,
+	__documentation__,
+	__contact__,
+	__donate__
+)
+
+#remove the creation of __pycache__.
+import sys
+sys.dont_write_bytecode = True
