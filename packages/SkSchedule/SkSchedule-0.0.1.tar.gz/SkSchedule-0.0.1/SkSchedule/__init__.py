e63ffdd368ce2c81c05af30e3a44e6af5870f200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:05 +0000

    Add package SkSchedule/0.0.1/SkSchedule-0.0.1.tar.gz

---
+++ packages/SkSchedule/SkSchedule-0.0.1.tar.gz/SkSchedule-0.0.1/SkSchedule/__init__.py
@@ -0,0 +1,10 @@
+# encoding=utf-8
+__all__=['SkSchedule']
+
+
+
+
+
+
+
+
---
+++ packages/SkSchedule/SkSchedule-0.0.1.tar.gz/SkSchedule-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='SkSchedule',
+    version='0.0.1',
+    author='ali',
+    author_email='2528104776@qq.com',
+    url='https://gitee.com/huang-hongzhe/sk-schedule.git',
+    description=u'为生科查课提供便利',
+    packages=['SkSchedule'],
+    install_requires=[],
+)
+
