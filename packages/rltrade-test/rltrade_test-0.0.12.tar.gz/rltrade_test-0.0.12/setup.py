8c89f68c4a210cdc87c604f54dc9cd5fb2cd8065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:43 +0000

    Add package rltrade-test/0.0.12/rltrade_test-0.0.12.tar.gz

---
+++ packages/rltrade-test/rltrade_test-0.0.12.tar.gz/rltrade_test-0.0.12/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt",'r+') as f:
+    lines = f.readlines()
+
+requirements = [str(x).strip() for x in lines]
+
+print(requirements)
+
+
+setup(
+    name='rltrade_test',
+    packages=find_packages(),
+    version='0.0.12',
+    description="Easy to use Reinforcement Library for finance",
+    install_requires=requirements
+)
