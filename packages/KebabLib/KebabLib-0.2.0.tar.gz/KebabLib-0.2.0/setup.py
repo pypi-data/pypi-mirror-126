7e37cb53d93707aac78ac13d636f652b5de0af93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:32 +0000

    Add package KebabLib/0.2.0/KebabLib-0.2.0.tar.gz

---
+++ packages/KebabLib/KebabLib-0.2.0.tar.gz/KebabLib-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='KebabLib',
+    version='0.2.0',    
+    description='The official library of ChickenProMeister',
+    author='ChickenProMeister',
+    author_email='felix.folkebrant@gmail.com',
+    license='BSD 2-clause',
+    install_requires=['pandas',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
