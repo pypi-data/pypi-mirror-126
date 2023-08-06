b50f9b62b9ab1fc764bce462da67385345cc43c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:48 +0000

    Add package AnteiAPI/0.0.1.dev4/AnteiAPI-0.0.1.dev4.tar.gz

---
+++ packages/AnteiAPI/AnteiAPI-0.0.1.dev4.tar.gz/AnteiAPI-0.0.1.dev4/anteiapi/models/TafsirMimpi.py
@@ -0,0 +1,13 @@
+class TafsirMimpi:
+    """
+    :param js:
+    """
+    def __init__(self, js: dict) -> None:
+        self.result = js['hasil']
+        self.count = js['total']
+    def __getitem__(self, x):
+        return self.result[1]
+    def __iter__(self):
+        return self.result
+    def __repr__(self):
+        return self.result.__str__()
