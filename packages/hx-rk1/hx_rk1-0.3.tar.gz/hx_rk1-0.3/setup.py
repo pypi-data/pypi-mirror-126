e28ba3dbd362f5b3ce20d4d396ceaac0c2cbfd7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:14 +0000

    Add package hx-rk1/0.3/hx_rk1-0.3.tar.gz

---
+++ packages/hx-rk1/hx_rk1-0.3.tar.gz/hx_rk1-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='hx_rk1',
+    version="0.3",
+    py_modules=["hx_shi"],
+    author="小石",
+    author_email="480523858@qq.com",
+    description="大数据1小石的升级版，"
+)
