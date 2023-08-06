249b8923ab2754267f5e7eaeb30e93365bb4eb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:37 +0000

    Add package hubit/0.4.1/hubit-0.4.1.tar.gz

---
+++ packages/hubit/hubit-0.4.1.tar.gz/hubit-0.4.1/hubit/__init__.py
@@ -0,0 +1,14 @@
+VERSION = "0.4.1"
+import shutil
+
+
+# Make HubitModel available as from hubit import model
+from .model import HubitModel, _CACHE_DIR
+
+
+def clear_hubit_cache():
+    """
+    Clear the cache for all models. Will delete all serialized model cache
+    from the disk.
+    """
+    shutil.rmtree(_CACHE_DIR, ignore_errors=True)
