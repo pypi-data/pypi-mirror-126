c23292f46551b6ace0ad8f9b7f6ea1560fd6c2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:43 +0000

    Add package Lee-math/1.0/Lee_math-1.0.tar.gz

---
+++ packages/Lee-math/Lee_math-1.0.tar.gz/Lee_math-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name='Lee_math',
+    version='1.0',
+    author='Cosmo Lee',
+    description='This is the first third-party library released by Cosmo Lee_math,a sophomore.',
+    long_description_content_type='text/markdown',
+    url='https://github.com',
+    packages=setuptools.find_packages(),
+    classfiers=['Programming Language :: Python :: 3',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: OS Independent',],
+
+)
