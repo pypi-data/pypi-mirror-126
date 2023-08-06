05e9f57b826ced671a1c2d18f308d4857ed190b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:14 +0000

    Add package TKinterModernThemes/1.7.0/TKinterModernThemes-1.7.0-py3-none-any.whl

---
+++ packages/TKinterModernThemes/TKinterModernThemes-1.7.0-py3-none-any.whl/TKinterModernThemes/examples/switch.py
@@ -0,0 +1,14 @@
+import TKinterModernThemes as TKMT
+import tkinter as tk
+
+class App(TKMT.ThemedTKinterFrame):
+    def __init__(self, theme, mode, usecommandlineargs=True, usethemeconfigfile=True):
+        super().__init__("Switch", theme, mode, usecommandlineargs=usecommandlineargs,
+                         useconfigfile=usethemeconfigfile)
+        self.switchframe = self.addLabelFrame("Switch Frame")
+        self.switchvar = tk.BooleanVar()
+        self.switchframe.SlideSwitch("Switch", self.switchvar)
+        self.run()
+
+if __name__ == "__main__":
+    App("park", "dark")
