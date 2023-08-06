d41321ba3b4c2fe9e6a1bfc0ca2e296299d848b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:42 +0000

    Add package number2PersianWord/0.0.0/number2PersianWord-0.0.0.tar.gz

---
+++ packages/number2PersianWord/number2PersianWord-0.0.0.tar.gz/number2PersianWord-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='number2PersianWord',
+    version='',
+    description='A short summary about your package',
+    Long_description=open('README').read() + '\n\n' + open('CHANGELOG').read(),
+    url='',
+    author='Alireza Esrafili',
+    author_email='aesi1050@gmail.com',
+    license='MIT',
+    keywords='number2PersianWord',
+    packages=find_packages(),
+    install_requires=['']
+)
