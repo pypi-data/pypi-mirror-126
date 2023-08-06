a6677cdc72aefbb34aab38dbd07c71ca5a7c91d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:40 +0000

    Add package distributions-im/0.1/distributions_im-0.1.tar.gz

---
+++ packages/distributions-im/distributions_im-0.1.tar.gz/distributions_im-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_im',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_im'],
+      zip_safe=False)
