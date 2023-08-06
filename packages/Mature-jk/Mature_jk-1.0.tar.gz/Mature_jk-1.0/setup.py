6548128fb9cf7eeac703ba6498a63cf77cf510b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:34 +0000

    Add package Mature-jk/1.0/Mature_jk-1.0.tar.gz

---
+++ packages/Mature-jk/Mature_jk-1.0.tar.gz/Mature_jk-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Mature_jk",
+    version="1.0",
+    py_modules=["immature_zcm"],
+    author="张青",
+    author_email="3085058568@qq.com",
+    description="张青mysql"
+)
