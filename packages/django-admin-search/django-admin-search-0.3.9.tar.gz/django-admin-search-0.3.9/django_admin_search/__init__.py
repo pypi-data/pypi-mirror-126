b38913364ccf2235c581260b9aaefb63ca6a74cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:27 +0000

    Add package django-admin-search/0.3.9/django-admin-search-0.3.9.tar.gz

---
+++ packages/django-admin-search/django-admin-search-0.3.9.tar.gz/django-admin-search-0.3.9/django_admin_search/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+try:
+    import django
+except ImportError:
+    django = None
+
+__version__ = '0.3.9'
+
+if django and django.VERSION < (3, 2):  # pragma: no cover
+    default_app_config = 'django_admin_inline_paginator.apps.DjangoAdminInlinePaginatorConfig'
