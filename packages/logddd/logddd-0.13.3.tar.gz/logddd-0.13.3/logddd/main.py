90eac352951be54f1da44c2f80dea95c7e85ceb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:58 +0000

    Add package logddd/0.13.3/logddd-0.13.3.tar.gz

---
+++ packages/logddd/logddd-0.13.3.tar.gz/logddd-0.13.3/logddd/main.py
@@ -0,0 +1,18 @@
+import inspect
+import time
+from os.path import basename
+
+
+def log(*args):
+    callFrame = inspect.currentframe().f_back
+    frameInfo = inspect.getframeinfo(callFrame)
+    filename = basename(frameInfo.filename)
+    code = str(frameInfo.code_context).replace(" ", '').replace('\\n', '').replace('\'', '').replace('[', '').replace(
+        ']', '')
+    cur_time = time.asctime()
+    log_location = f'{frameInfo.filename}:{frameInfo.lineno}'
+    res = f"\033[1;33m {filename}:{frameInfo.lineno}\tline:{frameInfo.lineno}\033[0m -> \033[32m{code}\033[0m : \033[1;34m {args}\033[0m {cur_time}"
+    # print(f'\n{log_location}\n{res}')
+    print(f'{res}')
+
+
