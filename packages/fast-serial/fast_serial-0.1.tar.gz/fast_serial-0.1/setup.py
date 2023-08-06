1385d64419edec88dbb009bd63f3c82384479cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:42 +0000

    Add package fast-serial/0.1/fast_serial-0.1.tar.gz

---
+++ packages/fast-serial/fast_serial-0.1.tar.gz/fast_serial-0.1/setup.py
@@ -0,0 +1,31 @@
+"""
+Copyright 2021 Micheal Wilson
+Fast_serial project founded by Micheal Wilson
+
+    This file is part of Fast_Serial.
+
+    Fast_Serial is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    Fast_Serial is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Fast_Serial.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from setuptools import setup
+
+setup(
+    name="fast_serial",
+    version="0.1",
+    description="Basic serial terminal",
+    author="Mike Wilson",
+    url="https://https://github.com/mj8w/fast_serial",
+    license="GNU General Public License, Version 2.0",    
+    py_modules=["fast_serial"],
+)
