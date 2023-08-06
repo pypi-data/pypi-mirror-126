39b4bea055189add4604c5e7e5ee318b2dd33fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:37 +0000

    Add package embutils/0.6.1/embutils-0.6.1-py3-none-any.whl

---
+++ packages/embutils/embutils-0.6.1-py3-none-any.whl/embutils/repo/version.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python
+# -*- coding: ascii -*-
+"""
+Version handler class.
+
+:date:      2021
+:author:    Christian Wiche
+:contact:   cwichel@gmail.com
+:license:   The MIT License (MIT)
+"""
+
+import datetime as dt
+import os
+import re
+
+import attr
+
+from ..utils.path import TPPath, as_path, path_validator
+from ..utils.subprocess import execute
+
+
+# -->> Definitions <<------------------
+#: Source path
+PATH_THIS = as_path(os.path.abspath(os.path.dirname(__file__)))
+#: TeMPLate path
+PATH_TMPL = PATH_THIS / "templates"
+
+
+# -->> API <<--------------------------
+@attr.s
+class Version:
+    """
+    Firmware version definition class.
+    """
+    #: Unversioned build number
+    UVER_BUILD = 99999
+    #: Version major
+    major: int = attr.ib(default=99, converter=int)
+    #: Version minor
+    minor: int = attr.ib(default=0, converter=int)
+    #: Version build
+    build: int = attr.ib(default=UVER_BUILD, converter=int)
+
+    def __str__(self) -> str:
+        """
+        Version as string: major.minor.build
+        """
+        return f"{self.major}.{self.minor}.{self.build}"
+
+    def update_build(self, path: TPPath) -> None:
+        """
+        Updates the build number for the current repo.
+
+        :param TPPath path: Path to repo.
+        """
+        # Unversioned: do nothing.
+        _ = path
+        self.build = self.UVER_BUILD
+
+    def save(self, path: TPPath, store_build: bool = False) -> None:
+        """
+        Stores the version number to the provided file.
+        The version number will be stored in the format "major.minor.build".
+
+        :param TPPath path:         Path were the version text file will be stored.
+        :param bool store_build:    If true, the build will be stored. False by default.
+        """
+        path = path_validator(path=path, allow_none=False, check_reachable=True)
+        with path.open(mode="w", encoding="utf-8") as file:
+            build = self.build if (store_build and self.build is not None) else "X"
+            file.write(f"{self.major}.{self.minor}.{build}")
+
+    @classmethod
+    def load(cls, path: TPPath) -> "Version":
+        """
+        Loads the version number from the provided file.
+        The version number needs to be in the format "major.minor.build".
+
+        :param TPPath path:         Path to the version file.
+        """
+        path = path_validator(path=path, allow_none=False, check_reachable=True)
+        with path.open(mode="r", encoding="utf-8") as ver_file:
+            tmp = ver_file.read().split(".")
+            ver = cls(
+                    major=int(tmp[0]),
+                    minor=int(tmp[1]),
+                    build=int(tmp[2]) if (tmp[2].upper() != "X") else Version.UVER_BUILD
+                    )
+            return ver
+
+
+@attr.s
+class VersionGit(Version):
+    """
+    Git version specialization.
+    """
+    def update_build(self, path: TPPath = os.getcwd()) -> None:
+        """
+        Updates the build number for the current repo.
+
+        :param TPPath path: Path to repository.
+        """
+        out = execute(cmd="git rev-parse --short HEAD", cwd=f"{path}", pipe=False)
+        msg = (out.stderr + out.stdout).strip().lower()
+        if "not a git" in msg:
+            self.build = self.UVER_BUILD
+        else:
+            self.build = int(msg, 16)
+
+
+@attr.s
+class VersionSVN(Version):
+    """
+    SVN version specialization.
+    """
+
+    def update_build(self, path: TPPath = os.getcwd()) -> None:
+        """
+        Updates the build number for the current repo.
+
+        :param TPPath path: Path to repository.
+        """
+        out = execute(cmd="svnversion .", cwd=f"{path}", pipe=False)
+        msg = (out.stderr + out.stdout).strip().lower()
+        if "unversioned directory" in msg:
+            self.build = self.UVER_BUILD
+        else:
+            tmp = re.findall(pattern=r"\d+", string=msg)
+            self.build = int(tmp[-1])
+
+
+def version_export_c(path: TPPath, ver: Version, author: str, note: str) -> None:
+    """
+    Exports the version to a C header file.
+
+    :param TPPath path:     Path to store the generated C header file.
+    :param Version ver:     Version instance.
+    :param str author:      Author to be declared on the header file.
+    :param str note:        Note to be added on the file documentation.
+
+    """
+    # Check target path
+    path = path_validator(path=path, allow_none=False, check_reachable=True)
+    tmpl = PATH_TMPL / "template_version_c.h"
+    base = tmpl.open(mode="r").read()
+    with path.open(mode="w", encoding="utf-8") as file:
+        file.write(base.format(
+            file=path.name, author=author, note=note, date=f"{dt.datetime.now():%x %X}",
+            major=ver.major, minor=ver.minor, build=f"0x{ver.build:X}",
+            version=f'"{ver.major}.{ver.minor}.{ver.build}"'
+            ))
---
+++ packages/embutils/embutils-0.6.1-py3-none-any.whl/embutils/utils/time.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: ascii -*-
+"""
+Timing utilities.
+
+:date:      2021
+:author:    Christian Wiche
+:contact:   cwichel@gmail.com
+:license:   The MIT License (MIT)
+"""
+
+import functools as fc
+import time
+import typing as tp
+
+from .common import TPAny, CBAny2Any
+
+
+# -->> Definitions <<------------------
+
+
+# -->> API <<--------------------------
+def timer(name: str = None, logger: CBAny2Any = print, precision: int = 6) -> tp.Callable[[CBAny2Any], CBAny2Any]:
+    """
+    Decorator.
+    Used to log the execution time of a function.
+
+    :param str name:            Timer name. Used to identify the measurement.
+    :param CBAny2Any logger:    Callable used to print the time logs. By default, system print.
+    :param float precision:     Number of decimals included on elapsed time.
+
+    :return: Decorated function wrapped on timer.
+    :rtype: Callable[[Callable[..., RT]], Callable[..., RT]]
+    """
+    def decorator(func: CBAny2Any) -> CBAny2Any:
+        @fc.wraps(func)
+        def wrapper(*args, **kwargs) -> TPAny:
+            start = time.time()
+            ret = func(*args, **kwargs)
+            total = elapsed(start)
+            if logger is not None:
+                logger(f"{name}: Execution time: {total:.{precision}f}[s]")
+            return ret
+        return wrapper
+    return decorator
+
+
+def elapsed(start: float) -> float:
+    """
+    Computes the elapsed time since the given timestamp.
+
+    :param float start: Start timestamp.
+
+    :returns: Time elapsed.
+    :rtype: float
+    """
+    return time.time() - start
