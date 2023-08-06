424621b8b87e3114cd814c10c8df0bd092577ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:47 +0000

    Add package python-framework/0.2.146/python_framework-0.2.146.tar.gz

---
+++ packages/python-framework/python_framework-0.2.146.tar.gz/python_framework-0.2.146/python_framework/api/src/util/UtcDateTimeUtil.py
@@ -0,0 +1,19 @@
+import datetime
+from python_helper import DateTimeHelper
+
+def now():
+    return datetime.datetime.utcnow()
+
+def timestamp():
+    return datetime.datetime.timestamp()
+
+def ofTimestamp(t):
+    return datetime.datetime.utcfromtimestamp(t)
+
+def ofPlainTimestamp(t):
+    return datetime.datetime.fromtimestamp(t)
+
+def plusMinutes(givenDateTime, minutes=None, deltaInMinutes=None) :
+    if ObjectHelper.isNotNone(minutes) :
+        deltaInMinutes = DateTimeHelper.timeDelta(seconds=minutes*60)
+    return givenDateTime + deltaInMinutes
