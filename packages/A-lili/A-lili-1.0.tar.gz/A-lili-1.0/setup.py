4a9e24884e2ef208550d15c9153d69970d98c026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:29 +0000

    Add package A-lili/1.0/A-lili-1.0.tar.gz

---
+++ packages/A-lili/A-lili-1.0.tar.gz/A-lili-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+      name = "A-lili",
+      version="1.0",
+      py_modules=["fc_xd_bigdate"],
+      author="大数据3",
+      author_email ="fc@bigdate.com",
+      description="大数据3"
+
+
+)
