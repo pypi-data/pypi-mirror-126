f61af9c67fc2a470bc345e7cea39586c66558a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:22 +0000

    Add package viecpro-vis/0.1.3/viecpro_vis-0.1.3.tar.gz

---
+++ packages/viecpro-vis/viecpro_vis-0.1.3.tar.gz/viecpro_vis-0.1.3/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['viecpro_vis', 'viecpro_vis.migrations']
+
+package_data = \
+{'': ['*'],
+ 'viecpro_vis': ['static/vis/css/*', 'static/vis/js/*', 'templates/*']}
+
+install_requires = \
+['apis-core>0.16.33,<=0.17.0']
+
+setup_kwargs = {
+    'name': 'viecpro-vis',
+    'version': '0.1.3',
+    'description': 'Django-App providing visualisations for the APIS-VieCPro instance.',
+    'long_description': None,
+    'author': 'Gregor Pirgie',
+    'author_email': 'gregor.pirgie@univie.ac.at',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<3.9',
+}
+
+
+setup(**setup_kwargs)
