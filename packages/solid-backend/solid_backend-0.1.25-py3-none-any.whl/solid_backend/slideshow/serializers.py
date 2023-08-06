e682173dcc9d181b533feeea7c03ded661a5a052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:51 +0000

    Add package solid-backend/0.1.25/solid_backend-0.1.25-py3-none-any.whl

---
+++ packages/solid-backend/solid_backend-0.1.25-py3-none-any.whl/solid_backend/slideshow/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from taggit.models import Tag
+from taggit.serializers import TagListSerializerField
+from solid_backend.photograph.serializers import PhotographSerializer
+from solid_backend.utils.serializers import DynamicExcludeModelSerializer
+
+from .models import Slideshow, SlideshowImage, SlideshowPage
+
+
+class SlideshowImageSerializer(DynamicExcludeModelSerializer):
+    image = PhotographSerializer()
+
+    class Meta:
+        model = SlideshowImage
+        fields = "__all__"
+
+
+class SlideshowPageSerializer(DynamicExcludeModelSerializer):
+    images = SlideshowImageSerializer(exclude="page", many=True, required=False)
+
+    class Meta:
+        model = SlideshowPage
+        fields = "__all__"
+
+
+class SlideshowSerializer(serializers.ModelSerializer):
+    title_image = PhotographSerializer(required=False)
+    pages = SlideshowPageSerializer(exclude="show", many=True, required=False)
+    categories = TagListSerializerField()
+
+    class Meta:
+        model = Slideshow
+        exclude = ["active"]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
---
+++ packages/solid-backend/solid_backend-0.1.25-py3-none-any.whl/solid_backend/slideshow/views.py
@@ -0,0 +1,72 @@
+from django.db.models import Prefetch
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from taggit.models import Tag
+
+from .models import Slideshow, SlideshowImage, SlideshowPage
+from .serializers import (
+    SlideshowImageSerializer,
+    SlideshowPageSerializer,
+    SlideshowSerializer,
+    CategorySerializer
+)
+
+
+class SlideshowEndpoint(ReadOnlyModelViewSet):
+    """
+    Endpoint that provides the database table of all active slideshows including their related pages with their related images.
+    """
+
+    queryset = (
+        Slideshow.objects.filter(active=True)
+        .order_by("position")
+        .prefetch_related(
+            Prefetch(
+                "pages",
+                queryset=SlideshowPage.objects.order_by("position").prefetch_related(
+                    Prefetch(
+                        "images", queryset=SlideshowImage.objects.order_by("position")
+                    )
+                ),
+            )
+        )
+    )
+
+    def get_queryset(self):
+        queryset = super(SlideshowEndpoint, self).get_queryset()
+        category = self.request.query_params.get("categories")
+
+        if category is not None:
+            queryset = queryset.filter(categories=category)
+
+        return queryset
+
+    serializer_class = SlideshowSerializer
+    name = "slideshow"
+
+
+class SlideshowPageEndpoint(ReadOnlyModelViewSet):
+    """
+    Endpoint that provides the database table of all slideshow pages including their related images.
+    """
+
+    queryset = SlideshowPage.objects.order_by("show", "position").prefetch_related(
+        Prefetch("images", queryset=SlideshowImage.objects.order_by("position"))
+    )
+    serializer_class = SlideshowPageSerializer
+    name = "slideshowpage"
+
+
+class SlideshowImageEndpoint(ReadOnlyModelViewSet):
+    """
+    Endpoint that provides the database table of all slideshow images.
+    """
+
+    queryset = SlideshowImage.objects.order_by("page", "position")
+    serializer_class = SlideshowImageSerializer
+    name = "slideshowimage"
+
+
+class CategoryEndpoint(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = CategorySerializer
+    name = "category"
