5090daf8ea1c36dbbf3c77d349b30ed1001b2c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:57 +0000

    Add package league-of-legends-ml/0.0.6/league_of_legends_ml-0.0.6-py3-none-any.whl

---
+++ packages/league-of-legends-ml/league_of_legends_ml-0.0.6-py3-none-any.whl/league_of_legends_ml/mastery/__init__.py
@@ -0,0 +1,37 @@
+from typing import Dict, Tuple
+import importlib.resources as pkg_resources
+from ..common import modes, CLASSIC
+from . import files
+import pickle
+import numpy
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+
+__models: Dict[str, MLPRegressor] = {}
+__scalers: Dict[str, StandardScaler] = {}
+
+
+def __get_model(mode: str = CLASSIC) -> Tuple[MLPRegressor, StandardScaler]:
+    mode = mode.lower()
+    if mode not in modes:
+        raise ValueError('Undefined mode for model')
+    with pkg_resources.open_binary(files, f'{mode}_model.pkl') as file:
+        model = pickle.load(file)
+    with pkg_resources.open_binary(files, f'{mode}_scaler.pkl') as file:
+        scaler = pickle.load(file)
+    # with pkg_resources.path(files, f'{mode.lower()}_model.pkl') as path:
+    #     async with aiofiles.open(path, 'rb') as file:
+    #         model = pickle.load(await file.read())
+    # with pkg_resources.open_binary(files, f'{mode.lower()}_scaler.pkl') as file:
+    #     scaler = pickle.load(file)
+    return model, scaler
+
+
+def predict(duration_seconds: int, victory: bool, mode: str = CLASSIC) -> int:
+    if mode not in __models or mode not in __scalers:
+        __models[mode], __scalers[mode] = __get_model(mode)
+    model, scaler = __models[mode], __scalers[mode]
+    data = numpy.array([[duration_seconds, int(victory)]])
+    scaled_data = scaler.transform(data)
+    result = model.predict(scaled_data)
+    return round(result[0])
