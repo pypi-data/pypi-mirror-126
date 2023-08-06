06abdb8fa552fa7482a70666c2d1d44ad56dcce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:38 +0000

    Add package svector/0.1.0/svector-0.1.0-py3-none-any.whl

---
+++ packages/svector/svector-0.1.0-py3-none-any.whl/svector/type_definitions.py
@@ -0,0 +1,24 @@
+from typing import TypeVar, Hashable, Protocol
+
+A_co = TypeVar("A_co", covariant=True)
+B = TypeVar("B")
+CanCompare = TypeVar("CanCompare", bound="Comparable")
+CanHash = TypeVar("CanHash", bound=Hashable)
+
+
+class Comparable(Protocol):
+    def __lt__(self: CanCompare, other: CanCompare) -> bool:
+        pass
+
+    def __gt__(self: CanCompare, other: CanCompare) -> bool:
+        pass
+
+    def __le__(self: CanCompare, other: CanCompare) -> bool:
+        pass
+
+    def __ge__(self: CanCompare, other: CanCompare) -> bool:
+        pass
+
+
+def identity(item: B) -> B:
+    return item
