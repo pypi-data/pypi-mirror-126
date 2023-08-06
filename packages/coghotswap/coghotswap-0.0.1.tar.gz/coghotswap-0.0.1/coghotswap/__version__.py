8991bf52719f1c1dcb4a44c7b07b82f41589fd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:07 +0000

    Add package coghotswap/0.0.1/coghotswap-0.0.1.tar.gz

---
+++ packages/coghotswap/coghotswap-0.0.1.tar.gz/coghotswap-0.0.1/coghotswap/__version__.py
@@ -0,0 +1,14 @@
+
+"""
+__version__.py
+~~~~~~~~~~~~~~
+Information about the current version of the coghotswap package.
+"""
+
+__title__ = 'coghotswap'
+__description__ = 'Allow hotswaping of extensions for pycord a fork of discord.py cogs'
+__version__ = '0.0.1'
+__author__ = 'TropicalShadow'
+__author_email__ = 'me@tropicalshadow.tech'
+__license__ = 'The MIT License (MIT)'
+__url__ = 'https://github.com/TropicalShadow/coghotswap'
