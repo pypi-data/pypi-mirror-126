5d3ac3029063f37a08987c4f61ffb7696c43eabe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:14 +0000

    Add package googlelyricsAPI/1.0/googlelyricsAPI-1.0.0-py3-none-any.whl

---
+++ packages/googlelyricsAPI/googlelyricsAPI-1.0.0-py3-none-any.whl/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='googlelyricsAPI',
+    version='1.0.0',
+    packages=[''],
+    url='https://www.erendarici.rocks',
+    license='',
+    author='Eren Darıcı',
+    author_email='erendarici@eskisehir.edu.tr',
+    description='a basic lyrics API that scrapes the songs from Google.'
+)
