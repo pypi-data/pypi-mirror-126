52830f1909d91321fe594509f98e7390a3f30139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:09 +0000

    Add package fc-bigdata/1.0/fc_bigdata-1.0.tar.gz

---
+++ packages/fc-bigdata/fc_bigdata-1.0.tar.gz/fc_bigdata-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="fc_bigdata",
+    version="1.0",
+    py_modules=["fc_xd_bigdata"],
+    author="大数据",
+    author_email="fc@bigdata.com",
+    description="大数据"
+)
