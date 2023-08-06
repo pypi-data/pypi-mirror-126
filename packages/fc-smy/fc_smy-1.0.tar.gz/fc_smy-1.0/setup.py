1206deadf17e627e3a857929887a03d6d9dfea6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:37 +0000

    Add package fc-smy/1.0/fc_smy-1.0.tar.gz

---
+++ packages/fc-smy/fc_smy-1.0.tar.gz/fc_smy-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name  = "fc_smy",
+    version = "1.0",
+    py_modules = ["fc_smy_bigdata"],
+    author = "计算机3班",
+    author_email = "fc@bigdata.com",
+    description = "计算机3班"
+)
