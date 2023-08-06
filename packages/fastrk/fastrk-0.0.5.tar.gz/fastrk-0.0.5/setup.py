2960ab6f7f18df26bba2ad67849a448ce9051977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:35 +0000

    Add package fastrk/0.0.5/fastrk-0.0.5.tar.gz

---
+++ packages/fastrk/fastrk-0.0.5.tar.gz/fastrk-0.0.5/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastrk",
+    version="0.0.5",
+    author="Bober S.A.",
+    author_email="stas.bober@gmail.com",
+    description=\
+        "FastRK, a generator of fast jit-compiled code for ODE propagation by ERK methods with adaptive step and events",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BoberSA/fastrk",
+    packages=setuptools.find_packages(),
+    # package_data={
+    #         'fastrk': ['_events'],
+    # },
+    license='MIT',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numba>=0.51.2',
+                      'numpy>=1.19.2',
+                      'sympy>=1.8',
+                      'scipy>=1.2'
+                      ],
+    python_requires='>=3.7',
+)
