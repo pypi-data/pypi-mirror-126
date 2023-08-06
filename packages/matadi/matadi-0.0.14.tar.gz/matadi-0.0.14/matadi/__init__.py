37c99ac849237d4983ee76d6c997dd68dc9d30f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:34 +0000

    Add package matadi/0.0.14/matadi-0.0.14.tar.gz

---
+++ packages/matadi/matadi-0.0.14.tar.gz/matadi-0.0.14/matadi/__init__.py
@@ -0,0 +1,9 @@
+import casadi
+
+Variable = casadi.SX.sym
+
+from ._material import Material
+from . import models
+from . import math
+from ._templates import ThreeFieldVariation, MaterialHyperelastic
+from ._lab import Lab
