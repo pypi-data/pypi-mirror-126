dab0b7432849076cfac9c6313a3eff1f26ade16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:34 +0000

    Add package google-trends/1.1/google_trends-1.1.tar.gz

---
+++ packages/google-trends/google_trends-1.1.tar.gz/google_trends-1.1/README.md
@@ -0,0 +1,64 @@
+[![Python >= 3.6](https://img.shields.io/badge/python->=3.6-red.svg)](https://www.python.org/downloads/) [![](https://badgen.net/github/release/deedy5/google_trends)](https://github.com/deedy5/google_trends/releases) [![](https://badge.fury.io/py/google-trends.svg)](https://pypi.org/project/google_trends)
+## google_trends
+
+Simple Google Trends API
+
+
+### Install
+
+```python3
+pip install -U google_trends
+```
+
+### Usage:
+*Warning: gtrends renamed to daily_trends*
+```python3
+from google_trends import daily_trends, realtime_trends
+```
+**1. Google daily search trends**
+```python3
+daily_trends(date=None, country='US', language='en-US', timezone='-180')
+    ''' Google daily search trends
+    
+    date = YYYYMMDD (example: 20210810) - trends on a given date, interval: today - 30 days ago;
+    country = 'US', 'RU', etc.;
+    language = 'en-US', 'ru-RU', etc.;
+    timezone = timezone offset, example: GMT-7 == -7*60 = '-420'.
+    '''
+
+# trends today
+today_trends = daily_trends(country='GB')
+print(today_trends)
+
+#trends on a given date, interval: today - 30 days ago.
+date_trends = daily_trends(20210810)
+print(date_trends)
+```
+---
+**2. Google realtime search trends**
+
+***Attention!*** *Google realtime trends are not available for all regions. The list of available countries can be viewed on the website: https://trends.google.com/trends/trendingsearches/realtime.*
+```python3
+realtime_trends(country='US', language='en-US', category='all', num_results=20, timezone='-180')
+    ''' Google realtime search trends
+
+    country = 'US', 'RU', etc.;
+    category = 'all' (all), 'b' (business), 'e' (entertainment), 
+               'm' (health), 's' (sports), 't' (sci/tech), 'h' (top stories);
+    language = 'en-US', 'ru-RU', etc.;
+    num_results = how many results to return, max num_results = 200;
+    timezone = timezone offset, example: GMT-7 == -7*60 = '-420'.
+    '''
+ 
+real_trends = realtime_trends(country='DE', category='t', language='en-UK', num_results=2)
+print(real_trends)
+
+[
+{'title': 'Sony, Funimation, PlayStation Network, Anime, Aniplex', 
+'entity_names': ['Sony', 'Funimation', 'PlayStation Network', 'Anime', 'Aniplex'], 
+'article_urls': ['https://www.gamepro.de/artikel/sony-crunchyroll-ps-plus-premium-abo,3372448.html', 'https://www.play3.de/2021/08/11/playstation-plus-premium-variante-mit-crunchyroll/', 'https://www.pcgames.de/PlayStation-Plus-Thema-260472/News/geruechte-um-premium-abo-mit-anime-inhalten-1377545/', 'https://stadt-bremerhaven.de/crunchyroll-uebernahme-durch-sony-abgeschlossen/', 'https://playfront.de/playstation-plus-premium-arbeitet-sony-an-einem-neuen-abo/', 'https://www.netzwelt.de/news/192097-milliardendeal-sony-uebernimmt-crunchyroll-bald-nur-noch-ultimativer-anime-streamingdienst.html', 'https://www.pattotv.de/animenews/sonys-funimation-global-group-schliesst-uebernahme-von-crunchyroll-ab/', 'https://shonakid.de/milliardendeal-sony-schliesst-ubernahme-von-crunchyroll-ab-und-jetzt-63014/', 'https://www.playcentral.de/anime-anbieter-crunchyroll-funimation-fusionieren/', 'https://game7.de/playstation-network/news/premium-version-plus-geplant-kkm/']}, 
+{'title': 'Diablo II, Blizzard Entertainment, Xbox One', 
+'entity_names': ['Diablo II', 'Blizzard Entertainment', 'Xbox One'], 
+'article_urls': ['https://www.giga.de/news/diablo-2-resurrected-blizzard-laesst-euch-kostenlos-reinschnuppern-so-gehts/', 'https://www.heise.de/news/Diablo-2-Resurrected-Offene-Beta-beginnt-am-20-August-6160795.html', 'https://www.tz.de/leben/games/diablo-resurrected-2-beta-starttermin-fuer-early-access-steht-fest-blizzard-games-zr-90914078.html', 'https://www.golem.de/news/resurrected-alte-savegames-muessen-bei-beta-von-diablo-2-noch-warten-2108-158820.html', 'https://www.netzwelt.de/news/192079-diablo-2-resurrected-blizzard-kuendigt-beta-start-pc-konsolen.html', 'https://www.gamepro.de/artikel/diablo-2-resurrected-starttermin-beta-offiziell,3372455.html', 'https://www.spieletipps.de/n_49329/', 'https://winfuture.de/news,124553.html', 'https://www.eurogamer.de/articles/2021-08-11-diablo-2-resurrected-offizielle-startzeiten-der-beta-bestaetigt', 'https://www.pcgameshardware.de/Diablo-2-Resurrected-Spiel-73212/News/Open-Beta-Starttermin-bekannt-gegeben-1377542/']},
+]
+```
---
+++ packages/google-trends/google_trends-1.1.tar.gz/google_trends-1.1/google_trends.py
@@ -0,0 +1,129 @@
+import json
+import requests
+
+__version__ = '1.1'
+
+def gtrends(*args, **kwargs):
+    print("Function 'gtrends' is deprecated: renamed to 'daily_trends'")
+    return daily_trends(*args, **kwargs)
+
+def daily_trends(date=None, country='US', language='en-US', timezone='-180'):
+    ''' Google daily search trends
+    date = YYYYMMDD, example: 20210810, trends on a given date, interval: today - 30 days ago;
+    country = 'US', 'RU', etc.;
+    language = 'en-US', 'ru-RU', etc.;
+    timezone = timezone offset, example: GMT-7 == -7*60 = '-420'.
+    '''    
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
+               "Accept": "application/json, text/plain, */*",
+               "Origin": "https://trends.google.com",
+               "DNT": "1",
+               "Connection": "keep-alive",
+               "Referer": f"https://trends.google.com/trends/trendingsearches/daily?geo={country}",
+               "Cache-Control": "max-age=0",
+               "TE": "Trailers",}
+    params = {
+        'hl': language,
+        'tz': timezone,
+        'ed': date,
+        'geo': country,
+        'ns': '15',
+        }    
+    r = requests.get("https://trends.google.com/trends/api/dailytrends", headers=headers, params=params)
+    i = r.text.index('{')
+    r = r.text[i:]
+    r = json.loads(r)    
+    r = r['default']['trendingSearchesDays'][0]
+    data = r['trendingSearches']
+    res = []
+    for element in data:
+        t = element['title']['query']
+        res.append(t)  
+    return res
+
+def realtime_trends(country='US', category='all', language='en-US', num_results=20, timezone='-180'):
+    ''' Google realtime search trends    
+    country = 'US', 'RU', etc.;
+    category = 'all' (all), 'b' (business), 'e' (entertainment), 
+               'm' (health), 's' (sports), 't' (sci/tech), 'h' (top stories);
+    language = 'en-US', 'ru-RU', etc.;
+    num_results = how many results to return, max num_results = 200;
+    timezone = timezone offset, example: GMT-7 == -7*60 = '-420'.
+    '''
+    with requests.Session() as s:
+        s.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
+                          "Accept": "application/json, text/plain, */*",
+                          "DNT": "1",
+                          "Connection": "keep-alive",
+                          "Referer": f"https://trends.google.com/trends/trendingsearches/realtime?geo={country}&category={category}",})
+        params = {
+            'hl': language,
+            'tz': timezone,
+            'cat': category,
+            'fi': '0',
+            'fs': '0',
+            'geo': country,
+            'ri': '300',
+            'rs': f'{num_results}',
+            'sort': '0',
+            }
+        # request json with real time trends
+        r = s.get('https://trends.google.com/trends/api/realtimetrends', params=params)
+        # fix the json and load it as a dictionary
+        i = r.text.index('{')
+        r = r.text[i:]
+        r = json.loads(r)
+        # extract information from json
+        data = r["storySummaries"]["trendingStories"]
+        res, ids_cache = [], set()
+        for element in data:
+            res.append({"title": element["title"],
+                        "entity_names": element["entityNames"],
+                        "article_urls": [x["url"] for x in element["articles"]],})
+            ids_cache.add(element["id"])
+
+        # if not enough results, request additional jsons by trending_story_ids
+        trending_story_ids = r["trendingStoryIds"]
+        while len(res) < num_results:
+            id_list = []
+            for trending_story_id in trending_story_ids:
+                if trending_story_id not in ids_cache:
+                    id_list.append(trending_story_id)
+                if len(id_list) == 20:
+                    break
+            if not id_list:
+                break
+            ids_cache.update(id_list)
+            params = {
+                'hl': language,
+                'tz': timezone,
+                'cat': category,
+                'id': id_list,
+                }
+            r = s.get('https://trends.google.com/trends/api/stories/summary', params=params)
+            i = r.text.index('{')
+            r = r.text[i:]
+            r = json.loads(r)
+            data = r["trendingStories"]
+            for element in data:
+                res.append({"title": element["title"],
+                            "entity_names": element["entityNames"],
+                            "article_urls": [x["url"] for x in element["articles"]],})
+                ids_cache.add(element["id"])
+
+        ''' ----- Another way, if not enough results -----
+        # request additional jsons per each trending_story_id
+        trending_story_ids = r["trendingStoryIds"]
+        i = 0
+        while len(res) < num_results and len(res) < len(trending_story_ids):
+            if trending_story_ids[i] not in ids_cache:
+                r = s.get(f'https://trends.google.com/trends/api/stories/{trending_story_ids[i]}?hl={language}&tz={timezone}')
+                i = r.text.index('{')
+                r = r.text[i:]
+                r = json.loads(r)
+                res.append({"title": r["title"],
+                            "entity_names": r["entityNames"],
+                            "article_urls": [x["url"] for x in r["widgets"][0]["articles"]],})
+            i += 1
+        '''
+    return res
