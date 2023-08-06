4e8a8d91c8386e73148f544064e7fc6926a007f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:18 +0000

    Add package everysk/1.0.10/everysk-1.0.10.tar.gz

---
+++ packages/everysk/everysk-1.0.10.tar.gz/everysk-1.0.10/everysk/api_resources/workflow_execution.py
@@ -0,0 +1,24 @@
+###############################################################################
+#
+# (C) Copyright 2020 EVERYSK TECHNOLOGIES
+#
+# This is an unpublished work containing confidential and proprietary
+# information of EVERYSK TECHNOLOGIES. Disclosure, use, or reproduction
+# without authorization of EVERYSK TECHNOLOGIES is prohibited.
+#
+###############################################################################
+from everysk.api_resources.api_resource import (
+    RetrievableAPIResource,
+    ListableAPIResource,
+    DeletableAPIResource
+)
+
+class WorkflowExecution(
+    RetrievableAPIResource,
+    ListableAPIResource,
+    DeletableAPIResource
+):
+    @classmethod
+    def class_name(cls):
+        return 'workflow_execution'
+
