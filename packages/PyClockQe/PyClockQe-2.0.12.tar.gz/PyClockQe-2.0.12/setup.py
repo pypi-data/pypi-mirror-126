54032a7c0b441287d65ed2c29df7a890b4ab69f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:46 +0000

    Add package PyClockQe/2.0.12/PyClockQe-2.0.12.tar.gz

---
+++ packages/PyClockQe/PyClockQe-2.0.12.tar.gz/PyClockQe-2.0.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+  name='PyClockQe',
+  version='2.0.12',
+  description='A python clock',
+  long_description="A advanced python clock configuration & page",
+  author='TEnLOcODE',
+  author_email='tantan.bil.2012@gmail.com',
+  keywords='pyclock,python clock, clockq, pyclockqe', 
+  packages=find_packages(),
+  install_requires=[],
+  python_requires='>=3.6',
+  classifiers=[
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+  ]
+)
