7135f20aeb714ad25e6a12f8f1b6e686604ed4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:12 +0000

    Add package worf/0.4.6/worf-0.4.6.tar.gz

---
+++ packages/worf/worf-0.4.6.tar.gz/worf-0.4.6/worf/serializers.py
@@ -0,0 +1,37 @@
+import json
+
+from django.core.exceptions import ImproperlyConfigured
+
+
+def deserialize(response):
+    return json.loads(response.content.decode("UTF-8"))
+
+
+class Serializer:
+    def create(self):
+        return []
+
+    def list(self, items):
+        return [self.read(item) for item in items]
+
+    def read(self, model):
+        return {}
+
+    def write(self):
+        return []
+
+
+class LegacySerializer(Serializer):
+    def __init__(self, model_class, api_method):
+        self.api_method = api_method
+        self.model_class = model_class
+
+    def read(self, model):
+        payload = getattr(model, self.api_method)()
+        if not isinstance(payload, dict):
+            msg = f"{model.__name__}.{self.api_method}() did not return a dictionary"
+            raise ImproperlyConfigured(msg)
+        return payload
+
+    def write(self):
+        return getattr(self.model_class(), f"{self.api_method}_update_fields")()
