b68182324d4fd1283b26bdb268c3f5947ef45c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:15 +0000

    Add package weeb-fast-hist-conflict/1.1.0/weeb_fast_hist_conflict-1.1.0.tar.gz

---
+++ packages/weeb-fast-hist-conflict/weeb_fast_hist_conflict-1.1.0.tar.gz/weeb_fast_hist_conflict-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='weeb_fast_hist_conflict',
+    version='1.1.0',    
+    description='A example Python package for itmo da course',
+    url='',
+    author='Garanin Artem',
+    author_email='',
+    packages=['weeb_fast_hist'],
+    install_requires=['matplotlib>3.0.0,<3.2.0'
+                      ]
+)    
