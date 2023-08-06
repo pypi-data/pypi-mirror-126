6e1f80c81224d2bb4efbe79c26572e49004cb691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:47 +0000

    Add package nq-fir/1.0/nq_fir-1.0.tar.gz

---
+++ packages/nq-fir/nq_fir-1.0.tar.gz/nq_fir-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "nq_fir",
+    version = "1.0",
+    py_modules = ["fc_Oct_shit"],
+    author = "HONOR",
+    author_email = "f81471716@163.com",
+    description = "大数据1"
+)
