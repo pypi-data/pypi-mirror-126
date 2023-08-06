c5f26154cc480a6223f47390fb82933194befdef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:05 +0000

    Add package fc-xz01/1.0/fc_xz01-1.0.tar.gz

---
+++ packages/fc-xz01/fc_xz01-1.0.tar.gz/fc_xz01-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name = "fc_xz01",
+    version = "1.0",
+    py_modules = ["fc_xz_xz01"],
+    author = "大数据1",
+    author_email = "fc@xz.com",
+    description = "大数据1"
+)
