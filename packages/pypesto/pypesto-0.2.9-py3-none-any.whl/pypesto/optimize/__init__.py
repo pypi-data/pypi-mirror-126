37ccc521ac627862f089a3c2a6ef7a8b1cc4336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:08 +0000

    Add package pypesto/0.2.9/pypesto-0.2.9-py3-none-any.whl

---
+++ packages/pypesto/pypesto-0.2.9-py3-none-any.whl/pypesto/optimize/__init__.py
@@ -0,0 +1,25 @@
+# noqa: D400,D205
+"""
+Optimize
+========
+
+Multistart optimization with support for various optimizers.
+"""
+
+from .options import OptimizeOptions
+from .optimize import (
+    minimize)
+from .optimizer import (
+    Optimizer,
+    ScipyOptimizer,
+    IpoptOptimizer,
+    DlibOptimizer,
+    PyswarmOptimizer,
+    CmaesOptimizer,
+    PyswarmsOptimizer,
+    ScipyDifferentialEvolutionOptimizer,
+    NLoptOptimizer,
+    FidesOptimizer,
+    read_result_from_file,
+)
+from .result import OptimizerResult
---
+++ packages/pypesto/pypesto-0.2.9-py3-none-any.whl/pypesto/startpoint/uniform.py
@@ -0,0 +1,61 @@
+"""Uniform sampling."""
+
+import numpy as np
+
+from .util import rescale
+from .base import StartpointMethod
+from ..objective import ObjectiveBase
+
+
+def _uniform(
+    n_starts: int,
+    lb: np.ndarray,
+    ub: np.ndarray,
+) -> np.ndarray:
+    """Generate uniform points.
+
+    Parameters
+    ----------
+    n_starts: Number of starts.
+    lb: Lower bound.
+    ub: Upper bound.
+
+    Returns
+    -------
+    xs: Uniformly sampled points in [lb, ub], shape (n_starts, n_x).
+    """
+    if not np.isfinite(ub).all() or not np.isfinite(lb).all():
+        raise ValueError(
+            "Cannot use uniform startpoint method with non-finite boundaries.",
+        )
+
+    # parse
+    dim = lb.size
+    lb = lb.reshape((1, -1))
+    ub = ub.reshape((1, -1))
+
+    # create uniform points in [0, 1]
+    xs = np.random.random((n_starts, dim))
+
+    # re-scale
+    xs = rescale(xs, lb, ub)
+
+    return xs
+
+
+class UniformStartpoints(StartpointMethod):
+    """Generate uniformly sampled startpoints."""
+
+    def __call__(
+        self,
+        n_starts: int,
+        lb: np.ndarray,
+        ub: np.ndarray,
+        objective: ObjectiveBase = None,
+    ) -> np.ndarray:
+        """Call function."""
+        return _uniform(n_starts=n_starts, lb=lb, ub=ub)
+
+
+# convenience and legacy
+uniform = UniformStartpoints()
