87f617195f1b7d97811031715e043f730dc340fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:17 +0000

    Add package fc-bigdate3/1.0/fc_bigdate3-1.0.tar.gz

---
+++ packages/fc-bigdate3/fc_bigdate3-1.0.tar.gz/fc_bigdate3-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+            name="fc_bigdate3",
+            version="1.0",
+            py_modules=["fc_xd_bigdate3"],
+            author="大数据3",
+            author_email="fc@bigdate.com",
+            description="大数据3"
+)
