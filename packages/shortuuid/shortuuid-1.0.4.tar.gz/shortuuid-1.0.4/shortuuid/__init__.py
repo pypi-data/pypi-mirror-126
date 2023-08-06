4b60cc7df935d0dcea1e7ff7213ddc2c88e1b68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:13 +0000

    Add package shortuuid/1.0.4/shortuuid-1.0.4.tar.gz

---
+++ packages/shortuuid/shortuuid-1.0.4.tar.gz/shortuuid-1.0.4/shortuuid/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+import pkg_resources
+
+from shortuuid.main import decode
+from shortuuid.main import encode
+from shortuuid.main import get_alphabet
+from shortuuid.main import random
+from shortuuid.main import set_alphabet
+from shortuuid.main import ShortUUID
+from shortuuid.main import uuid
+
+try:
+    __version__ = pkg_resources.get_distribution("shortuuid").version
+except pkg_resources.DistributionNotFound:
+    __version__ = "0.0.0"
