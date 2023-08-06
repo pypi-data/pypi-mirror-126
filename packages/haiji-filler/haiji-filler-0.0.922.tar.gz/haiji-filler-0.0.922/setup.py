f1a13c58f908cbd70a2dac98b44daacd02cdc220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:06 +0000

    Add package haiji-filler/0.0.922/haiji-filler-0.0.922.tar.gz

---
+++ packages/haiji-filler/haiji-filler-0.0.922.tar.gz/haiji-filler-0.0.922/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+
+def main():
+    description = 'Just for test'
+    setup(
+        # package name(editable)
+        # Note : name might end such as 'filler'
+        name='haiji-filler',
+        #name='pokemon-filler',
+        # version(editable)
+        version='0.0.922',
+        # entry point(editable)
+        entry_points = {
+            'console_scripts':
+            ['haiji_filler = haiji_filler.main:main'],
+                        },
+        #--- in this case, no need to edit below ---#
+        classifiers=['Programming Language :: Python :: 3.9', ],
+        author='iharuki',
+        author_email='iharuki@student.42tokyo.jp',
+        url='https://pypi.org/project/haiji-filler/',
+        description=description,
+        long_description=description,
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=[],
+        tests_require=[],
+        setup_requires=[],
+        )
+
+if __name__ == '__main__':
+    main()
