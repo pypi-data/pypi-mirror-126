bb1e48fe8cad0afb33516036d0895e2f9ff04ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:20 +0000

    Add package pretix-signature-question/1.0.1/pretix_signature_question-1.0.1-py3-none-any.whl

---
+++ packages/pretix-signature-question/pretix_signature_question-1.0.1-py3-none-any.whl/pretix_signature_question/__init__.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+__version__ = "1.0.1"
+
+
+class PluginApp(PluginConfig):
+    name = "pretix_signature_question"
+    verbose_name = "Signature Question"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("Signature Question")
+        author = "pretix"
+        description = gettext_lazy("Allows to draw a signature")
+        visible = True
+        version = __version__
+        category = "CUSTOMIZATION"
+        compatibility = "pretix>=2.7.0"
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = "pretix_signature_question.PluginApp"
