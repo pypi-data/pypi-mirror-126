416f812dc72170ff9a749be28b3f74f974815f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:51 +0000

    Add package tasomasa-filler/0.1.1/tasomasa_filler-0.1.1.tar.gz

---
+++ packages/tasomasa-filler/tasomasa_filler-0.1.1.tar.gz/tasomasa_filler-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+        name="tasomasa_filler",
+
+        version="0.1.1",
+
+        author="yasak",
+
+        author_email="yamato02031996@gmail.com",
+
+        description="my filler",
+
+        long_description=long_description,
+
+        long_description_content_type="text/markdown",
+
+        packages=setuptools.find_packages(),
+
+        license="42Tokyo_piscine101",
+
+        python_requires='>=3.7',
+
+        )
