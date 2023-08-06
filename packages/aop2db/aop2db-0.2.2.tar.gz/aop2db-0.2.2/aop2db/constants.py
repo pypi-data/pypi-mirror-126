c6abf1f82dc263d13497a42715a302580edbe063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:14 +0000

    Add package aop2db/0.2.2/aop2db-0.2.2.tar.gz

---
+++ packages/aop2db/aop2db-0.2.2.tar.gz/aop2db-0.2.2/aop2db/constants.py
@@ -0,0 +1,22 @@
+"""String definitions."""
+
+DATABASE = "DATABASE"
+
+ID = "@id"
+AOP_ID = "aop_id"
+APPLICABILITY = "applicability"
+TAXONOMY = "taxonomy"
+EVIDENCE = "evidence"
+SEX = "sex"
+LIFESTAGE = "life-stage"
+LAST_MODIFIED = "last_modified"
+LM_TIMESTAMP = "last-modification-timestamp"
+CREATION = "creation"
+CREATION_TIMESTAMP = "creation-timestamp"
+REFERENCES = "references"
+
+# API
+TAX_ID_LOOKUP = "https://rest.ensembl.org/taxonomy/id/{}?content-type=application/json"
+
+# AOP Download
+AOP_XML_DOWNLOAD = "https://aopwiki.org/downloads/aop-wiki-xml-2021-07-01.gz"
