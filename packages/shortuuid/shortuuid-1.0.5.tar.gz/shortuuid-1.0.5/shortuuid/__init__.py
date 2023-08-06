812ba60aec117ff84bc6785e1e5d68d1de91ff5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:57 +0000

    Add package shortuuid/1.0.5/shortuuid-1.0.5.tar.gz

---
+++ packages/shortuuid/shortuuid-1.0.5.tar.gz/shortuuid-1.0.5/shortuuid/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+import importlib.metadata
+
+from shortuuid.main import decode
+from shortuuid.main import encode
+from shortuuid.main import get_alphabet
+from shortuuid.main import random
+from shortuuid.main import set_alphabet
+from shortuuid.main import ShortUUID
+from shortuuid.main import uuid
+
+__version__ = "1.0.5"
