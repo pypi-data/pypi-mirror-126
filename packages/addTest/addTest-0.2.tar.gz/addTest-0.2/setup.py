0715e59571f2d16300db6262c3ed3430c0942c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:39 +0000

    Add package addTest/0.2/addTest-0.2.tar.gz

---
+++ packages/addTest/addTest-0.2.tar.gz/addTest-0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="addTest",
+    version="0.2",
+    license="MIT Licence",
+
+    url="https://github.com/lwxGitHub123/Test",
+    author="liudongbo",
+    author_email="350272438@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
