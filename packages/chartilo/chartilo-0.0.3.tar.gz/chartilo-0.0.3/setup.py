1b3f3a23ff38fc07d308bb17e27324d6375f6b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:40 +0000

    Add package chartilo/0.0.3/chartilo-0.0.3.tar.gz

---
+++ packages/chartilo/chartilo-0.0.3.tar.gz/chartilo-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Streaming video data via networks'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="chartilo",
+    version=VERSION,
+    author="Moryakov Vladislav",
+    author_email="<vlad_moryakov_prog@mail.ru>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["pyqt5"],
+    keywords=["chart", "python", "candle"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
