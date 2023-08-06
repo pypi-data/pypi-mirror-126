88ed688e1ef0d52ea624e8538945229c41fbfca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package fregataa/1.0.0.0/fregataa-1.0.0.0-py3-none-any.whl

---
+++ packages/fregataa/fregataa-1.0.0.0-py3-none-any.whl/fregataa/fregataa.py
@@ -0,0 +1,19 @@
+s = """Fregataa, by Team Algorima.
+
+Sweet heart.
+Enthusiasm.
+Steadiness.
+Considerate.
+Humorous.
+Reliable.
+Long-headed.
+Strong-willed."""
+
+print("".join([c for c in s]))
+
+print(r" _____                     _")
+print(r"|  ___| __ ___  __ _  __ _| |_ __ _  __ _")
+print(r"| |_ | '__/ _ \/ _` |/ _` | __/ _` |/ _` |")
+print(r"|  _|| | |  __/ (_| | (_| | || (_| | (_| |")
+print(r"|_|  |_|  \___|\__, |\__,_|\__\__,_|\__,_|")
+print(r"               |___/")
