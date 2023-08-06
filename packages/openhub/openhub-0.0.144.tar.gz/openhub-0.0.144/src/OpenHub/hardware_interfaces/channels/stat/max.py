fe18f125ea39fe170aac70170e9e07ca791a602b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:57 +0000

    Add package openhub/0.0.144/openhub-0.0.144.tar.gz

---
+++ packages/openhub/openhub-0.0.144.tar.gz/openhub-0.0.144/src/OpenHub/hardware_interfaces/channels/stat/max.py
@@ -0,0 +1,11 @@
+from .stat_interface import StatInterface
+
+class Max(StatInterface):
+    def __init__(self,dct=None,id=None,value=None):
+        super().__init__(dct=dct, id=None, value=None)
+
+
+    def update(self, value):
+        if value > self.value:
+            self.value = value
+            self.check_if_value_updated()
