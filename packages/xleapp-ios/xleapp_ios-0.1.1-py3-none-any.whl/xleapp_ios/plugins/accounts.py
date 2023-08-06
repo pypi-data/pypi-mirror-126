94026ad002cba105b5fe80bf6322c4bb52e731ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:49 +0000

    Add package xleapp-ios/0.1.1/xleapp_ios-0.1.1-py3-none-any.whl

---
+++ packages/xleapp-ios/xleapp_ios-0.1.1-py3-none-any.whl/xleapp_ios/plugins/accounts.py
@@ -0,0 +1,42 @@
+from xleapp import Artifact, Search, WebIcon
+
+
+class Accounts(Artifact):
+    def __post_init__(self):
+        self.name = "Accounts"
+        self.category = "Accounts"
+        self.web_icon = WebIcon.USER
+        self.report_headers = (
+            "Timestamp",
+            "Account Desc.",
+            "Username",
+            "Description",
+            "Identifier",
+            "Bundle ID",
+        )
+
+    @Search("**/Accounts3.sqlite")
+    def process(self):
+        for fp in self.found:
+            cursor = fp().cursor()
+            cursor.execute(
+                """
+                select
+                datetime(zdate+978307200,'unixepoch','utc' ),
+                zaccounttypedescription,
+                zusername,
+                zaccountdescription,
+                zaccount.zidentifier,
+                zaccount.zowningbundleid
+                from zaccount, zaccounttype
+                where zaccounttype.z_pk=zaccount.zaccounttype
+                """,
+            )
+
+            all_rows = cursor.fetchall()
+            usageentries = len(all_rows)
+            if usageentries > 0:
+                data_list = []
+                for row in all_rows:
+                    data_list.append((row[0], row[1], row[2], row[3], row[4], row[5]))
+                self.data = data_list
---
+++ packages/xleapp-ios/xleapp_ios-0.1.1-py3-none-any.whl/xleapp_ios/plugins/appGrouplisting.py
@@ -0,0 +1,41 @@
+import pathlib
+import plistlib
+import sys
+
+
+import biplist
+from xleapp import Artifact, WebIcon, Search
+
+
+class AppGroupListing(Artifact):
+    def __post_init__(self):
+        self.name = "App Group Listing"
+        self.description = (
+            "List can included once installed but not present "
+            "apps. Each file is named .com.apple.mobile_"
+            "container_manager.metadata.plist"
+        )
+        self.category = "Installed Apps"
+        self.report_headers = ("Bundle ID", "Type", "Directory GUID", "Path")
+        self.report_title = "Bundle ID by AppGroup & PluginKit IDs"
+        self.web_icon = WebIcon.PACKAGE
+
+    @Search(
+        "*/private/var/mobile/Containers/Shared/AppGroup/*/*.metadata.plist",
+        "**/PluginKitPlugin/*.metadata.plist",
+    )
+    def process(self):
+        data_list = []
+
+        for fp in self.found:
+            plist = plistlib.load(fp())
+            bundleid = plist["MCMMetadataIdentifier"]
+
+            path = fp.path
+            appgroupid = path.parent.name
+            fileloc = path.parent
+            typedir = path.parents[1].name
+
+            data_list.append((bundleid, typedir, appgroupid, fileloc))
+
+        self.data = data_list
