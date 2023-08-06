527f6a922ed99f95404d106e9fc1b5d7f3a2b98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:09 +0000

    Add package ERPTest/0.1/ERPTest-0.1.tar.gz

---
+++ packages/ERPTest/ERPTest-0.1.tar.gz/ERPTest-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'ERPTest',
+    packages = [],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'erfan.ghanbari',
+    author_email = 'erfan.ghanbari@topnorthamerica.com',
+    url = 'https://github.com/erfan-ghanbari/ERPTest.git',
+    download_url = 'https://github.com/erfan-ghanbari/ERPTest/archive/refs/tags/erp.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
