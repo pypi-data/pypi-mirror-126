ac06c35f47a01810d111ae1020e1d996b7007d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:16 +0000

    Add package hcube/0.1.2/hcube-0.1.2.tar.gz

---
+++ packages/hcube/hcube-0.1.2.tar.gz/hcube-0.1.2/hcube/api/models/filters.py
@@ -0,0 +1,42 @@
+import abc
+from enum import Enum
+from typing import Any
+
+from hcube.api.models.dimensions import Dimension
+
+
+class Filter(abc.ABC):
+    def __init__(self, dimension: Dimension):
+        self.dimension = dimension
+
+
+class ListFilter(Filter):
+    def __init__(self, dimension: Dimension, values: list):
+        super().__init__(dimension)
+        self.values = values
+
+
+class NegativeListFilter(Filter):
+    def __init__(self, dimension: Dimension, values: list):
+        super().__init__(dimension)
+        self.values = values
+
+
+class IsNullFilter(Filter):
+    def __init__(self, dimension: Dimension, is_null: bool):
+        super().__init__(dimension)
+        self.is_null = is_null
+
+
+class ComparisonType(Enum):
+    GT = ">"
+    GTE = ">="
+    LT = "<"
+    LTE = "<="
+
+
+class ComparisonFilter(Filter):
+    def __init__(self, dimension: Dimension, comparison: ComparisonType, value: Any):
+        super().__init__(dimension)
+        self.comparison = comparison
+        self.value = value
