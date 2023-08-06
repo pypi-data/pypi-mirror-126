be1d1252481d8701856bcf715bace5e61d86ed9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:11 +0000

    Add package english-to-arabic-transphonator/0.0.0.4/english_to_arabic_transphonator-0.0.0.4.tar.gz

---
+++ packages/english-to-arabic-transphonator/english_to_arabic_transphonator-0.0.0.4.tar.gz/english_to_arabic_transphonator-0.0.0.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+	name='english_to_arabic_transphonator',
+	version='0.0.0.4',
+	description='Transphonate English words using Arabic script',
+	url='https://github.com/AMR-KELEG/English-to-arabic-transphonator',
+	author='Amr Keleg',
+	author_email='amr_mohamed@live.com',
+	license='GPLv3',
+	packages=setuptools.find_packages(),
+	install_requires=[],
+	include_package_data=True,
+	zip_safe=False)
