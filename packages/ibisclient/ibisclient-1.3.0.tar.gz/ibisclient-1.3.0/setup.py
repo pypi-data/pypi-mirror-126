1ab67d8907d08f68801cea2d0aa50ba0146929f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:32 +0000

    Add package ibisclient/1.3.0/ibisclient-1.3.0.tar.gz

---
+++ packages/ibisclient/ibisclient-1.3.0.tar.gz/ibisclient-1.3.0/setup.py
@@ -0,0 +1,21 @@
+"""
+Lookup/Ibis web service API
+
+Python installation.
+"""
+
+import sys
+from setuptools import setup
+
+setup(
+    name="ibisclient",
+    version="1.3.0",
+    description="Lookup API client",
+    license="LGPL",
+    url="https://www.lookup.cam.ac.uk/doc/ws-doc/",
+    packages=["ibisclient"],
+    package_dir={"ibisclient": "src/python3/ibisclient"},
+    install_requires=[
+        "requests~=2.26",
+    ]
+)
