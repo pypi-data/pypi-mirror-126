604ff5d9a791000f7a79c55cd68166ccd3da23e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:33 +0000

    Add package VecDB/0.12.13/VecDB-0.12.13-py3-none-any.whl

---
+++ packages/VecDB/VecDB-0.12.13-py3-none-any.whl/vecdb/api/client.py
@@ -0,0 +1,21 @@
+"""API Client
+"""
+from .admin import Admin
+from .datasets import Datasets
+from .services import Services
+from ..base import Base
+from loguru import logger as loguru_logger
+
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+class APIClient(Base):
+    """API Client"""
+
+    def __init__(self, project: str, api_key: str, base_url: str):
+        self.datasets = Datasets(project=project, api_key=api_key, base_url=base_url)
+        self.services = Services(project=project, api_key=api_key, base_url=base_url)
+        self.admin = Admin(project=project, api_key=api_key, base_url=base_url)
+        super().__init__(project, api_key, base_url)
