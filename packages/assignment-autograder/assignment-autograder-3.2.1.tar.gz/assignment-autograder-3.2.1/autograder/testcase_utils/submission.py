34e87ec20b0f7ce495da720fbfc024f814f931f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:44 +0000

    Add package assignment-autograder/3.2.1/assignment-autograder-3.2.1.tar.gz

---
+++ packages/assignment-autograder/assignment-autograder-3.2.1.tar.gz/assignment-autograder-3.2.1/autograder/testcase_utils/submission.py
@@ -0,0 +1,65 @@
+import re
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple, Type
+
+from .abstract_testcase import TestCase
+
+
+def find_appropriate_source_file_stem(file: Path, possible_source_file_stems: List[str]) -> Optional[str]:
+    for s in possible_source_file_stems:
+        if s.lower() in file.stem.lower():
+            return s
+
+
+STUDENT_NAME_MATCHER = re.compile(r"(?P<student_name>[A-Za-z]+)_\d+_\d+_.+")
+
+
+def get_submission_name(submission: Path):
+    match = STUDENT_NAME_MATCHER.match(submission.stem)
+    return submission.name if match is None else match["student_name"]
+
+
+class Submission:
+    # Sowwy, dataclasses do not support slots until 3.10
+    __slots__ = "old_path", "temp_path", "temp_dir", "name", "type", "grades", "precompilation_error", "final_grade"
+
+    old_path: Path
+    temp_path: Path
+    temp_dir: Path  # Personal temporary dir, not the root one
+    name: str
+    type: Type[TestCase]
+    #            t_name,    grade  weight msg
+    grades: Dict[str, Tuple[float, float, str]]
+    precompilation_error: str
+    final_grade: int
+
+    def __init__(self, file: Path, testcase_type: Type[TestCase], temp_dir: Path):
+        self.old_path = file
+        self.temp_dir = temp_dir / file.name
+        self.temp_dir.mkdir()
+        self.temp_path = self.temp_dir / file.name
+        self.name = get_submission_name(file)
+        self.type = testcase_type
+        self.grades = {}
+        self.precompilation_error = ""
+        self.final_grade = -1
+
+    def add_grade(self, test_name: str, testcase_score: float, testcase_weight: float, message: str):
+        self.grades[test_name] = (testcase_score, testcase_weight, message)
+
+    def register_precompilation_error(self, error: str) -> None:
+        self.precompilation_error = error
+        self.final_grade = 0
+
+    def register_final_grade(self, total_score_to_100_ratio: float):
+        self.final_grade = self._calculate_final_grade(total_score_to_100_ratio)
+
+    def _calculate_final_grade(self, total_score_to_100_ratio: float) -> int:
+        total_score = 0
+        grades = self.grades.items()
+        total_testcase_weight = 0
+        for _, (test_grade, test_weight, _) in grades:
+            total_score += test_grade
+            total_testcase_weight += test_weight
+        normalized_score = total_score / total_testcase_weight * total_score_to_100_ratio
+        return round(normalized_score)
