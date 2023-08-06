a3c9df5679ba06af09b182863fdc686673584d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:37 +0000

    Add package fc-wangys/1.0/fc_wangys-1.0.tar.gz

---
+++ packages/fc-wangys/fc_wangys-1.0.tar.gz/fc_wangys-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fc_wangys",
+    version = "1.0",
+    py_modules = ["fc_wangyishuang"],
+    author = "大数据1",
+    author_email = "fc@wangyishuang.com",
+    description = "大数据1"
+)
