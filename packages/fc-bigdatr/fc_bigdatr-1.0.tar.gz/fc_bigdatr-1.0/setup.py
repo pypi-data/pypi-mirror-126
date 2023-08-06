53cca8657e3f9b5bcdb953682faadd379167f88d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:34 +0000

    Add package fc-bigdatr/1.0/fc_bigdatr-1.0.tar.gz

---
+++ packages/fc-bigdatr/fc_bigdatr-1.0.tar.gz/fc_bigdatr-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fc_bigdatr",
+    version = "1.0",#版本号
+    py_modules = ["fc_xd_bigdata"],
+    author = "wzdr",#作者
+    author_email = "3233175474@qq.com",
+    description = "数据3"
+)
