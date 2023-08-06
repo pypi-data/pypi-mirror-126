0f3c53dbb36cb4261be215d81705461292ccdb10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:09 +0000

    Add package passive-auto-design/0.1.6/passive-auto-design-0.1.6.tar.gz

---
+++ packages/passive-auto-design/passive-auto-design-0.1.6.tar.gz/passive-auto-design-0.1.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import pathlib
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+HERE = pathlib.Path(__file__).parent
+INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
+
+setuptools.setup(
+    name="passive-auto-design",
+    version="0.1.6",
+    author="Patarimi",
+    author_email="mpqqch@gmail.com",
+    description="Tools for fast prototyping of radio-frequence passive components",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Patarimi/PassiveAutoDesign",
+    packages=setuptools.find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
