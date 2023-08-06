3840cf7ace846acfc27d03e75aca07c872b351ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:13 +0000

    Add package lunespy/1.16.9/lunespy-1.16.9.tar.gz

---
+++ packages/lunespy/lunespy-1.16.9.tar.gz/lunespy-1.16.9/lunespy/client/wallet/__init__.py
@@ -0,0 +1,56 @@
+from lunespy.client.wallet.validators import validate_wallet
+
+
+class Account:
+    def __init__(self, **wallet: dict):        
+        __validated_wallet = validate_wallet(wallet)
+        
+        self.private_key = __validated_wallet['private_key']
+        self.public_key = __validated_wallet['public_key']
+        self.hash_seed = __validated_wallet['hash_seed']
+        self.network_id = __validated_wallet['network_id']
+        self.address = __validated_wallet['address']
+        self.nonce = __validated_wallet['nonce']
+        self.network = __validated_wallet['network']
+        self.seed = __validated_wallet['seed']
+
+        self.byte_private_key = __validated_wallet['byte_private_key']
+        self.byte_public_key = __validated_wallet['byte_public_key']
+        self.byte_address = __validated_wallet['byte_address']
+
+    def __str__(self) -> str:
+        from lunespy.utils.settings import bcolors
+
+        return f"\
+            \nseed\n {bcolors.OKGREEN + '└── ' +  self.seed + bcolors.ENDC}\
+            \nnetwork\n {bcolors.OKCYAN + '└── ' + self.network + bcolors.ENDC}\
+            \nnonce\n {bcolors.OKBLUE + '└── ' + str(self.nonce) + bcolors.ENDC}\
+            \nprivate key\n {bcolors.OKBLUE + '└── ' + self.private_key + bcolors.ENDC}\
+            \npublic key\n {bcolors.OKBLUE + '└── ' + self.public_key + bcolors.ENDC}\
+            \naddress\n {bcolors.OKBLUE + '└── ' + self.address + bcolors.ENDC}"
+
+    def __repr__(self) -> str:
+        return f"seed\n └──  {self.seed}\
+            \nnetwork\n └── {self.network}\
+            \nnonce\n └── {str(self.nonce)}\
+            \nprivate key\n └── {self.private_key}\
+            \npublic key\n └── {self.public_key}\
+            \naddress\n └── {self.address}"
+
+
+    def to_json(self, path: str = './') -> None:
+        from lunespy.utils.settings import bcolors
+        import json
+
+        wallet = {
+            "seed":self.seed,
+            "hash_seed":self.hash_seed,
+            "nonce":self.nonce,
+            "network":self.network,
+            "private_key":self.private_key,
+            "public_key":self.public_key,
+            "address":self.address
+            }
+        with open(f'{path}wallet-{self.network}.json', 'w') as file:
+            file.write(json.dumps(wallet))
+        print(f"{bcolors.OKGREEN}Your wallet has been saved in `{path}wallet-{self.network}.json`{bcolors.ENDC}")
---
+++ packages/lunespy/lunespy-1.16.9.tar.gz/lunespy-1.16.9/lunespy/server/address/__init__.py
@@ -0,0 +1,126 @@
+from lunespy.server import TOTAL_SUPPLY
+
+from lunespy.utils import export_dict
+from lunespy.client.wallet import Account
+from requests import get
+
+
+def aliases(address: str) -> str:
+    pass
+
+
+def all_address_balance(node_ip: str, node_api_key: str) -> dict:    
+    url = f"http://{node_ip}:5555/debug/state"
+    header = {"X-API-key": node_api_key}
+
+    all_address: dict = get(url, headers=header).json()
+
+    for addr, amount in all_address.items():
+        all_address[addr] = amount / 10e7
+
+    return all_address
+
+
+def balance(address: str, node_url: str) -> int:
+    response = get(f'{node_url}/addresses/balance/{address}')
+    if response.ok:
+        return response.json()['balance']
+    else:
+        return -1
+
+
+def percent_total(amount: float) -> float:
+    percent = amount / TOTAL_SUPPLY
+    return round(float(percent) * 100, 5)
+
+
+def rich_list(**kargs: dict) -> dict:
+    """
+    Example:
+        quantity=30,
+        node_ip="127.0.0.1",
+        node_api_key="",
+        export=True,
+        path='.'
+    """
+    wallets = all_address_balance(
+        kargs['node_ip'],
+        kargs['node_api_key']
+    )
+    only_address = sorted(wallets, key=wallets.get, reverse=True)
+    
+    wallets = {
+        addr: wallets[addr]
+        for addr in only_address[:kargs['quantity']] 
+    }    
+
+    rich_list = [
+        {
+            "address": address,
+            "amount": amount,
+            "percent": percent_total(amount),
+            "link": f"https://lunesnode.lunes.io/addresses/balance/details/{address}"
+        }
+        for address, amount in wallets.items()
+    ]
+
+    total_percent = round(
+        sum([
+            float(addr['percent'])
+            for addr in rich_list
+        ]),
+        5
+    )
+
+    report = {
+        "total_supply": TOTAL_SUPPLY,
+        "total_percent": total_percent,
+        "wallet_list": rich_list
+    }
+
+    if kargs.get('export', False):
+        export_dict(
+            kargs.get('path', '.'),
+            "rich_list.json",
+            report)
+    return report
+
+def node_leased_list_addres(node_address: str) -> list:
+    return [
+
+    ]
+   
+
+def reward_amount(node_address: str, time_interval: int, dividend: int) -> float:
+    # pegar as taxas do periodo
+    # calcula a porcentagem
+    pass
+
+
+def address_score(node_address: str, address: str) -> int:
+    total_lease = balance(node_address)
+    lease = balance(address)
+    response = int(
+        ( lease / (total_lease - lease) ) * 100
+    )
+    return response
+
+
+def distributing_lease_dividend(leaser, time_interval: int, list_address: list, dividend: int) -> dict:
+    from lunespy.client.transactions.mass import MassTransferToken
+
+    if len(list_address) > 100:
+        def listasMenores(lst, n):
+            for i in range(0, len(lst), n):
+                yield lst[i:i + n]
+            return lst
+    else:
+        list_address = [
+            {
+                "receiver": address,
+                "amount": reward_amount(leaser, time_interval, dividend) * address_score(address)
+            }
+            for address in list_address
+        ]
+    tx = MassTransferToken(leaser, list_adress).send()
+
