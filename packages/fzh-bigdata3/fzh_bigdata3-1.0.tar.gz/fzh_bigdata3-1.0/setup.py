991ddcc6074fbec911f5993b7497dfb9534ccff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:06 +0000

    Add package fzh-bigdata3/1.0/fzh_bigdata3-1.0.tar.gz

---
+++ packages/fzh-bigdata3/fzh_bigdata3-1.0.tar.gz/fzh_bigdata3-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fzh_bigdata3",
+    version = "1.0",
+    py_modules = ["fzh_xd_bigdata"],
+    author = "大数据1",
+    author_email = "fzh@bigdata.com",
+    description = "大数据1"
+)
