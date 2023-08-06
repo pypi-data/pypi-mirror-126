3446a43dec922624e637b16482d5e3f87c12f9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:35 +0000

    Add package mtutils/1.0.0/mtutils-1.0.0.tar.gz

---
+++ packages/mtutils/mtutils-1.0.0.tar.gz/mtutils-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mtutils',
+      version='1.0.0',
+      description='Commonly used function library by MT',
+      url='https://github.com/zywvvd/utils_vvd',
+      author='zywvvd',
+      author_email='zywvvd@mail.ustc.edu.cn',
+      license='MIT',
+      packages=['mtutils'],
+      zip_safe=False)
