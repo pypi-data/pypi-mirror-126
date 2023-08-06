6a558b8bc338022cb635d7bfd1f26176937bce64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:12 +0000

    Add package sphinx-notfound-page/0.8/sphinx-notfound-page-0.8.tar.gz

---
+++ packages/sphinx-notfound-page/sphinx-notfound-page-0.8.tar.gz/sphinx-notfound-page-0.8/tests/examples/default/conf.py
@@ -0,0 +1,11 @@
+# conf.py to run tests
+
+master_doc = 'index'
+extensions = [
+    'notfound.extension',
+]
+
+html_favicon = 'images/favicon.png'
+html_logo = 'images/logo.svg'
+
+templates_path = ['_templates']
