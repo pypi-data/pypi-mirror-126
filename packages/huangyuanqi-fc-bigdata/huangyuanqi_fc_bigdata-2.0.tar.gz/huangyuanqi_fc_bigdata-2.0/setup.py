7a0ac943583fad2c738496b58a603a75e44443fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:44 +0000

    Add package huangyuanqi-fc-bigdata/2.0/huangyuanqi_fc_bigdata-2.0.tar.gz

---
+++ packages/huangyuanqi-fc-bigdata/huangyuanqi_fc_bigdata-2.0.tar.gz/huangyuanqi_fc_bigdata-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+            name = "huangyuanqi_fc_bigdata",
+            version = "2.0",
+            py_modules = ["huangyuanqi_fc_bigdata"],
+            author="大数据三班",
+            author_email="2488084249@qq.com",
+            description="大数据三班"
+)
