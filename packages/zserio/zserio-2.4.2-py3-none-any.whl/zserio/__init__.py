318d2a8eff5375a05a98e5535f4b8590e9c99335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:09 +0000

    Add package zserio/2.4.2/zserio-2.4.2-py3-none-any.whl

---
+++ packages/zserio/zserio-2.4.2-py3-none-any.whl/zserio/__init__.py
@@ -0,0 +1,40 @@
+"""
+Zserio Python runtime library
+
+.. data:: VERSION_STRING
+
+   Zserio Python runtime library version string.
+"""
+
+from zserio.bitbuffer import BitBuffer
+from zserio.bitreader import BitStreamReader
+from zserio.bitwriter import BitStreamWriter
+from zserio.exception import PythonRuntimeException
+from zserio.service import ServiceData, ServiceInterface, ServiceClientInterface, ServiceException
+from zserio.pubsub import PubsubInterface, PubsubException
+
+from zserio.serialization import (serialize, deserialize, serialize_to_bytes, deserialize_bytes,
+                                  serialize_to_file, deserialize_from_file)
+
+import zserio.array as array
+import zserio.bitbuffer as bitbuffer
+import zserio.bitfield as bitfield
+import zserio.bitposition as bitposition
+import zserio.bitreader as bitreader
+import zserio.bitsizeof as bitsizeof
+import zserio.bitwriter as bitwriter
+import zserio.builtin as builtin
+import zserio.exception as exception
+import zserio.hashcode as hashcode
+import zserio.limits as limits
+import zserio.pubsub as pubsub
+import zserio.service as service
+import zserio.typeinfo as typeinfo
+
+VERSION_STRING = "2.4.2"
+
+"""
+Zserio runtime library extension built in PyPi package.
+"""
+
+from zserio.compiler import JavaNotFoundException, run_compiler, generate
