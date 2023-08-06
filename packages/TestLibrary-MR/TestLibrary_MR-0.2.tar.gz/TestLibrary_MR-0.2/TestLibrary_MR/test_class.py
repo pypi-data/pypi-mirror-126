921e9b938931c25d8a4f174a52dcb48a696349f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:51 +0000

    Add package TestLibrary-MR/0.2/TestLibrary_MR-0.2.tar.gz

---
+++ packages/TestLibrary-MR/TestLibrary_MR-0.2.tar.gz/TestLibrary_MR-0.2/TestLibrary_MR/test_class.py
@@ -0,0 +1,7 @@
+def author_details():
+    name = "Md. Masudur Rahman"
+    designation = "Software Engineer"
+    academic_details = "BS in CSE from North South University"
+    print(name)
+    print(designation)
+    print(academic_details)
