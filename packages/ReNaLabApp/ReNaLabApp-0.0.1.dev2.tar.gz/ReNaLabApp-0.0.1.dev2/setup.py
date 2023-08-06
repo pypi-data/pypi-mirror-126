8b9c97b6730d9c0b0a4cdff83a42cf67c68a169a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:42 +0000

    Add package ReNaLabApp/0.0.1.dev2/ReNaLabApp-0.0.1.dev2.tar.gz

---
+++ packages/ReNaLabApp/ReNaLabApp-0.0.1.dev2.tar.gz/ReNaLabApp-0.0.1.dev2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ReNaLabApp",
+    version="0.0.1 dev2",
+    author="ApocalyVec",
+    author_email="s-vector.lee@hotmail.com",
+    description="Reality Navigation Lab App",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ApocalyVec/RealityNavigation",
+    project_urls={
+        "Bug Tracker": "https://github.com/ApocalyVec/RealityNavigation/issues",
+        "Documentation": "https://realitynavigationdocs.readthedocs.io/en/latest/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
