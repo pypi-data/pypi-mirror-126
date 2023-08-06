7f58b3b0a4447ed17b20e437f9be20b288bef15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:02 +0000

    Add package xiaolanjing/1.0/xiaolanjing-1.0.tar.gz

---
+++ packages/xiaolanjing/xiaolanjing-1.0.tar.gz/xiaolanjing-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name = "xiaolanjing",
+    version = "1.0",
+    py_modules = ["lyj"],
+    author = "京京",
+    author_email = "1752844391@qq.com",
+    description = "大数据3" 
+)
