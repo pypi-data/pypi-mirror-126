eec6b0b1b2e3f6ff1702ebc57b6d014c8ca1212d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:16 +0000

    Add package ls-bigdate/1.0/ls_bigdate-1.0.tar.gz

---
+++ packages/ls-bigdate/ls_bigdate-1.0.tar.gz/ls_bigdate-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="ls_bigdate",
+    version="1.0",
+    py_modules="ls_xd_bigdate",
+    author= "大数据3",
+    author_email= "321119238@qq.com",
+    description= "大数据3"
+    )
