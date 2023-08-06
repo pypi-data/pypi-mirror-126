86c783144518f5638827db78d69851c2e77b4707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:54 +0000

    Add package mlspace/0.0.5/mlspace-0.0.5-py3-none-any.whl

---
+++ packages/mlspace/mlspace-0.0.5-py3-none-any.whl/mlspace/requirements/base.py
@@ -0,0 +1,11 @@
+requirements = """
+black==21.9b0
+click==8.0.3
+jupyter==1.0.0
+jupyterlab==3.2.1
+opencv-python==4.5.4.58
+pandas==1.3.4
+numpy==1.21.3
+scikit-learn==1.0.1
+scipy==1.7.1
+"""
---
+++ packages/mlspace/mlspace-0.0.5-py3-none-any.whl/mlspace/scripts/__init__.py
@@ -0,0 +1,4 @@
+from .install_docker import script as install_docker
+from .install_nvidia_docker import script as install_nvidia_docker
+from .install_nvidia_drivers import script as install_nvidia_drivers
+from .base import script as base_script
