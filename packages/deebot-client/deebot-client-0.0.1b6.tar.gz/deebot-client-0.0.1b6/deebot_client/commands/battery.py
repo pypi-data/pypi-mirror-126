5f1fbd99df824078c67cd23f6973d51c0a474371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package deebot-client/0.0.1b6/deebot-client-0.0.1b6.tar.gz

---
+++ packages/deebot-client/deebot-client-0.0.1b6.tar.gz/deebot-client-0.0.1b6/deebot_client/commands/battery.py
@@ -0,0 +1,26 @@
+"""Battery commands."""
+import logging
+from typing import Any, Dict
+
+from ..events import BatteryEvent
+from ..message import HandlingResult
+from .common import EventBus, _NoArgsCommand
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class GetBattery(_NoArgsCommand):
+    """Get battery command."""
+
+    name = "getBattery"
+
+    @classmethod
+    def _handle_body_data_dict(
+        cls, event_bus: EventBus, data: Dict[str, Any]
+    ) -> HandlingResult:
+        """Handle message->body->data and notify the correct event subscribers.
+
+        :return: A message response
+        """
+        event_bus.notify(BatteryEvent(data["value"]))
+        return HandlingResult.success()
