44c4355238e55d2444f48119003bbe0aef96f6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:07 +0000

    Add package COCOPLOTS3/0.2/COCOPLOTS3-0.2.tar.gz

---
+++ packages/COCOPLOTS3/COCOPLOTS3-0.2.tar.gz/COCOPLOTS3-0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="COCOPLOTS3",
+    version="0.2",
+    author="AGM Pietrow",
+    author_email="alex.pietrow@astro.su.se",
+    description="Python3 version of COCOpy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexPietrow/COCO",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
