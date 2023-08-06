24243f56984b3e5630e03682eedb1537d155dde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:08 +0000

    Add package b-lambda-layer-common/2.0.5/b_lambda_layer_common-2.0.5-py3-none-any.whl

---
+++ packages/b-lambda-layer-common/b_lambda_layer_common-2.0.5-py3-none-any.whl/b_lambda_layer_common/util/sfn_call.py
@@ -0,0 +1,63 @@
+import json
+import uuid
+from typing import Any, Dict, Optional, Union
+from json.decoder import JSONDecodeError
+
+import boto3
+
+
+class SfnCall:
+    def __init__(self, state_machine_arn: str):
+        self.__sfn_client = boto3.client('stepfunctions')
+        self.__state_machine_arn = state_machine_arn
+
+    def call(self, data: Dict[Any, Any]) -> Union[str, Dict[str, Any]]:
+        response = self.__sfn_client.start_sync_execution(
+            stateMachineArn=self.__state_machine_arn,
+            name=str(uuid.uuid4()),
+            input=json.dumps(data)
+        )
+
+        response_status = response.get('status')
+        response_error = response.get('error')
+
+        if response_status != 'SUCCEEDED':
+            raise ValueError(
+                f'Failed to call step functions state machine: {response_error}. '
+                f'Read logs for more info.'
+            )
+
+        output = json.loads(response.get('output', {}))
+        # Add http-like handling that does not break anything.
+        output = self.__http_like_handling(output)
+
+        return output
+
+    @staticmethod
+    def __http_like_handling(output: Dict[Any, Any]) -> Union[str, Dict[str, Any]]:
+        """
+        A function that treats responses/outputs from step functions state machine as an http response:
+        for example, it looks for keys like "http_status" or "body".
+
+        :param output: The output data from step functions state machine.
+
+        :return: Original output, if the output does not contain http-like features
+            Body of the output otherwise (JSON deserialized, if possible).
+        """
+        http_status: Optional[int] = output.get('http_status') or output.get('statusCode')
+
+        if isinstance(body := output.get('body'), str):
+            try:
+                http_body = json.loads(body)
+            except JSONDecodeError:
+                http_body = body
+        else:
+            http_body = body
+
+        if isinstance(http_status, int):
+            if http_status >= 400:
+                raise ValueError(json.dumps(http_body))
+
+            return http_body
+
+        return output
