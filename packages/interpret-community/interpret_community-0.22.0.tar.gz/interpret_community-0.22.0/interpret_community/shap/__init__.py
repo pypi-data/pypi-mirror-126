8c069477477cf1c53714ed7800453800ad7c2777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:41 +0000

    Add package interpret-community/0.22.0/interpret_community-0.22.0.tar.gz

---
+++ packages/interpret-community/interpret_community-0.22.0.tar.gz/interpret_community-0.22.0/interpret_community/shap/__init__.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Module for SHAP-based blackbox and greybox explainers."""
+from .deep_explainer import DeepExplainer
+from .kernel_explainer import KernelExplainer
+from .tree_explainer import TreeExplainer
+from .linear_explainer import LinearExplainer
+from .gpu_kernel_explainer import GPUKernelExplainer
+
+__all__ = ['DeepExplainer', 'KernelExplainer', 'TreeExplainer', 'LinearExplainer', 'GPUKernelExplainer']
