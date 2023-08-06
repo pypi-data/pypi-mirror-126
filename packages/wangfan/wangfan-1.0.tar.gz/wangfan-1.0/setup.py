6cd2e882829dd559140d7be7762391fcea0ccbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:10 +0000

    Add package wangfan/1.0/wangfan-1.0.tar.gz

---
+++ packages/wangfan/wangfan-1.0.tar.gz/wangfan-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="wangfan",
+    version="1.0",
+    py_modules=["wangfan"],
+    author="fan",
+    author_email="2061156797@qq.com",
+    description="617"
+)
