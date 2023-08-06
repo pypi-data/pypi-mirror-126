57f719d221a619895cb21f62cd9513d3f786f4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:29:53 +0000

    Add package lmu/0.4.0/lmu-0.4.0-py3-none-any.whl

---
+++ packages/lmu/lmu-0.4.0-py3-none-any.whl/lmu/tests/test_lmu.py
@@ -0,0 +1,17 @@
+from importlib import reload
+
+import keras_lmu
+
+import lmu
+
+
+def test_import_fallback(capsys):
+    reload(lmu)
+
+    assert (
+        "'lmu' is a metapackage that should not be imported" in capsys.readouterr().err
+    )
+
+    assert lmu.LMU is keras_lmu.LMU
+    assert lmu.LMUCell is keras_lmu.LMUCell
+    assert lmu.LMUFeedforward is keras_lmu.LMUFeedforward
