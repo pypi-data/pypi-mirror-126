9a71f293a46252f808a1455d9b38a8e032967ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:17 +0000

    Add package userscloud/0.0.8/userscloud-0.0.8-py3-none-any.whl

---
+++ packages/userscloud/userscloud-0.0.8-py3-none-any.whl/userscloud/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.0.8"
+__keywords__ = ["file host batch remote upload zippyshare captcha recaptcha bypass account register registration api cli manager archive"]
+
+
+# if not __version__.endswith(".0"):
+#     import re
+#     print("version {} is deployed for automatic commitments only".format(__version__), flush=True)
+#     print("install version " + re.sub(r"([0-9]+\.[0-9]+\.)[0-9]+", r"\g<1>0", __version__) + " instead")
+#     import os
+#     os._exit(1)
+
+
+from .core import *
+
