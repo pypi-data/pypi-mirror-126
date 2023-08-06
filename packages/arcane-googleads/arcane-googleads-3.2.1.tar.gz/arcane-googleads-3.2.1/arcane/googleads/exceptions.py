29d2960ffd72587ff4b223c506984769a2f655f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:31 +0000

    Add package arcane-googleads/3.2.1/arcane-googleads-3.2.1.tar.gz

---
+++ packages/arcane-googleads/arcane-googleads-3.2.1.tar.gz/arcane-googleads-3.2.1/arcane/googleads/exceptions.py
@@ -0,0 +1,12 @@
+class GoogleAdsAccountLostAccessException(Exception):
+    """ Raised when we cannot access to an account """
+    pass
+
+
+class GoogleAdsAccountNoAccessException(Exception):
+    """ Raised when we cannot access to an account """
+    pass
+
+class GoogleAdsAccountException(Exception):
+    """ Raised when a google ads error has occured expect user permission denied"""
+    pass
