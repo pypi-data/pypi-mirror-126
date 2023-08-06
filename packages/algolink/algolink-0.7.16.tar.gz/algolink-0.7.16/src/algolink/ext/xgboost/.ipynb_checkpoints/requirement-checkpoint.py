e44b2943421b0d002beadaf4d73041174b00fd45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:38 +0000

    Add package algolink/0.7.16/algolink-0.7.16.tar.gz

---
+++ packages/algolink/algolink-0.7.16.tar.gz/algolink-0.7.16/src/algolink/ext/xgboost/.ipynb_checkpoints/requirement-checkpoint.py
@@ -0,0 +1,10 @@
+from algolink.core.analyzer.requirement import RequirementHook
+from algolink.core.objects.requirements import InstallableRequirement, Requirement, Requirements, UnixPackageRequirement
+
+
+class XgboostRequirementHook(RequirementHook):
+    def can_process(self, obj: Requirement) -> bool:
+        return isinstance(obj, InstallableRequirement) and obj.package.lower() == 'xgboost'
+
+    def process(self, obj: Requirement, **kwargs) -> Requirements:
+        return Requirements([obj, UnixPackageRequirement('libgomp1')])
