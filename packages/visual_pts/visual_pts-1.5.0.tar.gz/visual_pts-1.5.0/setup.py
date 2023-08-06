0dfbcba579504c48c71670d280425447b0385b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:11 +0000

    Add package visual_pts/1.5.0/visual_pts-1.5.0.tar.gz

---
+++ packages/visual_pts/visual_pts-1.5.0.tar.gz/visual_pts-1.5.0/setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+import setuptools
+from setuptools import setup
+
+with open('README.md', 'r') as fp:
+    readme = fp.read()
+ 
+VERSION = "1.5.0"
+LICENSE = "MIT"
+
+setup(
+    name='visual_pts',
+    version=VERSION,
+    description=(
+        'Visualize Point Clouds (with bbox) by Plotly'
+    ),
+    long_description=readme,
+    author='JunbinGao, HaoRuan, LianguangLiu',
+    author_email='junbingao@hust.edu.cn',
+    maintainer='JunbinGao, HaoRuan, LianguangLiu',
+    maintainer_email='junbingao@hust.edu.cn',
+    license=LICENSE,
+    packages=setuptools.find_packages(),
+    platforms=["all"],
+    url='https://github.com/gaojunbin/visual_pts',
+    install_requires=[  
+        "plotly",  
+        "opencv-python",
+        "matplotlib",  
+        ],  
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
