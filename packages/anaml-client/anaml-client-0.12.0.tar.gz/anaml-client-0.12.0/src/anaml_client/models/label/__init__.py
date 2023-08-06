2c551a0c5aefc36d0c140eae68e755f7db135a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:12 +0000

    Add package anaml-client/0.12.0/anaml-client-0.12.0.tar.gz

---
+++ packages/anaml-client/anaml-client-0.12.0.tar.gz/anaml-client-0.12.0/src/anaml_client/models/label/__init__.py
@@ -0,0 +1,80 @@
+"""Generated implementation of label."""
+
+# WARNING DO NOT EDIT
+# This code was generated from label.mcn
+
+from __future__ import annotations
+
+import abc  # noqa: F401
+import dataclasses  # noqa: F401
+import datetime  # noqa: F401
+import enum  # noqa: F401
+import json  # noqa: F401
+import jsonschema  # noqa: F401
+import logging  # noqa: F401
+import typing  # noqa: F401
+import uuid  # noqa: F401
+
+
+@dataclasses.dataclass(frozen=True)
+class Label:
+    """Label metadata to apply to features and other objects.
+    
+    Args:
+        value (str): A data field.
+    """
+    
+    value: str
+    
+    def __str__(self):
+        """Return a str of the wrapped value."""
+        return str(self.value)
+    
+    @classmethod
+    def json_schema(cls):
+        """Return the JSON schema for Label data.
+        
+        Returns:
+            A Python dictionary describing the JSON schema.
+        """
+        return {
+            "type": "string"
+        }
+    
+    @classmethod
+    def from_json(cls, data: str):
+        """Validate and parse JSON data into an instance of Label.
+        
+        Args:
+            data (str): JSON data to validate and parse.
+        
+        Returns:
+            An instance of Label.
+        
+        Raises:
+            ValidationError: When schema validation fails.
+            KeyError: When a required field is missing from the JSON.
+        """
+        try:
+            jsonschema.validate(data, cls.json_schema())
+            return Label(str(data))
+        except jsonschema.exceptions.ValidationError as ex:
+            logging.debug("Invalid JSON data received while parsing Label", exc_info=ex)
+            raise
+    
+    def to_json(self):
+        """Serialise this instance as JSON.
+        
+        Returns:
+            Data ready to serialise as JSON.
+        """
+        return str(self.value)
+    
+    @classmethod
+    def from_json_key(cls, data: str):
+        """Parse a JSON string such as a dictionary key."""
+        return Label(str(data))
+    
+    def to_json_key(self):
+        """Serialise as a JSON string suitable for use as a dictionary key."""
+        return str(self.value)
