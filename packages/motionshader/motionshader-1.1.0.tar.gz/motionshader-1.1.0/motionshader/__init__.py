301e6ac944e5ea10c2570b3bafe890aa5c2eb004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:14 +0000

    Add package motionshader/1.1.0/motionshader-1.1.0.tar.gz

---
+++ packages/motionshader/motionshader-1.1.0.tar.gz/motionshader-1.1.0/motionshader/__init__.py
@@ -0,0 +1,7 @@
+from .core import GeospatialViewport
+from .core import TemporalPlayback
+from .core import Basemap
+from .core import Dataset
+from .core import MotionVideo
+from .core import FrameAnnotation
+from .core import FrameWatermark
