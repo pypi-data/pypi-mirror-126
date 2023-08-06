5f3e830792553186420ed1772efe2fcd92af1017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:31 +0000

    Add package lifx-photons-interactor/0.11.0/lifx-photons-interactor-0.11.0.tar.gz

---
+++ packages/lifx-photons-interactor/lifx-photons-interactor-0.11.0.tar.gz/lifx-photons-interactor-0.11.0/setup.py
@@ -0,0 +1,39 @@
+from interactor import VERSION
+
+from setuptools import setup, find_packages
+
+# fmt: off
+
+setup(
+      name = "lifx-photons-interactor"
+    , version = VERSION
+    , packages = find_packages(include="interactor.*", exclude=["tests*"])
+    , include_package_data = True
+
+    , python_requires = ">= 3.7"
+
+    , install_requires =
+      [ "lifx-photons-core>=0.41.0"
+      , "tornado>=6.1"
+      , "SQLAlchemy[asyncio]==1.4.23"
+      , "aiosqlite==0.17.0"
+      , "alembic==1.3.2"
+      , "whirlwind-web==0.12.0"
+      , "aiohttp==3.7.4"
+      ]
+
+    , entry_points =
+      { "lifx.photons": ["interactor = interactor.addon"]
+      }
+
+    # metadata for upload to PyPI
+    , url = "http://github.com/delfick/photons/apps/interactor"
+    , author = "Stephen Moore"
+    , author_email = "github@delfick.com"
+    , description = "A server for interacting with LIFX lights over the LAN"
+    , license = "MIT"
+    , keywords = "lifx photons"
+    , long_description = open("README.rst").read()
+    )
+
+# fmt: on
