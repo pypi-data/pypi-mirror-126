7e7656e2ed71794fd11f61264b4bdf4beabc85e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:13 +0000

    Add package flask-scaffolding/0.0.18/flask_scaffolding-0.0.18-py3-none-any.whl

---
+++ packages/flask-scaffolding/flask_scaffolding-0.0.18-py3-none-any.whl/scaffold/base/instance/config.py
@@ -0,0 +1,24 @@
+SECRET_KEY = 'qwerty123'
+
+# Create Session config
+SESSION_TYPE = 'sqlalchemy'
+SESSION_SQLALCHEMY_TABLE = 'sessions'
+
+# Create in-memory database
+DATABASE_FILE = 'sample_db.sqlite'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# Create Security Config
+SECURITY_PASSWORD_SALT ='123456789'
+SECURITY_EMAIL_SENDER='admin@app'
+
+# Create EMail Configuration
+MAIL_SERVER=''
+MAIL_PORT=0
+MAIL_USE_SSL=False
+MAIL_USERNAME=''
+MAIL_PASSWORD=''
+MAIL_USE_TLS=True
+
+
