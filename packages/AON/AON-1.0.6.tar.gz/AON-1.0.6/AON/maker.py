dab01207d7d2188f48e8b3ed9f2824379fcec7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:30 +0000

    Add package AON/1.0.6/AON-1.0.6.tar.gz

---
+++ packages/AON/AON-1.0.6.tar.gz/AON-1.0.6/AON/maker.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+import tempo
+class aon:
+	k=0
+	def make(self,path):
+	
+		self.empty('tempo.py')
+		file=open(path,'r')
+		content=file.read()
+	
+		fc=open('tempo.py','w')
+		fc.seek(0)
+		fc.write('tempo='+content)
+		fc.close()
+	
+	def load(self):
+		if self.k==0:
+			self.k=self.k+1
+			self.load()
+		else:
+			self.k=0
+			t=tempo.tempo
+			return t
+	
+	def empty(self,path):
+		fc=open(path,'w')
+		fc.write('')
+		fc.close()
+	
