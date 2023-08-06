dc7541eda8520a8a59999986c865404c600a077a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:24 +0000

    Add package weeb-fast-hist/3.0.0/weeb_fast_hist-3.0.0.tar.gz

---
+++ packages/weeb-fast-hist/weeb_fast_hist-3.0.0.tar.gz/weeb_fast_hist-3.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='weeb_fast_hist',
+    version='3.0.0',    
+    description='A example Python package for itmo da course',
+    url='',
+    author='Garanin Artem',
+    author_email='',    
+    packages=['weeb_fast_hist'],
+    install_requires=['matplotlib>3.0.0'
+                      ]
+)    
