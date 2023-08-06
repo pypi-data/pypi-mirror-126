717ab81f4b7f124cfde071346b7ca3ff265f3ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:11 +0000

    Add package fc-lch/2.0/fc_lch-2.0.tar.gz

---
+++ packages/fc-lch/fc_lch-2.0.tar.gz/fc_lch-2.0/set.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='fc_lch',
+    version='1.0',
+    py_modules="fc_mmt",
+    author="刘昌辉",
+    author_email="504178924@qq.com",
+    description='狗子'
+)
