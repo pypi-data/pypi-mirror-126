edcc1974eebd8f24aab5006a40439ef58092ff9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:17 +0000

    Add package LibBinTViewVariable/0.1.4.1/LibBinTViewVariable-0.1.4.1.tar.gz

---
+++ packages/LibBinTViewVariable/LibBinTViewVariable-0.1.4.1.tar.gz/LibBinTViewVariable-0.1.4.1/LibBinTViewVariable/TradingView.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Indicator:
+    Id: str
+    Name: str
+    Script: str
+    FirstLevel: str
+    SecondLevel: str
+
+
+@dataclass
+class SuperTrend(Indicator):
+    def __init__(self, Value_1: float = 0, Value_2: float = 0):
+        super().__init__(Id="STD;Supertrend",
+                         Name="SuperTrend",
+                         Script="Script@tv-scripting-101!",
+                         FirstLevel="",
+                         SecondLevel="")
+        self.Value_1 = Value_1
+        self.Value_2 = Value_2
+
+
+@dataclass
+class Volume(Indicator):
+    def __init__(self, Value_1: float = 0, Value_2: float = 0):
+        super().__init__(Id="",
+                         Name="Volume",
+                         Script="Volume",
+                         FirstLevel="",
+                         SecondLevel="")
+        self.Value_1 = Value_1
+        self.Value_2 = Value_2
+
+
+@dataclass
+class TripleMovingAverages(Indicator):
+    def __init__(self, Value_1: float = 0, Value_2: float = 0, Value_3: float = 0):
+        super().__init__(Id="PUB;y784PkOKflCjfhCiCB4ewuC0slMtB8PQ",
+                         Name="TripleMovingAverages",
+                         Script="Script@tv-scripting-101!",
+                         FirstLevel="",
+                         SecondLevel="")
+        self.Value_1 = Value_1
+        self.Value_2 = Value_2
+        self.Value_3 = Value_3
