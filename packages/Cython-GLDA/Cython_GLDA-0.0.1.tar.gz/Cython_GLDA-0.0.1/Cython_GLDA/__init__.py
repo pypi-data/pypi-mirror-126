3cddfe72f531e3d5372ea431bc1f5d88f2579a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:04 +0000

    Add package Cython-GLDA/0.0.1/Cython_GLDA-0.0.1.tar.gz

---
+++ packages/Cython-GLDA/Cython_GLDA-0.0.1.tar.gz/Cython_GLDA-0.0.1/Cython_GLDA/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Chenyu'
+from .object_two import Two
+from Cython_GLDA.cython_trainer._cython_code import fib_cpdef
---
+++ packages/Cython-GLDA/Cython_GLDA-0.0.1.tar.gz/Cython_GLDA-0.0.1/Cython_GLDA/cython_trainer/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'Chenyu Yang'
+from ._cython_code import fib_cpdef
