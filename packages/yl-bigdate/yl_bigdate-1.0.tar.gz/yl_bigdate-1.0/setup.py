2b7e773ec4f1b92683a8678ff93689827ea92914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:26 +0000

    Add package yl-bigdate/1.0/yl_bigdate-1.0.tar.gz

---
+++ packages/yl-bigdate/yl_bigdate-1.0.tar.gz/yl_bigdate-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+ 
+setup(
+    name="yl_bigdate",
+    version="1.0",
+    py_modules=["yl_xd_bigdate"],
+    author="bigdate",
+    author_email="",
+    description="bigdate"
+)
