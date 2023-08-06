cd4a45d6894f5842026fc6cdbf81a2c6f4603a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:54 +0000

    Add package wiser/0.0.1/wiser-0.0.1.tar.gz

---
+++ packages/wiser/wiser-0.0.1.tar.gz/wiser-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wiser",
+    version="0.0.1",
+    author="Nicola Massarenti",
+    author_email="nicola.massarenti@gmail.com",
+    description="A python package designed to free the developers from the burden of common operations with cloud technologies.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nicolamassarenti/wiser",
+    project_urls={
+        "Bug Tracker": "https://github.com/nicolamassarenti/wiser/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
