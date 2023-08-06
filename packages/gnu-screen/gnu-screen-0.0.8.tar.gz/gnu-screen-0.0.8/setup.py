200bede201ad66fbcae0354d4fc560d43546d965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:26 +0000

    Add package gnu-screen/0.0.8/gnu-screen-0.0.8.tar.gz

---
+++ packages/gnu-screen/gnu-screen-0.0.8.tar.gz/gnu-screen-0.0.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gnu-screen",
+    version="0.0.8",
+    author="Lucas Maillet",
+    author_email="loucas.maillet.pro@gmail.com",
+    description="GNU-Screen session module handler for NodeJs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LoucasMaillet/Lib-GNU-Screen/tree/main/Python",
+    project_urls={
+        "Bug Tracker": "https://github.com/LoucasMaillet/Lib-GNU-Screen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_dir={"gnu_screen": "src"},
+    packages=["gnu_screen"],
+    python_requires=">=3",
+)
