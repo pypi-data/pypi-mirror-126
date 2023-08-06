56c486cf6dd2915c0fcc8ceb9e8183ab12eb0fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:16 +0000

    Add package py-tgcalls/0.8.1/py_tgcalls-0.8.1-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/py-tgcalls/py_tgcalls-0.8.1-cp37-cp37m-macosx_10_14_x86_64.whl/pytgcalls/methods/stream/resume_stream.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from ...exceptions import NodeJSNotRunning
+from ...exceptions import NoMtProtoClientSet
+from ...scaffold import Scaffold
+
+
+class ResumeStream(Scaffold):
+    async def resume_stream(
+        self,
+        chat_id: int,
+    ):
+        if self._app is not None:
+            if self._wait_until_run is not None:
+                async def internal_sender():
+                    if not self._wait_until_run.done():
+                        await self._wait_until_run
+                    await self._binding.send({
+                        'action': 'resume',
+                        'chat_id': chat_id,
+                    })
+                active_call = self._call_holder.get_active_call(chat_id)
+                asyncio.ensure_future(internal_sender())
+                return active_call.status == 'paused'
+            else:
+                raise NodeJSNotRunning()
+        else:
+            raise NoMtProtoClientSet()
