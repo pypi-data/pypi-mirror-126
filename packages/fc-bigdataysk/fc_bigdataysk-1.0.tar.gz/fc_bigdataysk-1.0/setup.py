1b6d5c61e95fbdbad356ff85ca32a6a8296c2b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:16 +0000

    Add package fc-bigdataysk/1.0/fc_bigdataysk-1.0.tar.gz

---
+++ packages/fc-bigdataysk/fc_bigdataysk-1.0.tar.gz/fc_bigdataysk-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+            name = "fc_bigdataysk",
+            version = "1.0",
+            py_modules = ["fc_xd_bigdata"],
+            author = "大数据3",
+            author_email = "fc@bigdata.com",
+            description = "杨帅康"
+
+
+)
