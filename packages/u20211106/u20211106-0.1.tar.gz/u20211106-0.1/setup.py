09224e11114e12d15af51eb31269722aab2f11fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:15 +0000

    Add package u20211106/0.1/u20211106-0.1.tar.gz

---
+++ packages/u20211106/u20211106-0.1.tar.gz/u20211106-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name = "u20211106",
+        version="0.1",
+        py_modules=["unit_20211106_1"],
+        author="d3",
+        author_email="u20211106@d3.com",
+        description="d3"
+)
