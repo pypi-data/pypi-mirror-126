38ee00f4dbab204f881ef8b0f328b59b95bde484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:32 +0000

    Add package cicadad/1.4.1/cicadad-1.4.1.tar.gz

---
+++ packages/cicadad/cicadad-1.4.1.tar.gz/cicadad-1.4.1/src/cicadad/util/constants.py
@@ -0,0 +1,23 @@
+import base64
+import json
+
+CICADA_VERSION = "1.4.1"
+
+DEFAULT_DOCKER_NETWORK = "cicada-distributed-network"
+DEFAULT_KUBE_NAMESPACE = "default"
+DOCKER_CONTAINER_MODE = "DOCKER"
+KUBE_CONTAINER_MODE = "KUBE"
+DEFAULT_CONTAINER_MODE = DOCKER_CONTAINER_MODE
+
+DEFAULT_CONTEXT_STRING = base64.b64encode(json.dumps({}).encode("ascii")).decode(
+    "ascii"
+)
+
+# METRICS_STREAM = "metrics"
+
+LOG_FORMAT = "%(asctime)s | %(levelname)-8s | %(name)s:%(funcName)s -- %(message)s"
+DATE_FORMAT = "%m-%d %H:%M"
+
+DEFAULT_DATASTORE_ADDRESS = "cicada-distributed-datastore-client:8283"
+
+DEFAULT_CONTAINER_SERVICE_ADDRESS = "cicada-distributed-container-service:8284"
