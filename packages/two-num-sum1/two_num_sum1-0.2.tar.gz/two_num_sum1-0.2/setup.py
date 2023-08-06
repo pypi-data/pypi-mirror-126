9659eed18026ec4b4b1abe835125d396855c2440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:28 +0000

    Add package two-num-sum1/0.2/two_num_sum1-0.2.tar.gz

---
+++ packages/two-num-sum1/two_num_sum1-0.2.tar.gz/two_num_sum1-0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="two_num_sum1",
+    version="0.2",
+    license="MIT Licence",
+ 
+    url="https://github.com/lwxGitHub123/pip_demo",
+    author="liudongbo",
+    author_email="350272438@qq.com",
+ 
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
+
+
