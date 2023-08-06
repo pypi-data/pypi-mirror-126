c69959970c01d32f98c3d325ff1fa7018afa6213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:13 +0000

    Add package pipenv-update-check/0.2.4/pipenv_update_check-0.2.4.tar.gz

---
+++ packages/pipenv-update-check/pipenv_update_check-0.2.4.tar.gz/pipenv_update_check-0.2.4/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+from setuptools import find_packages, setup
+
+long_description = u'\n\n'.join((
+    open('README.rst').read(),
+))
+
+setup(
+  name = 'pipenv_update_check',
+  version = '0.2.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Pip environment update check',   # Give a short description about your library
+  long_description=long_description,
+  author = 'cuneyt',                   # Type in your name
+  author_email = 'cuneyt@3bfab.com',      # Type in your E-Mail
+  url = 'https://github.com/3bfab/pipenv-update-check',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/3bfab/pipenv-update-check/archive/refs/tags/v_0.2.4.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'terminaltables',
+          'toml',
+      ],
+  classifiers=[
+  ],
+  package_dir={"": "src"},
+    packages=find_packages(
+        where="src",
+    ),  
+  package_data={},
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'pipenv-update-check = pipenv_update_check:main',
+    ]},
+)
