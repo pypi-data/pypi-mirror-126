741dc45624c4a26f10e0a2e3f7d9e2253ffa4635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:29 +0000

    Add package yueyue/1.0/yueyue-1.0.tar.gz

---
+++ packages/yueyue/yueyue-1.0.tar.gz/yueyue-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "yueyue",
+    version= "1.0",
+    py_modules=["zz"],
+    author= "大数据3",
+    author_email="1423135072@qq.com",
+    description= "大数据3"
+)
