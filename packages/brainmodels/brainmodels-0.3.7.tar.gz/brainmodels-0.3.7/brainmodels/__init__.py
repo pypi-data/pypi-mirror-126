432827fe9d61b7cce8f94abb4b7e98c6f0b82068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:44 +0000

    Add package brainmodels/0.3.7/brainmodels-0.3.7.tar.gz

---
+++ packages/brainmodels/brainmodels-0.3.7.tar.gz/brainmodels-0.3.7/brainmodels/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.3.7"
+
+from . import channels
+from . import neurons
+from . import synapses
+from .neurons import *
+from .synapses import *
+from .channels import *
