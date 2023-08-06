27c3e31c39ef19547473a575087ad2cabae17e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:43 +0000

    Add package basx-bread/0.4.29/basx_bread-0.4.29-py3-none-any.whl

---
+++ packages/basx-bread/basx_bread-0.4.29-py3-none-any.whl/bread/formatters.py
@@ -0,0 +1,167 @@
+import datetime
+import numbers
+import os
+from collections.abc import Iterable
+
+import htmlgenerator as hg
+from dateutil import tz
+from django.conf import settings
+from django.db import models
+from django.utils.functional import Promise
+from django.utils.html import format_html, format_html_join, linebreaks, mark_safe
+from django_countries.fields import CountryField
+
+import bread.settings as app_settings
+
+from . import layout
+
+
+def format_value(value, fieldtype=None):
+    """Renders a python value in a nice way in HTML. If a field-definition has an attribute "renderer" set, that function will be used to render the value"""
+    if hasattr(fieldtype, "renderer"):
+        return fieldtype.renderer(value)
+
+    if isinstance(value, bool) or value is None:
+        return CONSTANTS[value]
+
+    # make referencing fields iterable (normaly RelatedManagers)
+    if isinstance(value, models.Manager):
+        value = value.all()
+
+    # If there is a hint passed via fieldtype, use the accoring conversion function first (identity otherwise)
+    # This is mostly helpfull for string-based fields like URLS, emails etc.
+    value = MODELFIELD_FORMATING_HELPERS.get(fieldtype.__class__, lambda a: a)(value)
+
+    if isinstance(value, bool) or value is None:
+        return CONSTANTS[value]
+    if isinstance(value, datetime.timedelta):
+        return as_duration(value)
+    if isinstance(value, datetime.datetime):
+        return as_datetime(value)
+    if isinstance(value, numbers.Number):
+        return f"{float(value):f}".rstrip("0").rstrip(".")
+    if isinstance(value, models.fields.files.FieldFile):
+        return as_download(value)
+    if isinstance(value, Iterable) and not isinstance(value, (str, bytes, Promise)):
+        return as_list(value)
+    return value
+
+
+# Formatting functions: never pass None, always return string
+
+
+def as_email(value):
+    return format_html('<a href="mailto:{}">{}</a>', value, value)
+
+
+def as_url(value):
+    return format_html(
+        '<a href="{}" target="_blank" rel="noopener noreferrer">{}</a>', value, value
+    )
+
+
+def as_text(value):
+    return mark_safe(linebreaks(value, autoescape=True))
+
+
+def as_time(value):
+    if value.tzinfo:
+        value = value.astimezone(tz.gettz(settings.TIME_ZONE))
+    return f"{value.hour:02}:{value.minute:02}:{value.second:02}"
+
+
+def as_duration(value):
+    return str(value - datetime.timedelta(microseconds=value.microseconds))
+
+
+def as_datetime(value):
+    if value.tzinfo:
+        value = value.astimezone(tz.gettz(settings.TIME_ZONE))
+
+    return value.isoformat(sep=" ", timespec="seconds").rsplit("+", 1)[0]
+
+
+def as_countries(value):
+    return as_list((c.name for c in value))
+
+
+def as_list(iterable, sep=", "):
+    return format_html_join(sep, "{}", ((format_value(v),) for v in iterable))
+
+
+def as_richtext(value):
+    return mark_safe(value)
+
+
+def as_download(value):
+    if not value:
+        return CONSTANTS[None]
+    if not value.storage.exists(value.name):
+        return mark_safe("<small><emph>File not found</emph></small>")
+    return mark_safe(
+        hg.render(
+            hg.BaseElement(
+                hg.A(
+                    layout.icon.Icon(
+                        "launch",
+                        size=16,
+                        style="vertical-align: middle; margin-right: 0.25rem;",
+                    ),
+                    hg.SPAN(os.path.basename(value.name)),
+                    newtab=True,
+                    href=value.url,
+                    style="margin-right: 0.5rem; margin-left: 0.5rem",
+                    onclick="event.stopPropagation();",
+                ),
+            ),
+            {},
+        )
+    )
+
+
+def as_audio(value):
+    if not value:
+        return CONSTANTS[None]
+    if not value.storage.exists(value.name):
+        return mark_safe("<small><emph>Audio file not found</emph></small>")
+    return format_html(
+        """
+        <audio controls controlsList="nodownload" preload="metadata">
+            <source src="{}" type="audio/mp3">
+        </audio>
+    """,
+        value.url,
+    )
+
+
+def as_video(value):
+    if not value:
+        return CONSTANTS[None]
+    if not value.storage.exists(value.name):
+        return mark_safe("<small><emph>Video file not found</emph></small>")
+    return format_html(
+        """
+        <video controls width="320" height="240" controlsList="nodownload" preload="metadata">
+            <source src="{}" type="video/mp4">
+        </video>
+    """,
+        value.url,
+    )
+
+
+MODELFIELD_FORMATING_HELPERS = {
+    None: lambda a: a,
+    models.EmailField: as_email,
+    models.FileField: as_download,
+    models.URLField: as_url,
+    models.TextField: as_text,
+    models.TimeField: as_time,
+    models.DateTimeField: as_datetime,
+    CountryField: as_countries,
+}
+
+CONSTANTS = {
+    None: getattr(settings, "HTML_NONE", app_settings.HTML_NONE),
+    True: getattr(settings, "HTML_TRUE", app_settings.HTML_TRUE),
+    False: getattr(settings, "HTML_FALSE", app_settings.HTML_FALSE),
+}
---
+++ packages/basx-bread/basx_bread-0.4.29-py3-none-any.whl/bread/views/read.py
@@ -0,0 +1,72 @@
+import htmlgenerator as hg
+from django import forms
+from django.http import HttpResponseNotAllowed
+
+from .. import layout as _layout  # prevent name clashing
+from ..forms.fields import FormsetField
+from ..forms.forms import breadmodelform_factory
+from .edit import EditView
+
+
+# Read view is the same as the edit view but form elements are disabled
+class ReadView(EditView):
+    """TODO: documentation"""
+
+    accept_global_perms = True
+    fields = None
+    urlparams = (("pk", int),)
+
+    def post(self, *args, **kwargs):
+        return HttpResponseNotAllowed(["GET"])
+
+    def get_form_class(self, form=forms.models.ModelForm):
+        return breadmodelform_factory(
+            request=self.request,
+            model=self.model,
+            layout=self._get_layout_cached(),
+            instance=self.object,
+            baseformclass=form,
+            cache_querysets=False,
+        )
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        for field in form.fields.values():
+            field.disabled = True
+            if isinstance(field, FormsetField):
+                for subfield in field.formsetclass.form.base_fields.values():
+                    subfield.disabled = True
+        return form
+
+    def get_layout(self):
+        return layoutasreadonly(super().get_layout())
+
+    def get_required_permissions(self, request):
+        return [f"{self.model._meta.app_label}.view_{self.model.__name__.lower()}"]
+
+
+def layoutasreadonly(layout):
+    layout.wrap(
+        lambda element, ancestors: isinstance(element, _layout.form.Form)
+        and element.standalone,
+        hg.FIELDSET(readonly="true"),
+    )
+
+    layout.delete(
+        lambda element, ancestors: any(
+            [isinstance(a, _layout.form.Form) for a in ancestors]
+        )
+        and (
+            (
+                isinstance(
+                    element,
+                    hg.BUTTON,
+                )
+                and "bx--tag" not in element.attributes.get("_class", "")
+            )
+            or getattr(element, "attributes", {}).get("type") == "submit"
+        )
+    )
+    for form in layout.filter(lambda element, ancestors: isinstance(element, hg.FORM)):
+        form.attributes["onsubmit"] = "return false;"
+    return layout
