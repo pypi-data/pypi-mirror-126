f61637a8f96e3d97ae920474466953f12ba06cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:13 +0000

    Add package senderscore/0.1.6/senderscore-0.1.6.tar.gz

---
+++ packages/senderscore/senderscore-0.1.6.tar.gz/senderscore-0.1.6/setup.py
@@ -0,0 +1,50 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+
+def read(*names, **kwargs):
+    """Read a file."""
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        if kwargs.get('type') == 'list':
+            content = list(map(lambda s: s.strip(), open_file.readlines()))
+        else:
+            content = open_file.read().strip()
+    return content
+
+
+long_description = read('README.md')
+requirements = read('requirements.in', type='list')
+
+setup(
+        name='senderscore',
+        version='0.1.6',
+        author='Thiago "Undersfx" Rodrigues',
+        url='https://github.com/undersfx/senderscore-lookup',
+        description='Senderscore command line lookup tool',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        license='MIT',
+        packages=find_packages(
+            exclude=[
+                "tests",
+            ]
+        ),
+        entry_points={
+            'console_scripts': [
+                'senderscore = senderscore.senderscore:main'
+            ]
+        },
+        classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords='sender score lookup cli undersfx',
+        install_requires=requirements,
+        zip_safe=False
+)
