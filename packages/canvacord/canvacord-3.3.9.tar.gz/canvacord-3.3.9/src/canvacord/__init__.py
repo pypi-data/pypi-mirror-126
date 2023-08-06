118c599eb42a5fa3e71818cd0a5303dbb4f003f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:03 +0000

    Add package canvacord/3.3.9/canvacord-3.3.9.tar.gz

---
+++ packages/canvacord/canvacord-3.3.9.tar.gz/canvacord-3.3.9/src/canvacord/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "3.3.9"
+
+from .generators import rankcard
+from .generators import trigger
+from .generators import communism
+from .generators import gay
+from .generators import jail
+from .generators import hitler
+from .generators import spank
+from .generators import aborted
+from .generators import affect
+from .generators import airpods
+from .generators import america
+from .generators import bed
+from .generators import wanted
+from .generators import jokeoverhead
+from .generators import checkversion
+from .generators import avatar
+from .generators import encrypt
+from .generators import decrypt
+from .generators import genkey
+from .generators import regenkey
