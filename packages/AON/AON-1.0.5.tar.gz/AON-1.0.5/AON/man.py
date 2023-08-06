d490fff228ec1065d2b924bd4a71d8b9bd05f0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:28 +0000

    Add package AON/1.0.5/AON-1.0.5.tar.gz

---
+++ packages/AON/AON-1.0.5.tar.gz/AON-1.0.5/AON/man.py
@@ -0,0 +1,21 @@
+def main():
+	print('''
+	AON (Advanced object notation)
+	------------------------------
+
+	Description:
+	-----------
+		*This is similar to JSON but it was developed using python.
+		*Python is having huge community and opensource so this is a base version.
+	Usage:
+	-----
+		*It has two method make inside maker package which will convert your AON file by taking path as arg.
+		*load method return the data from the AON file.
+		Note: without calling make method no data available.
+	Contact Details:
+	---------------
+	Koppula Praveen Kumar
+	koppulapraveen920@gmail.com
+	 ''')
+	f=open('tempo.py','w')
+	f.close()
