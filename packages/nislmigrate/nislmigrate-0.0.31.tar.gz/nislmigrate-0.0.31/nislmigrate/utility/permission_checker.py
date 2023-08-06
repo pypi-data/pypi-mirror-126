c0b871dd6e265ab785bac72d19e4d2a2d106646c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:08 +0000

    Add package nislmigrate/0.0.31/nislmigrate-0.0.31.tar.gz

---
+++ packages/nislmigrate/nislmigrate-0.0.31.tar.gz/nislmigrate-0.0.31/nislmigrate/utility/permission_checker.py
@@ -0,0 +1,41 @@
+import ctypes
+import os
+
+from nislmigrate.logs.migration_error import MigrationError
+from nislmigrate.migration_action import MigrationAction
+
+RESTORE_WITHOUT_FORCE_FLAG_WARNING = """
+
+'restore' will overwrite existing data for the services being restored,
+if you are sure you want to delete existing data and restore the
+captured data run the command again with the '-f/--force' flag
+"""
+
+
+def is_running_with_elevated_permissions():
+    """
+    Checks whether the current process is running with elevated (admin) permissions or not.
+    :return: True if the current process is running with elevated permissions.
+    """
+    try:
+        return os.getuid() == 0
+    except AttributeError:
+        return ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+
+def verify_elevated_permissions():
+    """
+    Checks whether the current process is running with elevated (admin)
+    permissions or not and raises an error if it is not.
+    """
+    if not is_running_with_elevated_permissions():
+        raise PermissionError('Please run the migration tool with administrator permissions.')
+
+
+def verify_force_if_restoring(is_force_set: bool, migration_action: MigrationAction):
+    """
+    Checks whether the current process is running with elevated (admin)
+    permissions or not and raises an error if it is not.
+    """
+    if not is_force_set and migration_action == MigrationAction.RESTORE:
+        raise MigrationError(RESTORE_WITHOUT_FORCE_FLAG_WARNING)
