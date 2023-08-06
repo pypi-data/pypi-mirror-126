2b64a0473e440885c1416ad8e767c306effbdd6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:44 +0000

    Add package p21octmmr-filler/0.0.5/p21octmmr-filler-0.0.5.tar.gz

---
+++ packages/p21octmmr-filler/p21octmmr-filler-0.0.5.tar.gz/p21octmmr-filler-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+entry_points = \
+        {'console_scripts': ['p21octmmr_filler = src.p21octmmr_filler:main']}
+
+setuptools.setup(
+    name="p21octmmr-filler",
+    version="0.0.5",
+    author="yyokoyam, myagyu, rishikaw",
+    author_email="yyokoyam@student.42tokyo.jp, myagyu@student.42tokyo.jp, rishikaw@student.42tokyo.jp",
+    description="",
+    package_dir={'src/': 'src'},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points=entry_points,
+)
