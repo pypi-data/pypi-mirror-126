057ee2793c25e8131e48e4e2cc39acbce3b8f829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:19 +0000

    Add package fc-bigdatazz/1.0/fc_bigdatazz-1.0.tar.gz

---
+++ packages/fc-bigdatazz/fc_bigdatazz-1.0.tar.gz/fc_bigdatazz-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name = "fc_bigdatazz",
+      version = "1.0",
+      py_modules = ["tingting"],
+      author = "大数据3",
+      author_email = "1478849004@qq.com",
+      description = "大数据3"
+      )
