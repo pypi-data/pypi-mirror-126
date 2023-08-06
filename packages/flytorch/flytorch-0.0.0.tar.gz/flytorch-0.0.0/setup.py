33583bea7a948f370a8946e9487fcfc5fe2a53da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:06 +0000

    Add package flytorch/0.0.0/flytorch-0.0.0.tar.gz

---
+++ packages/flytorch/flytorch-0.0.0.tar.gz/flytorch-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='flytorch',
+    version='0.0.0',
+    license='Apache-2.0',
+    author="The FLyTorch Team",
+    packages=find_packages(),
+    install_requires=[],
+)
