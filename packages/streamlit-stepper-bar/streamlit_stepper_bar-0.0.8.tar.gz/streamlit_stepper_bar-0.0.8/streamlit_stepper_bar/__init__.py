f7b7f4146fc00909e3afd54aaffdc9d2060a3f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:02 +0000

    Add package streamlit-stepper-bar/0.0.8/streamlit_stepper_bar-0.0.8.tar.gz

---
+++ packages/streamlit-stepper-bar/streamlit_stepper_bar-0.0.8.tar.gz/streamlit_stepper_bar-0.0.8/streamlit_stepper_bar/__init__.py
@@ -0,0 +1,4 @@
+from .streamlit_stepper_bar_horizontal import stepper_bar_horizontal
+from .streamlit_stepper_bar_vertical import stepper_bar_vertical
+from streamlit_stepper_bar.version import __release__, __version__
+
---
+++ packages/streamlit-stepper-bar/streamlit_stepper_bar-0.0.8.tar.gz/streamlit_stepper_bar-0.0.8/streamlit_stepper_bar/streamlit_stepper_bar_vertical/__init__.py
@@ -0,0 +1,19 @@
+import os
+import streamlit.components.v1 as components
+from streamlit.components.v1.components import CustomComponent
+from typing import List
+
+from streamlit_stepper_bar.version import __release__, __version__
+
+if __release__:
+    absolute_path = os.path.dirname(os.path.abspath(__file__))
+    build_path = os.path.join(absolute_path, "frontend/build")
+    _component_func = components.declare_component("stepper_bar_vertical", path=build_path)
+else:
+    _component_func = components.declare_component("stepper_bar_vertical", url="http://localhost:3000")
+
+def stepper_bar_vertical(steps: List[str], default=None, is_completed=False) -> CustomComponent:
+    component_value = _component_func(steps=steps, default=default, is_completed=is_completed)
+    if component_value is None:
+        return 0
+    return component_value
