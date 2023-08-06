6ad49aebc302eea2508d1d2e31f0aeab492d2442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:59 +0000

    Add package thirdweb-web3/5.24.7/thirdweb_web3-5.24.7-py2-none-any.whl

---
+++ packages/thirdweb-web3/thirdweb_web3-5.24.7-py2-none-any.whl/thirdweb_web3/auto/ipc.py
@@ -0,0 +1,6 @@
+from thirdweb_web3 import (
+    IPCProvider,
+    Web3,
+)
+
+w3 = Web3(IPCProvider())
---
+++ packages/thirdweb-web3/thirdweb_web3-5.24.7-py2-none-any.whl/thirdweb_web3/auto/websocket.py
@@ -0,0 +1,6 @@
+from thirdweb_web3 import (
+    Web3,
+    WebsocketProvider,
+)
+
+w3 = Web3(WebsocketProvider())
