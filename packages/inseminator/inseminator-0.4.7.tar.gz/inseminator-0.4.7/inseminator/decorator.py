4dcb3bac29d493e5907a9ab8c2ef095b0dd30c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:46 +0000

    Add package inseminator/0.4.7/inseminator-0.4.7.tar.gz

---
+++ packages/inseminator/inseminator-0.4.7.tar.gz/inseminator-0.4.7/inseminator/decorator.py
@@ -0,0 +1,87 @@
+import inspect
+import time
+from functools import wraps
+from threading import Lock
+from typing import Any, Callable, Dict, Mapping, Optional, Type, TypeVar, cast
+
+from .metrics import Metrics
+from .resolver import DependencyResolver
+
+
+class DecoratorResolver:
+    def __init__(
+        self, resolver: DependencyResolver, metrics: Optional[Metrics] = None, cache_enabled: bool = True
+    ) -> None:
+        self.__resolver = resolver
+        self.__metrics = metrics
+        self.__cache_enabled = cache_enabled
+        self.__cache: Dict[str, Any] = {}
+        self.__lock = Lock()
+        self.__parameters: Optional[Mapping[str, inspect.Parameter]] = None
+
+    def preload(self) -> None:
+        self.__cache = self.construct_dependencies()
+
+    def clear_cache(self) -> None:
+        self.__cache.clear()
+
+    def construct_dependencies(self) -> Dict[str, Any]:
+        injected_args: Dict[str, Any] = {}
+
+        if self.__parameters is None:
+            raise Exception("parameters not set")
+
+        for parameter_name, parameter in self.__parameters.items():
+            default = parameter.default
+
+            if (
+                parameter_name == "return"
+                or default == inspect.Signature.empty
+                or not isinstance(default, ParameterDependence)
+            ):
+                continue
+
+            dependency: Any = self.__resolver.resolve(default.parameter_dependency).get_instance()
+            injected_args[parameter_name] = dependency
+
+        return injected_args
+
+    def inject_function(self, fn: Callable[..., Any]) -> Callable[..., Any]:
+        self.__parameters = inspect.signature(fn).parameters
+
+        @wraps(fn)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            t1 = time.perf_counter()
+            injected_args: Dict[str, Any] = {}
+
+            if self.__cache_enabled:
+                self.__lock.acquire()
+
+                if not self.__cache:
+                    self.__cache = self.construct_dependencies()
+
+                self.__lock.release()
+
+                injected_args = self.__cache
+            else:
+                injected_args = self.construct_dependencies()
+
+            if self.__metrics is not None:
+                dt = time.perf_counter() - t1
+                self.__metrics.save_metric(fn.__name__, dt)
+
+            return fn(*args, **{**injected_args, **kwargs})
+
+        return wrapper
+
+
+class ParameterDependence:
+    def __init__(self, parameter_dependency: Any) -> None:
+        self.parameter_dependency = parameter_dependency
+
+
+T = TypeVar("T")
+
+
+def Depends(parameter_type: Type[T]) -> T:
+    return cast(T, ParameterDependence(parameter_type))
---
+++ packages/inseminator/inseminator-0.4.7.tar.gz/inseminator-0.4.7/inseminator/resolver.py
@@ -0,0 +1,75 @@
+import inspect
+from typing import Any, Callable, Dict, Optional, Protocol, Type, Union, cast, get_type_hints
+
+from pydantic import BaseSettings
+
+from .dependency import Dependency, StaticDependency
+from .exceptions import ResolverError
+from .scoped_dict import ScopedDict
+
+Dependable = Union[Callable[..., Any], Type[Any]]
+
+
+class DependencyResolver:
+    def __init__(self, container: ScopedDict[Dependable, Dependency]) -> None:
+        self._container = container
+
+    def resolve(self, dependency: Dependable, parameters: Optional[Dict[str, Any]] = None) -> Dependency:
+        if dependency in self._container:
+            return self._container[dependency]
+
+        callable_dependency: Callable[..., Any]
+        is_class = False
+        if inspect.isclass(dependency):
+            if issubclass(cast(type, dependency), BaseSettings):
+                return StaticDependency(dependency())
+
+            if issubclass(cast(type, dependency), cast(type, Protocol)):
+                raise ResolverError(f"Implementation for {dependency.__name__} protocol is not defined.")
+
+            callable_dependency = dependency.__init__  # type: ignore
+            is_class = True
+        elif inspect.isfunction(dependency):
+            callable_dependency = dependency
+        else:
+            raise ResolverError(f"Dependency must be a function or a class, {dependency} found.")
+
+        type_hints = get_type_hints(callable_dependency)
+        args: Dict[str, Any] = {}
+
+        if parameters:
+            parameter_names = type_hints.keys()
+
+            for parameter_name, parameter_value in parameters.items():
+                if parameter_name not in parameter_names:
+                    raise ResolverError(f"Parameter {parameter_name} it not part of {dependency.__name__}'s signature.")
+
+                args[parameter_name] = parameter_value
+
+        signature_parameters = inspect.signature(callable_dependency).parameters
+        for parameter_name, parameter_dependency in type_hints.items():
+            if parameter_name == "return" or parameter_name in args:
+                continue
+
+            if (default_value := signature_parameters[parameter_name].default) != inspect.Signature.empty:
+                args[parameter_name] = default_value
+            else:
+                try:
+                    args[parameter_name] = self.resolve(parameter_dependency).get_instance()
+                except ResolverError as e:
+                    raise ResolverError(f"{dependency} -> " + str(e))
+
+        skippable_params = (inspect.Parameter.VAR_POSITIONAL, inspect.Parameter.VAR_KEYWORD)
+        injectable_arguments = [p for n, p in signature_parameters.items() if p.kind not in skippable_params]
+        number_of_parameters = len(injectable_arguments)
+
+        if is_class:
+            # because the callable is init and the self must be skipped
+            number_of_parameters -= 1
+
+        if (missing := number_of_parameters - len(args)) > 0:
+            raise ResolverError(
+                f"Can resolve dependencies for {dependency}. All type annotations must be specified, {missing} missing."
+            )
+
+        return StaticDependency(instance=dependency(**args))
