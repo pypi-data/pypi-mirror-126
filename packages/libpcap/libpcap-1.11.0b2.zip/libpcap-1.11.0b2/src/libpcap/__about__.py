691dcdb8fd1ee41e85f6ecf45a7fd8d07453696a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:09 +0000

    Add package libpcap/1.11.0b2/libpcap-1.11.0b2.zip

---
+++ packages/libpcap/libpcap-1.11.0b2.zip/libpcap-1.11.0b2/src/libpcap/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2016-2021, Adam Karpierz
+# Licensed under the BSD license
+# https://opensource.org/licenses/BSD-3-Clause
+
+__import__("pkg_about").about()
+__copyright__ = f"Copyright (c) 2016-2021, {__author__}"  # noqa
