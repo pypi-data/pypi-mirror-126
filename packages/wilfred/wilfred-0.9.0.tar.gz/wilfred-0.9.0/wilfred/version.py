f05b3a7d0adf5779d82cd3422c9f45d4d6319207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:39 +0000

    Add package wilfred/0.9.0/wilfred-0.9.0.tar.gz

---
+++ packages/wilfred/wilfred-0.9.0.tar.gz/wilfred-0.9.0/wilfred/version.py
@@ -0,0 +1,16 @@
+#################################################################
+#                                                               #
+# Wilfred                                                       #
+# Copyright (C) 2020-2021, Vilhelm Prytz, <vilhelm@prytznet.se> #
+#                                                               #
+# Licensed under the terms of the MIT license, see LICENSE.     #
+# https://github.com/wilfred-dev/wilfred                        #
+#                                                               #
+#################################################################
+
+# use "0.0.0.dev0" to indicate incremental commit build
+# specify version "0.3.0" to indicate standard release
+# "commit_date" should be replaced upon building
+version = "0.9.0"
+commit_hash = "031f8d7b61e110c497587278366a8f9397ad8450"
+commit_date = "2021-11-09"
