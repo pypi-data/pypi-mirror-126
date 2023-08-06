ded26080972fb438c2be1bdc2a55d30c822a84bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:33 +0000

    Add package LaoNLP/0.5/LaoNLP-0.5.tar.gz

---
+++ packages/LaoNLP/LaoNLP-0.5.tar.gz/LaoNLP-0.5/laonlp/corpus/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import laonlp
+import os
+
+laonlp_path = os.path.dirname(laonlp.__file__)
+
+from laonlp.corpus.lao_words import *
+from laonlp.corpus.core import get_path_corpus
+
+__all__ = [
+    "lao_dictionary",
+    "lo_spellcheckdict",
+    "lao_words",
+    "lo_wannaphongdict",
+    "lo_wiktionarydict",
+    "get_path_corpus"
+]
---
+++ packages/LaoNLP/LaoNLP-0.5.tar.gz/LaoNLP-0.5/laonlp/tag/pos_tag.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from typing import List, Tuple
+from laonlp.corpus import get_path_corpus
+from pythainlp.tag import PerceptronTagger
+
+_FILENAME = "ptagger_SeqLabeling_corpus.json"
+_PATH = get_path_corpus(_FILENAME)
+_TAGGER = PerceptronTagger(path=_PATH)
+
+
+def pos_tag(
+    words: List[str],
+    engine: str = "perceptron",
+    corpus: str = "SeqLabeling"
+) -> List[Tuple[str, str]]:
+    """
+    Lao Part-of-speech
+
+    We use lao corpus from https://github.com/FoVNull/SeqLabeling
+
+    :param List[str] words:  a list of tokenized lao words
+    :param str engine: engine of pos_tag (perceptron engine)
+    :param str corpus: SeqLabeling (corpus from https://github.com/FoVNull/SeqLabeling)
+
+    :return: a list of tuples (word, POS tag)
+    :rtype: List[tuple[str, str]]
+
+    :Example:
+    ::
+        from laonlp.tokenize import word_tokenize
+        from laonlp.tag import pos_tag
+
+        sent = word_tokenize("ພາສາລາວໃນປັດຈຸບັນ.")
+        pos_tag(sent)
+        # output: [('ພາສາລາວ', 'N'), ('ໃນ', 'PRE'), ('ປັດຈຸບັນ', 'ADJ'), ('.', 'PUNCT')]
+    """
+    return _TAGGER.tag(words)
