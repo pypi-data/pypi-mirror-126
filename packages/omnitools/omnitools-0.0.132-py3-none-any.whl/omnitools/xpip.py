c5f03b4c8108e76ff989c83d229e697f5c0da593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:37 +0000

    Add package omnitools/0.0.132/omnitools-0.0.132-py3-none-any.whl

---
+++ packages/omnitools/omnitools-0.0.132-py3-none-any.whl/omnitools/xpip.py
@@ -0,0 +1,74 @@
+import pip
+
+
+def PIP(*args):
+    if hasattr(pip, "main"):
+        pip.main(list(args))
+    else:
+        pip._internal.main(list(args))
+
+
+def install(*args):
+    PIP("install", *args)
+
+
+def uninstall(*args):
+    PIP("uninstall", *args)
+
+
+def download(*args):
+    PIP("download", *args)
+
+
+def freeze(*args):
+    PIP("freeze", *args)
+
+
+def list(*args):
+    PIP("list", *args)
+
+
+def show(*args):
+    PIP("show", *args)
+
+
+def check(*args):
+    PIP("check", *args)
+
+
+def config(*args):
+    PIP("config", *args)
+
+
+def search(*args):
+    PIP("search", *args)
+
+
+def cache(*args):
+    PIP("cache", *args)
+
+
+def index(*args):
+    PIP("index", *args)
+
+
+def wheel(*args):
+    PIP("wheel", *args)
+
+
+def hash(*args):
+    PIP("hash", *args)
+
+
+def completion(*args):
+    PIP("completion", *args)
+
+
+def debug(*args):
+    PIP("debug", *args)
+
+
+def help(*args):
+    PIP("help", *args)
+
+
