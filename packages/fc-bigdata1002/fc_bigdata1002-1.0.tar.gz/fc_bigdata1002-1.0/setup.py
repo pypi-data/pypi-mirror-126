4714dc1c9fba678c87709afee8d25d29b11327ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:29 +0000

    Add package fc-bigdata1002/1.0/fc_bigdata1002-1.0.tar.gz

---
+++ packages/fc-bigdata1002/fc_bigdata1002-1.0.tar.gz/fc_bigdata1002-1.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+
+setup(
+    name = "fc_bigdata1002",
+    version="1.0",
+    py_modules =["fc_yyw-bigdata3"],
+    author = "tttt",
+    author_emall = "532816753@qq.com",
+    descrption ="ttt"
+
+)
