062042d4039d56b059ff4d7407ddce267d3f9803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:20 +0000

    Add package ufoLib2/0.12.0/ufoLib2-0.12.0.tar.gz

---
+++ packages/ufoLib2/ufoLib2-0.12.0.tar.gz/ufoLib2-0.12.0/src/ufoLib2/objects/features.py
@@ -0,0 +1,18 @@
+from attr import define
+
+
+@define
+class Features:
+    """A data class representing UFO features.
+
+    See http://unifiedfontobject.org/versions/ufo3/features.fea/.
+    """
+
+    text: str = ""
+    """Holds the content of the features.fea file."""
+
+    def __bool__(self) -> bool:
+        return bool(self.text)
+
+    def __str__(self) -> str:
+        return self.text
