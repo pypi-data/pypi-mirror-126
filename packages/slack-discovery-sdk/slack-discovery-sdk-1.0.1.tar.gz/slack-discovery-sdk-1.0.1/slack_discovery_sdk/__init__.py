19f619c7eac92cb8dfb4adc13c38fad5b5373927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:19 +0000

    Add package slack-discovery-sdk/1.0.1/slack-discovery-sdk-1.0.1.tar.gz

---
+++ packages/slack-discovery-sdk/slack-discovery-sdk-1.0.1.tar.gz/slack-discovery-sdk-1.0.1/slack_discovery_sdk/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2021, Slack Technologies, LLC. All rights reserved.
+# pytype: skip-file
+
+"""The Slack Web API allows you to build applications that interact with Slack
+in more complex ways than the integrations we provide out of the box."""
+from .client import DiscoveryClient  # noqa
+from .oauth import DiscoveryOAuthApp  # noqa
