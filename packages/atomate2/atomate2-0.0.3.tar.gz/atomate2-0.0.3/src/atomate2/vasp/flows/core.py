0c343ed82d8658b276786ad56037ecc0fd163664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:12 +0000

    Add package atomate2/0.0.3/atomate2-0.0.3.tar.gz

---
+++ packages/atomate2/atomate2-0.0.3.tar.gz/atomate2-0.0.3/src/atomate2/vasp/flows/core.py
@@ -0,0 +1,217 @@
+"""Core VASP flows."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from jobflow import Flow, Maker
+from pymatgen.core.structure import Structure
+
+from atomate2.vasp.jobs.base import BaseVaspMaker
+from atomate2.vasp.jobs.core import (
+    HSEBSMaker,
+    HSEStaticMaker,
+    NonSCFMaker,
+    RelaxMaker,
+    StaticMaker,
+)
+from atomate2.vasp.schemas.calculation import VaspObject
+
+__all__ = [
+    "DoubleRelaxMaker",
+    "BandStructureMaker",
+    "HSEBandStructureMaker",
+    "RelaxBandStructureMaker",
+]
+
+
+@dataclass
+class DoubleRelaxMaker(Maker):
+    """
+    Maker to perform a double VASP relaxation.
+
+    Parameters
+    ----------
+    name : str
+        Name of the flows produced by this maker.
+    relax_maker : .BaseVaspMaker
+        Maker to use to generate the relaxations.
+    """
+
+    name: str = "double relax"
+    relax_maker: BaseVaspMaker = field(default_factory=RelaxMaker)
+
+    def make(self, structure: Structure, prev_vasp_dir: str | Path | None = None):
+        """
+        Create a flow with two chained relaxations.
+
+        Parameters
+        ----------
+        structure : .Structure
+            A pymatgen structure object.
+        prev_vasp_dir : str or Path or None
+            A previous VASP calculation directory to copy output files from.
+
+        Returns
+        -------
+        Flow
+            A flow containing two relaxations.
+        """
+        relax1 = self.relax_maker.make(structure, prev_vasp_dir=prev_vasp_dir)
+        relax1.name += " 1"
+
+        relax2 = self.relax_maker.make(
+            relax1.output.structure, prev_vasp_dir=relax1.output.dir_name
+        )
+        relax2.name += " 2"
+
+        return Flow([relax1, relax2], relax2.output, name=self.name)
+
+
+@dataclass
+class BandStructureMaker(Maker):
+    """
+    Maker to generate VASP band structures.
+
+    This is a static calculation followed by two non-self-consistent field calculations,
+    one uniform and one line mode.
+
+    Parameters
+    ----------
+    name : str
+        Name of the flows produced by this maker.
+    bandstructure_type : str
+        The type of band structure to generate. Options are "line", "uniform" or "both".
+    static_maker : .BaseVaspMaker
+        The maker to use for the static calculation.
+    bs_maker : .BaseVaspMaker
+        The maker to use for the non-self-consistent field calculations.
+    """
+
+    name: str = "band structure"
+    bandstructure_type: str = "both"
+    static_maker: BaseVaspMaker = field(default_factory=StaticMaker)
+    bs_maker: BaseVaspMaker = field(default_factory=NonSCFMaker)
+
+    def make(self, structure: Structure, prev_vasp_dir: str | Path | None = None):
+        """
+        Create a band structure flow.
+
+        Parameters
+        ----------
+        structure : Structure
+            A pymatgen structure object.
+        prev_vasp_dir : str or Path or None
+            A previous VASP calculation directory to copy output files from.
+
+        Returns
+        -------
+        Flow
+            A band structure flow.
+        """
+        static_job = self.static_maker.make(structure, prev_vasp_dir=prev_vasp_dir)
+        jobs = [static_job]
+
+        outputs = {}
+        if self.bandstructure_type in ("both", "uniform"):
+            uniform_job = self.bs_maker.make(
+                structure, prev_vasp_dir=static_job.output.dir_name, mode="uniform"
+            )
+            uniform_job.name += " uniform"
+            jobs.append(uniform_job)
+            output = {
+                "uniform": uniform_job.output,
+                "uniform_bs": uniform_job.output.vasp_objects[VaspObject.BANDSTRUCTURE],
+            }
+            outputs.update(output)
+
+        if self.bandstructure_type in ("both", "line"):
+            line_job = self.bs_maker.make(
+                structure, prev_vasp_dir=static_job.output.dir_name, mode="line"
+            )
+            line_job.name += " line"
+            jobs.append(line_job)
+            output = {
+                "line": line_job.output,
+                "line_bs": line_job.output.vasp_objects[VaspObject.BANDSTRUCTURE],
+            }
+            outputs.update(output)
+
+        if self.bandstructure_type not in ("both", "line", "uniform"):
+            raise ValueError(
+                f"Unrecognised bandstructure type {self.bandstructure_type}"
+            )
+
+        return Flow(jobs, outputs, name=self.name)
+
+
+@dataclass
+class HSEBandStructureMaker(BandStructureMaker):
+    """
+    Maker to generate VASP HSE band structures.
+
+    This is a HSE06 static calculation followed by one HSE06 uniform calculation and
+    one HSE06 line mode calculation.
+
+    Parameters
+    ----------
+    name : str
+        Name of the flows produced by this maker.
+    bandstructure_type : str
+        The type of band structure to generate. Options are "line", "uniform" or "both".
+    static_maker : .BaseVaspMaker
+        The maker to use for the static calculation.
+    bs_maker : .BaseVaspMaker
+        The maker to use for the line and uniform band structure calculations.
+    """
+
+    name: str = "hse band structure"
+    bandstructure_type: str = "both"
+    static_maker: BaseVaspMaker = field(default_factory=HSEStaticMaker)
+    bs_maker: BaseVaspMaker = field(default_factory=HSEBSMaker)
+
+
+@dataclass
+class RelaxBandStructureMaker(Maker):
+    """
+    Make to create a flow with a relaxation and then band structure calculations.
+
+    By default, this workflow generates relaxations using the :obj:`.DoubleRelaxMaker`.
+
+    Parameters
+    ----------
+    name : str
+        Name of the flows produced by this maker.
+    relax_maker : .BaseVaspMaker
+        The maker to use for the static calculation.
+    band_structure_maker : .BaseVaspMaker
+        The maker to use for the line and uniform band structure calculations.
+    """
+
+    name: str = "relax and band structure"
+    relax_maker: BaseVaspMaker = field(default_factory=DoubleRelaxMaker)
+    band_structure_maker: BaseVaspMaker = field(default_factory=BandStructureMaker)
+
+    def make(self, structure: Structure, prev_vasp_dir: str | Path | None = None):
+        """
+        Run a relaxation and then calculate the uniform and line mode band structures.
+
+        Parameters
+        ----------
+        structure: .Structure
+            A pymatgen structure object.
+        prev_vasp_dir : str or Path or None
+            A previous VASP calculation directory to copy output files from.
+
+        Returns
+        -------
+        Flow
+            A relax and band structure flow.
+        """
+        relax_job = self.relax_maker.make(structure, prev_vasp_dir=prev_vasp_dir)
+        bs_flow = self.band_structure_maker.make(
+            relax_job.output.structure, prev_vasp_dir=relax_job.output.dir_name
+        )
+
+        return Flow([relax_job, bs_flow], bs_flow.output, name=self.name)
---
+++ packages/atomate2/atomate2-0.0.3.tar.gz/atomate2-0.0.3/src/atomate2/vasp/jobs/core.py
@@ -0,0 +1,411 @@
+"""Core jobs for running VASP calculations."""
+
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from pymatgen.core.structure import Structure
+
+from atomate2.vasp.jobs.base import BaseVaspMaker, vasp_job
+from atomate2.vasp.sets.base import VaspInputSetGenerator
+from atomate2.vasp.sets.core import (
+    HSEBSSetGenerator,
+    HSERelaxSetGenerator,
+    HSEStaticSetGenerator,
+    HSETightRelaxSetGenerator,
+    NonSCFSetGenerator,
+    RelaxSetGenerator,
+    StaticSetGenerator,
+    TightRelaxSetGenerator,
+)
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "StaticMaker",
+    "RelaxMaker",
+    "NonSCFMaker",
+    "DielectricMaker",
+    "HSEBSMaker",
+    "HSERelaxMaker",
+    "HSEStaticMaker",
+    "TightRelaxMaker",
+    "HSETightRelaxMaker",
+]
+
+
+@dataclass
+class StaticMaker(BaseVaspMaker):
+    """
+    Maker to create VASP static jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "static"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=StaticSetGenerator
+    )
+
+
+@dataclass
+class RelaxMaker(BaseVaspMaker):
+    """
+    Maker to create VASP relaxation jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "relax"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=RelaxSetGenerator
+    )
+
+
+@dataclass
+class TightRelaxMaker(BaseVaspMaker):
+    """
+    Maker to create tight VASP relaxation jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "tight relax"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=TightRelaxSetGenerator
+    )
+
+
+@dataclass
+class NonSCFMaker(BaseVaspMaker):
+    """
+    Maker to create non self consistent field VASP jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "non-scf"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=NonSCFSetGenerator
+    )
+
+    @vasp_job
+    def make(
+        self,
+        structure: Structure,
+        prev_vasp_dir: str | Path | None,
+        mode: str = "uniform",
+    ):
+        """
+        Run a non-scf VASP job.
+
+        Parameters
+        ----------
+        structure : .Structure
+            A pymatgen structure object.
+        prev_vasp_dir : str or Path or None
+            A previous VASP calculation directory to copy output files from.
+        mode : str
+            Type of band structure calculation. Options are:
+            - "line": Full band structure along symmetry lines.
+            - "uniform": Uniform mesh band structure.
+        """
+        self.input_set_generator.mode = mode
+
+        if "parse_dos" not in self.task_document_kwargs:
+            # parse DOS only for uniform band structure
+            self.task_document_kwargs["parse_dos"] = mode == "uniform"
+
+        if "parse_bandstructure" not in self.task_document_kwargs:
+            self.task_document_kwargs["parse_bandstructure"] = mode
+
+        # copy previous inputs
+        if "additional_vasp_files" not in self.copy_vasp_kwargs:
+            self.copy_vasp_kwargs["additional_vasp_files"] = ("CHGCAR",)
+
+        return super().make.original(self, structure, prev_vasp_dir)
+
+
+@dataclass
+class HSERelaxMaker(BaseVaspMaker):
+    """
+    Maker to create HSE06 relaxation jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "hse relax"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=HSERelaxSetGenerator
+    )
+
+
+@dataclass
+class HSETightRelaxMaker(BaseVaspMaker):
+    """
+    Maker to create tight VASP relaxation jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator
+        A generator used to make the input set.
+    write_input_set_kwargs
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    run_vasp_kwargs
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "hse tight relax"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=HSETightRelaxSetGenerator
+    )
+
+
+@dataclass
+class HSEStaticMaker(BaseVaspMaker):
+    """
+    Maker to create HSE06 static jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "hse static"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=HSEStaticSetGenerator
+    )
+
+
+@dataclass
+class HSEBSMaker(BaseVaspMaker):
+    """
+    Maker to create HSE06 band structure jobs.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .VaspInputSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "hse band structure"
+    input_set_generator: VaspInputSetGenerator = field(
+        default_factory=HSEBSSetGenerator
+    )
+
+    @vasp_job
+    def make(
+        self,
+        structure: Structure,
+        prev_vasp_dir: str | Path | None = None,
+        mode="uniform",
+    ):
+        """
+        Run a HSE06 band structure VASP job.
+
+        Parameters
+        ----------
+        structure : .Structure
+            A pymatgen structure object.
+        prev_vasp_dir : str or Path or None
+            A previous VASP calculation directory to copy output files from.
+        mode : str
+            Type of band structure calculation. Options are:
+            - "line": Full band structure along symmetry lines.
+            - "uniform": Uniform mesh band structure.
+            - "gap": Get the energy at the CBM and VBM.
+        """
+        self.input_set_generator.mode = mode
+
+        if mode == "gap" and prev_vasp_dir is None:
+            logger.warning(
+                "HSE band structure in 'gap' mode requires a previous VASP calculation "
+                "directory from which to extract the VBM and CBM k-points. This "
+                "calculation will instead be a standard uniform calculation."
+            )
+            mode = "uniform"
+
+        if "parse_dos" not in self.task_document_kwargs:
+            # parse DOS only for uniform band structure
+            self.task_document_kwargs["parse_dos"] = mode == "uniform"
+
+        if "parse_bandstructure" not in self.task_document_kwargs:
+            parse_bandstructure = "uniform" if mode == "gap" else mode
+            self.task_document_kwargs["parse_bandstructure"] = parse_bandstructure
+
+        # copy previous inputs
+        if (
+            prev_vasp_dir is not None
+            and "additional_vasp_files" not in self.copy_vasp_kwargs
+        ):
+            self.copy_vasp_kwargs["additional_vasp_files"] = ("CHGCAR",)
+
+        return super().make.original(self, structure, prev_vasp_dir)
+
+
+@dataclass
+class DielectricMaker(BaseVaspMaker):
+    """
+    Maker to create dielectric calculation VASP jobs.
+
+    .. Note::
+        The input structure should be well relaxed to avoid imaginary modes. For
+        example, using :obj:`TightRelaxMaker`.
+
+    Parameters
+    ----------
+    name : str
+        The job name.
+    input_set_generator : .StaticSetGenerator
+        A generator used to make the input set.
+    write_input_set_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.write_vasp_input_set`.
+    copy_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.copy_vasp_outputs`.
+    run_vasp_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.run_vasp`.
+    task_document_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.TaskDocument.from_directory`.
+    stop_children_kwargs : dict
+        Keyword arguments that will get passed to :obj:`.should_stop_children`.
+    write_additional_data : dict
+        Additional data to write to the current directory. Given as a dict of
+        {filename: data}.
+    """
+
+    name: str = "dielectric"
+    input_set_generator: StaticSetGenerator = field(
+        default_factory=lambda: StaticSetGenerator(lepsilon=True)
+    )
