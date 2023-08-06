4ec51940e880d48843b06f6b31f9939035f1df97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:16 +0000

    Add package DACPYLib/0.0.6/DACPYLib-0.0.6.tar.gz

---
+++ packages/DACPYLib/DACPYLib-0.0.6.tar.gz/DACPYLib-0.0.6/src/etl_tools/file_tools.py
@@ -0,0 +1,11 @@
+from pyspark import dbutils
+
+def file_exists(path):
+    try:
+        dbutils.fs.ls(path)
+        return True
+    except Exception as e:
+        if 'java.io.FileNotFoundException' in str(e):
+            return False
+        else:
+            raise
