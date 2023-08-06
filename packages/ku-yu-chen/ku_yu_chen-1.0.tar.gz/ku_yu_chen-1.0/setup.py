a2b192cb0d84d56ebe098ceadcb45140966c80a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:15 +0000

    Add package ku-yu-chen/1.0/ku_yu_chen-1.0.tar.gz

---
+++ packages/ku-yu-chen/ku_yu_chen-1.0.tar.gz/ku_yu_chen-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name = "ku_yu_chen",
+    version = "1.0",
+    py_modules = ["fc_yu_chen_chen"],
+    author = "于晨晨",
+    author_email = "2790896061@qq.com",
+    description = "使用方法：这个包只有一个变量name和age"
+)
