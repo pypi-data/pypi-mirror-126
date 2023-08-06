27566a194749ca5b617e38223820383f79410100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:36 +0000

    Add package test-pack-yasak2/0.1.2/test_pack_yasak2-0.1.2.tar.gz

---
+++ packages/test-pack-yasak2/test_pack_yasak2-0.1.2.tar.gz/test_pack_yasak2-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+        name="test_pack_yasak2",
+
+        version="0.1.2",
+
+        author="yasak",
+
+        author_email="yamato02031996@gmail.com",
+
+        description="this is a test package",
+
+        long_description=long_description,
+
+        long_description_content_type="text/markdown",
+
+        packages=setuptools.find_packages(),
+
+        license="42Tokyo",
+
+        python_requires='>=3.7',
+
+        )
