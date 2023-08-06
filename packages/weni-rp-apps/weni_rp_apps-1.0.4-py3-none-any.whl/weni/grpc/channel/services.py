1844f48ce9eaa4006d555f47dcc32cdd595bd02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:09 +0000

    Add package weni-rp-apps/1.0.4/weni_rp_apps-1.0.4-py3-none-any.whl

---
+++ packages/weni-rp-apps/weni_rp_apps-1.0.4-py3-none-any.whl/weni/grpc/channel/services.py
@@ -0,0 +1,28 @@
+from django_grpc_framework import generics, mixins
+
+from temba.channels.types.weniwebchat.type import WeniWebChatType
+from temba.channels.models import Channel
+from weni.grpc.channel.serializers import WeniWebChatProtoSerializer, ChannelProtoSerializer
+
+
+# this class will be deprecated
+class WeniWebChatService(mixins.CreateModelMixin, mixins.DestroyModelMixin, generics.GenericService):
+
+    channel_type = WeniWebChatType
+    serializer_class = WeniWebChatProtoSerializer
+
+
+class ChannelService(generics.DestroyService):
+
+    queryset = Channel.objects
+    lookup_field = "uuid"
+    serializer_class = ChannelProtoSerializer
+
+    def perform_destroy(self, instance):
+        serializer = self.get_serializer(message=self.request)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data.get("user")
+        instance.release(user)
+
+    def Create(self, request, context):
+        print("oi")
