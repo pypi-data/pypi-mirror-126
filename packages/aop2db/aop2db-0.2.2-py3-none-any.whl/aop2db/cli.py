5a7d95b1278dbbb8e65c2911cf6d0d55865f2d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:32:13 +0000

    Add package aop2db/0.2.2/aop2db-0.2.2-py3-none-any.whl

---
+++ packages/aop2db/aop2db-0.2.2-py3-none-any.whl/aop2db/cli.py
@@ -0,0 +1,43 @@
+"""Console script for aop2db."""
+import logging
+import sys
+
+import click
+
+from aop2db.aop.importer import import_aop_data
+from aop2db.utils import set_conn
+
+from sqlalchemy_utils import create_database, database_exists
+
+logger = logging.getLogger(__name__)
+
+
+@click.group(help=f"AOP2DB Database Framework Command Line Utilities on {sys.executable}")
+@click.version_option()
+def main():
+    """Console script for aop2db."""
+    pass
+
+
+@main.command()
+def load():
+    """Download the AO wiki data and imports into the relational database."""
+    import_aop_data()
+
+
+@main.command()
+@click.argument('conn_str')
+def conn(conn_str: str):
+    """Set the SQL connection string in the configuration file.
+
+    conn_str
+        A python compatible SQL connection string. Drivers (e.g. pymysql) will need to be downloaded manually.
+    """
+    if not database_exists(conn_str):
+        create_database(conn_str)
+
+    set_conn(conn_string=conn_str)
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
