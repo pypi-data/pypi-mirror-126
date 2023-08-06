b4c5318f69e9965368af51668cae2918540bb5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:03 +0000

    Add package wombat-db/0.0.16/wombat_db-0.0.16.tar.gz

---
+++ packages/wombat-db/wombat_db-0.0.16.tar.gz/wombat_db-0.0.16/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+from setuptools import Extension
+import numpy as np
+from Cython.Build import cythonize
+
+__version__ = "0.0.16"
+
+extensions = [
+    Extension(
+        name="cjoin", 
+        sources=["wombat_db/ops/cjoin.c"], 
+        include_dirs=[np.get_include()]
+    )
+]
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='wombat_db',
+    version=__version__,
+    description='Useful data crunching tools for pyarrow',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='APACHE',
+    packages=find_packages(),
+    author='Tom Scheffers',
+    author_email='tom@youngbulls.nl ',
+    keywords=['arrow', 'pyarrow', 'data', 'sql', 'dataframe'],
+    url='https://github.com/TomScheffers/wombat',
+    download_url='https://pypi.org/project/wombat-db/',
+
+    ext_modules=cythonize(extensions),
+    install_requires=[
+        'numpy>=1.19.2',
+        'pyarrow>=5.0.0'
+    ],
+)
