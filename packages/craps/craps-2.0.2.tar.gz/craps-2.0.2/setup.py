ab81a1c5aa61cb52342385bc83fe277a20411c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:18 +0000

    Add package craps/2.0.2/craps-2.0.2.tar.gz

---
+++ packages/craps/craps-2.0.2.tar.gz/craps-2.0.2/setup.py
@@ -0,0 +1,40 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="craps",
+    version="2.0.2",
+    description="A table game engine.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mhegarty/craps",
+    author="Mike Hegarty",
+    author_email="mike@petorca.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["craps",
+              "craps.algos"],
+    include_package_data=True,
+    install_requires=["appdirs"]
+
+    # include_package_data=True,
+    # entry_points={
+    #     "console_scripts": [
+    #         "realpython=reader.__main__:main",
+    #     ]
+    # },
+)
+
+# python setup.py sdist bdist_wheel
+# twine upload --skip-existing dist/*
