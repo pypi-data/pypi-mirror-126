cadba4ba91070848603b6fc2d6eff1985d0d324b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:16 +0000

    Add package MCfun/0.1.1.dev1/mcfun-0.1.1.dev1.tar.gz

---
+++ packages/MCfun/mcfun-0.1.1.dev1.tar.gz/MCfun-0.1.1.dev1/mcfun/__init__.py
@@ -0,0 +1,14 @@
+'''
+Author: Pu Zhichen (hoshishin)
+Date: 2021-11-03 09:36:53
+LastEditTime: 2021-11-07 14:33:53
+LastEditors: Pu Zhichen
+Description: 
+FilePath: \MCfun\mcfun\__init__.py
+
+  Multi-collinear approach
+'''
+__version__ = '0.1.1.dev1'
+
+from .core import eval_mcol
+from . import lib, xcutils
