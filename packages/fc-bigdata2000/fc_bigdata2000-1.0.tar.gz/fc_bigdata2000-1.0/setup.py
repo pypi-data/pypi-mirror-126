295ea762b53d48556e7fb263ac82d197738fd4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:32 +0000

    Add package fc-bigdata2000/1.0/fc_bigdata2000-1.0.tar.gz

---
+++ packages/fc-bigdata2000/fc_bigdata2000-1.0.tar.gz/fc_bigdata2000-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name ='fc_bigdata2000',
+    version = '1.0',
+    py_modules=['fc_xd_bigdata'],
+    author='wangya',
+    author_email='3450128893@qq.com',
+    description='一团棉花'
+)
