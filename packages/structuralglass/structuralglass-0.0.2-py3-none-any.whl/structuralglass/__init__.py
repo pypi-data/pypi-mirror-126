f167f08dc63630fd38219b4a798fed35e0d9340c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:30 +0000

    Add package structuralglass/0.0.2/structuralglass-0.0.2-py3-none-any.whl

---
+++ packages/structuralglass/structuralglass-0.0.2-py3-none-any.whl/structuralglass/__init__.py
@@ -0,0 +1,17 @@
+import pint
+
+from . import resources
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+# Load the file stream for the units file
+unit_file = pkg_resources.open_text(resources, "unit_def.txt")
+
+# Setup pint for the package
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
+ureg.load_definitions(unit_file)
