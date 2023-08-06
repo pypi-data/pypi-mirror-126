2e0e02a8a1e8eedc74f8241f74a2b44e1046b40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:37 +0000

    Add package fc-zcc1/1.0/fc_zcc1-1.0.tar.gz

---
+++ packages/fc-zcc1/fc_zcc1-1.0.tar.gz/fc_zcc1-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fc_zcc1",
+    version ="1.0",
+    py_modules = ["fc_ims"],
+    author = "赵陈陈1",
+    author_email = "fc@zcc.com",
+    description = "赵陈陈1"
+)
