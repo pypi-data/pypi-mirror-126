6fae708c00d68088a857d5dbb088c79eb3bc106b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:05 +0000

    Add package mtpandas/1.5.202111092013/mtpandas-1.5.202111092013-py3-none-any.whl

---
+++ packages/mtpandas/mtpandas-1.5.202111092013-py3-none-any.whl/mt/pandas/dftype.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+from mt import np, cv
+
+
+__all__ = ['isnull', 'get_dftype']
+
+
+def isnull(obj):
+    if obj is None or obj is pd.NaT:
+        return True
+    if isinstance(obj, float) and np.isnan(obj):
+        return True
+    return False
+
+
+def get_dftype(s):
+    '''Detects the dftype of the series.
+
+    Determine whether a series is an ndarray series, a sparse ndarray series, an Image series or a
+    normal series.
+
+    Parameters
+    ----------
+    s : pandas.Series
+        the series to investigate
+
+    Returns
+    -------
+    {'json', 'ndarray', 'SparseNdarray', 'Image', 'str', 'Timestamp','Timedelta', 'object', 'none', etc}
+        the type of the series. If it is a normal series, the string representing the dtype
+        attribute of the series is returned
+    '''
+    if len(s) == 0:
+        return 'object'
+
+    dftype = None
+    has_none = False
+    for x in s.tolist():
+        if isnull(x):
+            has_none = True
+            continue
+        if isinstance(x, str):
+            if dftype is None:
+                dftype = 'str'
+            elif dftype != 'str':
+                break
+            continue
+        if isinstance(x, (list, dict)):
+            if dftype is None:
+                dftype = 'json'
+            elif dftype != 'json':
+                break
+            continue
+        if isinstance(x, np.ndarray):
+            if dftype is None:
+                dftype = 'ndarray'
+            elif dftype != 'ndarray':
+                break
+            continue
+        if isinstance(x, np.SparseNdarray):
+            if dftype is None:
+                dftype = 'SparseNdarray'
+            elif dftype != 'SparseNdarray':
+                break
+            continue
+        if isinstance(x, cv.Image):
+            if dftype is None:
+                dftype = 'Image'
+            elif dftype != 'Image':
+                break
+            continue
+        if isinstance(x, pd.Timestamp):
+            if dftype is None:
+                dftype = 'Timestamp'
+            elif dftype != 'Timestamp':
+                break
+            continue
+        if isinstance(x, pd.Timedelta):
+            if dftype is None:
+                dftype = 'Timedelta'
+            elif dftype != 'Timedelta':
+                break
+            continue
+        dftype = 'object'
+        break
+
+    if dftype is None:
+        return 'none'
+
+    if dftype != 'object':
+        return dftype
+
+    if pd.api.types.is_numeric_dtype(s):
+        return 'float64'
+
+    return str(s.dtype)
