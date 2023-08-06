35a2d45719006bcf7435f4a796584f19ac22c140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package we-BigDatel/1.0/we_BigDatel-1.0.tar.gz

---
+++ packages/we-BigDatel/we_BigDatel-1.0.tar.gz/we_BigDatel-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name = "we_BigDatel",
+    version = "1.0",
+    py_modules= ["fc_ims"],
+    author = "大数据一班",
+    author_email ="fc@bigdata.com",
+    description="大数据"
+)
