3f4461a7fc7eff0ecaed640016b84e846c97f59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:18 +0000

    Add package fc-aaaa/1.0/fc_aaaa-1.0.tar.gz

---
+++ packages/fc-aaaa/fc_aaaa-1.0.tar.gz/fc_aaaa-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "fc_aaaa",
+    version= "1.0",
+    py_modules=["fc_xd_aaaa"],
+    author= "大数据3",
+    author_email="2073894093@qq.com",
+    description= "大数据3"
+)
