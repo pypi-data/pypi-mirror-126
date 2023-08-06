3d3fa520e1611657168d3e5d25063f9265ab0ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:22 +0000

    Add package iuShiWoDe/2.0/iuShiWoDe-2.0.tar.gz

---
+++ packages/iuShiWoDe/iuShiWoDe-2.0.tar.gz/iuShiWoDe-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="iuShiWoDe",     #项目名
+    version = "2.0",        #版本
+    py_modules=["fc_xd_bigdata"],   #生成模块
+    author="李龙辉大帅b",        #作者
+    author_email="dh15139589060@163.com",   #邮箱
+    description="项目信息"              #项目信息
+
+)
