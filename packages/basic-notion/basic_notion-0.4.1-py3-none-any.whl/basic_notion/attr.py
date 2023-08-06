4875477e9b7b27b5b4db020cd1243e0eeb3e4903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:31 +0000

    Add package basic-notion/0.4.1/basic_notion-0.4.1-py3-none-any.whl

---
+++ packages/basic-notion/basic_notion-0.4.1-py3-none-any.whl/basic_notion/attr.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Generic, Optional, TYPE_CHECKING, Type, TypeVar
+
+from basic_notion.utils import get_from_dict, set_to_dict
+
+if TYPE_CHECKING:
+    from basic_notion.base import NotionItemBase
+
+
+_PROP_ATTR_TV = TypeVar('_PROP_ATTR_TV')
+
+
+class ItemAttrDescriptor(Generic[_PROP_ATTR_TV]):
+    """
+    Accesses attributes of Notion objects (pages and page properties).
+    """
+
+    __slots__ = ('__key', '__name', '__editable')
+
+    def __init__(self, key: Optional[tuple[str, ...]] = None, editable: bool = False):
+        self.__key = key
+        self.__name: Optional[str] = None
+        self.__editable = editable
+
+    def __set_name__(self, owner: NotionItemBase, name: str) -> None:
+        self.__name = name
+        if self.__key is None:
+            self.__key = (name,)
+
+    @property
+    def key(self) -> tuple[str, ...]:
+        if self.__key is None:
+            raise AttributeError('Key is not specified')
+        return self.__key
+
+    @property
+    def editable(self) -> bool:
+        return self.__editable
+
+    def __get__(self, instance: NotionItemBase, owner: Type[NotionItemBase]) -> _PROP_ATTR_TV:
+        return get_from_dict(instance.data, self.key)
+
+    def __set__(self, instance: NotionItemBase, value: _PROP_ATTR_TV) -> None:
+        if not self.__editable:
+            raise AttributeError(f'Attribute {self.__name} is not editable')
+
+        set_to_dict(instance.data, self.key, value)
