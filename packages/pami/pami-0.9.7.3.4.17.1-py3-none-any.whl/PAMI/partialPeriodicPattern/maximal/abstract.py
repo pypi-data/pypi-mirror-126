044ddfbe39bd0c3f87a9e7f4ef3dab2999ddec4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:47 +0000

    Add package pami/0.9.7.3.4.17.1/pami-0.9.7.3.4.17.1-py3-none-any.whl

---
+++ packages/pami/pami-0.9.7.3.4.17.1-py3-none-any.whl/PAMI/partialPeriodicPattern/maximal/abstract.py
@@ -0,0 +1,185 @@
+from abc import ABC, abstractmethod
+import time
+import math
+import csv
+import pandas as pd
+from collections import defaultdict
+from itertools import combinations as c
+import os
+import os.path
+import psutil
+import sys
+import validators
+from urllib.request import urlopen
+
+
+class partialPeriodicPatterns(ABC):
+    """ This abstract base class defines the variables and methods that every frequent pattern mining algorithm must
+    employ in PAMI
+
+        ...
+
+        Attributes
+        ----------
+        iFile : str
+            Input file name or path of the input file
+        periodicSupport: float or int or str
+            The user can specify periodicSupport either in count or proportion of database size.
+            If the program detects the data type of periodicSupport is integer, then it treats periodicSupport is expressed in count.
+            Otherwise, it will be treated as float.
+            Example: periodicSupport=10 will be treated as integer, while periodicSupport=10.0 will be treated as float
+        period: float or int or str
+            The user can specify period either in count or proportion of database size.
+            If the program detects the data type of period is integer, then it treats period is expressed in count.
+            Otherwise, it will be treated as float.
+            Example: period=10 will be treated as integer, while period=10.0 will be treated as float
+        sep : str
+            This variable is used to distinguish items from one another in a transaction. The default seperator is tab space or \t.
+            However, the users can override their default separator.
+        startTime:float
+            To record the start time of the algorithm
+        endTime:float
+            To record the completion time of the algorithm
+        finalPatterns: dict
+            Storing the complete set of patterns in a dictionary variable
+        oFile : str
+            Name of the output file or path of the output file
+        memoryUSS : float
+            To store the total amount of USS memory consumed by the program
+        memoryRSS : float
+            To store the total amount of RSS memory consumed by the program
+
+        Methods
+        -------
+        startMine()
+            Mining process will start from here
+        getFrequentPatterns()
+            Complete set of patterns will be retrieved with this function
+        savePatterns(oFile)
+            Complete set of frequent patterns will be loaded in to a output file
+        getPatternsAsDataFrame()
+            Complete set of frequent patterns will be loaded in to data frame
+        getMemoryUSS()
+            Total amount of USS memory consumed by the program will be retrieved from this function
+        getMemoryRSS()
+            Total amount of RSS memory consumed by the program will be retrieved from this function
+        getRuntime()
+            Total amount of runtime taken by the program will be retrieved from this function
+    """
+
+    def __init__(self, iFile, periodicSupport, period, sep = '\t'):
+        """
+        :param iFile: Input file name or path of the input file
+        :type iFile: str
+        :param minSup: UserSpecified minimum support value. It has to be given in terms of count of total number of
+        transactions in the input database/file
+        :type minSup: float
+        """
+
+        self.iFile = iFile
+        self.periodicSupport = periodicSupport
+        self.period = period
+        self.sep = sep
+
+    @abstractmethod
+    def iFile(self):
+        """Variable to store the input file path/file name"""
+
+        pass
+
+    @abstractmethod
+    def periodicSupport(self):
+        """Variable to store the user-specified minimum support value"""
+
+        pass
+    def period(self):
+        """Variable to store the user specified maximum periodicity value"""
+
+        pass
+
+    @abstractmethod
+    def sep(self):
+        """Variable to store the input file path/file name"""
+
+        pass
+
+    @abstractmethod
+    def startTime(self):
+        """Variable to store the start time of the mining process"""
+
+        pass
+
+    @abstractmethod
+    def endTime(self):
+        """Variable to store the end time of the complete program"""
+
+        pass
+
+    @abstractmethod
+    def memoryUSS(self):
+        """Variable to store the end time of the complete program"""
+
+        pass
+
+    @abstractmethod
+    def memoryRSS(self):
+        """Variable to store the end time of the complete program"""
+
+        pass
+
+    @abstractmethod
+    def finalPatterns(self):
+        """Variable to store the complete set of patterns in a dictionary"""
+
+        pass
+
+    @abstractmethod
+    def oFile(self):
+        """Variable to store the name of the output file to store the complete set of frequent patterns"""
+
+        pass
+
+    @abstractmethod
+    def startMine(self):
+        """Code for the mining process will start from this function"""
+
+        pass
+
+    @abstractmethod
+    def getPatterns(self):
+        """Complete set of frequent patterns generated will be retrieved from this function"""
+
+        pass
+
+    @abstractmethod
+    def savePatterns(self, oFile):
+        """Complete set of frequent patterns will be saved in to an output file from this function
+
+        :param oFile: Name of the output file
+        :type oFile: file
+        """
+
+        pass
+
+    @abstractmethod
+    def getPatternsAsDataFrame(self):
+        """Complete set of frequent patterns will be loaded in to data frame from this function"""
+
+        pass
+
+    @abstractmethod
+    def getMemoryUSS(self):
+        """Total amount of USS memory consumed by the program will be retrieved from this function"""
+
+        pass
+
+    @abstractmethod
+    def getMemoryRSS(self):
+        """Total amount of RSS memory consumed by the program will be retrieved from this function"""
+        pass
+
+    @abstractmethod
+    def getRuntime(self):
+        """Total amount of runtime taken by the program will be retrieved from this function"""
+
+        pass
