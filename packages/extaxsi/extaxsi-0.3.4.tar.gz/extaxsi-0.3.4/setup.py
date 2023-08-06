86cf455e41138b0175938b115da9664c333b944d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:59 +0000

    Add package extaxsi/0.3.4/extaxsi-0.3.4.tar.gz

---
+++ packages/extaxsi/extaxsi-0.3.4.tar.gz/extaxsi-0.3.4/setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'extaxsi',
+  packages = ['extaxsi'],
+  version = '0.3.4',
+  license='MIT',
+  description = 'Extaxsi is a bioinformatic library aimed to elaborate and visualize molecular and taxonomic informations',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'alberto brusati',
+  author_email = 'alberto.brusati@gmail.com',
+  url = 'https://github.com/user/extaxsi',
+  download_url = 'https://github.com/user/extaxsi/archive/refs/tags/v_03.tar.gz',
+  keywords = ['bioinformatic', 'ncbi', 'molecular data', 'visualization', 'taxonomy', 'converter'],
+  install_requires=[
+          'biopython',
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'ipython',
+          'pandas',
+          'sympy',
+          'nose',
+          'plotly',
+          'ete3',
+          'kaleido'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
