fc3dc7409b14653d04e51526a1935f57ab1346ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:28 +0000

    Add package chroma-feedback/10.5.0/chroma-feedback-10.5.0.tar.gz

---
+++ packages/chroma-feedback/chroma-feedback-10.5.0.tar.gz/chroma-feedback-10.5.0/chroma_feedback/producer/__init__.py
@@ -0,0 +1,19 @@
+from .core import process
+
+__all__ =\
+[
+	'appveyor',
+	'azure',
+	'bamboo',
+	'bitbucket',
+	'buddy',
+	'circle',
+	'codeship',
+	'custom',
+	'github',
+	'gitlab',
+	'jenkins',
+	'teamcity',
+	'travis',
+	'wercker'
+]
