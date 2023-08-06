36c440155fdbd59af835011d89420ebc8abcd6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package fc-ims/3.0/fc_ims-3.0-py3-none-any.whl

---
+++ packages/fc-ims/fc_ims-3.0-py3-none-any.whl/fc_ims.py
@@ -0,0 +1,34 @@
+import pymysql
+
+class Student(object):
+    def __init__(self):
+        self.db = pymysql.connect(host = "localhost",
+                                  user="root",
+                                  password="Xd12345678",
+                                  db = "fc"
+                                  )
+        self.cursor = self.db.cursor()
+    
+    def _add(self, id, name, age):
+        sql = "insert into student values({},'{}',{})".format(id, name, age)
+        self.cursor.execute(sql)
+        self.db.commit()
+    
+    def _select(self):
+        pass
+
+    def _delete(self):
+        pass
+
+    def _update(self):
+        pass
+
+class Bigdata(Student):
+    def _select(self):
+        sql = "select * from student"
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return result
+
+    def _delete(self):
+        pass
