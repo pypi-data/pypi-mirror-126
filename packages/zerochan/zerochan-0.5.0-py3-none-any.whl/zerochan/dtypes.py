3523ef6b8e833988eb4df73abdccce109dc5cef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:23 +0000

    Add package zerochan/0.5.0/zerochan-0.5.0-py3-none-any.whl

---
+++ packages/zerochan/zerochan-0.5.0-py3-none-any.whl/zerochan/dtypes.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from enum import IntEnum, Enum
+from typing import List
+
+
+@dataclass
+class ZeroChanCategory:
+    name: str
+    image: str
+    type: str
+    description: str
+
+
+@dataclass
+class ZeroChanImage:
+    title: str
+    url: str
+    height: int  # in px
+    width: int  # in px
+    kbsize: int  # in KB
+
+    @property
+    def size(self):
+        return f"{self.height}x{self.width}"
+
+    # TODO: Download method?
+
+
+@dataclass
+class ZeroChanPage:
+    images: List[ZeroChanImage]
+    page: int
+    max_page: int
+
+
+class PictureSize(IntEnum):
+    ALL_SIZES = 0
+    BIGGER_AND_BETTER = 1
+    BIG_AND_HUGE = 2
+
+
+class SortBy(str, Enum):
+    LAST = "id"
+    POPULAR = "fav"
+    RANDOM = "random"
