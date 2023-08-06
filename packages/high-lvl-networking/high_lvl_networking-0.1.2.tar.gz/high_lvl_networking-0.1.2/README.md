64995f16d432306b7072ed1d5c71f6c595982e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:36 +0000

    Add package high-lvl-networking/0.1.2/high_lvl_networking-0.1.2.tar.gz

---
+++ packages/high-lvl-networking/high_lvl_networking-0.1.2.tar.gz/high_lvl_networking-0.1.2/README.md
@@ -0,0 +1,25 @@
+# High Level Networking
+
+## Requirements
+
+None
+
+## Install
+
+```bash
+pip install high_lvl_networking
+```
+
+## Usage
+
+### Server
+
+```python
+from high_lvl_networking import Server
+```
+
+### Client
+
+```python
+from high_lvl_networking import Client
+```
---
+++ packages/high-lvl-networking/high_lvl_networking-0.1.2.tar.gz/high_lvl_networking-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='high_lvl_networking',
+    packages=find_packages(include=['high_lvl_networking']),
+    version='0.1.2',
+    description='High level library to setup networking',
+    long_description="High level library to simplify communication between multiple devices",
+    author='Silvan Schmidt',
+    license='MIT',
+)
