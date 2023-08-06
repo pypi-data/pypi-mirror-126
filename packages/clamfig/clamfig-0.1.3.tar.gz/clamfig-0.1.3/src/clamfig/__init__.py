0f834ac66e910352570f01499db6daaf12788f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:20 +0000

    Add package clamfig/0.1.3/clamfig-0.1.3.tar.gz

---
+++ packages/clamfig/clamfig-0.1.3.tar.gz/clamfig-0.1.3/src/clamfig/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.3'
+
+__all__ = ['Serializable', 'serialize', 'deserialize']
+
+from clamfig.base import Serializable
+from clamfig.base import serialize
+from clamfig.base import deserialize
