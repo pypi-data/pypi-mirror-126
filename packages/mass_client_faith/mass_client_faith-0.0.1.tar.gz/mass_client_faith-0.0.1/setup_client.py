f401100df0671e50c5b1cc9d38ec8da2d19245ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:40 +0000

    Add package mass_client_faith/0.0.1/mass_client_faith-0.0.1.tar.gz

---
+++ packages/mass_client_faith/mass_client_faith-0.0.1.tar.gz/mass_client_faith-0.0.1/setup_client.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="mass_client_faith",
+      version="0.0.1",
+      description="mess_client",
+      author="Vera K",
+      author_email="faith.veyu0@gmail.com",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
