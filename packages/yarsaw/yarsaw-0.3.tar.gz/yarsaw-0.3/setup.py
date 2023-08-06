b453e98e839bb09bc23d82c100c070e812d80f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package yarsaw/0.3/yarsaw-0.3.tar.gz

---
+++ packages/yarsaw/yarsaw-0.3.tar.gz/yarsaw-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "yarsaw",
+    version = "0.3",
+    author = "Bruce",
+    author_email = "brucealt69@gmail.com",
+    description = ("An async wrapper for the Random Stuff API"),
+    license = "MIT",
+    keywords = "random-stuff-api rsa yarsaw prsaw randomstuff ai-chatbot covid weather jokes",
+    packages=['yarsaw'],
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'aiohttp',
+    ],
+    long_description_content_type= 'text/markdown',
+)
---
+++ packages/yarsaw/yarsaw-0.3.tar.gz/yarsaw-0.3/yarsaw/utils.py
@@ -0,0 +1,24 @@
+class Utils:
+    def __init__(self):
+        pass
+
+    async def format_joke(self, joke : dict, joined_by : str = '\n') -> dict:
+        if joke.get('type') == 'twopart':
+            return f"{joke.get('setup')}{joined_by}{joke.get('delivery')}"
+        else:
+            return joke.get("joke")
+
+PLANS = {
+    'free' : '',
+    'pro' : 'premium/pro',
+    'ultra' : 'premium/ultra',
+    'biz' : 'premium/biz',
+    'mega' : 'premium/mega'
+}
+
+class InvalidPlanError(Exception):
+    pass
+    
+class InvalidArgumentsError(Exception):
+    pass
+    
