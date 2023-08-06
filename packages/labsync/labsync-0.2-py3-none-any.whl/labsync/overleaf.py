dd7e1a6f6edb70d0c4bc6b3adf5ffc95ee79d551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:32 +0000

    Add package labsync/0.2/labsync-0.2-py3-none-any.whl

---
+++ packages/labsync/labsync-0.2-py3-none-any.whl/labsync/overleaf.py
@@ -0,0 +1,31 @@
+import sys
+import logging
+import argparse
+import os
+import subprocess
+from .utils import get_config
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=str, choices=['update'])
+    return parser
+
+def overleaf():
+    args = get_parser().parse_args(sys.argv[2:])
+    config = get_config()['overleaf']
+    patterns = config['patterns']
+    if args.command == 'update':
+        for p in patterns:
+            subprocess.run(['git', 'add', p],     
+            stdout=subprocess.DEVNULL, 
+            stderr=subprocess.STDOUT)
+        os.system('git status')
+        os.system('git commit -m "local update"')
+        os.system('git pull origin master --no-edit')
+        os.system('git push origin master')
+    elif args.command == 'compile':
+        raise NotImplementedError(args.command)
+    else:
+        raise ValueError(f'Unknown command {command}')
+    
+    # TODO resolve conflicts
---
+++ packages/labsync/labsync-0.2-py3-none-any.whl/labsync/watchfs.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import logging
+import argparse
+from .sync import Synchronizer
+from .fsevent import FSEventHandler
+from .server import Server
+from watchdog.observers import Observer
+from threading import Thread
+
+logger = logging.getLogger(__name__)
+
+def get_parser():
+    parser = argparse.ArgumentParser(prog='lab listen')
+    parser.add_argument('--path', '-p', type=str, default='~', help='Path of the working directory on remote servers')
+    return parser
+
+class WatchFS(Thread):
+    def __init__(self, config):
+        super().__init__()
+
+        args, _ = get_parser().parse_known_args(sys.argv[min(len(sys.argv), 1):])
+
+        path = '.'
+        self.servers = []
+        for server, conf in config['servers'].items():
+            if conf.get('enable', True):
+                path_remote = conf.get('path') or config.get('path') or args.path
+                self.servers.append(Server(server, conf, path_remote))
+
+        synchronizer = Synchronizer(self.servers, path)
+        self.event_handler = FSEventHandler(
+            synchronizer, 
+            patterns=config.get('patterns', None), 
+            ignore_patterns=config.get('ignore_patterns', None)
+        )
+        self.observer = Observer()
+        self.observer.schedule(self.event_handler, path, recursive=True)
+
+    def run(self):
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(10)
+        except KeyboardInterrupt:
+            self.observer.stop()
+        self.observer.join()
+
+    def pause(self):
+        self.event_handler.pause()
+        logger.info('WatchFS paused')
+
+    def resume(self):
+        self.event_handler.resume()
+        logger.info('WatchFS resumed')
