8c51c01800ad87c75e8b04ca4491be06b8ea289e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:15 +0000

    Add package mxr-bigdata/2.1.0/mxr_bigdata-2.1.0.tar.gz

---
+++ packages/mxr-bigdata/mxr_bigdata-2.1.0.tar.gz/mxr_bigdata-2.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "mxr_bigdata",
+    version = "2.1.0",
+    py_modules = ["fc_mxr_bigdata"],
+    author = "大数据1",
+    author_email = "1280184739@qq.com",
+    description = "这是一个只有两个变量的包" 
+
+)
+
+
+
+
+
+
+
