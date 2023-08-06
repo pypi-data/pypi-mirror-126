ef045492c3d02e84d70393aaab91a7f2238bdd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:10 +0000

    Add package fc-wxy/1.0/fc_wxy-1.0.tar.gz

---
+++ packages/fc-wxy/fc_wxy-1.0.tar.gz/fc_wxy-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name = "fc_wxy",
+      version = "1.0",
+      py_modules = ["fc_xy_bigdata"],
+      author = "大数据1",
+      author_email = "fc@wxy.com",
+      description = "大数据1"
+)
