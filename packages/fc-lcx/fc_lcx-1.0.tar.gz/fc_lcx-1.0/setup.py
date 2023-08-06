d6ddb8fceba50431dbee4cb2c7fb2f3d4d966832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:40 +0000

    Add package fc-lcx/1.0/fc_lcx-1.0.tar.gz

---
+++ packages/fc-lcx/fc_lcx-1.0.tar.gz/fc_lcx-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fc_lcx",
+    version="1.0",
+    py_modules="fc_bigdata2",
+    author="大数据3",
+    author_email="fc@bigdata.com",
+    description="大数据3"
+)
