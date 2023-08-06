5ff29f824359af5418662168695dd026d4db868b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:07 +0000

    Add package docedian/0.1.0/docedian-0.1.0-py3-none-any.whl

---
+++ packages/docedian/docedian-0.1.0-py3-none-any.whl/docedian/base/base.py
@@ -0,0 +1,19 @@
+Ambiente = {
+    '1': '1 - Produccion',
+    '2': '2 - Pruebas',
+}
+
+
+TipoDocumento = {
+    '11': '11 - Registro civil',
+    '12': '12 - Tarjeta de identidad',
+    '13': '13 - Cédula de ciudadanía',
+    '21': '21 - Tarjeta de extranjería',
+    '22': '22 - Cédula de extranjería',
+    '31': '31 - NIT',
+    '41': '41 - Pasaporte',
+    '42': '42 - Documento de identificación extranjero',
+    '47': '47 - PEP',
+    '50': '50 - NIT de otro país',
+    '91': '91 - NUIP * ',
+}
