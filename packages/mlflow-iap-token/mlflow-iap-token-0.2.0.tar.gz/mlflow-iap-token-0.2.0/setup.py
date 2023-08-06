cef31f71c4604a167751043c5ff192d0a1938f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:05 +0000

    Add package mlflow-iap-token/0.2.0/mlflow-iap-token-0.2.0.tar.gz

---
+++ packages/mlflow-iap-token/mlflow-iap-token-0.2.0.tar.gz/mlflow-iap-token-0.2.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mlflow_iap_token']
+
+package_data = \
+{'': ['*'], 'mlflow_iap_token': ['.pytest_cache/*', '.pytest_cache/v/cache/*']}
+
+entry_points = \
+{'mlflow.request_header_provider': ['unused = '
+                                    'mlflow_iap_token.iap_token:IdentityAwareProxyPluginRequestHeaderProvider']}
+
+setup_kwargs = {
+    'name': 'mlflow-iap-token',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Myung Kim',
+    'author_email': 'agilemlops@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
