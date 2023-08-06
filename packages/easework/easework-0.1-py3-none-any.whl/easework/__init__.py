5e29796a166bc8d0ae2ad4235315622ac7a84352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:10 +0000

    Add package easework/0.1/easework-0.1-py3-none-any.whl

---
+++ packages/easework/easework-0.1-py3-none-any.whl/easework/__init__.py
@@ -0,0 +1,23 @@
+def add(num1 , num2):
+	return num1 + num2
+
+def substract(num1 , num2):
+	return num1 - num2
+
+def multiply(num1 , num2):
+	return num1 * num2
+
+def divide(num1, num2):
+	return num1 / num2
+
+def sq(num):
+	return num * num 
+
+def cb(num):
+	return num * num * num 
+
+def sqrt(num):
+	return num ** 0.5 
+
+def cbrt(num):
+	return num ** 1/3
