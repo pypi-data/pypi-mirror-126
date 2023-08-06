c1cb0d151cebe4c6e6165d49ca4fc335360fcc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:27 +0000

    Add package pipeline-telemetry/0.1.0/pipeline_telemetry-0.1.0-py2.py3-none-any.whl

---
+++ packages/pipeline-telemetry/pipeline_telemetry-0.1.0-py2.py3-none-any.whl/pipeline_telemetry/settings/settings.py
@@ -0,0 +1,90 @@
+"""[summary]
+"""
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+from pipeline_telemetry.settings import exceptions
+
+DEFAULT_CREATE_DATA_SUB_PROCESS_TYPES = [
+    "RETRIEVE_RAW_DATA",
+    "DATA_CONVERSION",
+    "DATA_STORAGE",
+]
+
+DEFAULT_UPLOAD_DATA_SUB_PROCESS_TYPES = [
+    "DATA_SELECTION",
+    "DATA_CONVERSION",
+    "DATA_UPLOAD",
+]
+
+
+@dataclass(frozen=True)
+class ProcessType:
+    """Immutable dataclass to define a process type and its subtypes."""
+
+    process_type: str
+    subtypes: list = list
+
+    @property
+    def name(self) -> str:
+        """Property method to return process key name."""
+        return self.process_type
+
+    @property
+    def sub_processes(self) -> List[str]:
+        """Property method to return list of sub process types."""
+        return self.subtypes
+
+
+@dataclass(frozen=True)
+class TelemetryCounter:
+    """Immutable dataclass to define a process type and its subtypes."""
+
+    process_type: ProcessType
+    sub_process: str
+    counter_name: str
+    increment: int = 1
+
+    def validate_sub_process(self) -> None:
+        """
+        Raises exception if sub_process not define in ProcessType in scope.
+        """
+        if self.sub_process not in self.process_type.subtypes:
+            raise exceptions.InvalidSubProcessForProcessType
+
+
+class BaseEnumerator(Enum):
+    """
+    Class to define base enumerator with a keys method.
+    Only to be used for defining new enumerators.
+    """
+
+    @classmethod
+    def keys(cls):
+        return list(cls.__members__.keys())
+
+
+class DefaultProcessTypes(BaseEnumerator):
+    """
+    Class to define the defailt process types with their subtypes
+    """
+
+    CREATE_DATA_FROM_URL = ProcessType(
+        process_type="create_data_from_url",
+        subtypes=DEFAULT_CREATE_DATA_SUB_PROCESS_TYPES,
+    )
+
+    CREATE_DATA_FROM_API = ProcessType(
+        process_type="create_data_from_api",
+        subtypes=DEFAULT_CREATE_DATA_SUB_PROCESS_TYPES,
+    )
+
+    CREATE_DATA_FROM_FILE = ProcessType(
+        process_type="create_data_from_file",
+        subtypes=DEFAULT_CREATE_DATA_SUB_PROCESS_TYPES,
+    )
+
+    UPLOAD_DATA = ProcessType(
+        process_type="upload_data", subtypes=DEFAULT_UPLOAD_DATA_SUB_PROCESS_TYPES
+    )
