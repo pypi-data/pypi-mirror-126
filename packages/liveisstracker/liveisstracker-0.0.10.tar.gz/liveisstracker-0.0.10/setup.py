6d370972870b24fc7572fd2ca5f260a5c5a72667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:01 +0000

    Add package liveisstracker/0.0.10/liveisstracker-0.0.10.tar.gz

---
+++ packages/liveisstracker/liveisstracker-0.0.10.tar.gz/liveisstracker-0.0.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+requirements = [l.strip() for l in open('requirements.txt').readlines()]
+requirements.append('pytest')
+
+setup(
+    name='liveisstracker',
+    url='https://gitlab.com/manojm18',
+    version='0.0.10',
+    author='Manoj Manivannan',
+    author_email='manojm18@live.in',
+    description='A CLI based on the LiveIssTracker Project from https://gitlab.com/manojm18/liveisstracker',
+    packages=find_packages(),
+    entry_points={
+    'console_scripts': ['liveisstracker=liveisstracker.command_line:main'],
+    },
+    install_requires=requirements,
+    include_package_data=True,
+)
+
