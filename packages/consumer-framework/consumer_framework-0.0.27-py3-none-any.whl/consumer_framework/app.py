43a47fe95dbba30106739e9f348b26032ea6c8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:56 +0000

    Add package consumer-framework/0.0.27/consumer_framework-0.0.27-py3-none-any.whl

---
+++ packages/consumer-framework/consumer_framework-0.0.27-py3-none-any.whl/consumer_framework/app.py
@@ -0,0 +1,46 @@
+import logging
+
+from kafka import KafkaConsumer
+
+from consumer_framework.events import UndefinedEvent
+from consumer_framework.routers import Router
+
+logger = logging.getLogger(__name__)
+
+
+class ConsumerFramework:
+    _configs: dict = {}
+    _event_registry: dict = {}
+    _default_router = Router()
+    _routers = [_default_router]
+
+    def __init__(self, **configs):
+        self._configs = configs or {}
+
+    def run(self):
+        self._register_router()
+        for message in KafkaConsumer(*self._event_registry.keys(), **self._configs):
+            self._get_event(message.topic, message.key).consume(message)
+
+    def config(self, **configs):
+        self._configs.update(configs)
+
+    def event(self, *, topic, key, schema=None):
+        return self._default_router.event(topic=topic, key=key, schema=schema)
+
+    def include_router(self, router):
+        self._routers.append(router)
+
+    def _register_router(self):
+        for router in self._routers:
+            for topic, events in router.registry.items():
+                try:
+                    self._event_registry[topic].update(events)
+                except KeyError:
+                    self._event_registry[topic] = events
+
+    def _get_event(self, topic, key):
+        try:
+            return self._event_registry[topic][key]
+        except KeyError:
+            return UndefinedEvent(topic, key)
---
+++ packages/consumer-framework/consumer_framework-0.0.27-py3-none-any.whl/consumer_framework/events.py
@@ -0,0 +1,32 @@
+import logging
+
+from consumer_framework.exceptions import ValidationError
+
+logger = logging.getLogger(__name__)
+
+
+class Event:
+    _topic = None
+    _key = None
+    _consume = None
+    _schema = None
+
+    def __init__(self, topic, key, consume=None, schema=None):
+        self._topic = topic
+        self._key = key
+        self._consume = consume
+        self._schema = schema
+        logger.info(f'Registered Event: {self._topic}, {self._key}')
+
+    def consume(self, message):
+        self._validate(message)
+        self._consume(message)
+
+    def _validate(self, message):
+        if self._schema and not self._schema(message).is_valid():
+            raise ValidationError()
+
+
+class UndefinedEvent(Event):
+    def consume(self, message):
+        logger.warning(f'this event is undefined event. \'{self.message.key}\'')
