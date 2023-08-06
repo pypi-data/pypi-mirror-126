e24eb7ecf4a32e4a711435b420de914b1a318338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:14 +0000

    Add package fc-yuchen/2.0/fc_yuchen-2.0.tar.gz

---
+++ packages/fc-yuchen/fc_yuchen-2.0.tar.gz/fc_yuchen-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name = "fc_yuchen",
+    version = "2.0",
+    py_modules = ["fc_xd_bigdata"],
+    author = "于晨晨",
+    author_email = "2790896061@qq.com",
+    description = "使用方法：这个包只有一个变量name和age"
+)
