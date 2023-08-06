62195ffc94a6bd433cc5821d504758c5907ed4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:18 +0000

    Add package polidoro-cli/3.6.0/polidoro_cli-3.6.0-py3-none-any.whl

---
+++ packages/polidoro-cli/polidoro_cli-3.6.0-py3-none-any.whl/polidoro_cli/clis/github.py
@@ -0,0 +1,21 @@
+import re
+
+from polidoro_argument import Command
+
+from polidoro_cli import CLI
+
+
+class GitHub(object):
+    help='GitHub CLI commands'
+
+    @staticmethod
+    @Command(
+        aliases=['cr'],
+    )
+    def create_release():
+        for version in CLI.execute('grep "VERSION =" * -rh', capture_output=True, show_cmd=False)[0].split('\n'):
+            if version:
+                search = re.search(r'VERSION = \'([\d\\.]*)\'', version)
+                if search:
+                    version = search.groups()[0]
+                    CLI.execute(f'gh release create {version}')
