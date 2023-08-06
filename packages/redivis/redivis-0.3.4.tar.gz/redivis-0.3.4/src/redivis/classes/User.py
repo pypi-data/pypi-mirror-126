e093aa2ce363634f0d94908a36361377c0e72b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package redivis/0.3.4/redivis-0.3.4.tar.gz

---
+++ packages/redivis/redivis-0.3.4.tar.gz/redivis-0.3.4/src/redivis/classes/User.py
@@ -0,0 +1,15 @@
+import warnings
+
+from .Dataset import Dataset
+from .Project import Project
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+
+    def dataset(self, name, *, version="current"):
+        return Dataset(name, user=self, version=version)
+
+    def project(self, name):
+        return Project(name, user=self)
