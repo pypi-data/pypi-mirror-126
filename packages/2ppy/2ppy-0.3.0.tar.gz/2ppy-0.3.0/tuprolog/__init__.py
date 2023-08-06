7e14a10a4197ae0c90a6d26119478ff712965574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:56 +0000

    Add package 2ppy/0.3.0/2ppy-0.3.0.tar.gz

---
+++ packages/2ppy/2ppy-0.3.0.tar.gz/2ppy-0.3.0/tuprolog/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+# noinspection PyUnresolvedReferences
+import jpype
+import jpype.imports
+
+from .libs import CLASSPATH
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger('tuprolog')
+
+jars = [str(j.resolve()) for j in CLASSPATH.glob('*.jar')]
+
+jpype.startJVM(classpath=jars)
+
+# noinspection PyUnresolvedReferences
+from it.unibo.tuprolog import Info
+
+JVM_VERSION = '.'.join(map(str, jpype.getJVMVersion()))
+
+logger.info("Started JVM v" + JVM_VERSION + " with classpath: " + str(jars))
+
+logger.info("Using 2P-Kt v" + str(Info.VERSION))
