00c2aa79f2446f4874bf22a61cf263df59d73a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:21 +0000

    Add package text2ipa/1.3.0rc1/text2ipa-1.3.0rc1.tar.gz

---
+++ packages/text2ipa/text2ipa-1.3.0rc1.tar.gz/text2ipa-1.3.0rc1/text2ipa/__init__.py
@@ -0,0 +1,23 @@
+'''Library converting Text to IPA'''
+from typing import Any
+
+def get_IPA(text:str,language:str,proxy:Any=None) -> str:
+    '''Convert Text to IPA\n
+    Parameters:\n
+    - `text` : the content for converting to IPA
+    - `language`: must be "am" or "br" (US English or UK English)
+    - `proxy` : Optional parameter using for hide ip 
+    '''
+    ...
+
+def get_IPAs(bulk:list,language:str,proxy:Any=None) -> list:
+    '''Convert a list of text to IPA\n
+    Parameters:\n
+    - `bulk` : the list of contents for converting to IPA
+    - `language`: must be "am" or "br" (US English or UK English)
+    - `proxy` : Optional parameter using for hide ip 
+    '''
+    ...
+
+from .text2ipa import *
+__all__ = ['get_IPA','get_IPAs']
