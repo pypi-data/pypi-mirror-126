333a866c12fe27e2c19de89a1b40cd0a30f0e0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:33 +0000

    Add package minos-microservice-common/0.2.1/minos_microservice_common-0.2.1-py3-none-any.whl

---
+++ packages/minos-microservice-common/minos_microservice_common-0.2.1-py3-none-any.whl/minos/common/configuration/__init__.py
@@ -0,0 +1,14 @@
+from .config import (
+    BROKER,
+    COMMANDS,
+    DISCOVERY,
+    QUEUE,
+    REPOSITORY,
+    REST,
+    SAGA,
+    SERVICE,
+    SNAPSHOT,
+    STORAGE,
+    MinosConfig,
+    MinosConfigAbstract,
+)
