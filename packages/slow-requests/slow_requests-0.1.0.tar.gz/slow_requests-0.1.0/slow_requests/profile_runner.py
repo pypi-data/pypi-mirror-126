ab81be660c71d6134714f4370f576657f83bbc9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:37 +0000

    Add package slow-requests/0.1.0/slow_requests-0.1.0.tar.gz

---
+++ packages/slow-requests/slow_requests-0.1.0.tar.gz/slow_requests-0.1.0/slow_requests/profile_runner.py
@@ -0,0 +1,28 @@
+from slow_requests import SlowRequests
+from timeit import default_timer as timer
+from datetime import timedelta
+
+def run():
+  # setup to do 10 per second
+  sl = SlowRequests(offset=10, per_second=True)
+
+  url = "https://jsonplaceholder.typicode.com/todos/1"
+  test_data = [url] * 100
+
+  start = timer()
+  count = 0
+  for res in sl.execute(requests=test_data):
+    end = timer()
+    # if the total execution has been less than 1 second decrement the count
+    print(timedelta(seconds=end-start).total_seconds())
+    count += 1
+
+  end = timer()
+  print("******************************************")
+  print(timedelta(seconds=end-start).total_seconds())
+  print("******************************************")
+
+
+
+if __name__ == '__main__':
+  run()
