2f9ec8a779aa88321a66b806ac608ce169b24d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:09 +0000

    Add package openhub/0.0.129/openhub-0.0.129.tar.gz

---
+++ packages/openhub/openhub-0.0.129.tar.gz/openhub-0.0.129/src/OpenHub/hardware_interfaces/channels/dht22_humidity.py
@@ -0,0 +1,19 @@
+from .channel_interface import ChannelInterface
+import time
+import logging
+from OpenHub.globals import id_hardware_map
+
+
+class DHT22Humidity(ChannelInterface):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, config, hardware_serial_no=None, serial_no=None, dht_device=None, *args,
+                 **kwargs):
+        self.dht = id_hardware_map[config['hardware']]
+        self.serial_no = config['id']
+        self.type = config['type']
+
+        super().__init__(hardware_serial_no=self.dht.serial_no, serial_no=self.serial_no, *args, **kwargs)
+
+    async def get_raw_data(self):
+        return {'value':float(self.dht.get_humidity())}
