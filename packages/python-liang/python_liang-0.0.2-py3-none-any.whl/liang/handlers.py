659155ad2f30c4ff55ac8fbd5d60f58b25c1de0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:23 +0000

    Add package python-liang/0.0.2/python_liang-0.0.2-py3-none-any.whl

---
+++ packages/python-liang/python_liang-0.0.2-py3-none-any.whl/liang/handlers.py
@@ -0,0 +1,34 @@
+import abc
+import logging
+
+from liang import (
+    environment
+)
+
+logger = logging.getLogger(__name__)
+
+
+class FailureHandler(abc.ABC):
+
+    @abc.abstractmethod
+    def handle(self, context: environment.ExecutionContext):
+        pass
+
+
+class RaiseExceptionFailureHandler(FailureHandler):
+
+    def handle(self, context: environment.ExecutionContext):
+        message = f"Function {context.function.__name__!r} expected to have {context.metric_name} to be {context.metric_expected}"
+        if context.metric_actual:
+            message += f" but is actually {context.metric_actual}"
+        raise TimeoutError(message)
+
+
+class LogWarningFailureHandler(FailureHandler):
+
+    def handle(self, context: environment.ExecutionContext):
+        message = f"Function {context.function.__name__!r} expected to have {context.metric_name} to be {context.metric_expected}"
+        if context.metric_actual:
+            message += f" but is actually {context.metric_actual}"
+        logger.warning(message)
+
