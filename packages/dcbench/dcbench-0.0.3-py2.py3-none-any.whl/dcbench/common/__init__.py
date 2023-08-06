6f183cc152129412d8058052c9f42d0245e4185c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:27 +0000

    Add package dcbench/0.0.3/dcbench-0.0.3-py2.py3-none-any.whl

---
+++ packages/dcbench/dcbench-0.0.3-py2.py3-none-any.whl/dcbench/common/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+from .artefact import (
+    Artefact,
+    CSVArtefact,
+    DataPanelArtefact,
+    MeerkatDatasetArtefact,
+    ModelArtefact,
+)
+from .problem import Problem
+from .result import Result
+from .solution import Solution
---
+++ packages/dcbench/dcbench-0.0.3-py2.py3-none-any.whl/dcbench/common/problem.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import TYPE_CHECKING, Any, List, Optional
+
+
+from ..constants import PROBLEMS_DIR
+from .artefact import ArtefactContainer
+
+if TYPE_CHECKING:
+    from .solution import Solution
+
+
+class Problem(ArtefactContainer):
+    container_id = "problem"
+    task_id: str
+    result_metrics: Optional[List[str]] = None
+
+    container_dir = PROBLEMS_DIR
+
+    @property
+    def solutions(self):
+        pass
+
+    @abstractmethod
+    def solve(self, **kwargs: Any) -> Solution:
+        raise NotImplementedError
+
+    @abstractmethod
+    def evaluate(self, solution: Solution):
+        raise NotImplementedError
