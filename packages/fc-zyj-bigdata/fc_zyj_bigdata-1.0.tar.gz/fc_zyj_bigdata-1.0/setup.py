65ef429fe8db7c39ebd5170458481cc0ef73c5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:43 +0000

    Add package fc-zyj-bigdata/1.0/fc_zyj_bigdata-1.0.tar.gz

---
+++ packages/fc-zyj-bigdata/fc_zyj_bigdata-1.0.tar.gz/fc_zyj_bigdata-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name = "fc_zyj_bigdata",
+        version="1.0",
+        py_modules=["fc_zyj_bigdata"],
+        author="大数据3",
+        author_email="fc@bigdata.com",
+        description="大数据3"
+)
