14b8056f280d1adb06c9af1d7b2cfbbfe8373543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:36 +0000

    Add package fc-penwei250/1.0/fc_penwei250-1.0.tar.gz

---
+++ packages/fc-penwei250/fc_penwei250-1.0.tar.gz/fc_penwei250-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name = "fc_penwei250",
+    version = "1.0",
+    py_modules = ["fc_dc_bigdata"],
+    author = "大数据",
+    author_email = "fc@bigdata.com",
+    description = "大数据2"
+)
