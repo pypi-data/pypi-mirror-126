71f328a41657f45138e2bf37ea9c29a07a5cd534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:00 +0000

    Add package python-amazon-ad-api/0.1.0/python-amazon-ad-api-0.1.0.tar.gz

---
+++ packages/python-amazon-ad-api/python-amazon-ad-api-0.1.0.tar.gz/python-amazon-ad-api-0.1.0/src/ad_api/api/__init__.py
@@ -0,0 +1,20 @@
+from .profiles import Profiles
+from .invoices import Invoices
+from .eligibility import Eligibility
+from .metadata import Metadata
+from .history import History
+from .localization import Localization
+from .audiences import Audiences
+from .portfolios import Portfolios
+from .insights import Insights
+__all__ = [
+    "Profiles",
+    "Invoices",
+    "Eligibility",
+    "Metadata",
+    "History",
+    "Localization",
+    "Audiences",
+    "Portfolios",
+    "Insights"
+]
