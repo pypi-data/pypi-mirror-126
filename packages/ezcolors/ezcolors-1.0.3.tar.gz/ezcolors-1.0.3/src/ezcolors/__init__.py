6ef66cd57ddd49d58ebe02034883e2450b8ad1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:06 +0000

    Add package ezcolors/1.0.3/ezcolors-1.0.3.tar.gz

---
+++ packages/ezcolors/ezcolors-1.0.3.tar.gz/ezcolors-1.0.3/src/ezcolors/__init__.py
@@ -0,0 +1,10 @@
+""" This package contains a Color class to easily deal with colors """
+
+from .Colors import Color, avg_color
+from . import Colors
+from . import palettes
+from . import utilities
+
+__version__ = "1.0.3"
+
+Colour = Color #for us brits
