c97ac34329028422a0ae5fc53bc7f1bc80411101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:39 +0000

    Add package bvx-pytest/0.0.12/bvx_pytest-0.0.12-py3-none-any.whl

---
+++ packages/bvx-pytest/bvx_pytest-0.0.12-py3-none-any.whl/bvx_pytest/es/teardown_function.py
@@ -0,0 +1,39 @@
+import docker
+from bvx_docker import container_exists
+
+from .defines import BVX_PYTEST_DOCKER_ES_CONTAINER_NAME
+
+
+def teardown_function():
+    # create docker client
+    _client = docker.from_env()
+
+    # get docker containers
+    _containers = _client.containers.list()
+
+    if container_exists(BVX_PYTEST_DOCKER_ES_CONTAINER_NAME,
+                        containers=_containers):
+
+        # get container
+        _container = _client.containers.get(BVX_PYTEST_DOCKER_ES_CONTAINER_NAME)
+
+        # get container status
+        if _container.status == "running":
+            # stop container
+            _container.stop()
+
+        # remove container
+        _container.remove()
+
+    # get docker networks
+    _networks = _client.networks.list()
+
+    # prune docker network for pytest
+    _client.networks.prune()
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    teardown_function()
+
+# EOF
