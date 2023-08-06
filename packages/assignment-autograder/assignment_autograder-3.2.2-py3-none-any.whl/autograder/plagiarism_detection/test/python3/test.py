140ad9f193033770d9f8b91c7fbf8f4e69838d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:52 +0000

    Add package assignment-autograder/3.2.2/assignment_autograder-3.2.2-py3-none-any.whl

---
+++ packages/assignment-autograder/assignment_autograder-3.2.2-py3-none-any.whl/autograder/plagiarism_detection/test/python3/test.py
@@ -0,0 +1,12 @@
+# comment 1
+import exceptions
+
+print "hello, world"
+response = raw_input()
+try:
+    if True:
+        if not False:
+            pass
+except ValueError, e:
+    pass
+# comment 2
