56c6cda1634af0140b73c55f1151ab4ec15184ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:17 +0000

    Add package deepquantumNN/0.0.1/deepquantumNN-0.0.1.tar.gz

---
+++ packages/deepquantumNN/deepquantumNN-0.0.1.tar.gz/deepquantumNN-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="deepquantumNN",
+    version="0.0.1",
+    author="Turing Quantum",  #作者名字
+    author_email="",
+    description="a Quantum computation Framework.",
+    license="MIT",
+    url="",  #github地址或其他地址
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[
+            'torch>=1.9.0',  #所需要包的版本号
+            'numpy>=1.14.0'   #所需要包的版本号
+    ],
+    python_requires=">=3.6",
+)
