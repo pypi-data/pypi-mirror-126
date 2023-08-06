098487a50329c62783b2b14220e08c96d7494604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:10 +0000

    Add package mona-sdk/0.0.22/mona_sdk-0.0.22-py3-none-any.whl

---
+++ packages/mona-sdk/mona_sdk-0.0.22-py3-none-any.whl/mona_sdk/client_exceptions.py
@@ -0,0 +1,19 @@
+"""
+This module contains all exceptions classes for client use.
+"""
+
+
+class MonaExportException(Exception):
+    pass
+
+
+class MonaAuthenticationException(Exception):
+    pass
+
+
+class MonaConfigException(Exception):
+    pass
+
+
+class MonaInitializationException(Exception):
+    pass
