4f3d2fa3d1a658e64479cc77deb3f8247971aa46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:55 +0000

    Add package list2csv/1.2.0/list2csv-1.2.0.tar.gz

---
+++ packages/list2csv/list2csv-1.2.0.tar.gz/list2csv-1.2.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='list2csv',
+    version='1.2.0',
+    author='James Finnie-Ansley',
+    description='A simple package intended to help write iterables '
+                'of objects to CSV files',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/James-Ansley/list2csv',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=setuptools.find_packages(
+        where='src',
+    ),
+    package_dir={'': 'src'},
+    python_requires='>=3.8',
+    install_requires=[],
+)
