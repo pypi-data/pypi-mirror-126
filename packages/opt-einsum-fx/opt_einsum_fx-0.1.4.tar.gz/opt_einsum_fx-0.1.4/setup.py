532eb96ce35591dcd8243f9b0fd4d96658f7279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:23 +0000

    Add package opt-einsum-fx/0.1.4/opt_einsum_fx-0.1.4.tar.gz

---
+++ packages/opt-einsum-fx/opt_einsum_fx-0.1.4.tar.gz/opt_einsum_fx-0.1.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opt_einsum_fx",
+    version="0.1.4",  # remember to update in `__init__.py` too!
+    author="Linux-cpp-lisp",
+    url="https://github.com/Linux-cpp-lisp/opt_einsum_fx",
+    description="Einsum optimization using opt_einsum and PyTorch FX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    license_files="LICENSE",
+    project_urls={
+        "Bug Tracker": "https://github.com/Linux-cpp-lisp/opt_einsum_fx/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires=">=3.6",
+    install_requires=["torch>=1.8.0", "opt_einsum", "packaging"],
+    packages=["opt_einsum_fx"],
+)
