0d0c508d843721646995183b8c6c6cda3cc72e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:57 +0000

    Add package VecDB/0.12.16/VecDB-0.12.16-py3-none-any.whl

---
+++ packages/VecDB/VecDB-0.12.16-py3-none-any.whl/api/admin.py
@@ -0,0 +1,22 @@
+"""Admin-related functionalities
+"""
+
+from ..base import Base
+
+
+class Admin(Base):
+    def request_read_api_key(
+        self,
+        read_username: str,
+    ):
+        """Creates a read only key for your project.
+        Make sure to save the API key somewhere safe.
+        When doing a search the admin username should still be used.
+        """
+        return self.make_http_request(
+            "admin/request_read_api_key",
+            method="POST",
+            parameters={
+                "read_username": read_username,
+            },
+        )
