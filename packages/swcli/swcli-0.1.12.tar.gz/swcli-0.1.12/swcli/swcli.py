692114e369b27aa3e895ce5387fa58e997d0b3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:09 +0000

    Add package swcli/0.1.12/swcli-0.1.12.tar.gz

---
+++ packages/swcli/swcli-0.1.12.tar.gz/swcli-0.1.12/swcli/swcli.py
@@ -0,0 +1,73 @@
+import fire
+try:
+    import swcli.swcli.render as render
+except:
+    import render
+
+
+def films(id='', title=''):
+    """
+    Return a one or many movies on Star Wars trilogies by Title.
+    """
+    response = render(id, title, 'films')
+    return response
+
+
+def people(id='', name=''):
+    """
+    Returns a character on the Star Wars movies.
+    Like: Luke, Leia, Anakin, etc.
+    """
+    response = render(id, name, 'people')
+    return response
+
+
+def planets(id='', name=''):
+    """
+    Return a planet.
+    Like: Hoth, Naboo, etc.
+    """
+    response = render(id, name, 'planets')
+    return response
+
+
+def species(id='', name=''):
+    """
+    Return a species on the Star Wars universe.
+    Like: Wookie, Human, etc.
+    """
+    response = render(id, name, 'species')
+    return response
+
+
+def starships(id='', name=''):
+    """
+    Return a Starships on Star Wars universe.
+    Like: Death Star, Millenium Falcon, etc.
+    """
+    response = render(id, name, 'starships')
+    return response
+
+
+def vehicles(id='', name=''):
+    """
+    Return a vehicle used on Star Wars universe.
+    Like: TIE Fighter, Geonosian starfighter, etc.
+    """
+    response = render(id, name, 'vehicles')
+    return response
+
+
+def main():
+    fire.Fire({
+        'films': films,
+        'people': people,
+        'planets': planets,
+        'species': species,
+        'starships': starships,
+        'vehicles': vehicles,
+    })
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/swcli/swcli-0.1.12.tar.gz/swcli-0.1.12/swcli/utils.py
@@ -0,0 +1,45 @@
+try:
+    from swcli.swcli.exceptions import ResourceDoesNotExistError
+except:
+    from exceptions import ResourceDoesNotExistError
+from httpx import get
+
+
+def query_by_id(resource_id, resource_type):
+    """
+    Realize a query on SW API and returns a list.
+    """
+    url = f'https://swapi.dev/api/{resource_type}/{resource_id}/'
+    response = get(url)
+
+    if response.status_code != 200:
+        raise ResourceDoesNotExistError('Resource does not exist!')
+
+    # Include the return in a list
+    json_data = []
+    json_data.append(response.json())
+
+    return json_data
+
+
+def query_by_name(resource_name, resource_type):
+    """
+    Realize a query on SW API and returns a list.
+    """
+    url = f'https://swapi.dev/api/{resource_type}/?search={resource_name}'
+    response = get(url).json()
+
+    if not response['results']:
+        raise ResourceDoesNotExistError('Resource does not exist!')
+
+    json_data = response['results']
+
+    return json_data
+
+
+def get_resources_dict(resource, label):
+    resources_list = []
+    for r in resource:
+        resource_itens = get(r).json()[label]
+        resources_list.append(resource_itens)
+    return resources_list
