2bf343f51b5f0d6541406b20f3808d85e9fa9f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:04 +0000

    Add package DensityClust/1.0.2/DensityClust-1.0.2.tar.gz

---
+++ packages/DensityClust/DensityClust-1.0.2.tar.gz/DensityClust-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DensityClust',
+      version='1.0.2',
+      author='Luo Xiaoyu',
+      description='the local density clustering algorithm',
+      author_email='vastlxy@163.com',
+      packages=['DensityClust']
+      )
