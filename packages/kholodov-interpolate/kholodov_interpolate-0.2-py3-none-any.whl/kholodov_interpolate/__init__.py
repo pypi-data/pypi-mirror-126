31edd32fccd0251bb2aa7f5b0c64b70c6367d3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:44 +0000

    Add package kholodov-interpolate/0.2/kholodov_interpolate-0.2-py3-none-any.whl

---
+++ packages/kholodov-interpolate/kholodov_interpolate-0.2-py3-none-any.whl/kholodov_interpolate/__init__.py
@@ -0,0 +1,10 @@
+"""
+kholodov_interpolate
+
+Dummy module for lab
+"""
+
+__all__     = ['interpolate_function', 'plot', 'printQ']
+__version__ = "0.0.1"
+__author__  = 'Kholodov Aleksey'
+__credits__ = 'ITMO University'
