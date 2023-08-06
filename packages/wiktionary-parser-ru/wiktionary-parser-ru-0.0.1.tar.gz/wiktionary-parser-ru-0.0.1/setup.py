fef2cb427f3431c66dc2a1ac7f5167b5c7ffd428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:09 +0000

    Add package wiktionary-parser-ru/0.0.1/wiktionary-parser-ru-0.0.1.tar.gz

---
+++ packages/wiktionary-parser-ru/wiktionary-parser-ru-0.0.1.tar.gz/wiktionary-parser-ru-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup
+
+current_location = pathlib.Path(__file__).parent
+README = (current_location / "README.md").read_text()
+
+setup(
+    name="wiktionary-parser-ru",
+    version="0.0.1",
+    packages=["wiktionaryparserru", "tests"],
+    url="https://github.com/ShatteredMind/wiktionaryparserru",
+    license="MIT",
+    author="internethero",
+    author_email="sashalekoncev@gmail.com",
+    description="Basic parser for russian wiktionary",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=["beautifulsoup4", "requests"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
