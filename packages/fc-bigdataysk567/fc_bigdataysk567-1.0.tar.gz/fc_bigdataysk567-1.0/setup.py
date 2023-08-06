09de7013bfb057b8496f40ead86afce8ab69c29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:02 +0000

    Add package fc-bigdataysk567/1.0/fc_bigdataysk567-1.0.tar.gz

---
+++ packages/fc-bigdataysk567/fc_bigdataysk567-1.0.tar.gz/fc_bigdataysk567-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+        name="fc_bigdataysk567",#项目名
+        version = "1.0",    #版本号
+        py_modules=["fc_mkdxd_bigdata"],#生成模块
+        author="大数据3",       #作者
+        author_email="2294090178@qq.com",#邮箱
+        description="毛坤铎"    #项目信息
+)
