a0e737fdad431a922bc0ec9cdf8d45eba4243549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:09 +0000

    Add package disgames/1.2.3/disgames-1.2.3-py3-none-any.whl

---
+++ packages/disgames/disgames-1.2.3-py3-none-any.whl/disgames/__init__.py
@@ -0,0 +1,7 @@
+from discord.ext import commands
+
+def register_commands(bot, *, ignore: list = []):
+	lst = ['chesss','hangman','madlib','ttt']
+	for cog in lst:
+		if cog not in ignore:
+			bot.load_extension(cog)
---
+++ packages/disgames/disgames-1.2.3-py3-none-any.whl/disgames/mixins/madlib.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+import aiohttp
+
+
+class MadLib:
+    """
+    Madlib command
+    """
+    def __init__(self, bot) -> None:
+        self._session = aiohttp.ClientSession()
+        self.bot = bot
+
+    @property
+    def url(self):
+        return "http://madlibz.herokuapp.com/api/random"
+
+    @property
+    def session(self):
+        return self._session
+
+    async def request(self, min: int, max: int):
+        params = {"minlength": min, "maxlength": max}
+        response = await self.session.get(self.url, params=params)
+        return await response.json()
+
+    @commands.command()
+    async def madlib(self, ctx, min: int = 5, max: int = 25):
+        json = await self.request(min, max)
+        lst = []
+        for question in json["blanks"]:
+            await ctx.send(f"Please send: {question}")
+            answer = await ctx.bot.wait_for(
+                "message",
+                check=lambda m: m.author == ctx.author
+                and m.channel == ctx.channel,
+            )
+            lst.append(answer.content)
+        madlib = json["value"]
+        string = "".join(
+            f'{madlib[i]}{lst[i] if len(lst)-1 >= i else ""}'
+            for i in range(len(madlib) - 1)
+        )
+        # I dont understand this shit bruh - Marcus
+
+        await ctx.send(string)
