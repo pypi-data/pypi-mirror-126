53ef90d69bc828f35fc42b918ec8967d9d105ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:12 +0000

    Add package fc-l-xs/2.0/fc_l_xs-2.0.tar.gz

---
+++ packages/fc-l-xs/fc_l_xs-2.0.tar.gz/fc_l_xs-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+            name = "fc_l_xs",#项目名
+            version = "2.0",#版本
+            py_modules = ["fc_xd_bigdata3"],#生成模块
+            author = "大数据3",#作者
+            author_email = "fc@bigdata.com",#邮箱
+            description = "大数据1"#项目信息 
+)
