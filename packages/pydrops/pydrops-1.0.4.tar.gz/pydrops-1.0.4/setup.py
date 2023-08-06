e94184d8f82c9681b1ff0a9ded0e140f95faeca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:22 +0000

    Add package pydrops/1.0.4/pydrops-1.0.4.tar.gz

---
+++ packages/pydrops/pydrops-1.0.4.tar.gz/pydrops-1.0.4/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/11/8 17:29
+# @Author : nobody
+# @File : setup.py
+# @Software: PyCharm
+
+from setuptools import setup
+
+setup(
+    name="pydrops",
+    version="1.0.4",
+    packages=["hello"]
+)
---
+++ packages/pydrops/pydrops-1.0.4.tar.gz/pydrops-1.0.4/src/hello/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/11/8 18:02
+# @Author : nobody
+# @File : __init__.py
+# @Software: PyCharm
+
+from hello.rain import *
