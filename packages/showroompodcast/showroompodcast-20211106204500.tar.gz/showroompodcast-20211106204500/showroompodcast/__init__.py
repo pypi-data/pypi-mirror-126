05d78223e2342dbedbf5edcf9aef7b60268bdad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:38 +0000

    Add package showroompodcast/20211106204500/showroompodcast-20211106204500.tar.gz

---
+++ packages/showroompodcast/showroompodcast-20211106204500.tar.gz/showroompodcast-20211106204500/showroompodcast/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for SHOWROOM Podcast."""
+
+from typing import List
+
+from showroompodcast.config import Config
+
+__author__ = """Master"""
+__email__ = "roadmasternavi@gmail.com"
+__version__ = "20211106204500"
+
+__all__: List[str] = []
+
+CONFIG: Config = Config()
