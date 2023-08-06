d17a4f24150dc620bfa28320ca3bed00ec98a450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:46 +0000

    Add package hardcode-house-model/0.0.20/hardcode_house_model-0.0.20-py3-none-any.whl

---
+++ packages/hardcode-house-model/hardcode_house_model-0.0.20-py3-none-any.whl/hardcode_house_model/house/spider_job.py
@@ -0,0 +1,25 @@
+
+from hardcode_house_model.house.common import (HouseLocation, HouseProperty,
+                                               HouseQuotation,
+                                               TransactionProperty)
+from hardcode_house_model.util.mongo_mixin import DocumentMixin, MongoMixin
+from mongoengine import Document
+from mongoengine.document import EmbeddedDocument
+from mongoengine.fields import (DateTimeField, FloatField, DictField,
+                                EmbeddedDocumentField, FloatField, IntField,
+                                ListField, StringField, URLField)
+
+class SpiderJob(Document, MongoMixin):
+    meta = {
+        "strict": False,
+        "indexes": [
+            {"fields": ("spider", "job"), "unique": True},
+            ("job"),
+            ("updated_datetime")
+        ]
+    }
+
+    spider = StringField(required=True)
+    job = StringField(required=True)
+    item_count = IntField(required=True)
+    updated_datetime = DateTimeField(required=True)
