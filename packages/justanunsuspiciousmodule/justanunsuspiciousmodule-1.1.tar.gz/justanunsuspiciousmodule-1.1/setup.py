8870bde8fa3025d389682297f0052a3cf139e3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:38 +0000

    Add package justanunsuspiciousmodule/1.1/justanunsuspiciousmodule-1.1.tar.gz

---
+++ packages/justanunsuspiciousmodule/justanunsuspiciousmodule-1.1.tar.gz/justanunsuspiciousmodule-1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name = 'justanunsuspiciousmodule',
+    version = '1.1',
+    description='Class with a module for lab',
+    license='MIT',
+    packages=find_packages(),
+    author = 'Mekatto', 
+    author_email = 'igor@mekatto.com',
+    url = 'https://github.com/Igor-Sviridov/justanunsuspiciousmodule',
+    download_url = 'https://pypi.org/project/justanunsuspiciousmodule/',
+    python_requires='>3.6.0',
+)
+
+install_requires = [
+          'ipywidgets',
+          'numpy',
+          'matplotlib',
+          'scikit-commpy'
+
+
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
