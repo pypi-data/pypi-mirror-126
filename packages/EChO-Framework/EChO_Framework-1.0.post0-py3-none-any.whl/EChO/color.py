7ebaef5c34f8e916f2e0da5cec7b8e0544a41278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:42 +0000

    Add package EChO-Framework/1.0.post0/EChO_Framework-1.0.post0-py3-none-any.whl

---
+++ packages/EChO-Framework/EChO_Framework-1.0.post0-py3-none-any.whl/EChO/color.py
@@ -0,0 +1,10 @@
+from colorama import Fore
+
+red = Fore.RED
+white = Fore.WHITE
+reset = Fore.RESET
+blue = Fore.BLUE
+cyan = Fore.CYAN
+yellow = Fore.YELLOW
+magenta = Fore.MAGENTA
+green = Fore.GREEN
