07f81319da6cdee9c2e0f0d70624bb6efe5f8d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:09 +0000

    Add package myfds-package/0.7/myfds_package-0.7.tar.gz

---
+++ packages/myfds-package/myfds_package-0.7.tar.gz/myfds_package-0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='myfds_package',  # package name
+    version='0.7',  # version
+    description='My first package',  # short description
+    url='https://github.com/Ditmarscehen/myfds_package',  # package URL
+    install_requires=['scipy',
+                      'numpy<1.20.0'],  # list of packages this package depends
+    # on.
+    packages=['myfds_package'],  # List of module names that installing
+    # this package will provide.
+)
+
+#pip install -i https://test.pypi.org/simple/ myfds-package==0.2
