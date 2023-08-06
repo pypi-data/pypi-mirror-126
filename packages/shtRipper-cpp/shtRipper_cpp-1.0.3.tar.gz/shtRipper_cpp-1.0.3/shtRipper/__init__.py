c0365734248a3a37da93d7a6acc848f91e7a024b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:34 +0000

    Add package shtRipper-cpp/1.0.3/shtRipper_cpp-1.0.3.tar.gz

---
+++ packages/shtRipper-cpp/shtRipper_cpp-1.0.3.tar.gz/shtRipper_cpp-1.0.3/shtRipper/__init__.py
@@ -0,0 +1,6 @@
+from shtRipper import shtRipper_entry
+
+reaper = shtRipper_entry.Ripper()
+
+# require python >= 3.5 for ctypes compiler match
+# c++ toolchain: Visual Studio 2019 (pro). Version 16. Arc: x86_amd64. CMake: bundled
