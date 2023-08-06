5aa68301ca824eb881878e2576d7aa114cab8e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:43 +0000

    Add package skyflow/1.1.0/skyflow-1.1.0-py3-none-any.whl

---
+++ packages/skyflow/skyflow-1.1.0-py3-none-any.whl/skyflow/Vault/_config.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class Configuration:
+    def __init__(self, vaultID: str, vaultURL: str, tokenProvider):
+        self.vaultID = vaultID
+        self.vaultURL = vaultURL
+        self.tokenProvider = tokenProvider
+
+class InsertOptions:
+    def __init__(self, tokens: bool=True):
+        self.tokens = tokens
+
+class RequestMethod(Enum):
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    PATCH = 'PATCH'
+    DELETE = 'DELETE'
+
+class ConnectionConfig:
+    def __init__(self, connectionURL: str, methodName: RequestMethod, 
+    pathParams: dict={}, queryParams: dict={}, requestHeader: dict={}, requestBody: dict={}):
+        self.connectionURL = connectionURL.rstrip("/")
+        self.methodName = methodName
+        self.pathParams = pathParams
+        self.queryParams = queryParams
+        self.requestHeader = requestHeader
+        self.requestBody = requestBody
+
+class RedactionType(Enum):
+    PLAIN_TEXT = "PLAIN_TEXT"
+    MASKED = "MASKED"
+    REDACTED = "REDACTED"
+    DEFAULT = "DEFAULT"
