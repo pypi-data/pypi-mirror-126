6faecb1e2734de7af209aa86f1a79caeae710aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:32 +0000

    Add package opensea-api/0.1.2/opensea_api-0.1.2-py2.py3-none-any.whl

---
+++ packages/opensea-api/opensea_api-0.1.2-py2.py3-none-any.whl/opensea/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for OpenSea API Python wrapper."""
+
+__author__ = """Attila Toth"""
+__email__ = "hello@attilatoth.dev"
+__version__ = "__version__ = '0.1.2'"
+__all__ = ["Events", "Asset", "Assets", "Contract", "Collection",
+           "CollectionStats", "Collections", "Bundles"]
+
+from opensea.opensea import Events, Asset, Assets, Contract, Collection, \
+    CollectionStats, Collections, Bundles
