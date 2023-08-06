d99aeb8834da578c7157fe6f1a516021aecaa82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:02 +0000

    Add package target365-sdk/1.7.1/target365_sdk-1.7.1-py3-none-any.whl

---
+++ packages/target365-sdk/target365_sdk-1.7.1-py3-none-any.whl/target365_sdk/models/oneclick_config.py
@@ -0,0 +1,27 @@
+from .model import Model
+
+class OneClickConfig(Model):
+
+    def _accepted_params(self):
+        return [
+            'configId',
+            'shortNumber',
+            'merchantId',
+            'serviceCode',
+            'businessModel',
+            'preAuthServiceId',
+            'recurring',
+            'redirectUrl',
+            'onlineText',
+            'offlineText',
+            'age',
+            'isRestricted',
+            'invoiceText',
+            'price',
+            'subscriptionPrice',
+            'subscriptionInterval',
+            'subscriptionStartSms',
+            'timeout',
+            'created',
+            'lastModified',
+        ]
