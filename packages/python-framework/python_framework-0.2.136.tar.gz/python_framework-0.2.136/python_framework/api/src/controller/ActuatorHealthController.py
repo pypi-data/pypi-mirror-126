72f638b999877fdb8ec90871c3fe1fd585e7a62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:44 +0000

    Add package python-framework/0.2.136/python_framework-0.2.136.tar.gz

---
+++ packages/python-framework/python_framework-0.2.136.tar.gz/python_framework-0.2.136/python_framework/api/src/controller/ActuatorHealthController.py
@@ -0,0 +1,12 @@
+from python_framework.api.src.enumeration.HttpStatus import HttpStatus
+from python_framework.api.src.service.flask.FlaskManager import Controller, ControllerMethod
+from python_framework.api.src.dto import ActuatorHealthDto
+
+@Controller(url='/health', tag='HealthCheck', description='HealthCheck controller')
+class ActuatorHealthController:
+
+    @ControllerMethod(
+        responseClass=ActuatorHealthDto.ActuatorHealthResponseDto
+    )
+    def get(self):
+        return self.service.actuatorHealth.getStatus(), HttpStatus.OK
