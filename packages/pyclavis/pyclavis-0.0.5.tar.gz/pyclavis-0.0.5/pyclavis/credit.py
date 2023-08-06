3539c1f389baa00240d14cd9438c9ff5c98ccabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:41 +0000

    Add package pyclavis/0.0.5/pyclavis-0.0.5.tar.gz

---
+++ packages/pyclavis/pyclavis-0.0.5.tar.gz/pyclavis-0.0.5/pyclavis/credit.py
@@ -0,0 +1,19 @@
+class Credential(object):
+    def __init__(self, name=None, user=None, passwd=None, category=None):
+        self.name = name
+        self.user = user
+        self.passwd = passwd
+        self.category = (
+            category if category != None and len(category.strip()) > 0 else None
+        )
+
+    def __repr__(self):
+        return (
+            self.__class__.__name__
+            + "("
+            + ", ".join([self.name, self.user, str(self.category)])
+            + ")"
+        )
+
+    def todict(self):
+        return {"USER": self.user, "PASSWORD": self.passwd}
