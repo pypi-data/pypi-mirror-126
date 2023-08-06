d0e62baa3573f4eef883446895d5fb877634f3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:05 +0000

    Add package qwh/1.0/qwh-1.0.tar.gz

---
+++ packages/qwh/qwh-1.0.tar.gz/qwh-1.0/pw.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "qwh",
+    version=  "1.0",
+    py_modules= ["fc_ims"],
+    author= "大数据1",
+    author_email= "fc@bigdata.com",
+    description= "大数据1"
+)
