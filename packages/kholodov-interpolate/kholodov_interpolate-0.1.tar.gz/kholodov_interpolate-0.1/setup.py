2296086ce0d341ea4eaef7c1b930f8e29ce8090f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:38 +0000

    Add package kholodov-interpolate/0.1/kholodov_interpolate-0.1.tar.gz

---
+++ packages/kholodov-interpolate/kholodov_interpolate-0.1.tar.gz/kholodov_interpolate-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(name='kholodov_interpolate',
+      version='0.1',
+      description='dummy projec',
+      packages=find_packages(),
+      author_email='ksteklo@mail.ru',
+      zip_safe=False)
