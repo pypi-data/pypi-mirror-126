ab71c1b0354d30ecb87c7d8126460c46a2756d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package corsid/0.1.0/corsid-0.1.0.tar.gz

---
+++ packages/corsid/corsid-0.1.0.tar.gz/corsid-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='corsid',
+    packages=["corsid"],
+    description="Core Sequence Identifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.1.0',
+    url='http://github.com/elkebir-group/CORSID ',
+    author='Chuanyi Zhang',
+    author_email='chuanyi5@illinois.edu',
+    license='MIT',
+    python_requires='>=3.7',
+    scripts=[
+        'scripts/corsid',
+        'scripts/corsid_a',
+    ],
+    install_requires=[
+        "numpy",
+        "pysam",
+        "pandas",
+        "pytablewriter",
+    ],
+)
